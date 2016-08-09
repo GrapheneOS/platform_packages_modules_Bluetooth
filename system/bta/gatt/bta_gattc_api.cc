GrapheneOS/platform_packages_modules_Bluetooth

commit 3a1586dca4863a6b18b319a803f8b61076c4c38f

Rmove write type from GATT descriptor writes (4/4)
According to the Bluetooth Core specification v4.2, Vol 3, Part G,
section 4.12.3: "The Attribute Protocol WRITE REQUEST is used
for this sub-procedure".

Change-Id: I60bb0b1489b417af96975265a023c356ace293bd

system/bta/gatt/bta_gattc_api.cc
@@ -458,7 +458,6 @@ void BTA_GATTC_WriteCharValue ( uint16_t conn_id,
 **
 ** Parameters       conn_id - connection ID
 **                  handle - descriptor hadle to write.
-**                  write_type - write type.
 **                  value - the value to be written.
 **
 ** Returns          None
@@ -466,7 +465,6 @@ void BTA_GATTC_WriteCharValue ( uint16_t conn_id,
 *******************************************************************************/
 void BTA_GATTC_WriteCharDescr (uint16_t conn_id,
                                uint16_t handle,
-                               tBTA_GATTC_WRITE_TYPE  write_type,
                                std::vector<uint8_t> value,
                                tBTA_GATT_AUTH_REQ auth_req,
                                GATT_WRITE_OP_CB callback,
@@ -479,7 +477,6 @@ void BTA_GATTC_WriteCharDescr (uint16_t conn_id,
     p_buf->hdr.layer_specific = conn_id;
     p_buf->auth_req = auth_req;
     p_buf->handle = handle;
-    p_buf->write_type = write_type;
     p_buf->write_cb = callback;
     p_buf->write_cb_data = cb_data;
 

system/bta/hh/bta_hh_le.cc
@@ -190,9 +190,8 @@ static void gatt_execute_next_op(uint16_t conn_id) {
         const tBTA_GATTC_DESCRIPTOR *p_desc = BTA_GATTC_GetDescriptor(conn_id, op.handle);
         act_write_cb = op.write_cb;
         act_write_cb_data = op.write_cb_data;
-        BTA_GATTC_WriteCharDescr(conn_id, p_desc->handle, BTA_GATTC_TYPE_WRITE,
-                                 std::move(op.value), BTA_GATT_AUTH_REQ_NONE,
-                                 gatt_write_op_finished, NULL);
+        BTA_GATTC_WriteCharDescr(conn_id, p_desc->handle, std::move(op.value),
+                                 BTA_GATT_AUTH_REQ_NONE, gatt_write_op_finished, NULL);
     }
 
     gatt_ops.pop_front();

system/bta/include/bta_gatt_api.h
@@ -797,7 +797,6 @@ void BTA_GATTC_WriteCharValue ( uint16_t conn_id,
 **
 ** Parameters       conn_id - connection ID
 **                  handle - descriptor handle to write.
-**                  write_type - type of write.
 **                  value - the value to be written.
 **
 ** Returns          None
@@ -805,7 +804,6 @@ void BTA_GATTC_WriteCharValue ( uint16_t conn_id,
 *******************************************************************************/
 void BTA_GATTC_WriteCharDescr (uint16_t conn_id,
                                uint16_t handle,
-                               tBTA_GATTC_WRITE_TYPE  write_type,
                                vector<uint8_t> value,
                                tBTA_GATT_AUTH_REQ auth_req,
                                GATT_WRITE_OP_CB callback,

system/btif/src/btif_gatt_client.cc
@@ -809,15 +809,14 @@ void write_descr_cb(uint16_t conn_id, tGATT_STATUS status, uint16_t handle,
 }
 
 bt_status_t btif_gattc_write_char_descr(int conn_id, uint16_t handle,
-                                        int write_type, int auth_req,
-                                        vector<uint8_t> value) {
+                                        int auth_req, vector<uint8_t> value) {
   CHECK_BTGATT_INIT();
 
   if (value.size() > BTGATT_MAX_ATTR_LEN)
     value.resize(BTGATT_MAX_ATTR_LEN);
 
   return do_in_jni_thread(Bind(&BTA_GATTC_WriteCharDescr, conn_id, handle,
-                               write_type, std::move(value), auth_req,
+                               std::move(value), auth_req,
                                write_descr_cb, nullptr));
 }
 

commit d8c8c87a8b9106d492d8a59a03ffeb1e448f94ac

Fix descriptor write
This patch fixes error introduced in commit
3a1586dca4863a6b18b319a803f8b61076c4c38f
Writing descriptor was broken, ACTS tests were failing.

Change-Id: Ied2ff4a12df1053961677fdcb824838b7946dbaa

system/bta/gatt/bta_gattc_api.cc
@@ -477,6 +477,7 @@ void BTA_GATTC_WriteCharDescr (uint16_t conn_id,
     p_buf->hdr.layer_specific = conn_id;
     p_buf->auth_req = auth_req;
     p_buf->handle = handle;
+    p_buf->write_type = BTA_GATTC_TYPE_WRITE;
     p_buf->write_cb = callback;
     p_buf->write_cb_data = cb_data;
 