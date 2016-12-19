GrapheneOS/platform_packages_modules_Bluetooth

commit 307fe304445c0e36af510bdb58d3ae667a1f0dfe

Don't try to stop advertising on death of GATT clients
We should no longer stop advertising for GATT clients. Advertisers
are kept in AdvertiseClient, and their death is handled in
AdvertiserDeathRecipient.

Bug: 30622771
Test: Kill app with GATT client registered
Change-Id: I21f246042d1632824ed3d2d8937f13bcafe380ab

android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -248,10 +248,6 @@ public class GattService extends ProfileService {
                 ScanClient client = new ScanClient(mAppIf, false);
                 client.appDied = true;
                 stopScan(client);
-            } else {
-                AdvertiseClient client = new AdvertiseClient(mAppIf);
-                client.appDied = true;
-                stopMultiAdvertising(client);
             }
         }
 

commit 71311596f0f218d9dd0aa83a522fb6f6ed6d5efc

Fix cleanup of dead GATT clients
A bug was introduced in commit
307fe304445c0e36af510bdb58d3ae667a1f0dfe which causes normal
GATT clients (neither scanners nor advertisers) not being freed
properly when remote process terminates, leading to out of space
of available GATT clients after some time.

This patch adds back a ClientDeathRecipient to handle this.

Test: kill an app that has gatt client, restart it and create new
gatt client, verify the client id is same as before in logcat

Change-Id: I318cb1693984444a172cb81be2023a064dbc02a4

android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -305,6 +305,19 @@ public class GattService extends ProfileService {
         }
     }
 
+    class ClientDeathRecipient implements IBinder.DeathRecipient {
+        int mAppIf;
+
+        public ClientDeathRecipient(int appIf) {
+            mAppIf = appIf;
+        }
+
+        public void binderDied() {
+            if (DBG) Log.d(TAG, "Binder is dead - unregistering client (" + mAppIf + ")!");
+            unregisterClient(mAppIf);
+        }
+    }
+
     /**
      * Handlers for incoming service calls
      */
@@ -675,6 +688,7 @@ public class GattService extends ProfileService {
         if (app != null) {
             if (status == 0) {
                 app.id = clientIf;
+                app.linkToDeath(new ClientDeathRecipient(clientIf));
             } else {
                 mClientMap.remove(uuid);
             }