GrapheneOS/platform_packages_modules_Bluetooth

commit 2238a88f2fd632d4e8f1aa3e932df9dba34c06fc

Use one type for UUID (1/5)
Currently, we have few different representations for UUID in stack:
tBT_UUID, tSDP_UUID, bt_uuid_t, bluetooth:UUID, or uint8_t*.

Additionally, tBT_UUID and bt_uuid_t are used to hold UUID as 128bit
as Little Endian or Big Endian, depending on which part of stack (GATT
or SDP) is using it.

This patch is creating one type, bluetooth::Uuid, that will replace all
other types.

Bug: 66912853
Test: all sl4a tests for GATT and RFCOMM
Merged-In: Ia42d3233146db0488728ed6f878f99b368fe8838
Change-Id: Ia42d3233146db0488728ed6f878f99b368fe8838

system/stack/gatt/gatt_db.cc
@@ -35,10 +35,12 @@
 #include "osi/include/osi.h"
 
 using base::StringPrintf;
+using bluetooth::Uuid;
+
 /*******************************************************************************
  *             L O C A L    F U N C T I O N     P R O T O T Y P E S            *
  ******************************************************************************/
-static tGATT_ATTR& allocate_attr_in_db(tGATT_SVC_DB& db, const tBT_UUID& uuid,
+static tGATT_ATTR& allocate_attr_in_db(tGATT_SVC_DB& db, const Uuid& uuid,
                                        tGATT_PERM perm);
 static tGATT_STATUS gatts_send_app_read_request(
     tGATT_TCB& tcb, uint8_t op_code, uint16_t handle, uint16_t offset,
@@ -47,8 +49,8 @@ static tGATT_STATUS gatts_send_app_read_request(
 /**
  * Initialize a memory space to be a service database.
  */
-void gatts_init_service_db(tGATT_SVC_DB& db, tBT_UUID* p_service, bool is_pri,
-                           uint16_t s_hdl, uint16_t num_handle) {
+void gatts_init_service_db(tGATT_SVC_DB& db, const Uuid& service_uuid,
+                           bool is_pri, uint16_t s_hdl, uint16_t num_handle) {
   db.attr_list.reserve(num_handle);
 
   VLOG(1) << StringPrintf("%s: s_hdl= %d num_handle= %d", __func__, s_hdl,
@@ -59,14 +61,14 @@ void gatts_init_service_db(tGATT_SVC_DB& db, tBT_UUID* p_service, bool is_pri,
   db.end_handle = s_hdl + num_handle;
 
   /* add service declration record */
-  tBT_UUID uuid = {LEN_UUID_16, {0}};
-  uuid.uu.uuid16 = is_pri ? GATT_UUID_PRI_SERVICE : GATT_UUID_SEC_SERVICE;
+  Uuid uuid =
+      Uuid::From16Bit(is_pri ? GATT_UUID_PRI_SERVICE : GATT_UUID_SEC_SERVICE);
   tGATT_ATTR& attr = allocate_attr_in_db(db, uuid, GATT_PERM_READ);
-  attr.p_value.reset((tGATT_ATTR_VALUE*)(new tBT_UUID));
-  memcpy(&attr.p_value->uuid, p_service, sizeof(tBT_UUID));
+  attr.p_value.reset((tGATT_ATTR_VALUE*)(new Uuid));
+  attr.p_value->uuid = service_uuid;
 }
 
-tBT_UUID* gatts_get_service_uuid(tGATT_SVC_DB* p_db) {
+Uuid* gatts_get_service_uuid(tGATT_SVC_DB* p_db) {
   if (!p_db || p_db->attr_list.empty()) {
     LOG(ERROR) << "service DB empty";
     return NULL;
@@ -119,8 +121,8 @@ static tGATT_STATUS gatts_check_attr_readability(const tGATT_ATTR& attr,
     return GATT_INSUF_KEY_SIZE;
   }
 
-  if (read_long && attr.uuid.len == LEN_UUID_16) {
-    switch (attr.uuid.uu.uuid16) {
+  if (read_long && attr.uuid.Is16Bit()) {
+    switch (attr.uuid.As16Bit()) {
       case GATT_UUID_PRI_SERVICE:
       case GATT_UUID_SEC_SERVICE:
       case GATT_UUID_CHAR_DECLARE:
@@ -162,75 +164,70 @@ static tGATT_STATUS read_attr_value(tGATT_ATTR& attr16, uint16_t offset,
                                     uint8_t** p_data, bool read_long,
                                     uint16_t mtu, uint16_t* p_len,
                                     tGATT_SEC_FLAG sec_flag, uint8_t key_size) {
-  uint16_t len = 0, uuid16 = 0;
   uint8_t* p = *p_data;
 
-  VLOG(1)
-      << __func__
-      << StringPrintf(
-             " uuid=0x%04x perm=0x%02x sec_flag=0x%x offset=%d read_long=%d",
-             attr16.uuid.uu.uuid16, attr16.permission, sec_flag, offset,
-             read_long);
+  VLOG(1) << __func__ << " uuid=" << attr16.uuid
+          << StringPrintf(" perm=0x%02x sec_flag=0x%x offset=%d read_long=%d",
+                          attr16.permission, sec_flag, offset, read_long);
 
   tGATT_STATUS status = gatts_check_attr_readability(attr16, offset, read_long,
                                                      sec_flag, key_size);
-
   if (status != GATT_SUCCESS) return status;
 
-  if (attr16.uuid.len == LEN_UUID_16) uuid16 = attr16.uuid.uu.uuid16;
-
-  status = GATT_NO_RESOURCES;
-
-  if (uuid16 == GATT_UUID_PRI_SERVICE || uuid16 == GATT_UUID_SEC_SERVICE) {
-    len = attr16.p_value->uuid.len;
-    if (mtu >= attr16.p_value->uuid.len) {
-      gatt_build_uuid_to_stream(&p, attr16.p_value->uuid);
-      status = GATT_SUCCESS;
-    }
-  } else if (uuid16 == GATT_UUID_CHAR_DECLARE) {
-    tGATT_ATTR* val_attr = &attr16 + 1;
-    len = (val_attr->uuid.len == LEN_UUID_16) ? 5 : 19;
-
-    if (mtu >= len) {
-      UINT8_TO_STREAM(p, attr16.p_value->char_decl.property);
-      UINT16_TO_STREAM(p, attr16.p_value->char_decl.char_val_handle);
-
-      if (val_attr->uuid.len == LEN_UUID_16) {
-        UINT16_TO_STREAM(p, val_attr->uuid.uu.uuid16);
-      }
-      /* convert a 32bits UUID to 128 bits */
-      else if (val_attr->uuid.len == LEN_UUID_32) {
-        gatt_convert_uuid32_to_uuid128(p, val_attr->uuid.uu.uuid32);
-        p += LEN_UUID_128;
-      } else {
-        ARRAY_TO_STREAM(p, val_attr->uuid.uu.uuid128, LEN_UUID_128);
-      }
-      status = GATT_SUCCESS;
-    }
-
-  } else if (uuid16 == GATT_UUID_INCLUDE_SERVICE) {
-    if (attr16.p_value->incl_handle.service_type.len == LEN_UUID_16)
-      len = 6;
-    else
-      len = 4;
-
-    if (mtu >= len) {
-      UINT16_TO_STREAM(p, attr16.p_value->incl_handle.s_handle);
-      UINT16_TO_STREAM(p, attr16.p_value->incl_handle.e_handle);
-
-      if (attr16.p_value->incl_handle.service_type.len == LEN_UUID_16) {
-        UINT16_TO_STREAM(p, attr16.p_value->incl_handle.service_type.uu.uuid16);
-      }
-      status = GATT_SUCCESS;
-    }
-  } else /* characteristic description or characteristic value */
-  {
-    status = GATT_PENDING;
+  if (!attr16.uuid.Is16Bit()) {
+    /* characteristic description or characteristic value */
+    return GATT_PENDING;
   }
 
-  *p_len = len;
-  *p_data = p;
-  return status;
+  uint16_t uuid16 = attr16.uuid.As16Bit();
+
+  if (uuid16 == GATT_UUID_PRI_SERVICE || uuid16 == GATT_UUID_SEC_SERVICE) {
+    *p_len = attr16.p_value->uuid.GetShortestRepresentationSize();
+    if (mtu < *p_len) return GATT_NO_RESOURCES;
+
+    gatt_build_uuid_to_stream(&p, attr16.p_value->uuid);
+    return GATT_SUCCESS;
+  }
+
+  if (uuid16 == GATT_UUID_CHAR_DECLARE) {
+    tGATT_ATTR* val_attr = &attr16 + 1;
+    uint8_t val_len = val_attr->uuid.GetShortestRepresentationSize();
+    *p_len = (val_len == Uuid::kNumBytes16) ? 5 : 19;
+
+    if (mtu < *p_len) return GATT_NO_RESOURCES;
+
+    UINT8_TO_STREAM(p, attr16.p_value->char_decl.property);
+    UINT16_TO_STREAM(p, attr16.p_value->char_decl.char_val_handle);
+
+    if (val_len == Uuid::kNumBytes16) {
+      UINT16_TO_STREAM(p, val_attr->uuid.As16Bit());
+    } else {
+      /* if 32 bit UUID, convert to 128 bit */
+      ARRAY_TO_STREAM(p, val_attr->uuid.To128BitLE(), (int)Uuid::kNumBytes128);
+    }
+    return GATT_SUCCESS;
+  }
+
+  if (uuid16 == GATT_UUID_INCLUDE_SERVICE) {
+    tGATT_INCL_SRVC& incl_handle = attr16.p_value->incl_handle;
+    if (incl_handle.service_type.Is16Bit())
+      *p_len = 6;
+    else
+      *p_len = 4;
+
+    if (mtu < *p_len) return GATT_NO_RESOURCES;
+
+    UINT16_TO_STREAM(p, incl_handle.s_handle);
+    UINT16_TO_STREAM(p, incl_handle.e_handle);
+
+    if (incl_handle.service_type.Is16Bit()) {
+      UINT16_TO_STREAM(p, incl_handle.service_type.As16Bit());
+    }
+    return GATT_SUCCESS;
+  }
+
+  /* characteristic description or characteristic value (again) */
+  return GATT_PENDING;
 }
 
 /*******************************************************************************
@@ -253,7 +250,7 @@ static tGATT_STATUS read_attr_value(tGATT_ATTR& attr16, uint16_t offset,
  ******************************************************************************/
 tGATT_STATUS gatts_db_read_attr_value_by_type(
     tGATT_TCB& tcb, tGATT_SVC_DB* p_db, uint8_t op_code, BT_HDR* p_rsp,
-    uint16_t s_handle, uint16_t e_handle, tBT_UUID type, uint16_t* p_len,
+    uint16_t s_handle, uint16_t e_handle, const Uuid& type, uint16_t* p_len,
     tGATT_SEC_FLAG sec_flag, uint8_t key_size, uint32_t trans_id,
     uint16_t* p_cur_handle) {
   tGATT_STATUS status = GATT_NOT_FOUND;
@@ -262,9 +259,7 @@ tGATT_STATUS gatts_db_read_attr_value_by_type(
 
   if (p_db) {
     for (tGATT_ATTR& attr : p_db->attr_list) {
-      tBT_UUID attr_uuid = attr.uuid;
-
-      if (attr.handle >= s_handle && gatt_uuid_compare(type, attr_uuid)) {
+      if (attr.handle >= s_handle && type == attr.uuid) {
         if (*p_len <= 2) {
           status = GATT_NO_RESOURCES;
           break;
@@ -304,7 +299,7 @@ tGATT_STATUS gatts_db_read_attr_value_by_type(
   uint8_t flag = 0;
   if (BTM_GetSecurityFlags(tcb.peer_bda, &flag)) {
     if ((tcb.att_lcid == L2CAP_ATT_CID) && (status == GATT_PENDING) &&
-        (type.uu.uuid16 == GATT_UUID_GAP_DEVICE_NAME)) {
+        (type.As16Bit() == GATT_UUID_GAP_DEVICE_NAME)) {
       if ((flag & (BTM_SEC_LINK_KEY_KNOWN | BTM_SEC_FLAG_ENCRYPTED)) ==
           BTM_SEC_LINK_KEY_KNOWN) {
         tACL_CONN* p = btm_bda_to_acl(tcb.peer_bda, BT_TRANSPORT_LE);
@@ -328,13 +323,14 @@ tGATT_STATUS gatts_db_read_attr_value_by_type(
  *
  */
 uint16_t gatts_add_included_service(tGATT_SVC_DB& db, uint16_t s_handle,
-                                    uint16_t e_handle, tBT_UUID service) {
-  tBT_UUID uuid = {LEN_UUID_16, {GATT_UUID_INCLUDE_SERVICE}};
+                                    uint16_t e_handle, const Uuid& service) {
+  Uuid uuid = Uuid::From16Bit(GATT_UUID_INCLUDE_SERVICE);
 
-  VLOG(1) << StringPrintf("%s: s_hdl = 0x%04x e_hdl = 0x%04x uuid = 0x%04x",
-                          __func__, s_handle, e_handle, service.uu.uuid16);
+  VLOG(1) << __func__
+          << StringPrintf(": s_hdl=0x%04x e_hdl=0x%04x ", s_handle, e_handle)
+          << "service uuid = " << service;
 
-  if (service.len == 0 || s_handle == 0 || e_handle == 0) {
+  if (service.IsEmpty() || s_handle == 0 || e_handle == 0) {
     LOG(ERROR) << __func__ << ": Illegal Params.";
     return 0;
   }
@@ -344,7 +340,7 @@ uint16_t gatts_add_included_service(tGATT_SVC_DB& db, uint16_t s_handle,
   attr.p_value.reset((tGATT_ATTR_VALUE*)(new tGATT_INCL_SRVC));
   attr.p_value->incl_handle.s_handle = s_handle;
   attr.p_value->incl_handle.e_handle = e_handle;
-  memcpy(&attr.p_value->incl_handle.service_type, &service, sizeof(tBT_UUID));
+  attr.p_value->incl_handle.service_type = service;
 
   return attr.handle;
 }
@@ -366,8 +362,8 @@ uint16_t gatts_add_included_service(tGATT_SVC_DB& db, uint16_t s_handle,
  ******************************************************************************/
 uint16_t gatts_add_characteristic(tGATT_SVC_DB& db, tGATT_PERM perm,
                                   tGATT_CHAR_PROP property,
-                                  tBT_UUID& char_uuid) {
-  tBT_UUID uuid = {LEN_UUID_16, {GATT_UUID_CHAR_DECLARE}};
+                                  const Uuid& char_uuid) {
+  Uuid uuid = Uuid::From16Bit(GATT_UUID_CHAR_DECLARE);
 
   VLOG(1) << StringPrintf("%s: perm=0x%0x property=0x%0x", __func__, perm,
                           property);
@@ -380,46 +376,6 @@ uint16_t gatts_add_characteristic(tGATT_SVC_DB& db, tGATT_PERM perm,
   char_decl.p_value->char_decl.char_val_handle = char_val.handle;
   char_val.gatt_type = BTGATT_DB_CHARACTERISTIC;
   return char_val.handle;
-}
-
-/*******************************************************************************
- *
- * Function         gatt_convertchar_descr_type
- *
- * Description      Convert a char descript UUID into descriptor type.
- *
- * Returns          descriptor type.
- *
- ******************************************************************************/
-uint8_t gatt_convertchar_descr_type(tBT_UUID* p_descr_uuid) {
-  tBT_UUID std_descr = {LEN_UUID_16, {GATT_UUID_CHAR_EXT_PROP}};
-
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid))
-    return GATT_DESCR_EXT_DSCPTOR;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid))
-    return GATT_DESCR_USER_DSCPTOR;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid)) return GATT_DESCR_CLT_CONFIG;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid)) return GATT_DESCR_SVR_CONFIG;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid))
-    return GATT_DESCR_PRES_FORMAT;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid))
-    return GATT_DESCR_AGGR_FORMAT;
-
-  std_descr.uu.uuid16++;
-  if (gatt_uuid_compare(std_descr, *p_descr_uuid))
-    return GATT_DESCR_VALID_RANGE;
-
-  return GATT_DESCR_UNKNOWN;
 }
 
 /*******************************************************************************
@@ -437,9 +393,9 @@ uint8_t gatt_convertchar_descr_type(tBT_UUID* p_descr_uuid) {
  *
  ******************************************************************************/
 uint16_t gatts_add_char_descr(tGATT_SVC_DB& db, tGATT_PERM perm,
-                              tBT_UUID& descr_uuid) {
-  VLOG(1) << StringPrintf("gatts_add_char_descr uuid=0x%04x",
-                          descr_uuid.uu.uuid16);
+                              const Uuid& descr_uuid) {
+  VLOG(1) << StringPrintf("gatts_add_char_descr uuid=%s",
+                          descr_uuid.ToString().c_str());
 
   /* Add characteristic descriptors */
   tGATT_ATTR& char_dscptr = allocate_attr_in_db(db, descr_uuid, perm);
@@ -626,13 +582,14 @@ tGATT_STATUS gatts_write_attr_perm_check(tGATT_SVC_DB* p_db, uint8_t op_code,
     status = GATT_INSUF_AUTHENTICATION;
     LOG(ERROR) << __func__
                << ": GATT_INSUF_AUTHENTICATION: LE security mode 2 required";
-  } else /* writable: must be char value declaration or char descritpors
-            */
+  } else /* writable: must be char value declaration or char descritpors */
   {
     uint16_t max_size = 0;
 
-    if (p_attr->uuid.len == LEN_UUID_16) {
-      switch (p_attr->uuid.uu.uuid16) {
+    if (p_attr->uuid.IsEmpty()) {
+      status = GATT_INVALID_PDU;
+    } else if (p_attr->uuid.Is16Bit()) {
+      switch (p_attr->uuid.As16Bit()) {
         case GATT_UUID_CHAR_PRESENT_FORMAT: /* should be readable only */
         case GATT_UUID_CHAR_EXT_PROP:       /* should be readable only */
         case GATT_UUID_CHAR_AGG_FORMAT:     /* should be readable only */
@@ -650,56 +607,33 @@ tGATT_STATUS gatts_write_attr_perm_check(tGATT_SVC_DB* p_db, uint8_t op_code,
           status = GATT_SUCCESS;
           break;
       }
-    } else if (p_attr->uuid.len == LEN_UUID_128 ||
-               p_attr->uuid.len == LEN_UUID_32) {
+    } else {  // 32 or 128 bit UUID
       status = GATT_SUCCESS;
-    } else {
-      status = GATT_INVALID_PDU;
     }
 
     if (p_data == NULL && len > 0) {
-      status = GATT_INVALID_PDU;
+      return GATT_INVALID_PDU;
     }
+
     /* these attribute does not allow write blob */
-    else if ((p_attr->uuid.len == LEN_UUID_16) &&
-             (p_attr->uuid.uu.uuid16 == GATT_UUID_CHAR_CLIENT_CONFIG ||
-              p_attr->uuid.uu.uuid16 == GATT_UUID_CHAR_SRVR_CONFIG)) {
-      if (op_code == GATT_REQ_PREPARE_WRITE &&
-          offset != 0) /* does not allow write blob */
-      {
+    if (p_attr->uuid.Is16Bit() &&
+        (p_attr->uuid.As16Bit() == GATT_UUID_CHAR_CLIENT_CONFIG ||
+         p_attr->uuid.As16Bit() == GATT_UUID_CHAR_SRVR_CONFIG)) {
+      if (op_code == GATT_REQ_PREPARE_WRITE && offset != 0) {
+        /* does not allow write blob */
         status = GATT_NOT_LONG;
         LOG(ERROR) << __func__ << ": GATT_NOT_LONG";
-      } else if (len != max_size) /* data does not match the required format */
-      {
+      } else if (len != max_size) {
+        /* data does not match the required format */
         status = GATT_INVALID_ATTR_LEN;
         LOG(ERROR) << __func__ << ": GATT_INVALID_PDU";
       } else {
-        status = GATT_SUCCESS;
+        return GATT_SUCCESS;
       }
     }
   }
 
   return status;
-}
-
-static void uuid_to_str(const tBT_UUID bt_uuid, char* str_buf, size_t buf_len) {
-  if (bt_uuid.len == LEN_UUID_16) {
-    snprintf(str_buf, buf_len, "0x%04x", bt_uuid.uu.uuid16);
-  } else if (bt_uuid.len == LEN_UUID_32) {
-    snprintf(str_buf, buf_len, "0x%08x", bt_uuid.uu.uuid32);
-  } else if (bt_uuid.len == LEN_UUID_128) {
-    int x = snprintf(str_buf, buf_len, "%02x%02x%02x%02x-%02x%02x-%02x%02x-",
-                     bt_uuid.uu.uuid128[15], bt_uuid.uu.uuid128[14],
-                     bt_uuid.uu.uuid128[13], bt_uuid.uu.uuid128[12],
-                     bt_uuid.uu.uuid128[11], bt_uuid.uu.uuid128[10],
-                     bt_uuid.uu.uuid128[9], bt_uuid.uu.uuid128[8]);
-    snprintf(&str_buf[x], buf_len - x, "%02x%02x-%02x%02x%02x%02x%02x%02x",
-             bt_uuid.uu.uuid128[7], bt_uuid.uu.uuid128[6],
-             bt_uuid.uu.uuid128[5], bt_uuid.uu.uuid128[4],
-             bt_uuid.uu.uuid128[3], bt_uuid.uu.uuid128[2],
-             bt_uuid.uu.uuid128[1], bt_uuid.uu.uuid128[0]);
-  } else
-    snprintf(str_buf, buf_len, "Unknown (len=%d)", bt_uuid.len);
 }
 
 /**
@@ -713,7 +647,7 @@ static void uuid_to_str(const tBT_UUID bt_uuid, char* str_buf, size_t buf_len) {
  * Returns          pointer to the newly allocated attribute.
  *
  */
-static tGATT_ATTR& allocate_attr_in_db(tGATT_SVC_DB& db, const tBT_UUID& uuid,
+static tGATT_ATTR& allocate_attr_in_db(tGATT_SVC_DB& db, const Uuid& uuid,
                                        tGATT_PERM perm) {
   if (db.next_handle >= db.end_handle) {
     LOG(FATAL) << __func__
@@ -726,10 +660,6 @@ static tGATT_ATTR& allocate_attr_in_db(tGATT_SVC_DB& db, const tBT_UUID& uuid,
   attr.handle = db.next_handle++;
   attr.uuid = uuid;
   attr.permission = perm;
-
-  char uuid_str[37];
-  uuid_to_str(attr.uuid, uuid_str, sizeof(uuid_str));
-
   return attr;
 }
 

commit 9e54b05f9f95902ea8b5a1ed5991f13477608b03

Fix GATT Characteristic discovery (server side)
The pointer was not properly incremented, which resulted in trashes
being sent during GATT discovery.

This issue was introduced in commit
2238a88f2fd632d4e8f1aa3e932df9dba34c06fc (Use one type for UUID)

Bug: 66912853
Test: sl4a GATT read test
Change-Id: Ib57ad050ff17852f9b2fec9c51cad246235e3e80

system/stack/gatt/gatt_db.cc
@@ -186,6 +186,7 @@ static tGATT_STATUS read_attr_value(tGATT_ATTR& attr16, uint16_t offset,
     if (mtu < *p_len) return GATT_NO_RESOURCES;
 
     gatt_build_uuid_to_stream(&p, attr16.p_value->uuid);
+    *p_data = p;
     return GATT_SUCCESS;
   }
 
@@ -205,6 +206,7 @@ static tGATT_STATUS read_attr_value(tGATT_ATTR& attr16, uint16_t offset,
       /* if 32 bit UUID, convert to 128 bit */
       ARRAY_TO_STREAM(p, val_attr->uuid.To128BitLE(), (int)Uuid::kNumBytes128);
     }
+    *p_data = p;
     return GATT_SUCCESS;
   }
 
@@ -223,6 +225,7 @@ static tGATT_STATUS read_attr_value(tGATT_ATTR& attr16, uint16_t offset,
     if (incl_handle.service_type.Is16Bit()) {
       UINT16_TO_STREAM(p, incl_handle.service_type.As16Bit());
     }
+    *p_data = p;
     return GATT_SUCCESS;
   }
 