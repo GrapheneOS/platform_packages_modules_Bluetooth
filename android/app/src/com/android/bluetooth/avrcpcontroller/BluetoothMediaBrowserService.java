LineageOS/android_packages_apps_Bluetooth

AVRCP Controller transient loss while idle
While gaining and losing focus due to transient focus requests ensure
Bluetooth restores to the correct playback state.  This corrects issues
that could arise during non media sonification durring a transient
focus loss.

Bug: 136092891
Test: atest com.android.bluetooth.a2dpsink.A2dpSinkStreamHandlerTest
Change-Id: I58997b3b8b96da309ff4c31a2b3039dccf9b3578

src/com/android/bluetooth/avrcpcontroller/BluetoothMediaBrowserService.java
@@ -185,6 +185,20 @@ public class BluetoothMediaBrowserService extends MediaBrowserServiceCompat {
     }
 
     /**
+     * Get playback state
+     */
+    public static synchronized int getPlaybackState() {
+        if (sBluetoothMediaBrowserService != null) {
+            PlaybackStateCompat currentPlaybackState =
+                    sBluetoothMediaBrowserService.mSession.getController().getPlaybackState();
+            if (currentPlaybackState != null) {
+                return currentPlaybackState.getState();
+            }
+        }
+        return PlaybackStateCompat.STATE_ERROR;
+    }
+
+    /**
      * Get object for controlling playback
      */
     public static synchronized MediaControllerCompat.TransportControls getTransportControls() {

GrapheneOS/platform_packages_modules_Bluetooth

Merged-In: I58997b3b8b96da309ff4c31a2b3039dccf9b3578
(cherry picked from commit b61b321f8fe9ef881e95bad5481143be0462dfeb)
Change-Id: If21c7a631c084ed1bc450d58cf3618cc1672699a

android/app/src/com/android/bluetooth/avrcpcontroller/BluetoothMediaBrowserService.java
@@ -177,6 +177,20 @@ public class BluetoothMediaBrowserService extends MediaBrowserService {
     }
 
     /**
+     * Get playback state
+     */
+    public static synchronized int getPlaybackState() {
+        if (sBluetoothMediaBrowserService != null) {
+            PlaybackState currentPlaybackState =
+                    sBluetoothMediaBrowserService.mSession.getController().getPlaybackState();
+            if (currentPlaybackState != null) {
+                return currentPlaybackState.getState();
+            }
+        }
+        return PlaybackState.STATE_ERROR;
+    }
+
+    /**
      * Get object for controlling playback
      */
     public static synchronized MediaController.TransportControls getTransportControls() {