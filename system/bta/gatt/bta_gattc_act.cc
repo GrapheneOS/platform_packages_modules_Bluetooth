GrapheneOS/platform_packages_modules_Bluetooth

commit 13c39b9b3528cd2d2860e4d1b2e4aac2d9414b6e

stack/gatt: Extend calls with CID
This is preparation for EATT

Tag: #feature
Bug: 159786353
Sponsor: jpawlowski@
Test: atest net_test_stack
Change-Id: Ie209b705f6cc458286e9566919280ed5e2de60c9

system/stack/gatt/att_protocol.cc
@@ -322,13 +322,14 @@ BT_HDR* attp_build_value_cmd(uint16_t payload_size, uint8_t op_code,
  * Description      Send message to L2CAP.
  *
  ******************************************************************************/
-tGATT_STATUS attp_send_msg_to_l2cap(tGATT_TCB& tcb, BT_HDR* p_toL2CAP) {
+tGATT_STATUS attp_send_msg_to_l2cap(tGATT_TCB& tcb, uint16_t lcid,
+                                    BT_HDR* p_toL2CAP) {
   uint16_t l2cap_ret;
 
-  if (tcb.att_lcid == L2CAP_ATT_CID)
-    l2cap_ret = L2CA_SendFixedChnlData(L2CAP_ATT_CID, tcb.peer_bda, p_toL2CAP);
+  if (lcid == L2CAP_ATT_CID)
+    l2cap_ret = L2CA_SendFixedChnlData(lcid, tcb.peer_bda, p_toL2CAP);
   else
-    l2cap_ret = (uint16_t)L2CA_DataWrite(tcb.att_lcid, p_toL2CAP);
+    l2cap_ret = (uint16_t)L2CA_DataWrite(lcid, p_toL2CAP);
 
   if (l2cap_ret == L2CAP_DW_FAILED) {
     LOG(ERROR) << __func__ << ": failed to write data to L2CAP";
@@ -394,11 +395,11 @@ BT_HDR* attp_build_sr_msg(tGATT_TCB& tcb, uint8_t op_code,
  *
  *
  ******************************************************************************/
-tGATT_STATUS attp_send_sr_msg(tGATT_TCB& tcb, BT_HDR* p_msg) {
+tGATT_STATUS attp_send_sr_msg(tGATT_TCB& tcb, uint16_t cid, BT_HDR* p_msg) {
   if (p_msg == NULL) return GATT_NO_RESOURCES;
 
   p_msg->offset = L2CAP_MIN_OFFSET;
-  return attp_send_msg_to_l2cap(tcb, p_msg);
+  return attp_send_msg_to_l2cap(tcb, cid, p_msg);
 }
 
 /*******************************************************************************
@@ -417,13 +418,14 @@ tGATT_STATUS attp_cl_send_cmd(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
                               uint8_t cmd_code, BT_HDR* p_cmd) {
   cmd_code &= ~GATT_AUTH_SIGN_MASK;
 
-  if (!tcb.cl_cmd_q.empty() && cmd_code != GATT_HANDLE_VALUE_CONF) {
+  if (gatt_tcb_is_cid_busy(tcb, p_clcb->cid) &&
+      cmd_code != GATT_HANDLE_VALUE_CONF) {
     gatt_cmd_enq(tcb, p_clcb, true, cmd_code, p_cmd);
     return GATT_CMD_STARTED;
   }
 
   /* no pending request or value confirmation */
-  tGATT_STATUS att_ret = attp_send_msg_to_l2cap(tcb, p_cmd);
+  tGATT_STATUS att_ret = attp_send_msg_to_l2cap(tcb, p_clcb->cid, p_cmd);
   if (att_ret != GATT_CONGESTED && att_ret != GATT_SUCCESS) {
     return GATT_INTERNAL_ERROR;
   }
@@ -458,6 +460,8 @@ tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
                               uint8_t op_code, tGATT_CL_MSG* p_msg) {
   BT_HDR* p_cmd = NULL;
   uint16_t offset = 0, handle;
+  uint16_t payload_size = gatt_tcb_get_payload_size_tx(tcb, p_clcb->cid);
+
   switch (op_code) {
     case GATT_REQ_MTU:
       if (p_msg->mtu > GATT_MAX_MTU_SIZE) return GATT_ILLEGAL_PARAMETER;
@@ -504,7 +508,7 @@ tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
         return GATT_ILLEGAL_PARAMETER;
 
       p_cmd = attp_build_value_cmd(
-          tcb.payload_size, op_code, p_msg->attr_value.handle, offset,
+          payload_size, op_code, p_msg->attr_value.handle, offset,
           p_msg->attr_value.len, p_msg->attr_value.value);
       break;
 
@@ -513,14 +517,14 @@ tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
       break;
 
     case GATT_REQ_FIND_TYPE_VALUE:
-      p_cmd = attp_build_read_by_type_value_cmd(tcb.payload_size,
+      p_cmd = attp_build_read_by_type_value_cmd(payload_size,
                                                 &p_msg->find_type_value);
       break;
 
     case GATT_REQ_READ_MULTI:
-      p_cmd = attp_build_read_multi_cmd(tcb.payload_size,
-                                        p_msg->read_multi.num_handles,
-                                        p_msg->read_multi.handles);
+      p_cmd =
+          attp_build_read_multi_cmd(payload_size, p_msg->read_multi.num_handles,
+                                    p_msg->read_multi.handles);
       break;
 
     default:

system/stack/gatt/gatt_api.cc
@@ -431,7 +431,11 @@ tGATT_STATUS GATTS_HandleValueIndication(uint16_t conn_id, uint16_t attr_handle,
   memcpy(indication.value, p_val, val_len);
   indication.auth_req = GATT_AUTH_REQ_NONE;
 
-  if (GATT_HANDLE_IS_VALID(p_tcb->indicate_handle)) {
+  uint16_t* indicate_handle_p = NULL;
+  uint16_t cid;
+
+  if (!gatt_tcb_get_cid_available_for_indication(p_tcb, p_reg->eatt_support,
+                                                 &indicate_handle_p, &cid)) {
     VLOG(1) << "Add a pending indication";
     gatt_add_pending_ind(p_tcb, &indication);
     return GATT_SUCCESS;
@@ -443,9 +447,9 @@ tGATT_STATUS GATTS_HandleValueIndication(uint16_t conn_id, uint16_t attr_handle,
       attp_build_sr_msg(*p_tcb, GATT_HANDLE_VALUE_IND, &gatt_sr_msg);
   if (!p_msg) return GATT_NO_RESOURCES;
 
-  tGATT_STATUS cmd_status = attp_send_sr_msg(*p_tcb, p_msg);
+  tGATT_STATUS cmd_status = attp_send_sr_msg(*p_tcb, cid, p_msg);
   if (cmd_status == GATT_SUCCESS || cmd_status == GATT_CONGESTED) {
-    p_tcb->indicate_handle = indication.handle;
+    *indicate_handle_p = indication.handle;
     gatt_start_conf_timer(p_tcb);
   }
   return cmd_status;
@@ -494,10 +498,13 @@ tGATT_STATUS GATTS_HandleValueNotification(uint16_t conn_id,
   tGATT_STATUS cmd_sent;
   tGATT_SR_MSG gatt_sr_msg;
   gatt_sr_msg.attr_value = notif;
+
+  uint16_t cid = gatt_tcb_get_att_cid(*p_tcb);
+
   BT_HDR* p_buf =
       attp_build_sr_msg(*p_tcb, GATT_HANDLE_VALUE_NOTIF, &gatt_sr_msg);
   if (p_buf != NULL) {
-    cmd_sent = attp_send_sr_msg(*p_tcb, p_buf);
+    cmd_sent = attp_send_sr_msg(*p_tcb, cid, p_buf);
   } else
     cmd_sent = GATT_NO_RESOURCES;
   return cmd_sent;
@@ -519,7 +526,6 @@ tGATT_STATUS GATTS_HandleValueNotification(uint16_t conn_id,
  ******************************************************************************/
 tGATT_STATUS GATTS_SendRsp(uint16_t conn_id, uint32_t trans_id,
                            tGATT_STATUS status, tGATTS_RSP* p_msg) {
-  tGATT_STATUS cmd_sent = GATT_ILLEGAL_PARAMETER;
   tGATT_IF gatt_if = GATT_GET_GATT_IF(conn_id);
   uint8_t tcb_idx = GATT_GET_TCB_IDX(conn_id);
   tGATT_REG* p_reg = gatt_get_regcb(gatt_if);
@@ -533,16 +539,17 @@ tGATT_STATUS GATTS_SendRsp(uint16_t conn_id, uint32_t trans_id,
     return (tGATT_STATUS)GATT_INVALID_CONN_ID;
   }
 
-  if (p_tcb->sr_cmd.trans_id != trans_id) {
+  tGATT_SR_CMD* sr_res_p = gatt_sr_get_cmd_by_trans_id(p_tcb, trans_id);
+
+  if (!sr_res_p) {
     LOG(ERROR) << "conn_id=" << loghex(conn_id)
-               << " waiting for op_code=" << loghex(p_tcb->sr_cmd.op_code);
+               << " waiting for other op_code ";
     return (GATT_WRONG_STATE);
   }
-  /* Process App response */
-  cmd_sent = gatt_sr_process_app_rsp(*p_tcb, gatt_if, trans_id,
-                                     p_tcb->sr_cmd.op_code, status, p_msg);
 
-  return cmd_sent;
+  /* Process App response */
+  return gatt_sr_process_app_rsp(*p_tcb, gatt_if, trans_id, sr_res_p->op_code,
+                                 status, p_msg, sr_res_p);
 }
 
 /******************************************************************************/
@@ -593,6 +600,8 @@ tGATT_STATUS GATTC_ConfigureMTU(uint16_t conn_id, uint16_t mtu) {
   tGATT_CLCB* p_clcb = gatt_clcb_alloc(conn_id);
   if (!p_clcb) return GATT_NO_RESOURCES;
 
+  /* For this request only ATT CID is valid */
+  p_clcb->cid = L2CAP_ATT_CID;
   p_clcb->p_tcb->payload_size = mtu;
   p_clcb->operation = GATTC_OPTYPE_CONFIG;
   tGATT_CL_MSG gatt_cl_msg;
@@ -709,7 +718,8 @@ tGATT_STATUS GATTC_Read(uint16_t conn_id, tGATT_READ_TYPE type,
   p_clcb->op_subtype = type;
   p_clcb->auth_req = p_read->by_handle.auth_req;
   p_clcb->counter = 0;
-  p_clcb->read_req_current_mtu = p_tcb->payload_size;
+  p_clcb->read_req_current_mtu =
+      gatt_tcb_get_payload_size_tx(*p_tcb, p_clcb->cid);
 
   switch (type) {
     case GATT_READ_BY_TYPE:

commit 34465601672a5e329ace024dbcc597c7704d6ec4

gatt: Add CID for sending notification confirmation
This patch fixes a regression after

commit 13c39b9b3528cd2d2860e4d1b2e4aac2d9414b6e
stack/gatt: Extend calls with CID

Notification confirmation was not taken into account.
For notification function attp_send_cl_msg() is called with p_clcb NULL
which leads to a crash in the following line:

uint16_t payload_size = gatt_tcb_get_payload_size_tx(tcb, p_clcb->cid);

This patch introduces a new function:  attp_send_cl_confirmation_msg()

which is used only for sending confirmation.
Also this patch makes sure that cid is provided to the GATT client so it
can be used in the confirmation call.
Note: handle was removed from the confirmation call as it is redundant.

The only missing part in this patch is handling timers per each
indication and this will be fixed in following patches which will add
eatt support.

Tag: #feature
Bug: 159786353
Sponsor: jpawlowski@
Test: compile & manual

Change-Id: I8bdb38ec673e11744d670223e5416be01666e1a6

system/bta/gatt/bta_gattc_act.cc
@@ -824,11 +824,11 @@ void bta_gattc_execute(tBTA_GATTC_CLCB* p_clcb, tBTA_GATTC_DATA* p_data) {
 
 /** send handle value confirmation */
 void bta_gattc_confirm(tBTA_GATTC_CLCB* p_clcb, tBTA_GATTC_DATA* p_data) {
-  uint16_t handle = p_data->api_confirm.handle;
+  uint16_t cid = p_data->api_confirm.cid;
 
   if (GATTC_SendHandleValueConfirm(p_data->api_confirm.hdr.layer_specific,
-                                   handle) != GATT_SUCCESS) {
-    LOG(ERROR) << __func__ << ": to handle=" << loghex(handle) << " failed";
+                                   cid) != GATT_SUCCESS) {
+    LOG(ERROR) << __func__ << ": to cid=" << loghex(cid) << " failed";
   } else {
     /* if over BR_EDR, inform PM for mode change */
     if (p_clcb->transport == BT_TRANSPORT_BR_EDR) {
@@ -1162,7 +1162,7 @@ bool bta_gattc_process_srvc_chg_ind(uint16_t conn_id, tBTA_GATTC_RCB* p_clrcb,
       }
     }
     /* send confirmation here if this is an indication, it should always be */
-    GATTC_SendHandleValueConfirm(conn_id, att_value->handle);
+    GATTC_SendHandleValueConfirm(conn_id, p_notify->cid);
 
     /* if connection available, refresh cache by doing discovery now */
     if (p_clcb) bta_gattc_sm_execute(p_clcb, BTA_GATTC_INT_DISCOVER_EVT, NULL);
@@ -1213,7 +1213,7 @@ void bta_gattc_process_indicate(uint16_t conn_id, tGATTC_OPTYPE op,
   if (!GATT_GetConnectionInfor(conn_id, &gatt_if, remote_bda, &transport)) {
     LOG(ERROR) << __func__ << ": indication/notif for unknown app";
     if (op == GATTC_OPTYPE_INDICATION)
-      GATTC_SendHandleValueConfirm(conn_id, handle);
+      GATTC_SendHandleValueConfirm(conn_id, p_data->cid);
     return;
   }
 
@@ -1221,7 +1221,7 @@ void bta_gattc_process_indicate(uint16_t conn_id, tGATTC_OPTYPE op,
   if (p_clrcb == NULL) {
     LOG(ERROR) << __func__ << ": indication/notif for unregistered app";
     if (op == GATTC_OPTYPE_INDICATION)
-      GATTC_SendHandleValueConfirm(conn_id, handle);
+      GATTC_SendHandleValueConfirm(conn_id, p_data->cid);
     return;
   }
 
@@ -1229,13 +1229,14 @@ void bta_gattc_process_indicate(uint16_t conn_id, tGATTC_OPTYPE op,
   if (p_srcb == NULL) {
     LOG(ERROR) << __func__ << ": indication/notif for unknown device, ignore";
     if (op == GATTC_OPTYPE_INDICATION)
-      GATTC_SendHandleValueConfirm(conn_id, handle);
+      GATTC_SendHandleValueConfirm(conn_id, p_data->cid);
     return;
   }
 
   tBTA_GATTC_CLCB* p_clcb = bta_gattc_find_clcb_by_conn_id(conn_id);
 
   notify.handle = handle;
+  notify.cid = p_data->cid;
 
   /* if service change indication/notification, don't forward to application */
   if (bta_gattc_process_srvc_chg_ind(conn_id, p_clrcb, p_srcb, p_clcb, &notify,
@@ -1265,7 +1266,7 @@ void bta_gattc_process_indicate(uint16_t conn_id, tGATTC_OPTYPE op,
   /* no one intersted and need ack? */
   else if (op == GATTC_OPTYPE_INDICATION) {
     VLOG(1) << __func__ << " no one interested, ack now";
-    GATTC_SendHandleValueConfirm(conn_id, handle);
+    GATTC_SendHandleValueConfirm(conn_id, p_data->cid);
   }
 }
 

system/bta/gatt/bta_gattc_api.cc
@@ -599,21 +599,21 @@ void BTA_GATTC_ExecuteWrite(uint16_t conn_id, bool is_execute) {
  * Description      This function is called to send handle value confirmation.
  *
  * Parameters       conn_id - connection ID.
- *                    p_char_id - characteristic ID to confirm.
+ *                  cid
  *
  * Returns          None
  *
  ******************************************************************************/
-void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t handle) {
+void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t cid) {
   tBTA_GATTC_API_CONFIRM* p_buf =
       (tBTA_GATTC_API_CONFIRM*)osi_calloc(sizeof(tBTA_GATTC_API_CONFIRM));
 
-  VLOG(1) << __func__ << ": conn_id=" << +conn_id << " handle=0x" << std::hex
-          << +handle;
+  VLOG(1) << __func__ << ": conn_id=" << +conn_id << " cid=0x" << std::hex
+          << +cid;
 
   p_buf->hdr.event = BTA_GATTC_API_CONFIRM_EVT;
   p_buf->hdr.layer_specific = conn_id;
-  p_buf->handle = handle;
+  p_buf->cid = cid;
 
   bta_sys_sendmsg(p_buf);
 }

system/bta/gatt/bta_gattc_int.h
@@ -134,7 +134,7 @@ typedef struct {
 
 typedef struct {
   BT_HDR hdr;
-  uint16_t handle;
+  uint16_t cid;
 } tBTA_GATTC_API_CONFIRM;
 
 typedef tGATT_CL_COMPLETE tBTA_GATTC_CMPL;

system/bta/include/bta_gatt_api.h
@@ -165,6 +165,7 @@ typedef struct {
   uint16_t len;
   uint8_t value[GATT_MAX_ATTR_LEN];
   bool is_notify;
+  uint16_t cid;
 } tBTA_GATTC_NOTIFY;
 
 typedef struct {
@@ -643,12 +644,12 @@ void BTA_GATTC_WriteCharDescr(uint16_t conn_id, uint16_t handle,
  * Description      This function is called to send handle value confirmation.
  *
  * Parameters       conn_id - connection ID.
- *                  handle - characteristic handle to confirm.
+ *                  cid - channel id
  *
  * Returns          None
  *
  ******************************************************************************/
-extern void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t handle);
+extern void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t cid);
 
 /*******************************************************************************
  *

system/stack/gatt/att_protocol.cc
@@ -442,6 +442,35 @@ tGATT_STATUS attp_cl_send_cmd(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
 
 /*******************************************************************************
  *
+ * Function         attp_send_cl_confirmation_msg
+ *
+ * Description      This function sends the client confirmation
+ *                  message to server.
+ *
+ * Parameter        p_tcb: pointer to the connection control block.
+ *                  cid: channel id
+ *
+ * Returns          GATT_SUCCESS if successfully sent; otherwise error code.
+ *
+ *
+ ******************************************************************************/
+tGATT_STATUS attp_send_cl_confirmation_msg(tGATT_TCB& tcb, uint16_t cid) {
+  BT_HDR* p_cmd = NULL;
+  p_cmd = attp_build_opcode_cmd(GATT_HANDLE_VALUE_CONF);
+
+  if (p_cmd == NULL) return GATT_NO_RESOURCES;
+
+  /* no pending request or value confirmation */
+  tGATT_STATUS att_ret = attp_send_msg_to_l2cap(tcb, cid, p_cmd);
+  if (att_ret != GATT_CONGESTED && att_ret != GATT_SUCCESS) {
+    return GATT_INTERNAL_ERROR;
+  }
+
+  return att_ret;
+}
+
+/*******************************************************************************
+ *
  * Function         attp_send_cl_msg
  *
  * Description      This function sends the client request or confirmation
@@ -460,6 +489,12 @@ tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
                               uint8_t op_code, tGATT_CL_MSG* p_msg) {
   BT_HDR* p_cmd = NULL;
   uint16_t offset = 0, handle;
+
+  if (!p_clcb) {
+    LOG(ERROR) << "Missing p_clcb" << +op_code;
+    return GATT_ILLEGAL_PARAMETER;
+  }
+
   uint16_t payload_size = gatt_tcb_get_payload_size_tx(tcb, p_clcb->cid);
 
   switch (op_code) {
@@ -492,10 +527,6 @@ tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
       if (!GATT_HANDLE_IS_VALID(handle)) return GATT_ILLEGAL_PARAMETER;
 
       p_cmd = attp_build_handle_cmd(op_code, handle, offset);
-      break;
-
-    case GATT_HANDLE_VALUE_CONF:
-      p_cmd = attp_build_opcode_cmd(op_code);
       break;
 
     case GATT_REQ_PREPARE_WRITE:

system/stack/gatt/gatt_api.cc
@@ -861,14 +861,14 @@ tGATT_STATUS GATTC_ExecuteWrite(uint16_t conn_id, bool is_execute) {
  *                  as response to a handle value notification from server.
  *
  * Parameters       conn_id: connection identifier.
- *                  handle: the handle of the attribute confirmation.
+ *                  cid: channel id.
  *
  * Returns          GATT_SUCCESS if command started successfully.
  *
  ******************************************************************************/
-tGATT_STATUS GATTC_SendHandleValueConfirm(uint16_t conn_id, uint16_t handle) {
+tGATT_STATUS GATTC_SendHandleValueConfirm(uint16_t conn_id, uint16_t cid) {
   VLOG(1) << __func__ << " conn_id=" << loghex(conn_id)
-          << ", handle=" << loghex(handle);
+          << ", cid=" << loghex(cid);
 
   tGATT_TCB* p_tcb = gatt_get_tcb_by_idx(GATT_GET_TCB_IDX(conn_id));
   if (!p_tcb) {
@@ -882,14 +882,12 @@ tGATT_STATUS GATTC_SendHandleValueConfirm(uint16_t conn_id, uint16_t handle) {
     return GATT_SUCCESS;
   }
 
+  /*TODO Introduce timer per CID */
   alarm_cancel(p_tcb->ind_ack_timer);
 
   VLOG(1) << "notif_count= " << p_tcb->ind_count;
   /* send confirmation now */
-  tGATT_CL_MSG gatt_cl_msg;
-  gatt_cl_msg.handle = handle;
-  tGATT_STATUS ret =
-      attp_send_cl_msg(*p_tcb, nullptr, GATT_HANDLE_VALUE_CONF, &gatt_cl_msg);
+  tGATT_STATUS ret = attp_send_cl_confirmation_msg(*p_tcb, cid);
 
   p_tcb->ind_count = 0;
 

system/stack/gatt/gatt_cl.cc
@@ -608,8 +608,8 @@ void gatt_process_prep_write_rsp(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
  * Returns          void
  *
  ******************************************************************************/
-void gatt_process_notification(tGATT_TCB& tcb, uint8_t op_code, uint16_t len,
-                               uint8_t* p_data) {
+void gatt_process_notification(tGATT_TCB& tcb, uint16_t cid, uint8_t op_code,
+                               uint16_t len, uint8_t* p_data) {
   tGATT_VALUE value;
   tGATT_REG* p_reg;
   uint16_t conn_id;
@@ -639,7 +639,7 @@ void gatt_process_notification(tGATT_TCB& tcb, uint8_t op_code, uint16_t len,
   if (!GATT_HANDLE_IS_VALID(value.handle)) {
     /* illegal handle, send ack now */
     if (op_code == GATT_HANDLE_VALUE_IND)
-      attp_send_cl_msg(tcb, nullptr, GATT_HANDLE_VALUE_CONF, NULL);
+      attp_send_cl_confirmation_msg(tcb, cid);
     return;
   }
 
@@ -671,14 +671,16 @@ void gatt_process_notification(tGATT_TCB& tcb, uint8_t op_code, uint16_t len,
   if (event == GATTC_OPTYPE_INDICATION) {
     /* start a timer for app confirmation */
     if (tcb.ind_count > 0)
-      gatt_start_ind_ack_timer(tcb);
+      gatt_start_ind_ack_timer(tcb, cid);
     else /* no app to indicate, or invalid handle */
-      attp_send_cl_msg(tcb, nullptr, GATT_HANDLE_VALUE_CONF, NULL);
+      attp_send_cl_confirmation_msg(tcb, cid);
   }
 
   encrypt_status = gatt_get_link_encrypt_status(tcb);
   tGATT_CL_COMPLETE gatt_cl_complete;
   gatt_cl_complete.att_value = value;
+  gatt_cl_complete.cid = cid;
+
   for (i = 0, p_reg = gatt_cb.cl_rcb; i < GATT_MAX_APPS; i++, p_reg++) {
     if (p_reg->in_use && p_reg->app_cb.p_cmpl_cb) {
       conn_id = GATT_CREATE_CONN_ID(tcb.tcb_idx, p_reg->gatt_if);
@@ -1092,7 +1094,7 @@ void gatt_client_handle_server_rsp(tGATT_TCB& tcb, uint16_t cid,
       return;
     }
 
-    gatt_process_notification(tcb, op_code, len, p_data);
+    gatt_process_notification(tcb, cid, op_code, len, p_data);
     return;
   }
 

system/stack/gatt/gatt_int.h
@@ -438,6 +438,7 @@ extern bool gatt_profile_get_eatt_support(
     base::OnceCallback<void(const RawAddress&, bool)> cb);
 
 /* Functions provided by att_protocol.cc */
+extern tGATT_STATUS attp_send_cl_confirmation_msg(tGATT_TCB& tcb, uint16_t cid);
 extern tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
                                      uint8_t op_code, tGATT_CL_MSG* p_msg);
 extern BT_HDR* attp_build_sr_msg(tGATT_TCB& tcb, uint8_t op_code,
@@ -465,7 +466,7 @@ extern void gatt_start_conf_timer(tGATT_TCB* p_tcb);
 extern void gatt_rsp_timeout(void* data);
 extern void gatt_indication_confirmation_timeout(void* data);
 extern void gatt_ind_ack_timeout(void* data);
-extern void gatt_start_ind_ack_timer(tGATT_TCB& tcb);
+extern void gatt_start_ind_ack_timer(tGATT_TCB& tcb, uint16_t cid);
 extern tGATT_STATUS gatt_send_error_rsp(tGATT_TCB& tcb, uint16_t cid,
                                         uint8_t err_code, uint8_t op_code,
                                         uint16_t handle, bool deq);

system/stack/gatt/gatt_utils.cc
@@ -571,8 +571,9 @@ void gatt_start_conf_timer(tGATT_TCB* p_tcb) {
  * Returns          void
  *
  ******************************************************************************/
-void gatt_start_ind_ack_timer(tGATT_TCB& tcb) {
+void gatt_start_ind_ack_timer(tGATT_TCB& tcb, uint16_t cid) {
   /* start notification cache timer */
+  /* TODO: set timer per CID */
   alarm_set_on_mloop(tcb.ind_ack_timer, GATT_WAIT_FOR_RSP_TIMEOUT_MS,
                      gatt_ind_ack_timeout, &tcb);
 }
@@ -664,7 +665,10 @@ void gatt_ind_ack_timeout(void* data) {
 
   LOG(WARNING) << __func__ << ": send ack now";
   p_tcb->ind_count = 0;
-  attp_send_cl_msg(*p_tcb, nullptr, GATT_HANDLE_VALUE_CONF, NULL);
+  /*TODO: For now ATT used only, but we need to have timeout per CID
+   * and use it here corretly.
+   */
+  attp_send_cl_confirmation_msg(*p_tcb, L2CAP_ATT_CID);
 }
 /*******************************************************************************
  *

system/stack/include/gatt_api.h
@@ -471,6 +471,7 @@ typedef union {
   tGATT_VALUE att_value;
   uint16_t mtu;
   uint16_t handle;
+  uint16_t cid;
 } tGATT_CL_COMPLETE;
 
 /* GATT client operation type, used in client callback function

system/btif/src/btif_gatt_test.cc
@@ -88,7 +88,7 @@ static void btif_test_command_complete_cback(uint16_t conn_id, tGATTC_OPTYPE op,
       break;
 
     case GATTC_OPTYPE_INDICATION:
-      GATTC_SendHandleValueConfirm(conn_id, p_data->handle);
+      GATTC_SendHandleValueConfirm(conn_id, p_data->cid);
       break;
 
     default:

system/stack/test/gatt/gatt_sr_test.cc
@@ -64,6 +64,9 @@ BT_HDR* attp_build_sr_msg(tGATT_TCB& tcb, uint8_t op_code,
   test_state_.attp_build_sr_msg.op_code_ = op_code;
   return nullptr;
 }
+tGATT_STATUS attp_send_cl_confirmation_msg(tGATT_TCB& tcb, uint16_t cid) {
+  return 0;
+}
 tGATT_STATUS attp_send_cl_msg(tGATT_TCB& tcb, tGATT_CLCB* p_clcb,
                               uint8_t op_code, tGATT_CL_MSG* p_msg) {
   return 0;