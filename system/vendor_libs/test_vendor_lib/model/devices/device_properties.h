5d377ee7d63240577723103c54de4cee654ae17b
David Duarte <licorne@google.com>
2021-06-02 12:16:57 +0000

    RootCanal: Use EventMask
    Don't send events that have been masked
    
    Test: gd/cert/run
    Tag: #testing
    Change-Id: Icb8a94db570fb77c852054faa2e0e411fe2b23a8
    Signed-off-by: David Duarte <licorne@google.com>

--- system/vendor_libs/test_vendor_lib/model/devices/device_properties.h
+++ system/vendor_libs/test_vendor_lib/model/devices/device_properties.h
@@ -30,6 +30,7 @@ namespace test_vendor_lib {
 
 using ::bluetooth::hci::Address;
 using ::bluetooth::hci::ClassOfDevice;
+using ::bluetooth::hci::EventCode;
 
 class DeviceProperties {
  public:
@@ -228,6 +229,11 @@ class DeviceProperties {
   uint64_t GetEventMask() const { return event_mask_; }
 
   void SetEventMask(uint64_t mask) { event_mask_ = mask; }
+
+  bool IsUnmasked(EventCode event) const {
+    uint64_t bit = UINT64_C(1) << (static_cast<uint8_t>(event) - 1);
+    return (event_mask_ & bit) != 0;
+  }
 
   // Low-Energy functions
   const Address& GetLeAddress() const {
