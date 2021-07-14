22736c01a5025a1285e55a2696a38acadcab8303
Sonny Sasaka <sonnysasaka@google.com>
2021-07-14 13:54:19 -0700

    floss: client: Add basic help command and autocomplete
    This adds help and autocomplete to btclient. Currently only supports
    list of commands to be displayed or autocompleted.
    
    Bug: 188718349
    Tag: #floss
    Test: manual - Ran btclient, type a string and Tab.
    
    Change-Id: I27f112427f453649d786defb89fb3c21231bbb4c

--- system/gd/rust/linux/client/src/main.rs
+++ system/gd/rust/linux/client/src/main.rs
@@ -98,9 +98,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             objpath: String::from("/org/chromium/bluetooth/client/bluetooth_manager_callback"),
         }));
 
-        let mut handler = CommandHandler::new(api.bluetooth_manager.clone(), api.bluetooth.clone());
+        // TODO(b/193719802): Refactor this into a dedicated "Help" data structure.
+        let commands = vec![
+            String::from("help"),
+            String::from("enable"),
+            String::from("disable"),
+            String::from("get_address"),
+            String::from("start_discovery"),
+            String::from("cancel_discovery"),
+            String::from("create_bond"),
+        ];
+
+        let mut handler = CommandHandler::new(
+            api.bluetooth_manager.clone(),
+            api.bluetooth.clone(),
+            commands.clone(),
+        );
 
         let mut handle_cmd = move |cmd: String| match cmd.split(' ').collect::<Vec<&str>>()[0] {
+            "help" => handler.cmd_help(),
+
             "enable" => handler.cmd_enable(cmd),
             "disable" => handler.cmd_disable(cmd),
             "get_address" => handler.cmd_get_address(cmd),
@@ -115,7 +132,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => print_info!("Command \"{}\" not recognized", cmd),
         };
 
-        let editor = AsyncEditor::new();
+        let editor = AsyncEditor::new(commands.clone());
 
         loop {
             let result = editor.readline().await;
