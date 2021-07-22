05332200a2025c20266eee3b3a0b9cb451a75a4b
Hansong Zhang <hsz@google.com>
2021-07-21 17:48:47 -0700

    Add Android build target and A2dp sink for topshim
    Make it compile.
    
    Test: build in Android and chromeos
    Bug: 181590011
    Tag: #refactor
    Change-Id: I2dd0dbc8277e41f964da31ee0e59b6c7d23d56fa

---
+++ system/gd/rust/topshim/btav_sink/btav_sink_shim.cc
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+
+#include "gd/rust/topshim/btav_sink/btav_sink_shim.h"
+#include "include/hardware/bluetooth.h"
+
+namespace bluetooth {
+namespace topshim {
+namespace rust {
+namespace internal {
+static A2dpSinkIntf* g_a2dp_sink_if;
+
+static void connection_state_cb(const RawAddress&, btav_connection_state_t) {}
+static void audio_state_cb(const RawAddress&, btav_audio_state_t) {}
+static void audio_config_cb(const RawAddress&, uint32_t, uint8_t) {}
+
+btav_sink_callbacks_t g_a2dp_sink_callbacks = {
+    sizeof(btav_sink_callbacks_t),
+    connection_state_cb,
+    audio_state_cb,
+    audio_config_cb,
+};
+}  // namespace internal
+
+A2dpSinkIntf::~A2dpSinkIntf() {
+  // TODO
+}
+
+std::unique_ptr<A2dpSinkIntf> GetA2dpSinkProfile(const unsigned char* btif) {
+  if (internal::g_a2dp_sink_if) std::abort();
+
+  const bt_interface_t* btif_ = reinterpret_cast<const bt_interface_t*>(btif);
+
+  auto a2dp_sink = std::make_unique<A2dpSinkIntf>(
+      reinterpret_cast<const btav_sink_interface_t*>(btif_->get_profile_interface("a2dp_sink")));
+  internal::g_a2dp_sink_if = a2dp_sink.get();
+  return a2dp_sink;
+}
+
+int A2dpSinkIntf::init() {
+  return intf_->init(&internal::g_a2dp_sink_callbacks, 1);
+}
+
+void A2dpSinkIntf::cleanup() {
+  // TODO: Implement.
+}
+
+}  // namespace rust
+}  // namespace topshim
+}  // namespace bluetooth
