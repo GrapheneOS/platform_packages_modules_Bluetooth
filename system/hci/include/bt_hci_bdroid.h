df07a8054a51a1d89cc9dde4eeac539163f346f5
Chris Manton <cmanton@google.com>
2021-08-21 11:24:55 -0700

    legacy: Remove parts hci/include/bt_hci_bdroid
    Bug: 197150934
    Tag: #refactor
    Test: gd/cert/run
    Change-Id: I5afc484bdb38d64b3826aec331bd0513538becc5

--- system/hci/include/bt_hci_bdroid.h
+++ system/hci/include/bt_hci_bdroid.h
@@ -33,6 +33,9 @@
 #include "bdroid_buildcfg.h"
 #endif
 
+#pragma message "This header file bt_hci_bdroid.h is no longer supported"
+#pragma message "Please update to remove these declarations"
+
 /******************************************************************************
  *  Constants & Macros
  *****************************************************************************/
@@ -40,37 +43,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define HCI_ACL_MAX_SIZE 1024
-#define HCI_MAX_FRAME_SIZE (HCI_ACL_MAX_SIZE + 4)
-
-/* Host/Controller lib internal event ID */
-typedef enum {
-  HC_EVENT_LPM_IDLE_TIMEOUT,
-} bthc_event_t;
-
-/* Message event mask across Host/Controller lib and stack */
-#define MSG_EVT_MASK 0xFF00     /* eq. BT_EVT_MASK */
-#define MSG_SUB_EVT_MASK 0x00FF /* eq. BT_SUB_EVT_MASK */
-
-/* Message event ID passed from Host/Controller lib to stack */
-#define MSG_HC_TO_STACK_HCI_ACL 0x1100      /* eq. BT_EVT_TO_BTU_HCI_ACL */
-#define MSG_HC_TO_STACK_HCI_SCO 0x1200      /* eq. BT_EVT_TO_BTU_HCI_SCO */
-#define MSG_HC_TO_STACK_HCI_ERR 0x1300      /* eq. BT_EVT_TO_BTU_HCIT_ERR */
-#define MSG_HC_TO_STACK_HCI_ISO 0x1700      /* eq. BT_EVT_TO_BTU_HCI_ISO */
-#define MSG_HC_TO_STACK_HCI_EVT 0x1000      /* eq. BT_EVT_TO_BTU_HCI_EVT */
-
-/* Message event ID passed from stack to vendor lib */
-#define MSG_STACK_TO_HC_HCI_ACL 0x2100 /* eq. BT_EVT_TO_LM_HCI_ACL */
-#define MSG_STACK_TO_HC_HCI_SCO 0x2200 /* eq. BT_EVT_TO_LM_HCI_SCO */
-#define MSG_STACK_TO_HC_HCI_ISO 0x2d00 /* eq. BT_EVT_TO_LM_HCI_ISO */
 #define MSG_STACK_TO_HC_HCI_CMD 0x2000 /* eq. BT_EVT_TO_LM_HCI_CMD */
-
-/* Local Bluetooth Controller ID for BR/EDR */
-#define LOCAL_BR_EDR_CONTROLLER_ID 0
-
-/******************************************************************************
- *  Type definitions and return values
- *****************************************************************************/
 
 typedef struct {
   uint16_t event;
@@ -81,17 +54,3 @@ typedef struct {
 } HC_BT_HDR;
 
 #define BT_HC_HDR_SIZE (sizeof(HC_BT_HDR))
-
-/******************************************************************************
- *  Extern variables and functions
- *****************************************************************************/
-
-/******************************************************************************
- *  Functions
- *****************************************************************************/
-
-// Called when a buffer has been produced by the serial layer and should be
-// processed by the HCI layer.
-void bthc_rx_ready(void);
-void bthc_tx(HC_BT_HDR* buf);
-void bthc_idle_timeout(void);
