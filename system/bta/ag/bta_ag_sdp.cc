99d2e38e026f55fba046c0bf299411c7721a0644
Chris Manton <cmanton@google.com>
2021-09-08 15:04:10 -0700

    legacy: Replace BTA_AG_CODEC_MSBC => BTM_SCO_CODEC_MSBC
    Bug: 163134718
    Tag: #refactor
    Test: gd/cert/run
    
    Change-Id: Ic5f741370a3adba9f065d68c42323c16daf07097

--- system/bta/ag/bta_ag_sdp.cc
+++ system/bta/ag/bta_ag_sdp.cc
@@ -369,7 +369,7 @@ bool bta_ag_sdp_find_attr(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
           // 2. But do not send required AT+BAC command
           // Will assume mSBC is enabled and try codec negotiation by default
           p_scb->codec_updated = true;
-          p_scb->peer_codecs = BTM_SCO_CODEC_CVSD & BTA_AG_CODEC_MSBC;
+          p_scb->peer_codecs = BTM_SCO_CODEC_CVSD & BTM_SCO_CODEC_MSBC;
           p_scb->sco_codec = UUID_CODEC_MSBC;
         }
         if (sdp_features != p_scb->peer_sdp_features) {
