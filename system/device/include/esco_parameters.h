bc82d590f9a8cab6d15688815176b80afe857b39
Hansong Zhang <hsz@google.com>
2021-09-14 15:40:37 -0700

    BTM_SCO HCI path handling
    Only handle CVSD case for now (just PCM).
    Read data from audio service and send when we receive a packet from the
    remote for now.
    
    Tag: #refactor
    Test: cert/run
    Bug: 195344796
    Change-Id: I647ba0392a19ba50ca4dd659aa02ade240a43afb

--- system/device/include/esco_parameters.h
+++ system/device/include/esco_parameters.h
@@ -60,8 +60,7 @@ typedef uint8_t esco_pcm_data_format_t;
 
 // SCO Data Path
 #define ESCO_DATA_PATH_PCM 1                /* 0x01-0xFE (PCM Chan) */
-#define ESCO_DATA_PATH_HCI ((uint8_t)0x00)  /* HCI-0, 0x01-0xFE (PCM Chan) */
-#define ESCO_DATA_PATH_TEST ((uint8_t)0xFF) /* 0xFF-Audio Test */
+#define ESCO_DATA_PATH_HCI 0                /* HCI-0, 0x01-0xFE (PCM Chan) */
 typedef uint8_t esco_data_path_t;
 
 // eSCO constants
