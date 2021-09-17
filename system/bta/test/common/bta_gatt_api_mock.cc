90617877bbbc3996197f005f77c83a1cb64a1e3f
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2021-09-17 07:51:24 +0000

    bta/test: Improve gatt mock api
    Bug: 150670922
    Tag: #feature
    Test: Na
    Sponsor: jpawlowski@
    
    Change-Id: I6dc89d7cc10dbdbc4a32808a77fe56b6de1738d0

--- system/bta/test/common/bta_gatt_api_mock.cc
+++ system/bta/test/common/bta_gatt_api_mock.cc
@@ -26,54 +26,72 @@ void gatt::SetMockBtaGattInterface(
 
 void BTA_GATTC_AppRegister(tBTA_GATTC_CBACK* p_client_cb,
                            BtaAppRegisterCallback cb, bool eatt_support) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->AppRegister(p_client_cb, cb, eatt_support);
 }
 
 void BTA_GATTC_AppDeregister(tGATT_IF client_if) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->AppDeregister(client_if);
 }
 
 void BTA_GATTC_Open(tGATT_IF client_if, const RawAddress& remote_bda,
-                    bool is_direct, tBT_TRANSPORT transport,
-                    bool opportunistic) {
+                    bool is_direct, tBT_TRANSPORT transport, bool opportunistic,
+                    uint8_t initiating_phys) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->Open(client_if, remote_bda, is_direct, transport,
-                       opportunistic);
+                       opportunistic, initiating_phys);
 }
 
 void BTA_GATTC_Open(tGATT_IF client_if, const RawAddress& remote_bda,
                     bool is_direct, bool opportunistic) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->Open(client_if, remote_bda, is_direct, opportunistic);
 }
 
 void BTA_GATTC_CancelOpen(tGATT_IF client_if, const RawAddress& remote_bda,
                           bool is_direct) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->CancelOpen(client_if, remote_bda, is_direct);
 }
 
-void BTA_GATTC_Close(uint16_t conn_id) { gatt_interface->Close(conn_id); }
+void BTA_GATTC_Close(uint16_t conn_id) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
+  gatt_interface->Close(conn_id);
+}
 
 void BTA_GATTC_ServiceSearchRequest(uint16_t conn_id,
                                     const bluetooth::Uuid* p_srvc_uuid) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   gatt_interface->ServiceSearchRequest(conn_id, p_srvc_uuid);
 }
 
+void BTA_GATTC_SendIndConfirm(uint16_t conn_id, uint16_t cid) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
+  gatt_interface->SendIndConfirm(conn_id, cid);
+}
+
 const std::list<gatt::Service>* BTA_GATTC_GetServices(uint16_t conn_id) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   return gatt_interface->GetServices(conn_id);
 }
 
 const gatt::Characteristic* BTA_GATTC_GetCharacteristic(uint16_t conn_id,
                                                         uint16_t handle) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   return gatt_interface->GetCharacteristic(conn_id, handle);
 }
 
 const gatt::Service* BTA_GATTC_GetOwningService(uint16_t conn_id,
                                                 uint16_t handle) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   return gatt_interface->GetOwningService(conn_id, handle);
 }
 
 tGATT_STATUS BTA_GATTC_RegisterForNotifications(tGATT_IF client_if,
                                                 const RawAddress& remote_bda,
                                                 uint16_t handle) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   return gatt_interface->RegisterForNotifications(client_if, remote_bda,
                                                   handle);
 }
@@ -81,6 +99,7 @@ tGATT_STATUS BTA_GATTC_RegisterForNotifications(tGATT_IF client_if,
 tGATT_STATUS BTA_GATTC_DeregisterForNotifications(tGATT_IF client_if,
                                                   const RawAddress& remote_bda,
                                                   uint16_t handle) {
+  LOG_ASSERT(gatt_interface) << "Mock GATT interface not set!";
   return gatt_interface->DeregisterForNotifications(client_if, remote_bda,
                                                     handle);
 }
