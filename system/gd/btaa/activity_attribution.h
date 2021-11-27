miyu-xu/packages-modules-Bluetooth

BTAA: propagate app uid and package name to BTAA module
Tag: #feature

Bug: 202527952
Bug: 172501038

Test: mmma -j packages/modules/Bluetooth

Signed-off-by: Mingguang Xu <mingguangxu@google.com>
Change-Id: I0da56b8648ac1218497f3d89555bd9db488e712c

system/gd/btaa/activity_attribution.h
@@ -57,6 +57,7 @@ class ActivityAttribution : public bluetooth::Module {
   void OnWakelockReleased();
   void OnWakeup();
   void RegisterActivityAttributionCallback(ActivityAttributionCallback* callback);
+  void NotifyActivityAttributionInfo(int uid, const std::string& packageName, const std::string& deviceAddress);
 
   static const ModuleFactory Factory;
 

system/gd/btaa/android/activity_attribution.cc
@@ -161,6 +161,10 @@ struct ActivityAttribution::impl {
     callback_ = callback;
   }
 
+  void notify_activity_attribution_info(int uid, const std::string& packageName, const std::string& deviceAddress) {
+    attribution_processor_.NotifyActivityAttributionInfo(uid, packageName, deviceAddress);
+  }
+
   void Dump(
       std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder) {
     attribution_processor_.Dump(std::move(promise), fb_builder);
@@ -210,6 +214,11 @@ void ActivityAttribution::OnWakeup() {
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {
   CallOn(pimpl_.get(), &impl::register_callback, callback);
+}
+
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& packageName, const std::string& deviceAddress) {
+  CallOn(pimpl_.get(), &impl::notify_activity_attribution_info, uid, packageName, deviceAddress);
 }
 
 std::string ActivityAttribution::ToString() const {

system/gd/btaa/attribution_processor.h
@@ -55,6 +55,7 @@ class AttributionProcessor {
   void OnBtaaPackets(std::vector<BtaaHciPacket> btaa_packets);
   void OnWakelockReleased(uint32_t duration_ms);
   void OnWakeup();
+  void NotifyActivityAttributionInfo(int uid, const std::string& packageName, const std::string& deviceAddress);
   void Dump(
       std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder);
 

system/gd/btaa/host/activity_attribution.cc
@@ -31,6 +31,9 @@ void ActivityAttribution::OnWakeup() {}
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {}
 
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& packageName, const std::string& deviceAddress) {}
+
 std::string ActivityAttribution::ToString() const {
   return "Btaa Module";
 }

system/gd/btaa/linux/activity_attribution.cc
@@ -34,6 +34,9 @@ void ActivityAttribution::Capture(const hal::HciPacket& packet, hal::SnoopLogger
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {}
 
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& packageName, const std::string& deviceAddress) {}
+
 std::string ActivityAttribution::ToString() const {
   return "Btaa Module";
 }

system/gd/btaa/linux_generic/attribution_processor.cc
@@ -108,6 +108,9 @@ void AttributionProcessor::OnWakeup() {
   wakeup_ = true;
 }
 
+void AttributionProcessor::NotifyActivityAttributionInfo(
+    int uid, const std::string& packageName, const std::string& deviceAddress) {}
+
 void AttributionProcessor::Dump(
     std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder) {
   // Dump wakeup attribution data

system/include/hardware/bt_activity_attribution.h
@@ -68,6 +68,12 @@ class ActivityAttributionInterface {
 
   /** Closes the interface. */
   virtual void Cleanup(void) = 0;
+
+  /** Notify the UID and package name of the app, and the address of associated
+   * active device */
+  virtual void NotifyActivityAttributionInfo(
+      int uid, const std::string& packageName,
+      const std::string& deviceAddress) = 0;
 };
 
 }  // namespace activity_attribution

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/btaa/activity_attribution.h
@@ -57,6 +57,7 @@ class ActivityAttribution : public bluetooth::Module {
   void OnWakelockReleased();
   void OnWakeup();
   void RegisterActivityAttributionCallback(ActivityAttributionCallback* callback);
+  void NotifyActivityAttributionInfo(int uid, const std::string& package_name, const std::string& device_address);
 
   static const ModuleFactory Factory;
 

system/gd/btaa/android/activity_attribution.cc
@@ -161,6 +161,10 @@ struct ActivityAttribution::impl {
     callback_ = callback;
   }
 
+  void notify_activity_attribution_info(int uid, const std::string& package_name, const std::string& device_address) {
+    attribution_processor_.NotifyActivityAttributionInfo(uid, package_name, device_address);
+  }
+
   void Dump(
       std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder) {
     attribution_processor_.Dump(std::move(promise), fb_builder);
@@ -210,6 +214,11 @@ void ActivityAttribution::OnWakeup() {
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {
   CallOn(pimpl_.get(), &impl::register_callback, callback);
+}
+
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& package_name, const std::string& device_address) {
+  CallOn(pimpl_.get(), &impl::notify_activity_attribution_info, uid, package_name, device_address);
 }
 
 std::string ActivityAttribution::ToString() const {

system/gd/btaa/attribution_processor.h
@@ -55,6 +55,7 @@ class AttributionProcessor {
   void OnBtaaPackets(std::vector<BtaaHciPacket> btaa_packets);
   void OnWakelockReleased(uint32_t duration_ms);
   void OnWakeup();
+  void NotifyActivityAttributionInfo(int uid, const std::string& package_name, const std::string& device_address);
   void Dump(
       std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder);
 

system/gd/btaa/host/activity_attribution.cc
@@ -31,6 +31,9 @@ void ActivityAttribution::OnWakeup() {}
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {}
 
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& package_name, const std::string& device_address) {}
+
 std::string ActivityAttribution::ToString() const {
   return "Btaa Module";
 }

system/gd/btaa/linux/activity_attribution.cc
@@ -34,6 +34,9 @@ void ActivityAttribution::Capture(const hal::HciPacket& packet, hal::SnoopLogger
 
 void ActivityAttribution::RegisterActivityAttributionCallback(ActivityAttributionCallback* callback) {}
 
+void ActivityAttribution::NotifyActivityAttributionInfo(
+    int uid, const std::string& package_name, const std::string& device_address) {}
+
 std::string ActivityAttribution::ToString() const {
   return "Btaa Module";
 }

system/gd/btaa/linux_generic/attribution_processor.cc
@@ -108,6 +108,9 @@ void AttributionProcessor::OnWakeup() {
   wakeup_ = true;
 }
 
+void AttributionProcessor::NotifyActivityAttributionInfo(
+    int uid, const std::string& package_name, const std::string& device_address) {}
+
 void AttributionProcessor::Dump(
     std::promise<flatbuffers::Offset<ActivityAttributionData>> promise, flatbuffers::FlatBufferBuilder* fb_builder) {
   // Dump wakeup attribution data

system/include/hardware/bt_activity_attribution.h
@@ -68,6 +68,12 @@ class ActivityAttributionInterface {
 
   /** Closes the interface. */
   virtual void Cleanup(void) = 0;
+
+  /** Notify the UID and package name of the app, and the address of associated
+   * active device */
+  virtual void NotifyActivityAttributionInfo(
+      int uid, const std::string& package_name,
+      const std::string& device_address) = 0;
 };
 
 }  // namespace activity_attribution