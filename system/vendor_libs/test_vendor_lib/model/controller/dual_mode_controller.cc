miyu-xu/packages-modules-Bluetooth

Root-Canal: Implement support for SCO connections
- Add the deprecated HCI command Add_SCO_Connection,
  used by the PTS to setup SCO (and not eSCO) connections.
- Implement SCO link parameter negotiation, report
  correct link type in Synchronous Connection Complete events

Tag: #feature
Bug: 209800014
Test: cert/run
Change-Id: Iefbe5b64594bf0b1a55110342733f4e3a8a39ed0

system/vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
@@ -122,6 +122,7 @@ DualModeController::DualModeController(const std::string& properties_filename, u
   SET_SUPPORTED(SWITCH_ROLE, SwitchRole);
   SET_SUPPORTED(READ_REMOTE_SUPPORTED_FEATURES, ReadRemoteSupportedFeatures);
   SET_SUPPORTED(READ_CLOCK_OFFSET, ReadClockOffset);
+  SET_SUPPORTED(ADD_SCO_CONNECTION, AddScoConnection);
   SET_SUPPORTED(SETUP_SYNCHRONOUS_CONNECTION, SetupSynchronousConnection);
   SET_SUPPORTED(ACCEPT_SYNCHRONOUS_CONNECTION, AcceptSynchronousConnection);
   SET_SUPPORTED(REJECT_SYNCHRONOUS_CONNECTION, RejectSynchronousConnection);
@@ -657,6 +658,22 @@ void DualModeController::ReadClockOffset(CommandView command) {
       OpCode::READ_CLOCK_OFFSET, command_view.GetPayload(), handle);
 
   auto packet = bluetooth::hci::ReadClockOffsetStatusBuilder::Create(
+      status, kNumCommandPackets);
+  send_event_(std::move(packet));
+}
+
+// Deprecated command, removed in v4.2.
+// Support is provided to satisfy PTS tester requirements.
+void DualModeController::AddScoConnection(CommandView command) {
+  auto command_view = gd_hci::AddScoConnectionView::Create(
+      gd_hci::ConnectionManagementCommandView::Create(
+          gd_hci::AclCommandView::Create(command)));
+  ASSERT(command_view.IsValid());
+
+  auto status = link_layer_controller_.AddScoConnection(
+      command_view.GetConnectionHandle(), command_view.GetPacketType());
+
+  auto packet = bluetooth::hci::AddScoConnectionStatusBuilder::Create(
       status, kNumCommandPackets);
   send_event_(std::move(packet));
 }

GrapheneOS/platform_packages_modules_Bluetooth

system/vendor_libs/test_vendor_lib/model/controller/dual_mode_controller.cc
@@ -122,6 +122,7 @@ DualModeController::DualModeController(const std::string& properties_filename, u
   SET_SUPPORTED(SWITCH_ROLE, SwitchRole);
   SET_SUPPORTED(READ_REMOTE_SUPPORTED_FEATURES, ReadRemoteSupportedFeatures);
   SET_SUPPORTED(READ_CLOCK_OFFSET, ReadClockOffset);
+  SET_HANDLER(ADD_SCO_CONNECTION, AddScoConnection);
   SET_SUPPORTED(SETUP_SYNCHRONOUS_CONNECTION, SetupSynchronousConnection);
   SET_SUPPORTED(ACCEPT_SYNCHRONOUS_CONNECTION, AcceptSynchronousConnection);
   SET_SUPPORTED(REJECT_SYNCHRONOUS_CONNECTION, RejectSynchronousConnection);
@@ -657,6 +658,22 @@ void DualModeController::ReadClockOffset(CommandView command) {
       OpCode::READ_CLOCK_OFFSET, command_view.GetPayload(), handle);
 
   auto packet = bluetooth::hci::ReadClockOffsetStatusBuilder::Create(
+      status, kNumCommandPackets);
+  send_event_(std::move(packet));
+}
+
+// Deprecated command, removed in v4.2.
+// Support is provided to satisfy PTS tester requirements.
+void DualModeController::AddScoConnection(CommandView command) {
+  auto command_view = gd_hci::AddScoConnectionView::Create(
+      gd_hci::ConnectionManagementCommandView::Create(
+          gd_hci::AclCommandView::Create(command)));
+  ASSERT(command_view.IsValid());
+
+  auto status = link_layer_controller_.AddScoConnection(
+      command_view.GetConnectionHandle(), command_view.GetPacketType());
+
+  auto packet = bluetooth::hci::AddScoConnectionStatusBuilder::Create(
       status, kNumCommandPackets);
   send_event_(std::move(packet));
 }