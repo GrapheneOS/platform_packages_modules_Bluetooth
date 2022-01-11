miyu-xu/packages-modules-Bluetooth

gtbs: Add Generic Telephone Bearer Service implementation
This adds Generic Telephone Bearer Service (GTBS) implementation
and exposes API that allows other TBS instances to be registered
by applications which want to expose their call control interfaces
over Bluetooth.

The GTBS aggregates call states of all the registered TBSes.
Each Call from GATT Client perspective is identified by a call
index assigned by TbsServerService, which is unique across TBS
instances and GTBS. From an application perspective, a call is
identified by the BluetoothTbsCall instance which has a unique UUID.

Bug: 150670922
Tag: #feature
Sponsor: jpawlowski@
test: Test: atest BluetoothInstrumentationTests
Change-Id: I284ddacfc8c7b48d633dd4097e3abf030bc1ef2a

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -49,6 +49,7 @@ import com.android.bluetooth.pbap.BluetoothPbapService;
 import com.android.bluetooth.pbapclient.PbapClientService;
 import com.android.bluetooth.sap.SapService;
 import com.android.bluetooth.vc.VolumeControlService;
+import com.android.bluetooth.tbs.TbsService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -111,6 +112,8 @@ public class Config {
                     (1 << BluetoothProfile.VOLUME_CONTROL)),
             new ProfileConfig(McpService.class, R.bool.profile_supported_mcp_server,
                     (1 << BluetoothProfile.MCP_SERVER)),
+            new ProfileConfig(TbsService.class, R.bool.profile_supported_tbs,
+                    (1 << BluetoothProfile.TBS)),
             new ProfileConfig(HearingAidService.class,
                     com.android.internal.R.bool.config_hearing_aid_profile_supported,
                     (1 << BluetoothProfile.HEARING_AID)),

android/app/src/com/android/bluetooth/telephony/BluetoothCall.java
@@ -16,10 +16,12 @@
 
 package com.android.bluetooth.telephony;
 
+import android.bluetooth.BluetoothTbs;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.telecom.Call;
+import android.telecom.DisconnectCause;
 import android.telecom.GatewayInfo;
 import android.telecom.InCallService;
 import android.telecom.PhoneAccountHandle;
@@ -28,6 +30,7 @@ import com.android.internal.annotations.VisibleForTesting;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * A proxy class of android.telecom.Call that
@@ -43,6 +46,7 @@ import java.util.List;
 public class BluetoothCall {
 
     private Call mCall;
+    private UUID mCallId;
 
     public Call getCall() {
         return mCall;
@@ -58,6 +62,20 @@ public class BluetoothCall {
 
     public BluetoothCall(Call call) {
         mCall = call;
+        mCallId = UUID.randomUUID();
+    }
+
+    public BluetoothCall(Call call, UUID callId) {
+        mCall = call;
+        mCallId = callId;
+    }
+
+    public UUID getTbsCallId() {
+        return mCallId;
+    }
+
+    public void setTbsCallId(UUID callId) {
+        mCallId = callId;
     }
 
     public String getRemainingPostDialSequence() {
@@ -295,6 +313,10 @@ public class BluetoothCall {
     public boolean wasConferencePreviouslyMerged() {
         return can(Call.Details.CAPABILITY_SWAP_CONFERENCE) &&
                 !can(Call.Details.CAPABILITY_MERGE_CONFERENCE);
+    }
+
+    public DisconnectCause getDisconnectCause() {
+        return getDetails().getDisconnectCause();
     }
 
     /**

android/app/tests/unit/src/com/android/bluetooth/btservice/storage/DatabaseManagerTest.java
@@ -1082,6 +1082,29 @@ public final class DatabaseManagerTest {
         }
     }
 
+    @Test
+    public void testDatabaseMigration_108_109() throws IOException {
+        String testString = "TEST STRING";
+        // Create a database with version 108
+        SupportSQLiteDatabase db = testHelper.createDatabase(DB_NAME, 108);
+        // insert a device to the database
+        ContentValues device = new ContentValues();
+        device.put("address", TEST_BT_ADDR);
+        device.put("migrated", false);
+        assertThat(db.insert("metadata", SQLiteDatabase.CONFLICT_IGNORE, device),
+                CoreMatchers.not(-1));
+        // Migrate database from 108 to 109
+        db.close();
+        db = testHelper.runMigrationsAndValidate(DB_NAME, 109, true,
+                MetadataDatabase.MIGRATION_108_109);
+        Cursor cursor = db.query("SELECT * FROM metadata");
+        assertHasColumn(cursor, "tbs_connection_policy", true);
+        while (cursor.moveToNext()) {
+            // Check the new columns was added with default value
+            assertColumnIntData(cursor, "tbs_connection_policy", 100);
+        }
+    }
+
     /**
      * Helper function to check whether the database has the expected column
      */

GrapheneOS/platform_packages_modules_Bluetooth

Change-Id: I652a40b3f1ce01fc57f0afea451ed1d39781ed36

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -49,6 +49,7 @@ import com.android.bluetooth.pbap.BluetoothPbapService;
 import com.android.bluetooth.pbapclient.PbapClientService;
 import com.android.bluetooth.sap.SapService;
 import com.android.bluetooth.vc.VolumeControlService;
+import com.android.bluetooth.tbs.TbsService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -121,6 +122,8 @@ public class Config {
                     (1 << BluetoothProfile.VOLUME_CONTROL)),
             new ProfileConfig(McpService.class, R.bool.profile_supported_mcp_server,
                     (1 << BluetoothProfile.MCP_SERVER)),
+            new ProfileConfig(TbsService.class, R.bool.profile_supported_le_call_control,
+                    (1 << BluetoothProfile.LE_CALL_CONTROL)),
             new ProfileConfig(HearingAidService.class,
                     com.android.internal.R.bool.config_hearing_aid_profile_supported,
                     (1 << BluetoothProfile.HEARING_AID)),

android/app/src/com/android/bluetooth/telephony/BluetoothCall.java
@@ -16,10 +16,12 @@
 
 package com.android.bluetooth.telephony;
 
+import android.bluetooth.BluetoothLeCallControl;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.telecom.Call;
+import android.telecom.DisconnectCause;
 import android.telecom.GatewayInfo;
 import android.telecom.InCallService;
 import android.telecom.PhoneAccountHandle;
@@ -28,6 +30,7 @@ import com.android.internal.annotations.VisibleForTesting;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * A proxy class of android.telecom.Call that
@@ -43,6 +46,7 @@ import java.util.List;
 public class BluetoothCall {
 
     private Call mCall;
+    private UUID mCallId;
 
     public Call getCall() {
         return mCall;
@@ -58,6 +62,20 @@ public class BluetoothCall {
 
     public BluetoothCall(Call call) {
         mCall = call;
+        mCallId = UUID.randomUUID();
+    }
+
+    public BluetoothCall(Call call, UUID callId) {
+        mCall = call;
+        mCallId = callId;
+    }
+
+    public UUID getTbsCallId() {
+        return mCallId;
+    }
+
+    public void setTbsCallId(UUID callId) {
+        mCallId = callId;
     }
 
     public String getRemainingPostDialSequence() {
@@ -295,6 +313,10 @@ public class BluetoothCall {
     public boolean wasConferencePreviouslyMerged() {
         return can(Call.Details.CAPABILITY_SWAP_CONFERENCE) &&
                 !can(Call.Details.CAPABILITY_MERGE_CONFERENCE);
+    }
+
+    public DisconnectCause getDisconnectCause() {
+        return getDetails().getDisconnectCause();
     }
 
     /**

android/app/tests/unit/src/com/android/bluetooth/btservice/storage/DatabaseManagerTest.java
@@ -1082,6 +1082,29 @@ public final class DatabaseManagerTest {
         }
     }
 
+    @Test
+    public void testDatabaseMigration_108_109() throws IOException {
+        String testString = "TEST STRING";
+        // Create a database with version 108
+        SupportSQLiteDatabase db = testHelper.createDatabase(DB_NAME, 108);
+        // insert a device to the database
+        ContentValues device = new ContentValues();
+        device.put("address", TEST_BT_ADDR);
+        device.put("migrated", false);
+        assertThat(db.insert("metadata", SQLiteDatabase.CONFLICT_IGNORE, device),
+                CoreMatchers.not(-1));
+        // Migrate database from 108 to 109
+        db.close();
+        db = testHelper.runMigrationsAndValidate(DB_NAME, 109, true,
+                MetadataDatabase.MIGRATION_108_109);
+        Cursor cursor = db.query("SELECT * FROM metadata");
+        assertHasColumn(cursor, "le_call_control_connection_policy", true);
+        while (cursor.moveToNext()) {
+            // Check the new columns was added with default value
+            assertColumnIntData(cursor, "le_call_control_connection_policy", 100);
+        }
+    }
+
     /**
      * Helper function to check whether the database has the expected column
      */