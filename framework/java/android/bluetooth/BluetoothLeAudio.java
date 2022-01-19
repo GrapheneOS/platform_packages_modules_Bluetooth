miyu-xu/packages-modules-Bluetooth

leaudio: Add method to get device group lead
When Le Audio group is active in the system, it is represented by one
group member which becames Active Device.
With this patch, we want to expose group lead device which will be used
as an Active Device for connected group.

This is needed e.g. by telecom so it knows, which devices should be
presented in the Dialer for each Le Audio group.

Bug: 150670922
Tag: #feature
Sponsor: jpawlowski@
Change-Id: I401ef3ceaeb334b35697dd88e2c1a9b1d5d2eac9

framework/java/android/bluetooth/BluetoothLeAudio.java
@@ -451,6 +451,45 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
     }
 
     /**
+     * Get lead device for a group.
+     *
+     * Lead device is the device that can be used as an active device in the system.
+     * Active devices points to the Audio Device for the Le Audio group.
+     * This method returns a list of Lead devices for all the connected LE Audio
+     * groups and those devices should be used in the setActiveDevice() method by other parts
+     * of the system, which wants to setActive a particular Le Audio Group.
+     *
+     * Note: getActiveDevice() returns the Lead device for the currently active LE Audio group.
+     * Note: When lead device gets disconnected, there will be new lead device for the group.
+     *
+     * @param group_id The group id.
+     * @return group lead device.
+     */
+    @RequiresBluetoothConnectPermission
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
+    public @Nullable BluetoothDevice getConnectedGroupLeadDevice(int group_id) {
+        if (VDBG) log("getConnectedGroupLeadDevice()");
+        final IBluetoothLeAudio service = getService();
+        final BluetoothDevice defaultValue = null;
+        if (service == null) {
+            Log.w(TAG, "Proxy not attached to service");
+            if (DBG) log(Log.getStackTraceString(new Throwable()));
+        } else if (mAdapter.isEnabled()) {
+            try {
+                final SynchronousResultReceiver<BluetoothDevice> recv =
+                        new SynchronousResultReceiver();
+                service.getConnectedGroupLeadDevice(group_id, mAttributionSource, recv);
+                return Attributable.setAttributionSource(
+                        recv.awaitResultNoInterrupt(getSyncTimeout()).getValue(defaultValue),
+                        mAttributionSource);
+            } catch (RemoteException | TimeoutException e) {
+                Log.e(TAG, e.toString() + "\n" + Log.getStackTraceString(new Throwable()));
+            }
+        }
+        return defaultValue;
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override

system/binder/android/bluetooth/IBluetoothLeAudio.aidl
@@ -48,7 +48,7 @@ oneway interface IBluetoothLeAudio {
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
     void getConnectionPolicy(in BluetoothDevice device, in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
-    void getConnectedGroupLeadDevices(in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
+    void getConnectedGroupLeadDevice(int group_id, in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
 
     /* Same value as bluetooth::groups::kGroupUnknown */
     const int LE_AUDIO_GROUP_ID_INVALID = -1;

GrapheneOS/platform_packages_modules_Bluetooth

Test: build

framework/java/android/bluetooth/BluetoothLeAudio.java
@@ -451,6 +451,45 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
     }
 
     /**
+     * Get lead device for a group.
+     *
+     * Lead device is the device that can be used as an active device in the system.
+     * Active devices points to the Audio Device for the Le Audio group.
+     * This method returns a list of Lead devices for all the connected LE Audio
+     * groups and those devices should be used in the setActiveDevice() method by other parts
+     * of the system, which wants to setActive a particular Le Audio Group.
+     *
+     * Note: getActiveDevice() returns the Lead device for the currently active LE Audio group.
+     * Note: When lead device gets disconnected, there will be new lead device for the group.
+     *
+     * @param groupId The group id.
+     * @return group lead device.
+     */
+    @RequiresBluetoothConnectPermission
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
+    public @Nullable BluetoothDevice getConnectedGroupLeadDevice(int groupId) {
+        if (VDBG) log("getConnectedGroupLeadDevice()");
+        final IBluetoothLeAudio service = getService();
+        final BluetoothDevice defaultValue = null;
+        if (service == null) {
+            Log.w(TAG, "Proxy not attached to service");
+            if (DBG) log(Log.getStackTraceString(new Throwable()));
+        } else if (mAdapter.isEnabled()) {
+            try {
+                final SynchronousResultReceiver<BluetoothDevice> recv =
+                        new SynchronousResultReceiver();
+                service.getConnectedGroupLeadDevice(groupId, mAttributionSource, recv);
+                return Attributable.setAttributionSource(
+                        recv.awaitResultNoInterrupt(getSyncTimeout()).getValue(defaultValue),
+                        mAttributionSource);
+            } catch (RemoteException | TimeoutException e) {
+                Log.e(TAG, e.toString() + "\n" + Log.getStackTraceString(new Throwable()));
+            }
+        }
+        return defaultValue;
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override

system/binder/android/bluetooth/IBluetoothLeAudio.aidl
@@ -48,7 +48,7 @@ oneway interface IBluetoothLeAudio {
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
     void getConnectionPolicy(in BluetoothDevice device, in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
-    void getConnectedGroupLeadDevices(in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
+    void getConnectedGroupLeadDevice(int groupId, in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
 
     /* Same value as bluetooth::groups::kGroupUnknown */
     const int LE_AUDIO_GROUP_ID_INVALID = -1;