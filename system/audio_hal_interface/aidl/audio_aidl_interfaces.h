miyu-xu/packages-modules-Bluetooth

HAL: Add AIDL interface support
Tag: #refactor
Bug: 203490261
Test: atest BluetoothInstrumentationTests
Change-Id: I866ad2cc5088b7aa691c62a59be091dab1d3f387

system/audio_hal_interface/aidl/audio_aidl_interfaces.h
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+/***
+ * Because some dependencies of AIDL libraries use syslog LOG macros, we must
+ * include them in the following order to use in libbluetooth.
+ ***/
+
+// clang-format off
+
+#include <aidl/android/hardware/audio/common/SinkMetadata.h>
+#include <aidl/android/hardware/audio/common/SourceMetadata.h>
+#include <aidl/android/hardware/bluetooth/audio/BnBluetoothAudioPort.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProvider.h>
+
+#include <aidl/android/hardware/bluetooth/audio/AacCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AacConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AacObjectType.h>
+#include <aidl/android/hardware/bluetooth/audio/AptxCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AptxConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioLocation.h>
+#include <aidl/android/hardware/bluetooth/audio/BluetoothAudioStatus.h>
+#include <aidl/android/hardware/bluetooth/audio/BroadcastConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/ChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecType.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioPort.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProvider.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProviderFactory.h>
+#include <aidl/android/hardware/bluetooth/audio/Lc3Capabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/Lc3Configuration.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacQualityIndex.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioCodecConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioMode.h>
+#include <aidl/android/hardware/bluetooth/audio/PcmCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/PcmConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/PresentationPosition.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcAllocMethod.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/SessionType.h>
+#include <aidl/android/hardware/bluetooth/audio/UnicastConfiguration.h>
+
+
+
+#ifdef LOG_INFO
+#undef LOG_INFO
+#endif
+#ifdef LOG_WARNING
+#undef LOG_WARNING
+#endif
+#ifdef LOG_DEBUG
+#undef LOG_DEBUG
+#endif
+
+#include <base/logging.h>
+
+// clang-format on

system/audio_hal_interface/aidl/le_audio_software.h
@@ -1,0 +1,196 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../le_audio_software.h"
+#include "audio_aidl_interfaces.h"
+#include "bta/le_audio/le_audio_types.h"
+#include "client_interface.h"
+
+namespace bluetooth {
+namespace audio {
+namespace aidl {
+namespace le_audio {
+
+using ::aidl::android::hardware::bluetooth::audio::LeAudioCapabilities;
+using ::aidl::android::hardware::bluetooth::audio::PcmConfiguration;
+using ::aidl::android::hardware::bluetooth::audio::SessionType;
+using ::bluetooth::audio::aidl::BluetoothAudioCtrlAck;
+using ::le_audio::set_configurations::AudioSetConfiguration;
+using ::le_audio::set_configurations::CodecCapabilitySetting;
+
+constexpr uint8_t kChannelNumberMono = 1;
+constexpr uint8_t kChannelNumberStereo = 2;
+
+constexpr uint32_t kSampleRate48000 = 48000;
+constexpr uint32_t kSampleRate44100 = 44100;
+constexpr uint32_t kSampleRate32000 = 32000;
+constexpr uint32_t kSampleRate24000 = 24000;
+constexpr uint32_t kSampleRate16000 = 16000;
+constexpr uint32_t kSampleRate8000 = 8000;
+
+constexpr uint8_t kBitsPerSample16 = 16;
+constexpr uint8_t kBitsPerSample24 = 24;
+constexpr uint8_t kBitsPerSample32 = 32;
+
+using ::bluetooth::audio::le_audio::StreamCallbacks;
+
+void flush_sink();
+void flush_source();
+bool halConfigToCodecCapabilitySetting(LeAudioCapabilities halConfig,
+                                       CodecCapabilitySetting& codecCapability);
+
+bool is_source_hal_enabled();
+bool is_sink_hal_enabled();
+
+std::vector<AudioSetConfiguration> get_offload_capabilities();
+
+class LeAudioTransport {
+ public:
+  LeAudioTransport(void (*flush)(void), StreamCallbacks stream_cb,
+                   PcmConfiguration pcm_config);
+
+  BluetoothAudioCtrlAck StartRequest();
+
+  BluetoothAudioCtrlAck SuspendRequest();
+
+  void StopRequest();
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_processed,
+                               timespec* data_position);
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata);
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata);
+
+  void ResetPresentationPosition();
+
+  void LogBytesProcessed(size_t bytes_processed);
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+ private:
+  void (*flush_)(void);
+  StreamCallbacks stream_cb_;
+  uint16_t remote_delay_report_ms_;
+  uint64_t total_bytes_processed_;
+  timespec data_position_;
+  PcmConfiguration pcm_config_;
+  bool is_pending_start_request_;
+};
+
+// Sink transport implementation for Le Audio
+class LeAudioSinkTransport
+    : public ::bluetooth::audio::aidl::IBluetoothSinkTransportInstance {
+ public:
+  LeAudioSinkTransport(SessionType session_type, StreamCallbacks stream_cb);
+
+  ~LeAudioSinkTransport();
+
+  BluetoothAudioCtrlAck StartRequest() override;
+
+  BluetoothAudioCtrlAck SuspendRequest() override;
+
+  void StopRequest() override;
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_read,
+                               timespec* data_position) override;
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata) override;
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata) override;
+
+  void ResetPresentationPosition() override;
+
+  void LogBytesRead(size_t bytes_read) override;
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+  static inline LeAudioSinkTransport* instance = nullptr;
+  static inline BluetoothAudioSinkClientInterface* interface = nullptr;
+
+ private:
+  LeAudioTransport* transport_;
+};
+
+class LeAudioSourceTransport
+    : public ::bluetooth::audio::aidl::IBluetoothSourceTransportInstance {
+ public:
+  LeAudioSourceTransport(StreamCallbacks stream_cb);
+
+  ~LeAudioSourceTransport();
+
+  BluetoothAudioCtrlAck StartRequest() override;
+
+  BluetoothAudioCtrlAck SuspendRequest() override;
+
+  void StopRequest() override;
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_written,
+                               timespec* data_position) override;
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata) override;
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata) override;
+
+  void ResetPresentationPosition() override;
+
+  void LogBytesWritten(size_t bytes_written) override;
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+  static inline LeAudioSourceTransport* instance = nullptr;
+  static inline BluetoothAudioSourceClientInterface* interface = nullptr;
+
+ private:
+  LeAudioTransport* transport_;
+};
+
+}  // namespace le_audio
+}  // namespace aidl
+}  // namespace audio
+}  // namespace bluetooth

system/audio_hal_interface/aidl/transport_instance.h
@@ -1,0 +1,121 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <hardware/audio.h>
+
+#include "audio_aidl_interfaces.h"
+#include "audio_ctrl_ack.h"
+
+namespace bluetooth {
+namespace audio {
+namespace aidl {
+
+using ::aidl::android::hardware::bluetooth::audio::AudioConfiguration;
+using ::aidl::android::hardware::bluetooth::audio::SessionType;
+
+/***
+ * An IBluetoothTransportInstance needs to be implemented by a Bluetooth
+ * audio transport, such as A2DP or Hearing Aid, to handle callbacks from Audio
+ * HAL.
+ ***/
+class IBluetoothTransportInstance {
+ public:
+  IBluetoothTransportInstance(SessionType sessionType,
+                              AudioConfiguration audioConfig)
+      : session_type_(sessionType), audio_config_(std::move(audioConfig)){};
+  virtual ~IBluetoothTransportInstance() = default;
+
+  SessionType GetSessionType() const { return session_type_; }
+
+  AudioConfiguration GetAudioConfiguration() const { return audio_config_; }
+
+  void UpdateAudioConfiguration(const AudioConfiguration& audio_config) {
+    switch (audio_config.getTag()) {
+      case AudioConfiguration::pcmConfig:
+        audio_config_.set<AudioConfiguration::pcmConfig>(
+            audio_config.get<AudioConfiguration::pcmConfig>());
+        break;
+      case AudioConfiguration::codecConfig:
+        audio_config_.set<AudioConfiguration::codecConfig>(
+            audio_config.get<AudioConfiguration::codecConfig>());
+        break;
+      case AudioConfiguration::leAudioConfig:
+        audio_config_.set<AudioConfiguration::leAudioConfig>(
+            audio_config.get<AudioConfiguration::leAudioConfig>());
+        break;
+    }
+  }
+
+  virtual BluetoothAudioCtrlAck StartRequest() = 0;
+
+  virtual BluetoothAudioCtrlAck SuspendRequest() = 0;
+
+  virtual void StopRequest() = 0;
+
+  virtual bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                                       uint64_t* total_bytes_readed,
+                                       timespec* data_position) = 0;
+
+  virtual void SourceMetadataChanged(
+      const source_metadata_t& source_metadata) = 0;
+  virtual void SinkMetadataChanged(const sink_metadata_t& sink_metadata) = 0;
+
+  /***
+   * Invoked when the transport is requested to reset presentation position
+   ***/
+  virtual void ResetPresentationPosition() = 0;
+
+ private:
+  const SessionType session_type_;
+  AudioConfiguration audio_config_;
+};
+
+/***
+ * An IBluetoothSinkTransportInstance needs to be implemented by a Bluetooth
+ * audio transport, such as A2DP, Hearing Aid or LeAudio, to handle callbacks
+ * from Audio HAL.
+ ***/
+class IBluetoothSinkTransportInstance : public IBluetoothTransportInstance {
+ public:
+  IBluetoothSinkTransportInstance(SessionType sessionType,
+                                  AudioConfiguration audioConfig)
+      : IBluetoothTransportInstance{sessionType, audioConfig} {}
+  virtual ~IBluetoothSinkTransportInstance() = default;
+
+  /***
+   * Invoked when the transport is requested to log bytes read
+   ***/
+  virtual void LogBytesRead(size_t bytes_readed) = 0;
+};
+
+class IBluetoothSourceTransportInstance : public IBluetoothTransportInstance {
+ public:
+  IBluetoothSourceTransportInstance(SessionType sessionType,
+                                    AudioConfiguration audioConfig)
+      : IBluetoothTransportInstance{sessionType, audioConfig} {}
+  virtual ~IBluetoothSourceTransportInstance() = default;
+
+  /***
+   * Invoked when the transport is requested to log bytes written
+   ***/
+  virtual void LogBytesWritten(size_t bytes_written) = 0;
+};
+
+}  // namespace aidl
+}  // namespace audio
+}  // namespace bluetooth

GrapheneOS/platform_packages_modules_Bluetooth

system/audio_hal_interface/aidl/audio_aidl_interfaces.h
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+/***
+ * Because some dependencies of AIDL libraries use syslog LOG macros, we must
+ * include them in the following order to use in libbluetooth.
+ ***/
+
+// clang-format off
+
+#include <aidl/android/hardware/audio/common/SinkMetadata.h>
+#include <aidl/android/hardware/audio/common/SourceMetadata.h>
+#include <aidl/android/hardware/bluetooth/audio/BnBluetoothAudioPort.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProvider.h>
+
+#include <aidl/android/hardware/bluetooth/audio/AacCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AacConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AacObjectType.h>
+#include <aidl/android/hardware/bluetooth/audio/AptxCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AptxConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/AudioLocation.h>
+#include <aidl/android/hardware/bluetooth/audio/BluetoothAudioStatus.h>
+#include <aidl/android/hardware/bluetooth/audio/BroadcastConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/ChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/CodecType.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioPort.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProvider.h>
+#include <aidl/android/hardware/bluetooth/audio/IBluetoothAudioProviderFactory.h>
+#include <aidl/android/hardware/bluetooth/audio/Lc3Capabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/Lc3Configuration.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LdacQualityIndex.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioCodecCapabilitiesSetting.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioCodecConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/LeAudioMode.h>
+#include <aidl/android/hardware/bluetooth/audio/PcmCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/PcmConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/PresentationPosition.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcAllocMethod.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcCapabilities.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcChannelMode.h>
+#include <aidl/android/hardware/bluetooth/audio/SbcConfiguration.h>
+#include <aidl/android/hardware/bluetooth/audio/SessionType.h>
+#include <aidl/android/hardware/bluetooth/audio/UnicastConfiguration.h>
+
+
+
+#ifdef LOG_INFO
+#undef LOG_INFO
+#endif
+#ifdef LOG_WARNING
+#undef LOG_WARNING
+#endif
+#ifdef LOG_DEBUG
+#undef LOG_DEBUG
+#endif
+
+#include <base/logging.h>
+
+// clang-format on

system/audio_hal_interface/aidl/le_audio_software.h
@@ -1,0 +1,200 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../le_audio_software.h"
+#include "audio_aidl_interfaces.h"
+#include "bta/le_audio/le_audio_types.h"
+#include "client_interface.h"
+
+namespace bluetooth {
+namespace audio {
+namespace aidl {
+namespace le_audio {
+
+using ::aidl::android::hardware::bluetooth::audio::LeAudioConfiguration;
+using ::aidl::android::hardware::bluetooth::audio::PcmConfiguration;
+using ::aidl::android::hardware::bluetooth::audio::SessionType;
+using ::aidl::android::hardware::bluetooth::audio::UnicastCapability;
+using ::bluetooth::audio::aidl::BluetoothAudioCtrlAck;
+using ::le_audio::set_configurations::AudioSetConfiguration;
+using ::le_audio::set_configurations::CodecCapabilitySetting;
+
+constexpr uint8_t kChannelNumberMono = 1;
+constexpr uint8_t kChannelNumberStereo = 2;
+
+constexpr uint32_t kSampleRate48000 = 48000;
+constexpr uint32_t kSampleRate44100 = 44100;
+constexpr uint32_t kSampleRate32000 = 32000;
+constexpr uint32_t kSampleRate24000 = 24000;
+constexpr uint32_t kSampleRate16000 = 16000;
+constexpr uint32_t kSampleRate8000 = 8000;
+
+constexpr uint8_t kBitsPerSample16 = 16;
+constexpr uint8_t kBitsPerSample24 = 24;
+constexpr uint8_t kBitsPerSample32 = 32;
+
+using ::bluetooth::audio::le_audio::StreamCallbacks;
+
+void flush_sink();
+void flush_source();
+bool hal_ucast_capability_to_stack_format(
+    const UnicastCapability& ucast_capability,
+    CodecCapabilitySetting& stack_capability);
+AudioConfiguration offload_config_to_hal_audio_config(
+    const ::le_audio::offload_config& offload_config);
+
+bool is_source_hal_enabled();
+bool is_sink_hal_enabled();
+
+std::vector<AudioSetConfiguration> get_offload_capabilities();
+
+class LeAudioTransport {
+ public:
+  LeAudioTransport(void (*flush)(void), StreamCallbacks stream_cb,
+                   PcmConfiguration pcm_config);
+
+  BluetoothAudioCtrlAck StartRequest();
+
+  BluetoothAudioCtrlAck SuspendRequest();
+
+  void StopRequest();
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_processed,
+                               timespec* data_position);
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata);
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata);
+
+  void ResetPresentationPosition();
+
+  void LogBytesProcessed(size_t bytes_processed);
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+ private:
+  void (*flush_)(void);
+  StreamCallbacks stream_cb_;
+  uint16_t remote_delay_report_ms_;
+  uint64_t total_bytes_processed_;
+  timespec data_position_;
+  PcmConfiguration pcm_config_;
+  bool is_pending_start_request_;
+};
+
+// Sink transport implementation for Le Audio
+class LeAudioSinkTransport
+    : public ::bluetooth::audio::aidl::IBluetoothSinkTransportInstance {
+ public:
+  LeAudioSinkTransport(SessionType session_type, StreamCallbacks stream_cb);
+
+  ~LeAudioSinkTransport();
+
+  BluetoothAudioCtrlAck StartRequest() override;
+
+  BluetoothAudioCtrlAck SuspendRequest() override;
+
+  void StopRequest() override;
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_read,
+                               timespec* data_position) override;
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata) override;
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata) override;
+
+  void ResetPresentationPosition() override;
+
+  void LogBytesRead(size_t bytes_read) override;
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+  static inline LeAudioSinkTransport* instance = nullptr;
+  static inline BluetoothAudioSinkClientInterface* interface = nullptr;
+
+ private:
+  LeAudioTransport* transport_;
+};
+
+class LeAudioSourceTransport
+    : public ::bluetooth::audio::aidl::IBluetoothSourceTransportInstance {
+ public:
+  LeAudioSourceTransport(SessionType session_type, StreamCallbacks stream_cb);
+
+  ~LeAudioSourceTransport();
+
+  BluetoothAudioCtrlAck StartRequest() override;
+
+  BluetoothAudioCtrlAck SuspendRequest() override;
+
+  void StopRequest() override;
+
+  bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                               uint64_t* total_bytes_written,
+                               timespec* data_position) override;
+
+  void SourceMetadataChanged(const source_metadata_t& source_metadata) override;
+
+  void SinkMetadataChanged(const sink_metadata_t& sink_metadata) override;
+
+  void ResetPresentationPosition() override;
+
+  void LogBytesWritten(size_t bytes_written) override;
+
+  void SetRemoteDelay(uint16_t delay_report_ms);
+
+  const PcmConfiguration& LeAudioGetSelectedHalPcmConfig();
+
+  void LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz, uint8_t bit_rate,
+                                      uint8_t channels_count,
+                                      uint32_t data_interval);
+
+  bool IsPendingStartStream(void);
+  void ClearPendingStartStream(void);
+
+  static inline LeAudioSourceTransport* instance = nullptr;
+  static inline BluetoothAudioSourceClientInterface* interface = nullptr;
+
+ private:
+  LeAudioTransport* transport_;
+};
+
+}  // namespace le_audio
+}  // namespace aidl
+}  // namespace audio
+}  // namespace bluetooth

system/audio_hal_interface/aidl/transport_instance.h
@@ -1,0 +1,121 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <hardware/audio.h>
+
+#include "audio_aidl_interfaces.h"
+#include "audio_ctrl_ack.h"
+
+namespace bluetooth {
+namespace audio {
+namespace aidl {
+
+using ::aidl::android::hardware::bluetooth::audio::AudioConfiguration;
+using ::aidl::android::hardware::bluetooth::audio::SessionType;
+
+/***
+ * An IBluetoothTransportInstance needs to be implemented by a Bluetooth
+ * audio transport, such as A2DP or Hearing Aid, to handle callbacks from Audio
+ * HAL.
+ ***/
+class IBluetoothTransportInstance {
+ public:
+  IBluetoothTransportInstance(SessionType sessionType,
+                              AudioConfiguration audioConfig)
+      : session_type_(sessionType), audio_config_(std::move(audioConfig)){};
+  virtual ~IBluetoothTransportInstance() = default;
+
+  SessionType GetSessionType() const { return session_type_; }
+
+  AudioConfiguration GetAudioConfiguration() const { return audio_config_; }
+
+  void UpdateAudioConfiguration(const AudioConfiguration& audio_config) {
+    switch (audio_config.getTag()) {
+      case AudioConfiguration::pcmConfig:
+        audio_config_.set<AudioConfiguration::pcmConfig>(
+            audio_config.get<AudioConfiguration::pcmConfig>());
+        break;
+      case AudioConfiguration::a2dpConfig:
+        audio_config_.set<AudioConfiguration::a2dpConfig>(
+            audio_config.get<AudioConfiguration::a2dpConfig>());
+        break;
+      case AudioConfiguration::leAudioConfig:
+        audio_config_.set<AudioConfiguration::leAudioConfig>(
+            audio_config.get<AudioConfiguration::leAudioConfig>());
+        break;
+    }
+  }
+
+  virtual BluetoothAudioCtrlAck StartRequest() = 0;
+
+  virtual BluetoothAudioCtrlAck SuspendRequest() = 0;
+
+  virtual void StopRequest() = 0;
+
+  virtual bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
+                                       uint64_t* total_bytes_readed,
+                                       timespec* data_position) = 0;
+
+  virtual void SourceMetadataChanged(
+      const source_metadata_t& source_metadata) = 0;
+  virtual void SinkMetadataChanged(const sink_metadata_t& sink_metadata) = 0;
+
+  /***
+   * Invoked when the transport is requested to reset presentation position
+   ***/
+  virtual void ResetPresentationPosition() = 0;
+
+ private:
+  const SessionType session_type_;
+  AudioConfiguration audio_config_;
+};
+
+/***
+ * An IBluetoothSinkTransportInstance needs to be implemented by a Bluetooth
+ * audio transport, such as A2DP, Hearing Aid or LeAudio, to handle callbacks
+ * from Audio HAL.
+ ***/
+class IBluetoothSinkTransportInstance : public IBluetoothTransportInstance {
+ public:
+  IBluetoothSinkTransportInstance(SessionType sessionType,
+                                  AudioConfiguration audioConfig)
+      : IBluetoothTransportInstance{sessionType, audioConfig} {}
+  virtual ~IBluetoothSinkTransportInstance() = default;
+
+  /***
+   * Invoked when the transport is requested to log bytes read
+   ***/
+  virtual void LogBytesRead(size_t bytes_readed) = 0;
+};
+
+class IBluetoothSourceTransportInstance : public IBluetoothTransportInstance {
+ public:
+  IBluetoothSourceTransportInstance(SessionType sessionType,
+                                    AudioConfiguration audioConfig)
+      : IBluetoothTransportInstance{sessionType, audioConfig} {}
+  virtual ~IBluetoothSourceTransportInstance() = default;
+
+  /***
+   * Invoked when the transport is requested to log bytes written
+   ***/
+  virtual void LogBytesWritten(size_t bytes_written) = 0;
+};
+
+}  // namespace aidl
+}  // namespace audio
+}  // namespace bluetooth