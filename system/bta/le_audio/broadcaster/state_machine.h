miyu-xu/packages-modules-Bluetooth

leaudio: Add BAP Broadcaster implementation
This implements the single-instance Broadcaster entity
for creating, configuring and managing audio broadcasts.
Each registered broadcast is an independently controlled
state machine, which can be started, stopped, suspended or
destroyed. The implementation takes care of all the
advertising, ISO channel creation and audio data routing

Note, that currently all the system audio is trasfered over
the broadcast stream including all the sonification events
like clicks and notifications. Current audio session client
implementation is designed to work with either unicast or
broadcast, but not both at the same time.

Bug: 150670922
Tag: #feature
Test: atest BluetoothInstrumentationTests bluetooth_test_broadcaster bluetooth_test_broadcaster_sm bluetooth_le_audio_test
Sponsor: jpawlowski@
Change-Id: I4e9c95d4b6a4fe0a7911bb8702ee7f41ff500470

system/bta/le_audio/broadcaster/state_machine.h
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2021 HIMSA II K/S - www.himsa.com.
+ * Represented by EHIMA - www.ehima.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <array>
+#include <limits>
+#include <optional>
+#include <type_traits>
+
+#include "base/callback.h"
+#include "broadcaster_types.h"
+#include "bta_le_audio_broadcaster_api.h"
+
+namespace {
+template <int S, typename StateT = uint8_t>
+class StateMachine {
+ public:
+  StateMachine() : state_(std::numeric_limits<StateT>::min()) {}
+
+ protected:
+  StateT GetState() const { return state_; }
+  void SetState(StateT state) {
+    if (state < S) state_ = state;
+  }
+
+ private:
+  StateT state_;
+};
+} /* namespace */
+
+/* Broadcast Stream state machine possible states:
+ * Stopped    - No broadcast Audio Stream is being transmitted.
+ * Configured - The Broadcast Source has configured its controller for the
+ *              broadcast Audio Stream using implementation-specific
+ *              information or information provided by a higher-layer
+ *              specification. It advertises the information to allow
+ *              Broadcast Sinks and Scan Offloaders to detect the Audio Stream
+ *              and transmits extended advertisements that contain Broadcast
+ *              Audio Announcements, which associate periodic advertising
+ *              trains with broadcast Audio Streams, and transmits periodic
+ *              advertising trains. The periodic advertising trains carry
+ *              Basic Audio Announcements that contain the broadcast Audio
+ *              Stream parameters and metadata. No Audio Data packets are sent
+ *              over the air from the Broadcast Source in this state. The
+ *              periodic advertising trains do not carry the BIGInfo data
+ *              required to synchronize to broadcast Audio Streams.
+ * Streaming  - The broadcast Audio Stream is enabled on the Broadcast Source,
+ *              allowing audio packets to be transmitted. The Broadcast Source
+ *              transmits extended advertisements that contain Broadcast Audio
+ *              Announcements, which associate periodic advertising trains with
+ *              the broadcast Audio Stream. The Broadcast Source also transmits
+ *              Basic Audio Announcements that contain broadcast Audio Stream
+ *              parameters and metadata and the BIGInfo data required for
+ *              synchronization to the broadcast Audio Stream by using periodic
+ *              advertisements while transmitting the broadcast Audio Stream.
+ *              The Broadcast Source may also transmit control parameters in
+ *              control packets within the broadcast Audio Stream.
+ */
+namespace le_audio {
+namespace broadcaster {
+
+class IBroadcastStateMachineCallbacks;
+
+struct BigConfig {
+  uint8_t status;
+  uint8_t big_id;
+  uint32_t big_sync_delay;
+  uint32_t transport_latency_big;
+  uint8_t phy;
+  uint8_t nse;
+  uint8_t bn;
+  uint8_t pto;
+  uint8_t irc;
+  uint16_t max_pdu;
+  uint16_t iso_interval;
+  std::vector<uint16_t> connection_handles;
+};
+
+struct BroadcastStateMachineConfig {
+  bluetooth::le_audio::BroadcastId broadcast_id;
+  uint8_t streaming_phy;
+  BroadcastCodecWrapper codec_wrapper;
+  BasicAudioAnnouncementData announcement;
+  std::optional<LeAudioBroadcaster::Code> broadcast_code;
+};
+
+class BroadcastStateMachine : public StateMachine<3> {
+ public:
+  static constexpr uint8_t kInstanceIdUndefined = 0xFF;
+  static constexpr uint8_t kPaIntervalMax = 0xA0; /* 160 * 0.625 = 100ms */
+  static constexpr uint8_t kPaIntervalMin = 0x50; /* 80 * 0.625 = 50ms */
+
+  static void Initialize(IBroadcastStateMachineCallbacks*);
+  static std::unique_ptr<BroadcastStateMachine> CreateInstance(
+      BroadcastStateMachineConfig msg);
+
+  enum class Message : uint8_t {
+    START = 0,
+    SUSPEND,
+    STOP,
+  };
+  static const std::underlying_type<Message>::type MESSAGE_MAX =
+      static_cast<std::underlying_type<Message>::type>(Message::STOP) + 1;
+
+  enum class State : uint8_t {
+    STOPPED = 0,
+    CONFIGURED,
+    STREAMING,
+  };
+  static const std::underlying_type<State>::type STATE_MAX =
+      static_cast<std::underlying_type<State>::type>(State::STREAMING) + 1;
+
+  inline State GetState(void) const {
+    return static_cast<State>(StateMachine::GetState());
+  }
+
+  inline uint8_t GetInstanceId() const { return instance_id_; }
+
+  virtual bool Initialize() = 0;
+  virtual const BroadcastCodecWrapper& GetCodecConfig() const = 0;
+  virtual std::optional<BigConfig> const& GetBigConfig() = 0;
+  virtual void RequestOwnAddress(
+      base::Callback<void(uint8_t /* address_type*/, RawAddress /*address*/)>
+          cb) = 0;
+  virtual void RequestOwnAddress() = 0;
+  virtual RawAddress GetOwnAddress() = 0;
+  virtual uint8_t GetOwnAddressType() = 0;
+  virtual bluetooth::le_audio::BroadcastId GetBroadcastId() = 0;
+  virtual void UpdateBroadcastAnnouncement(
+      BasicAudioAnnouncementData announcement) = 0;
+  void SetMuted(bool muted) { is_muted_ = muted; };
+  bool IsMuted() const { return is_muted_; };
+
+  virtual void HandleHciEvent(uint16_t event, void* data) = 0;
+  virtual void OnSetupIsoDataPath(uint8_t status, uint16_t conn_handle) = 0;
+  virtual void OnRemoveIsoDataPath(uint8_t status, uint16_t conn_handle) = 0;
+
+  virtual void ProcessMessage(Message event, const void* data = nullptr) = 0;
+  virtual ~BroadcastStateMachine() {}
+
+ protected:
+  BroadcastStateMachine() = default;
+
+  void SetState(State state) {
+    StateMachine::SetState(
+        static_cast<std::underlying_type<State>::type>(state));
+  }
+
+  uint8_t instance_id_ = kInstanceIdUndefined;
+  bool is_muted_ = false;
+
+  RawAddress addr_ = RawAddress::kEmpty;
+  uint8_t addr_type_ = 0;
+};
+
+class IBroadcastStateMachineCallbacks {
+ public:
+  IBroadcastStateMachineCallbacks() = default;
+  virtual ~IBroadcastStateMachineCallbacks() = default;
+  virtual void OnStateMachineCreateStatus(uint8_t instance_id,
+                                          bool initialized) = 0;
+  virtual void OnStateMachineDestroyed(uint8_t instance_id) = 0;
+  virtual void OnStateMachineEvent(uint8_t instance_id,
+                                   BroadcastStateMachine::State state,
+                                   const void* data = nullptr) = 0;
+  virtual void OnOwnAddressResponse(uint8_t instance_id, uint8_t addr_type,
+                                    RawAddress address) = 0;
+  virtual uint8_t GetNumRetransmit(uint8_t instance_id) = 0;
+  virtual uint32_t GetSduItv(uint8_t instance_id) = 0;
+  virtual uint16_t GetMaxTransportLatency(uint8_t instance_id) = 0;
+};
+
+} /* namespace broadcaster */
+} /* namespace le_audio */
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const le_audio::broadcaster::BroadcastStateMachine::Message& state);
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const le_audio::broadcaster::BroadcastStateMachine::State& state);

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/le_audio/broadcaster/state_machine.h
@@ -1,0 +1,205 @@
+/*
+ * Copyright 2021 HIMSA II K/S - www.himsa.com.
+ * Represented by EHIMA - www.ehima.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <array>
+#include <limits>
+#include <optional>
+#include <type_traits>
+
+#include "base/callback.h"
+#include "broadcaster_types.h"
+#include "bta_le_audio_broadcaster_api.h"
+
+namespace {
+template <int S, typename StateT = uint8_t>
+class StateMachine {
+ public:
+  StateMachine() : state_(std::numeric_limits<StateT>::min()) {}
+
+ protected:
+  StateT GetState() const { return state_; }
+  void SetState(StateT state) {
+    if (state < S) state_ = state;
+  }
+
+ private:
+  StateT state_;
+};
+} /* namespace */
+
+/* Broadcast Stream state machine possible states:
+ * Stopped    - No broadcast Audio Stream is being transmitted.
+ * Configuring- Configuration process was started.
+ * Configured - The Broadcast Source has configured its controller for the
+ *              broadcast Audio Stream using implementation-specific
+ *              information or information provided by a higher-layer
+ *              specification. It advertises the information to allow
+ *              Broadcast Sinks and Scan Offloaders to detect the Audio Stream
+ *              and transmits extended advertisements that contain Broadcast
+ *              Audio Announcements, which associate periodic advertising
+ *              trains with broadcast Audio Streams, and transmits periodic
+ *              advertising trains. The periodic advertising trains carry
+ *              Basic Audio Announcements that contain the broadcast Audio
+ *              Stream parameters and metadata. No Audio Data packets are sent
+ *              over the air from the Broadcast Source in this state. The
+ *              periodic advertising trains do not carry the BIGInfo data
+ *              required to synchronize to broadcast Audio Streams.
+ * Stopping   - Broadcast Audio stream and advertisements are being stopped.
+ * Streaming  - The broadcast Audio Stream is enabled on the Broadcast Source,
+ *              allowing audio packets to be transmitted. The Broadcast Source
+ *              transmits extended advertisements that contain Broadcast Audio
+ *              Announcements, which associate periodic advertising trains with
+ *              the broadcast Audio Stream. The Broadcast Source also transmits
+ *              Basic Audio Announcements that contain broadcast Audio Stream
+ *              parameters and metadata and the BIGInfo data required for
+ *              synchronization to the broadcast Audio Stream by using periodic
+ *              advertisements while transmitting the broadcast Audio Stream.
+ *              The Broadcast Source may also transmit control parameters in
+ *              control packets within the broadcast Audio Stream.
+ */
+namespace le_audio {
+namespace broadcaster {
+
+class IBroadcastStateMachineCallbacks;
+
+struct BigConfig {
+  uint8_t status;
+  uint8_t big_id;
+  uint32_t big_sync_delay;
+  uint32_t transport_latency_big;
+  uint8_t phy;
+  uint8_t nse;
+  uint8_t bn;
+  uint8_t pto;
+  uint8_t irc;
+  uint16_t max_pdu;
+  uint16_t iso_interval;
+  std::vector<uint16_t> connection_handles;
+};
+
+struct BroadcastStateMachineConfig {
+  bluetooth::le_audio::BroadcastId broadcast_id;
+  uint8_t streaming_phy;
+  BroadcastCodecWrapper codec_wrapper;
+  BasicAudioAnnouncementData announcement;
+  std::optional<LeAudioBroadcaster::Code> broadcast_code;
+};
+
+class BroadcastStateMachine : public StateMachine<5> {
+ public:
+  static constexpr uint8_t kInstanceIdUndefined = 0xFF;
+  static constexpr uint8_t kPaIntervalMax = 0xA0; /* 160 * 0.625 = 100ms */
+  static constexpr uint8_t kPaIntervalMin = 0x50; /* 80 * 0.625 = 50ms */
+
+  static void Initialize(IBroadcastStateMachineCallbacks*);
+  static std::unique_ptr<BroadcastStateMachine> CreateInstance(
+      BroadcastStateMachineConfig msg);
+
+  enum class Message : uint8_t {
+    START = 0,
+    SUSPEND,
+    STOP,
+  };
+  static const std::underlying_type<Message>::type MESSAGE_COUNT =
+      static_cast<std::underlying_type<Message>::type>(Message::STOP) + 1;
+
+  enum class State : uint8_t {
+    STOPPED = 0,
+    CONFIGURING,
+    CONFIGURED,
+    STOPPING,
+    STREAMING,
+  };
+  static const std::underlying_type<State>::type STATE_COUNT =
+      static_cast<std::underlying_type<State>::type>(State::STREAMING) + 1;
+
+  inline State GetState(void) const {
+    return static_cast<State>(StateMachine::GetState());
+  }
+
+  inline uint8_t GetInstanceId() const { return instance_id_; }
+
+  virtual bool Initialize() = 0;
+  virtual const BroadcastCodecWrapper& GetCodecConfig() const = 0;
+  virtual std::optional<BigConfig> const& GetBigConfig() = 0;
+  virtual void RequestOwnAddress(
+      base::Callback<void(uint8_t /* address_type*/, RawAddress /*address*/)>
+          cb) = 0;
+  virtual void RequestOwnAddress() = 0;
+  virtual RawAddress GetOwnAddress() = 0;
+  virtual uint8_t GetOwnAddressType() = 0;
+  virtual bluetooth::le_audio::BroadcastId GetBroadcastId() const = 0;
+  virtual void UpdateBroadcastAnnouncement(
+      BasicAudioAnnouncementData announcement) = 0;
+  void SetMuted(bool muted) { is_muted_ = muted; };
+  bool IsMuted() const { return is_muted_; };
+
+  virtual void HandleHciEvent(uint16_t event, void* data) = 0;
+  virtual void OnSetupIsoDataPath(uint8_t status, uint16_t conn_handle) = 0;
+  virtual void OnRemoveIsoDataPath(uint8_t status, uint16_t conn_handle) = 0;
+
+  virtual void ProcessMessage(Message event, const void* data = nullptr) = 0;
+  virtual ~BroadcastStateMachine() {}
+
+ protected:
+  BroadcastStateMachine() = default;
+
+  void SetState(State state) {
+    StateMachine::SetState(
+        static_cast<std::underlying_type<State>::type>(state));
+  }
+
+  uint8_t instance_id_ = kInstanceIdUndefined;
+  bool is_muted_ = false;
+
+  RawAddress addr_ = RawAddress::kEmpty;
+  uint8_t addr_type_ = 0;
+};
+
+class IBroadcastStateMachineCallbacks {
+ public:
+  IBroadcastStateMachineCallbacks() = default;
+  virtual ~IBroadcastStateMachineCallbacks() = default;
+  virtual void OnStateMachineCreateStatus(uint8_t instance_id,
+                                          bool initialized) = 0;
+  virtual void OnStateMachineDestroyed(uint8_t instance_id) = 0;
+  virtual void OnStateMachineEvent(uint8_t instance_id,
+                                   BroadcastStateMachine::State state,
+                                   const void* data = nullptr) = 0;
+  virtual void OnOwnAddressResponse(uint8_t instance_id, uint8_t addr_type,
+                                    RawAddress address) = 0;
+  virtual uint8_t GetNumRetransmit(uint8_t instance_id) = 0;
+  virtual uint32_t GetSduItv(uint8_t instance_id) = 0;
+  virtual uint16_t GetMaxTransportLatency(uint8_t instance_id) = 0;
+};
+
+} /* namespace broadcaster */
+} /* namespace le_audio */
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const le_audio::broadcaster::BroadcastStateMachine::Message& state);
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const le_audio::broadcaster::BroadcastStateMachine::State& state);
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const le_audio::broadcaster::BroadcastStateMachine& machine);