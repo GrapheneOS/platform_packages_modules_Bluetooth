miyu-xu/packages-modules-Bluetooth

Update ASHA profile sysprop API name to be in line with other profiles
Tag: #refactor
Bug: 215225542
Test: atest BluetoothInstrumentationTests
Change-Id: I58809972712c4862033634b955401dc78dc1d03e

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -157,7 +157,7 @@ public class Config {
             boolean supported = false;
             if (config.mClass == HearingAidService.class) {
                 supported =
-                        BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false);
+                        BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false);
             } else {
                 supported = resources.getBoolean(config.mSupported);
             }

android/app/tests/unit/src/com/android/bluetooth/btservice/ActiveDeviceManagerTest.java
@@ -220,7 +220,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_clearA2dpAndHeadsetActive() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
 
         a2dpConnected(mA2dpHeadsetDevice);
         headsetConnected(mA2dpHeadsetDevice);
@@ -238,7 +238,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_dontSetA2dpAndHeadsetActive() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         a2dpConnected(mA2dpHeadsetDevice);
@@ -255,7 +255,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_setA2dpActiveExplicitly() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         a2dpConnected(mA2dpHeadsetDevice);
@@ -275,7 +275,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_setHeadsetActiveExplicitly() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         headsetConnected(mA2dpHeadsetDevice);

android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidServiceTest.java
@@ -84,7 +84,7 @@ public class HearingAidServiceTest {
     public void setUp() throws Exception {
         mTargetContext = InstrumentationRegistry.getTargetContext();
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
         // Set up mocks and test assets
         MockitoAnnotations.initMocks(this);
 
@@ -127,7 +127,7 @@ public class HearingAidServiceTest {
 
     @After
     public void tearDown() throws Exception {
-        if (!BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false)) {
+        if (!BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false)) {
             return;
         }
         stopService();

android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidStateMachineTest.java
@@ -64,7 +64,7 @@ public class HearingAidStateMachineTest {
     public void setUp() throws Exception {
         mTargetContext = InstrumentationRegistry.getTargetContext();
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false));
         // Set up mocks and test assets
         MockitoAnnotations.initMocks(this);
         TestUtils.setAdapterService(mAdapterService);
@@ -86,7 +86,7 @@ public class HearingAidStateMachineTest {
 
     @After
     public void tearDown() throws Exception {
-        if (!BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false)) {
+        if (!BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false)) {
             return;
         }
         mHearingAidStateMachine.doQuit();

service/java/com/android/server/bluetooth/BluetoothManagerService.java
@@ -495,7 +495,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         mUserManager = mContext.getSystemService(UserManager.class);
 
         mIsHearingAidProfileSupported =
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false);
+                BluetoothProperties.isAudioStreamingForHearingAidEnabled().orElse(false);
 
         // TODO: We need a more generic way to initialize the persist keys of FeatureFlagUtils
         String value = SystemProperties.get(FeatureFlagUtils.PERSIST_PREFIX + FeatureFlagUtils.HEARING_AID_SETTINGS);

GrapheneOS/platform_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -157,7 +157,7 @@ public class Config {
             boolean supported = false;
             if (config.mClass == HearingAidService.class) {
                 supported =
-                        BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false);
+                        BluetoothProperties.isProfileAshaCentralEnabled().orElse(false);
             } else {
                 supported = resources.getBoolean(config.mSupported);
             }

android/app/tests/unit/src/com/android/bluetooth/btservice/ActiveDeviceManagerTest.java
@@ -220,7 +220,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_clearA2dpAndHeadsetActive() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
 
         a2dpConnected(mA2dpHeadsetDevice);
         headsetConnected(mA2dpHeadsetDevice);
@@ -238,7 +238,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_dontSetA2dpAndHeadsetActive() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         a2dpConnected(mA2dpHeadsetDevice);
@@ -255,7 +255,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_setA2dpActiveExplicitly() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         a2dpConnected(mA2dpHeadsetDevice);
@@ -275,7 +275,7 @@ public class ActiveDeviceManagerTest {
     @Test
     public void hearingAidActive_setHeadsetActiveExplicitly() {
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
 
         hearingAidActiveDeviceChanged(mHearingAidDevice);
         headsetConnected(mA2dpHeadsetDevice);

android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidServiceTest.java
@@ -84,7 +84,7 @@ public class HearingAidServiceTest {
     public void setUp() throws Exception {
         mTargetContext = InstrumentationRegistry.getTargetContext();
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
         // Set up mocks and test assets
         MockitoAnnotations.initMocks(this);
 
@@ -127,7 +127,7 @@ public class HearingAidServiceTest {
 
     @After
     public void tearDown() throws Exception {
-        if (!BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false)) {
+        if (!BluetoothProperties.isProfileAshaCentralEnabled().orElse(false)) {
             return;
         }
         stopService();

android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidStateMachineTest.java
@@ -64,7 +64,7 @@ public class HearingAidStateMachineTest {
     public void setUp() throws Exception {
         mTargetContext = InstrumentationRegistry.getTargetContext();
         Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false));
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false));
         // Set up mocks and test assets
         MockitoAnnotations.initMocks(this);
         TestUtils.setAdapterService(mAdapterService);
@@ -86,7 +86,7 @@ public class HearingAidStateMachineTest {
 
     @After
     public void tearDown() throws Exception {
-        if (!BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false)) {
+        if (!BluetoothProperties.isProfileAshaCentralEnabled().orElse(false)) {
             return;
         }
         mHearingAidStateMachine.doQuit();

service/java/com/android/server/bluetooth/BluetoothManagerService.java
@@ -495,7 +495,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         mUserManager = mContext.getSystemService(UserManager.class);
 
         mIsHearingAidProfileSupported =
-                BluetoothProperties.audioStreamingForHearingAidSupported().orElse(false);
+                BluetoothProperties.isProfileAshaCentralEnabled().orElse(false);
 
         // TODO: We need a more generic way to initialize the persist keys of FeatureFlagUtils
         String value = SystemProperties.get(FeatureFlagUtils.PERSIST_PREFIX + FeatureFlagUtils.HEARING_AID_SETTINGS);