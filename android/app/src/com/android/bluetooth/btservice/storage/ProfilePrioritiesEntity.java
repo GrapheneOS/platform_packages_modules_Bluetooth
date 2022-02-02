miyu-xu/packages-modules-Bluetooth

hap: Add initial implementation
Implements Hearing Access Profile client.

Bug: 150670922
Tag: #feature
Test: atest LeAudioHasClientTest LeAudioHasClientStateMachineTest
bluetooth_has_test
Sponsor: jpawlowski@
Change-Id: Idc94cfd043632a998784c0fb1c42aa52f76b3ebd

Tag: #feature
Bug: 150670922
Sponsor: jpawlowski@
Change-Id: I0d611daf536fd0fe5355c917640fe79898ed956c

android/app/src/com/android/bluetooth/btservice/storage/ProfilePrioritiesEntity.java
@@ -34,6 +34,7 @@ class ProfilePrioritiesEntity {
     public int map_connection_policy;
     public int sap_connection_policy;
     public int hearing_aid_connection_policy;
+    public int hap_client_connection_policy;
     public int map_client_connection_policy;
     public int le_audio_connection_policy;
     public int volume_control_connection_policy;
@@ -51,6 +52,7 @@ class ProfilePrioritiesEntity {
         map_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         sap_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         hearing_aid_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
+        hap_client_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         map_client_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         le_audio_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         volume_control_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
@@ -71,6 +73,7 @@ class ProfilePrioritiesEntity {
                 .append("|MAP=").append(map_connection_policy)
                 .append("|MAP_CLIENT=").append(map_client_connection_policy)
                 .append("|SAP=").append(sap_connection_policy)
+                .append("|HAS=").append(hap_client_connection_policy)
                 .append("|HEARING_AID=").append(hearing_aid_connection_policy)
                 .append("|LE_AUDIO=").append(le_audio_connection_policy)
                 .append("|VOLUME_CONTROL=").append(volume_control_connection_policy);

GrapheneOS/platform_packages_modules_Bluetooth

Test: atest BluetoothInstrumentationTests bluetooth_has_test
Change-Id: If3cc9f2146d08326800eb922d7e2f795cf110e7d
Merged-In: If3cc9f2146d08326800eb922d7e2f795cf110e7d
(cherry picked from commit 8e723645df2f56f0edc346302c9c1e5a39f82795)

android/app/src/com/android/bluetooth/btservice/storage/ProfilePrioritiesEntity.java
@@ -34,6 +34,7 @@ class ProfilePrioritiesEntity {
     public int map_connection_policy;
     public int sap_connection_policy;
     public int hearing_aid_connection_policy;
+    public int hap_client_connection_policy;
     public int map_client_connection_policy;
     public int le_audio_connection_policy;
     public int volume_control_connection_policy;
@@ -52,6 +53,7 @@ class ProfilePrioritiesEntity {
         map_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         sap_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         hearing_aid_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
+        hap_client_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         map_client_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         le_audio_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
         volume_control_connection_policy = BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
@@ -73,6 +75,7 @@ class ProfilePrioritiesEntity {
                 .append("|MAP=").append(map_connection_policy)
                 .append("|MAP_CLIENT=").append(map_client_connection_policy)
                 .append("|SAP=").append(sap_connection_policy)
+                .append("|HAP=").append(hap_client_connection_policy)
                 .append("|HEARING_AID=").append(hearing_aid_connection_policy)
                 .append("|LE_AUDIO=").append(le_audio_connection_policy)
                 .append("|VOLUME_CONTROL=").append(volume_control_connection_policy)