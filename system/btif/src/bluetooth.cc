miyu-xu/packages-modules-Bluetooth

Floss: BTIF API: ClearEventFilter
Expose ClearEventFilter to BTIF consumer

Bug: 216477258
Test: Compiles
Tag: #floss
Change-Id: Ib1314442b280f721ef92500bac8355d90af2ba31

system/btif/src/bluetooth.cc
@@ -402,6 +402,14 @@ static int read_energy_info() {
   return BT_STATUS_SUCCESS;
 }
 
+static int clear_event_filter() {
+  LOG_INFO("%s", __func__);
+  if (!interface_ready()) return BT_STATUS_NOT_READY;
+
+  do_in_main_thread(FROM_HERE, base::BindOnce(btif_dm_clear_event_filter));
+  return BT_STATUS_SUCCESS;
+}
+
 static void dump(int fd, const char** arguments) {
   btif_debug_conn_dump(fd);
   btif_debug_bond_event_dump(fd);
@@ -641,7 +649,8 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     get_metric_id,
     set_dynamic_audio_buffer_size,
     generate_local_oob_data,
-    allow_low_latency_audio};
+    allow_low_latency_audio,
+    clear_event_filter};
 
 // callback reporting helpers
 

GrapheneOS/platform_packages_modules_Bluetooth

system/btif/src/bluetooth.cc
@@ -412,6 +412,14 @@ static int read_energy_info() {
   return BT_STATUS_SUCCESS;
 }
 
+static int clear_event_filter() {
+  LOG_VERBOSE("%s", __func__);
+  if (!interface_ready()) return BT_STATUS_NOT_READY;
+
+  do_in_main_thread(FROM_HERE, base::BindOnce(btif_dm_clear_event_filter));
+  return BT_STATUS_SUCCESS;
+}
+
 static void dump(int fd, const char** arguments) {
   btif_debug_conn_dump(fd);
   btif_debug_bond_event_dump(fd);
@@ -662,7 +670,8 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     get_metric_id,
     set_dynamic_audio_buffer_size,
     generate_local_oob_data,
-    allow_low_latency_audio};
+    allow_low_latency_audio,
+    clear_event_filter};
 
 // callback reporting helpers
 