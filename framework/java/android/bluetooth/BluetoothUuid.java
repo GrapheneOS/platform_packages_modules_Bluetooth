miyu-xu/packages-modules-Bluetooth

Bluetooth: Add Broadcast Assistant UUID
Change-Id: Ifa4be661b69a1a20aedfb55d1bd7a528739a6489
sponsor: siyuanh@
Test: compile
Tag: #feature

framework/java/android/bluetooth/BluetoothUuid.java
@@ -199,6 +199,11 @@ public final class BluetoothUuid {
     /** @hide */
     @NonNull
     @SystemApi
+    public static final ParcelUuid BASS_ID =
+            ParcelUuid.fromString("0000184F-0000-1000-8000-00805F9B34FB");
+    /** @hide */
+    @NonNull
+    @SystemApi
     public static final ParcelUuid BASE_UUID =
             ParcelUuid.fromString("00000000-0000-1000-8000-00805F9B34FB");
 

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 208223966

framework/java/android/bluetooth/BluetoothUuid.java
@@ -199,6 +199,11 @@ public final class BluetoothUuid {
     /** @hide */
     @NonNull
     @SystemApi
+    public static final ParcelUuid BASS =
+            ParcelUuid.fromString("0000184F-0000-1000-8000-00805F9B34FB");
+    /** @hide */
+    @NonNull
+    @SystemApi
     public static final ParcelUuid BASE_UUID =
             ParcelUuid.fromString("00000000-0000-1000-8000-00805F9B34FB");
 