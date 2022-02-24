894aa6dd117c601b9e99f8bce9358d63bd45fabf
Vance Yu <vanceyu@google.com>
2022-02-24 06:58:17 +0000

    Fix the BT settings is crashing after Toggle OFF/ON
    Fix java NullPointerException since some object was not created
    when users didn't performed BLE advertising.
    
    Bug: 213610155
    Bug: 242230899
    Tag: #stability
    Test: turn on/off Bluetooth in the setting then check if Bluetooth crashes happen.
    Merged-In: I3adccd699e10fef79579880f0110ae8474f36955
    Change-Id: I3adccd699e10fef79579880f0110ae8474f36955
    (cherry picked from commit 6e9bd12ecaf6b60f03cf360b44bc19982b4a70db)

--- android/app/src/com/android/bluetooth/gatt/ContextMap.java
+++ android/app/src/com/android/bluetooth/gatt/ContextMap.java
@@ -194,7 +194,8 @@ import java.util.UUID;
 
     private static final int ADVERTISE_STATE_MAX_SIZE = 5;
 
-    private EvictingQueue<AppAdvertiseStats> mLastAdvertises;
+    private final EvictingQueue<AppAdvertiseStats> mLastAdvertises =
+            EvictingQueue.create(ADVERTISE_STATE_MAX_SIZE);
 
     /** Internal list of connected devices **/
     private Set<Connection> mConnections = new HashSet<Connection>();
@@ -243,10 +244,6 @@ import java.util.UUID;
             if (app == null) {
                 app = new App(appUid, callback, appName);
                 mApps.add(app);
-            }
-
-            if (mLastAdvertises == null) {
-                mLastAdvertises = EvictingQueue.create(ADVERTISE_STATE_MAX_SIZE);
             }
             return app;
         }
@@ -628,7 +625,9 @@ import java.util.UUID;
             while (i.hasNext()) {
                 App entry = i.next();
                 entry.unlinkToDeath();
-                entry.appScanStats.isRegistered = false;
+                if (entry.appScanStats != null) {
+                    entry.appScanStats.isRegistered = false;
+                }
                 i.remove();
             }
         }
