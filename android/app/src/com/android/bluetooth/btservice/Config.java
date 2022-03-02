miyu-xu/packages-modules-Bluetooth

BT LE broadcast assistant implementation
BT LE broadcast assistant implementation

Test:compile
Tag: #feature
sponsor: siyuanh@

Change-Id: Ia183608f3551319f79f8e2aa933445346c42c028

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -52,6 +52,7 @@ import com.android.bluetooth.pbapclient.PbapClientService;
 import com.android.bluetooth.sap.SapService;
 import com.android.bluetooth.tbs.TbsService;
 import com.android.bluetooth.vc.VolumeControlService;
+import com.android.bluetooth.bc.BCService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -136,6 +137,9 @@ public class Config {
                     (1 << BluetoothProfile.CSIP_SET_COORDINATOR)),
             new ProfileConfig(HapClientService.class, R.bool.profile_supported_hap_client,
                     (1 << BluetoothProfile.HAP_CLIENT)),
+            new ProfileConfig(BCService.class,
+                    R.bool.profile_supported_bc,
+                    (1 << BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT)),
     };
 
     private static Class[] sSupportedProfiles = new Class[0];

android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -49,6 +49,7 @@ import com.android.bluetooth.hid.HidHostService;
 import com.android.bluetooth.le_audio.LeAudioService;
 import com.android.bluetooth.pan.PanService;
 import com.android.bluetooth.vc.VolumeControlService;
+import com.android.bluetooth.bc.BCService;
 import com.android.internal.annotations.VisibleForTesting;
 
 import java.util.HashSet;
@@ -296,6 +297,7 @@ class PhonePolicy {
              mFactory.getCsipSetCoordinatorService();
         VolumeControlService volumeControlService =
              mFactory.getVolumeControlService();
+        BCService bcService = mFactory.getBCService();
 
         // Set profile priorities only for the profiles discovered on the remote device.
         // This avoids needless auto-connect attempts to profiles non-existent on the remote device
@@ -363,6 +365,14 @@ class PhonePolicy {
             debugLog("setting volume control profile priority for device " + device);
             mAdapterService.getDatabase().setProfileConnectionPolicy(device,
                     BluetoothProfile.VOLUME_CONTROL, BluetoothProfile.CONNECTION_POLICY_ALLOWED);
+        }
+        if ((bcService != null) && Utils.arrayContains(uuids,
+                BluetoothUuid.BC_ID) && (bcService.getConnectionPolicy(device)
+                == BluetoothProfile.CONNECTION_POLICY_UNKNOWN)) {
+            debugLog("setting broadcast assistant profile priority for device " + device);
+            mAdapterService.getDatabase().setProfileConnectionPolicy(device,
+                    BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT,
+                    BluetoothProfile.CONNECTION_POLICY_ALLOWED);
         }
     }
 

android/app/src/com/android/bluetooth/btservice/storage/Metadata.java
@@ -122,6 +122,9 @@ class Metadata {
             case BluetoothProfile.LE_CALL_CONTROL:
                 profileConnectionPolicies.le_call_control_connection_policy = connectionPolicy;
                 break;
+            case BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT:
+                profileConnectionPolicies.bc_connection_policy = connectionPolicy;
+                break;
             default:
                 throw new IllegalArgumentException("invalid profile " + profile);
         }
@@ -161,6 +164,8 @@ class Metadata {
                 return profileConnectionPolicies.csip_set_coordinator_connection_policy;
             case BluetoothProfile.LE_CALL_CONTROL:
                 return profileConnectionPolicies.le_call_control_connection_policy;
+            case BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT:
+                return profileConnectionPolicies.bc_connection_policy;
         }
         return BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
     }

GrapheneOS/platform_packages_modules_Bluetooth

Test: compile
Signed-off-by: Myles Watson <mylesgw@google.com>

android/app/src/com/android/bluetooth/btservice/Config.java
@@ -33,6 +33,7 @@ import com.android.bluetooth.a2dp.A2dpService;
 import com.android.bluetooth.a2dpsink.A2dpSinkService;
 import com.android.bluetooth.avrcp.AvrcpTargetService;
 import com.android.bluetooth.avrcpcontroller.AvrcpControllerService;
+import com.android.bluetooth.bass_client.BassClientService;
 import com.android.bluetooth.csip.CsipSetCoordinatorService;
 import com.android.bluetooth.gatt.GattService;
 import com.android.bluetooth.hap.HapClientService;
@@ -136,6 +137,9 @@ public class Config {
                     (1 << BluetoothProfile.CSIP_SET_COORDINATOR)),
             new ProfileConfig(HapClientService.class, R.bool.profile_supported_hap_client,
                     (1 << BluetoothProfile.HAP_CLIENT)),
+            new ProfileConfig(BassClientService.class,
+                    R.bool.profile_supported_bass_client,
+                    (1 << BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT)),
     };
 
     private static Class[] sSupportedProfiles = new Class[0];

android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -41,6 +41,7 @@ import android.util.Log;
 import com.android.bluetooth.R;
 import com.android.bluetooth.Utils;
 import com.android.bluetooth.a2dp.A2dpService;
+import com.android.bluetooth.bass_client.BassClientService;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
 import com.android.bluetooth.csip.CsipSetCoordinatorService;
 import com.android.bluetooth.hap.HapClientService;
@@ -298,6 +299,7 @@ class PhonePolicy {
         VolumeControlService volumeControlService =
              mFactory.getVolumeControlService();
         HapClientService hapClientService = mFactory.getHapClientService();
+        BassClientService bcService = mFactory.getBassClientService();
 
         // Set profile priorities only for the profiles discovered on the remote device.
         // This avoids needless auto-connect attempts to profiles non-existent on the remote device
@@ -373,6 +375,15 @@ class PhonePolicy {
             debugLog("setting hearing access profile priority for device " + device);
             mAdapterService.getDatabase().setProfileConnectionPolicy(device,
                     BluetoothProfile.HAP_CLIENT, BluetoothProfile.CONNECTION_POLICY_ALLOWED);
+        }
+
+        if ((bcService != null) && Utils.arrayContains(uuids,
+                BluetoothUuid.BASS) && (bcService.getConnectionPolicy(device)
+                == BluetoothProfile.CONNECTION_POLICY_UNKNOWN)) {
+            debugLog("setting broadcast assistant profile priority for device " + device);
+            mAdapterService.getDatabase().setProfileConnectionPolicy(device,
+                    BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT,
+                    BluetoothProfile.CONNECTION_POLICY_ALLOWED);
         }
     }
 

android/app/src/com/android/bluetooth/btservice/storage/Metadata.java
@@ -125,6 +125,9 @@ class Metadata {
             case BluetoothProfile.LE_CALL_CONTROL:
                 profileConnectionPolicies.le_call_control_connection_policy = connectionPolicy;
                 break;
+            case BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT:
+                profileConnectionPolicies.bass_client_connection_policy = connectionPolicy;
+                break;
             default:
                 throw new IllegalArgumentException("invalid profile " + profile);
         }
@@ -166,6 +169,8 @@ class Metadata {
                 return profileConnectionPolicies.csip_set_coordinator_connection_policy;
             case BluetoothProfile.LE_CALL_CONTROL:
                 return profileConnectionPolicies.le_call_control_connection_policy;
+            case BluetoothProfile.LE_AUDIO_BROADCAST_ASSISTANT:
+                return profileConnectionPolicies.bass_client_connection_policy;
         }
         return BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
     }