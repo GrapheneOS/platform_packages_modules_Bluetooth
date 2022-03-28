ae9f652bc1931442d84cd6a976f19e7248b13a0a
Sonny Sasaka <sonnysasaka@google.com>
2022-03-28 10:31:34 -0700

    Floss: Add protobuf build to btmanagerd
    Prepares build system to support protobuf with powerd D-Bus integration.
    
    Bug: 224606285
    Tag: #floss
    Test: Manual - build.py
    
    Change-Id: I9b701a84d11dde3ed9414711a371707106f28b57

--- system/gd/rust/linux/mgmt/src/lib.rs
+++ system/gd/rust/linux/mgmt/src/lib.rs
@@ -1,1 +1,5 @@
 pub mod iface_bluetooth_manager;
+
+// protoc-rust generates all modules and exports them in mod.rs
+// We have to include them all here to make them available for crate export.
+include!(concat!(env!("OUT_DIR"), "/proto_out/mod.rs"));
