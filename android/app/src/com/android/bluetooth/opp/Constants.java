32a8940af557163622f66b9cdb01efe88141ad61
Rahul Sabnis <rahulsabnis@google.com>
2022-04-02 00:21:16 -0700

    Remove hardcoded references to "com.android.bluetooth.services"
    Tag: #feature
    Bug: 220807590
    Test: Manual
    Change-Id: Iba79127d1d94e4eea04eafecddb26efd0e94f657

--- android/app/src/com/android/bluetooth/opp/Constants.java
+++ android/app/src/com/android/bluetooth/opp/Constants.java
@@ -163,8 +163,6 @@ public class Constants {
     /** the intent that gets sent when clicking a incoming file confirm notification */
     static final String ACTION_INCOMING_FILE_CONFIRM = "android.btopp.intent.action.CONFIRM";
 
-    static final String THIS_PACKAGE_NAME = "com.android.bluetooth.services";
-
     /** The column that is used to remember whether the media scanner was invoked */
     static final String MEDIA_SCANNED = "scanned";
 
@@ -239,7 +237,7 @@ public class Constants {
     static void sendIntentIfCompleted(Context context, Uri contentUri, int status) {
         if (BluetoothShare.isStatusCompleted(status)) {
             Intent intent = new Intent(BluetoothShare.TRANSFER_COMPLETED_ACTION);
-            intent.setClassName(THIS_PACKAGE_NAME, BluetoothOppReceiver.class.getName());
+            intent.setClassName(context, BluetoothOppReceiver.class.getName());
             intent.setDataAndNormalize(contentUri);
             context.sendBroadcast(intent);
         }
