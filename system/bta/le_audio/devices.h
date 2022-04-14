miyu-xu/packages-modules-Bluetooth

leaudio: Improve tracking cig state
With this patch, cig state is changed to enum so we can track if group
is idle, creating, created or removing.
Group shall not be removed while CIG is not in idle.
This patch also adds asserts to catch invalid behaviour

Bug: 226709330
Bug: 150670922
test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test
sponsor: @jpawlowski
Change-Id: If3a14dbf3c8283748c2ac1c43a13c94d4568fd8f

system/bta/le_audio/devices.h
@@ -182,7 +182,7 @@ class LeAudioDevices {
 class LeAudioDeviceGroup {
  public:
   const int group_id_;
-  bool cig_created_;
+  types::CigState cig_state_;
 
   struct stream_configuration stream_conf;
 
@@ -192,7 +192,7 @@ class LeAudioDeviceGroup {
 
   explicit LeAudioDeviceGroup(const int group_id)
       : group_id_(group_id),
-        cig_created_(false),
+        cig_state_(types::CigState::IDLE),
         stream_conf({}),
         audio_directions_(0),
         transport_latency_mtos_us_(0),

system/bta/le_audio/le_audio_client_test.cc
@@ -634,7 +634,7 @@ class UnicastTestNoInit : public Test {
           streaming_groups[group->group_id_] = group;
 
           /* Assume CIG is created */
-          group->cig_created_ = true;
+          group->cig_state_ = le_audio::types::CigState::CREATED;
 
           do_in_main_thread(
               FROM_HERE, base::BindOnce(
@@ -700,7 +700,7 @@ class UnicastTestNoInit : public Test {
           }
 
           if (group->IsEmpty()) {
-            group->cig_created_ = false;
+            group->cig_state_ = le_audio::types::CigState::IDLE;
             InjectCigRemoved(group->group_id_);
           }
         });

system/bta/le_audio/le_audio_types.cc
@@ -457,6 +457,15 @@ uint8_t GetMaxCodecFramesPerSduFromPac(const acs_ac_record* pac) {
 }
 
 namespace types {
+std::ostream& operator<<(std::ostream& os, const types::CigState& state) {
+  static const char* char_value_[4] = {"IDLE", "CREATING", "CREATED",
+                                       "REMOVING"};
+
+  os << char_value_[static_cast<uint8_t>(state)] << " ("
+     << "0x" << std::setfill('0') << std::setw(2) << static_cast<int>(state)
+     << ")";
+  return os;
+}
 std::ostream& operator<<(std::ostream& os, const types::AseState& state) {
   static const char* char_value_[7] = {
       "IDLE",      "CODEC_CONFIGURED", "QOS_CONFIGURED", "ENABLING",

system/bta/le_audio/le_audio_types.h
@@ -302,6 +302,8 @@ constexpr uint16_t kMaxTransportLatencyMin = 0x0005;
 constexpr uint16_t kMaxTransportLatencyMax = 0x0FA0;
 
 /* Enums */
+enum class CigState : uint8_t { IDLE, CREATING, CREATED, REMOVING };
+
 /* ASE states according to BAP defined state machine states */
 enum class AseState : uint8_t {
   BTA_LE_AUDIO_ASE_STATE_IDLE = 0x00,
@@ -566,7 +568,7 @@ using AudioLocations = std::bitset<32>;
 using AudioContexts = std::bitset<16>;
 
 std::ostream& operator<<(std::ostream& os, const AseState& state);
-
+std::ostream& operator<<(std::ostream& os, const CigState& state);
 std::ostream& operator<<(std::ostream& os, const LeAudioLc3Config& config);
 }  // namespace types
 

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/le_audio/devices.h
@@ -182,7 +182,7 @@ class LeAudioDevices {
 class LeAudioDeviceGroup {
  public:
   const int group_id_;
-  bool cig_created_;
+  types::CigState cig_state_;
 
   struct stream_configuration stream_conf;
 
@@ -192,7 +192,7 @@ class LeAudioDeviceGroup {
 
   explicit LeAudioDeviceGroup(const int group_id)
       : group_id_(group_id),
-        cig_created_(false),
+        cig_state_(types::CigState::NONE),
         stream_conf({}),
         audio_directions_(0),
         transport_latency_mtos_us_(0),

system/bta/le_audio/le_audio_client_test.cc
@@ -634,7 +634,7 @@ class UnicastTestNoInit : public Test {
           streaming_groups[group->group_id_] = group;
 
           /* Assume CIG is created */
-          group->cig_created_ = true;
+          group->cig_state_ = le_audio::types::CigState::CREATED;
 
           do_in_main_thread(
               FROM_HERE, base::BindOnce(
@@ -700,7 +700,7 @@ class UnicastTestNoInit : public Test {
           }
 
           if (group->IsEmpty()) {
-            group->cig_created_ = false;
+            group->cig_state_ = le_audio::types::CigState::NONE;
             InjectCigRemoved(group->group_id_);
           }
         });

system/bta/le_audio/le_audio_types.cc
@@ -457,6 +457,15 @@ uint8_t GetMaxCodecFramesPerSduFromPac(const acs_ac_record* pac) {
 }
 
 namespace types {
+std::ostream& operator<<(std::ostream& os, const types::CigState& state) {
+  static const char* char_value_[4] = {"NONE", "CREATING", "CREATED",
+                                       "REMOVING"};
+
+  os << char_value_[static_cast<uint8_t>(state)] << " ("
+     << "0x" << std::setfill('0') << std::setw(2) << static_cast<int>(state)
+     << ")";
+  return os;
+}
 std::ostream& operator<<(std::ostream& os, const types::AseState& state) {
   static const char* char_value_[7] = {
       "IDLE",      "CODEC_CONFIGURED", "QOS_CONFIGURED", "ENABLING",

system/bta/le_audio/le_audio_types.h
@@ -302,6 +302,8 @@ constexpr uint16_t kMaxTransportLatencyMin = 0x0005;
 constexpr uint16_t kMaxTransportLatencyMax = 0x0FA0;
 
 /* Enums */
+enum class CigState : uint8_t { NONE, CREATING, CREATED, REMOVING };
+
 /* ASE states according to BAP defined state machine states */
 enum class AseState : uint8_t {
   BTA_LE_AUDIO_ASE_STATE_IDLE = 0x00,
@@ -566,7 +568,7 @@ using AudioLocations = std::bitset<32>;
 using AudioContexts = std::bitset<16>;
 
 std::ostream& operator<<(std::ostream& os, const AseState& state);
-
+std::ostream& operator<<(std::ostream& os, const CigState& state);
 std::ostream& operator<<(std::ostream& os, const LeAudioLc3Config& config);
 }  // namespace types
 