miyu-xu/packages-modules-Bluetooth

le_audio: Add support for Broadcaster session for Bluetoot stack
Patch extends possibility for stack, Bluetooth Audio Hal part to use
Broadcaster session for broadcasting.

Tag: #feature
Test: atest bluetooth_le_audio_client_test bluetooth_test_broadcaster
Sponsor: jpawlowski@
Bug:
Change-Id: I4d5402f060a853456751e348072b941173848e04

system/audio_hal_interface/le_audio_software.h
@@ -80,6 +80,7 @@ class LeAudioClientInterface {
  public:
   class Sink : public IClientInterfaceEndpoint {
    public:
+    Sink(bool is_broadcaster = false) : is_broadcaster_(is_broadcaster){};
     virtual ~Sink() = default;
 
     void Cleanup() override;
@@ -94,6 +95,10 @@ class LeAudioClientInterface {
     void SuspendedForReconfiguration() override;
     // Read the stream of bytes sinked to us by the upper layers
     size_t Read(uint8_t* p_buf, uint32_t len);
+    bool IsBroadcater() { return is_broadcaster_; }
+
+   private:
+    bool is_broadcaster_ = false;
   };
   class Source : public IClientInterfaceEndpoint {
    public:
@@ -116,9 +121,12 @@ class LeAudioClientInterface {
   // Get LE Audio sink client interface if it's not previously acquired and not
   // yet released.
   Sink* GetSink(StreamCallbacks stream_cb,
-                bluetooth::common::MessageLoopThread* message_loop);
-  // This should be called before trying to get sink interface
-  bool IsSinkAcquired();
+                bluetooth::common::MessageLoopThread* message_loop,
+                bool is_broadcasting_session_type);
+  // This should be called before trying to get unicast sink interface
+  bool IsUnicastSinkAcquired();
+  // This should be called before trying to get broadcast sink interface
+  bool IsBroadcastSinkAcquired();
   // Release sink interface if belongs to LE audio client interface
   bool ReleaseSink(Sink* sink);
 
@@ -136,7 +144,8 @@ class LeAudioClientInterface {
 
  private:
   static LeAudioClientInterface* interface;
-  Sink* sink_ = nullptr;
+  Sink* unicast_sink_ = nullptr;
+  Sink* broadcast_sink_ = nullptr;
   Source* source_ = nullptr;
 };
 

system/bta/le_audio/client_audio.h
@@ -19,6 +19,7 @@
 #include <future>
 
 #include "audio_hal_interface/le_audio_software.h"
+#include "common/repeating_timer.h"
 
 /* Implementations of Le Audio will also implement this interface */
 class LeAudioClientAudioSinkReceiver {
@@ -104,32 +105,79 @@ struct LeAudioCodecConfiguration {
 /* Represents source of audio for le audio client */
 class LeAudioClientAudioSource {
  public:
-  static bool Start(const LeAudioCodecConfiguration& codecConfiguration,
-                    LeAudioClientAudioSinkReceiver* audioReceiver);
-  static void Stop();
-  static const void* Acquire();
-  static void Release(const void* instance);
-  static void ConfirmStreamingRequest();
-  static void CancelStreamingRequest();
-  static void UpdateRemoteDelay(uint16_t remote_delay_ms);
-  static void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
-  static void SuspendedForReconfiguration();
+  virtual ~LeAudioClientAudioSource() = default;
+
+  virtual bool Start(const LeAudioCodecConfiguration& codecConfiguration,
+                     LeAudioClientAudioSinkReceiver* audioReceiver);
+  virtual void Stop();
+  virtual void Release(const void* instance);
+  virtual void ConfirmStreamingRequest();
+  virtual void CancelStreamingRequest();
+  virtual void UpdateRemoteDelay(uint16_t remote_delay_ms);
+  virtual void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
+  virtual void SuspendedForReconfiguration();
+
   static void DebugDump(int fd);
+
+ protected:
+  const void* Acquire(bool is_broadcasting_session_type);
+  bool InitAudioSinkThread(const std::string name);
+
+  bluetooth::common::MessageLoopThread* worker_thread_;
+
+ private:
+  bool SinkOnResumeReq(bool start_media_task);
+  bool SinkOnSuspendReq();
+  bool SinkOnMetadataUpdateReq(const source_metadata_t& source_metadata);
+
+  void StartAudioTicks();
+  void StopAudioTicks();
+  void SendAudioData();
+
+  bluetooth::common::RepeatingTimer audio_timer_;
+  LeAudioCodecConfiguration source_codec_config_;
+  LeAudioClientAudioSinkReceiver* audioSinkReceiver_;
+  bluetooth::audio::le_audio::LeAudioClientInterface::Sink*
+      sinkClientInterface_;
+
+  /* Guard audio sink receiver mutual access from stack with internal mutex */
+  std::mutex sinkInterfaceMutex_;
 };
 
 /* Represents audio sink for le audio client */
 class LeAudioClientAudioSink {
  public:
-  static bool Start(const LeAudioCodecConfiguration& codecConfiguration,
-                    LeAudioClientAudioSourceReceiver* audioReceiver);
-  static void Stop();
-  static const void* Acquire();
-  static void Release(const void* instance);
-  static size_t SendData(uint8_t* data, uint16_t size);
-  static void ConfirmStreamingRequest();
-  static void CancelStreamingRequest();
-  static void UpdateRemoteDelay(uint16_t remote_delay_ms);
-  static void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
-  static void SuspendedForReconfiguration();
+  virtual ~LeAudioClientAudioSink() = default;
+
+  virtual bool Start(const LeAudioCodecConfiguration& codecConfiguration,
+                     LeAudioClientAudioSourceReceiver* audioReceiver);
+  virtual void Stop();
+  virtual const void* Acquire();
+  virtual void Release(const void* instance);
+  virtual size_t SendData(uint8_t* data, uint16_t size);
+  virtual void ConfirmStreamingRequest();
+  virtual void CancelStreamingRequest();
+  virtual void UpdateRemoteDelay(uint16_t remote_delay_ms);
+  virtual void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
+  virtual void SuspendedForReconfiguration();
+
   static void DebugDump(int fd);
+
+ private:
+  bool SourceOnResumeReq(bool start_media_task);
+  bool SourceOnSuspendReq();
+
+  LeAudioClientAudioSourceReceiver* audioSourceReceiver_;
+  bluetooth::audio::le_audio::LeAudioClientInterface::Source*
+      sourceClientInterface_;
+};
+
+class LeAudioUnicastClientAudioSource : public LeAudioClientAudioSource {
+ public:
+  virtual const void* Acquire();
+};
+
+class LeAudioBroadcastClientAudioSource : public LeAudioClientAudioSource {
+ public:
+  virtual const void* Acquire();
 };

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 210986334

system/audio_hal_interface/le_audio_software.h
@@ -80,6 +80,7 @@ class LeAudioClientInterface {
  public:
   class Sink : public IClientInterfaceEndpoint {
    public:
+    Sink(bool is_broadcaster = false) : is_broadcaster_(is_broadcaster){};
     virtual ~Sink() = default;
 
     void Cleanup() override;
@@ -94,6 +95,10 @@ class LeAudioClientInterface {
     void SuspendedForReconfiguration() override;
     // Read the stream of bytes sinked to us by the upper layers
     size_t Read(uint8_t* p_buf, uint32_t len);
+    bool IsBroadcaster() { return is_broadcaster_; }
+
+   private:
+    bool is_broadcaster_ = false;
   };
   class Source : public IClientInterfaceEndpoint {
    public:
@@ -116,9 +121,12 @@ class LeAudioClientInterface {
   // Get LE Audio sink client interface if it's not previously acquired and not
   // yet released.
   Sink* GetSink(StreamCallbacks stream_cb,
-                bluetooth::common::MessageLoopThread* message_loop);
-  // This should be called before trying to get sink interface
-  bool IsSinkAcquired();
+                bluetooth::common::MessageLoopThread* message_loop,
+                bool is_broadcasting_session_type);
+  // This should be called before trying to get unicast sink interface
+  bool IsUnicastSinkAcquired();
+  // This should be called before trying to get broadcast sink interface
+  bool IsBroadcastSinkAcquired();
   // Release sink interface if belongs to LE audio client interface
   bool ReleaseSink(Sink* sink);
 
@@ -136,7 +144,8 @@ class LeAudioClientInterface {
 
  private:
   static LeAudioClientInterface* interface;
-  Sink* sink_ = nullptr;
+  Sink* unicast_sink_ = nullptr;
+  Sink* broadcast_sink_ = nullptr;
   Source* source_ = nullptr;
 };
 

system/bta/le_audio/client_audio.h
@@ -19,6 +19,7 @@
 #include <future>
 
 #include "audio_hal_interface/le_audio_software.h"
+#include "common/repeating_timer.h"
 
 /* Implementations of Le Audio will also implement this interface */
 class LeAudioClientAudioSinkReceiver {
@@ -104,32 +105,79 @@ struct LeAudioCodecConfiguration {
 /* Represents source of audio for le audio client */
 class LeAudioClientAudioSource {
  public:
-  static bool Start(const LeAudioCodecConfiguration& codecConfiguration,
-                    LeAudioClientAudioSinkReceiver* audioReceiver);
-  static void Stop();
-  static const void* Acquire();
-  static void Release(const void* instance);
-  static void ConfirmStreamingRequest();
-  static void CancelStreamingRequest();
-  static void UpdateRemoteDelay(uint16_t remote_delay_ms);
-  static void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
-  static void SuspendedForReconfiguration();
+  virtual ~LeAudioClientAudioSource() = default;
+
+  virtual bool Start(const LeAudioCodecConfiguration& codecConfiguration,
+                     LeAudioClientAudioSinkReceiver* audioReceiver);
+  virtual void Stop();
+  virtual void Release(const void* instance);
+  virtual void ConfirmStreamingRequest();
+  virtual void CancelStreamingRequest();
+  virtual void UpdateRemoteDelay(uint16_t remote_delay_ms);
+  virtual void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
+  virtual void SuspendedForReconfiguration();
+
   static void DebugDump(int fd);
+
+ protected:
+  const void* Acquire(bool is_broadcasting_session_type);
+  bool InitAudioSinkThread(const std::string name);
+
+  bluetooth::common::MessageLoopThread* worker_thread_;
+
+ private:
+  bool SinkOnResumeReq(bool start_media_task);
+  bool SinkOnSuspendReq();
+  bool SinkOnMetadataUpdateReq(const source_metadata_t& source_metadata);
+
+  void StartAudioTicks();
+  void StopAudioTicks();
+  void SendAudioData();
+
+  bluetooth::common::RepeatingTimer audio_timer_;
+  LeAudioCodecConfiguration source_codec_config_;
+  LeAudioClientAudioSinkReceiver* audioSinkReceiver_;
+  bluetooth::audio::le_audio::LeAudioClientInterface::Sink*
+      sinkClientInterface_;
+
+  /* Guard audio sink receiver mutual access from stack with internal mutex */
+  std::mutex sinkInterfaceMutex_;
 };
 
 /* Represents audio sink for le audio client */
-class LeAudioClientAudioSink {
+class LeAudioUnicastClientAudioSink {
  public:
-  static bool Start(const LeAudioCodecConfiguration& codecConfiguration,
-                    LeAudioClientAudioSourceReceiver* audioReceiver);
-  static void Stop();
-  static const void* Acquire();
-  static void Release(const void* instance);
-  static size_t SendData(uint8_t* data, uint16_t size);
-  static void ConfirmStreamingRequest();
-  static void CancelStreamingRequest();
-  static void UpdateRemoteDelay(uint16_t remote_delay_ms);
-  static void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
-  static void SuspendedForReconfiguration();
+  virtual ~LeAudioUnicastClientAudioSink() = default;
+
+  virtual bool Start(const LeAudioCodecConfiguration& codecConfiguration,
+                     LeAudioClientAudioSourceReceiver* audioReceiver);
+  virtual void Stop();
+  virtual const void* Acquire();
+  virtual void Release(const void* instance);
+  virtual size_t SendData(uint8_t* data, uint16_t size);
+  virtual void ConfirmStreamingRequest();
+  virtual void CancelStreamingRequest();
+  virtual void UpdateRemoteDelay(uint16_t remote_delay_ms);
+  virtual void UpdateAudioConfigToHal(const ::le_audio::offload_config& config);
+  virtual void SuspendedForReconfiguration();
+
   static void DebugDump(int fd);
+
+ private:
+  bool SourceOnResumeReq(bool start_media_task);
+  bool SourceOnSuspendReq();
+
+  LeAudioClientAudioSourceReceiver* audioSourceReceiver_;
+  bluetooth::audio::le_audio::LeAudioClientInterface::Source*
+      sourceClientInterface_;
+};
+
+class LeAudioUnicastClientAudioSource : public LeAudioClientAudioSource {
+ public:
+  virtual const void* Acquire();
+};
+
+class LeAudioBroadcastClientAudioSource : public LeAudioClientAudioSource {
+ public:
+  virtual const void* Acquire();
 };