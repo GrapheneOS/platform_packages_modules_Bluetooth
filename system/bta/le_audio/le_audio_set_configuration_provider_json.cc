31102df200fdc7b3f900e17ae8e4b23a1b8e42d6
Łukasz Rymanowski <rlukasz@google.com>
2022-04-22 10:30:33 +0000

    leaudio/json: Add way to provide QoS for Sink and Source
    Qos_config is a vector now.
    First element is a Sink Qos and second is Source QoS.
    If Source is not provided, then Sink QoS is used for Source.
    
    Bug: 224933710
    Test: atest --host bluetooth_le_audio_test
    Sponsor: @jpawlowski
    Change-Id: I0a439d169a2db4fbd3ea4e998af66ad75919a378
    (cherry picked from commit fdc7ad3230e371bfb67de9a851606a552128a6df)

--- system/bta/le_audio/le_audio_set_configuration_provider_json.cc
+++ system/bta/le_audio/le_audio_set_configuration_provider_json.cc
@@ -238,24 +238,62 @@ struct AudioSetConfigurationProviderJson {
       std::vector<const bluetooth::le_audio::CodecConfiguration*>* codec_cfgs,
       std::vector<const bluetooth::le_audio::QosConfiguration*>* qos_cfgs) {
     std::vector<SetConfiguration> subconfigs;
-    QosConfigSetting qos;
+    QosConfigSetting qos_sink;
+    QosConfigSetting qos_source;
     const bluetooth::le_audio::CodecConfiguration* codec_cfg = NULL;
-    const bluetooth::le_audio::QosConfiguration* qos_cfg = NULL;
+    const bluetooth::le_audio::QosConfiguration* qos_sink_cfg = NULL;
+    const bluetooth::le_audio::QosConfiguration* qos_source_cfg = NULL;
 
     const char* codec_config_key = flat_cfg->codec_config_name()->c_str();
-    const char* qos_config_key = flat_cfg->qos_config_name()->c_str();
+    auto* qos_config_key_array = flat_cfg->qos_config_name();
+
+    char default_qos[] = "QoS_Config_Server_Preferred";
+
+    const char* qos_sink_key = default_qos;
+    const char* qos_source_key = default_qos;
+
+    /* We expect maximum two QoS settings. First for Sink and second for Source
+     */
+    if (qos_config_key_array->size() > 0) {
+      qos_sink_key = qos_config_key_array->Get(0)->c_str();
+      if (qos_config_key_array->size() > 1) {
+        qos_source_key = qos_config_key_array->Get(1)->c_str();
+      } else {
+        qos_source_key = qos_sink_key;
+      }
+    }
+
+    LOG_DEBUG("Config name %s, qos_sink %s, qos_source %s", codec_config_key,
+              qos_sink_key, qos_source_key);
 
     for (auto i = qos_cfgs->begin(); i != qos_cfgs->end(); ++i) {
-      if (0 == strcmp((*i)->name()->c_str(), qos_config_key)) {
-        qos_cfg = *i;
+      if (0 == strcmp((*i)->name()->c_str(), qos_sink_key)) {
+        qos_sink_cfg = *i;
         break;
       }
     }
-    if (qos_cfg != NULL) {
-      qos.retransmission_number = qos_cfg->retransmission_number();
-      qos.max_transport_latency = qos_cfg->max_transport_latency();
+
+    for (auto i = qos_cfgs->begin(); i != qos_cfgs->end(); ++i) {
+      if (0 == strcmp((*i)->name()->c_str(), qos_source_key)) {
+        qos_source_cfg = *i;
+        break;
+      }
+    }
+
+    if (qos_sink_cfg != NULL) {
+      qos_sink.retransmission_number = qos_sink_cfg->retransmission_number();
+      qos_sink.max_transport_latency = qos_sink_cfg->max_transport_latency();
     } else {
-      LOG_ERROR("No qos config matching key %s found", qos_config_key);
+      LOG_ERROR("No qos config matching key %s found", qos_sink_key);
+    }
+
+    if (qos_source_cfg != NULL) {
+      qos_source.retransmission_number =
+          qos_source_cfg->retransmission_number();
+      qos_source.max_transport_latency =
+          qos_source_cfg->max_transport_latency();
+    } else {
+      LOG_ERROR("No qos config matching key %s found", qos_source_key);
     }
 
     for (auto i = codec_cfgs->begin(); i != codec_cfgs->end(); ++i) {
@@ -267,7 +305,13 @@ struct AudioSetConfigurationProviderJson {
     if (codec_cfg != NULL && codec_cfg->subconfigurations()) {
       /* Load subconfigurations */
       for (auto subconfig : *codec_cfg->subconfigurations()) {
-        subconfigs.push_back(SetConfigurationFromFlatSubconfig(subconfig, qos));
+        if (subconfig->direction() == le_audio::types::kLeAudioDirectionSink) {
+          subconfigs.push_back(
+              SetConfigurationFromFlatSubconfig(subconfig, qos_sink));
+        } else {
+          subconfigs.push_back(
+              SetConfigurationFromFlatSubconfig(subconfig, qos_source));
+        }
       }
     } else {
       if (codec_cfg == NULL) {
