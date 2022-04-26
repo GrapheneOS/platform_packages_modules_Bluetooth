0130cc3660dfd063980bad869eb41198c8b8b2d0
Jack He <siyuanh@google.com>
2022-04-26 03:46:46 -0700

    BT: Include all Java tests without native dependency
    * With mainline module, all Bluetooth services are included in the APEX.
      Therefore, there is no need to skip a test when it is not enabled
      unless the test has native or BT controller dependencies
    
    Bug: 230446867
    Test: atest BluetoothInstrumentationTests
    Change-Id: Ib9854348ffabddb2c230fb5215d83363dc0956cc
    Merged-In: Ib9854348ffabddb2c230fb5215d83363dc0956cc
    (cherry picked from commit 18280b2579013e0963a6d3fbb891d8f3ec1a46a9)

--- android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidStateMachineTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidStateMachineTest.java
@@ -63,8 +63,6 @@ public class HearingAidStateMachineTest {
     @Before
     public void setUp() throws Exception {
         mTargetContext = InstrumentationRegistry.getTargetContext();
-        Assume.assumeTrue("Ignore test when HearingAidService is not enabled",
-                HearingAidService.isEnabled());
         // Set up mocks and test assets
         MockitoAnnotations.initMocks(this);
         TestUtils.setAdapterService(mAdapterService);
@@ -86,9 +84,6 @@ public class HearingAidStateMachineTest {
 
     @After
     public void tearDown() throws Exception {
-        if (!HearingAidService.isEnabled()) {
-            return;
-        }
         mHearingAidStateMachine.doQuit();
         mHandlerThread.quit();
         TestUtils.clearAdapterService(mAdapterService);
