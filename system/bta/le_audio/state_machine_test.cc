d8042721605fa2acca113556214192f8e62d8aa9
Patty Huang <plhuang@google.com>
2022-05-09 12:13:06 +0800

    Fix crash for getting offload capability
    Load the LE Audio offload capability only if offload is enabled
    
    Bug: 231359883
    Tag: #refactor
    Test: make build
    Change-Id: If4f12f14454cea80c0fb25e2376063ee8a06d9c1

--- system/bta/le_audio/state_machine_test.cc
+++ system/bta/le_audio/state_machine_test.cc
@@ -393,9 +393,7 @@ class StateMachineTest : public Test {
     ASSERT_NE(codec_manager_, nullptr);
     std::vector<bluetooth::le_audio::btle_audio_codec_config_t>
         mock_offloading_preference(0);
-    std::vector<set_configurations::AudioSetConfiguration>
-        mock_adsp_capabilities(0);
-    codec_manager_->Start(mock_offloading_preference, mock_adsp_capabilities);
+    codec_manager_->Start(mock_offloading_preference);
     mock_codec_manager_ = MockCodecManager::GetInstance();
     ASSERT_NE(mock_codec_manager_, nullptr);
     ON_CALL(*mock_codec_manager_, GetCodecLocation())
