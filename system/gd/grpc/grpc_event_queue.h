df236fc7f3f62a5b1eb3265c8f653ac8fab6901a
Jack He <siyuanh@google.com>
2022-06-16 03:34:13 -0700

    Test: Further stability improvements to ACL tests
    * erase items before calling std::map::emplace in le_acl_facade
    * add more logging to LE and classic ACL facades
    * reduce GrpcEventQueue's wait time from 500ms to 100ms so that
      it can be terminated faster
    * force a wait of 100ms after closing gRPC streams in Python
    
    Bug: 228619929
    Bug: 235872679
    Bug: 232048916
    Test: gd/cer/run --sl4a
    Ignore-AOSP-First: cherry-pick
    
    Merged-In: Idbb12b55db96b00bb695c54af1fd0efcc1f65841
    Change-Id: Idbb12b55db96b00bb695c54af1fd0efcc1f65841

--- system/gd/grpc/grpc_event_queue.h
+++ system/gd/grpc/grpc_event_queue.h
@@ -51,8 +51,8 @@ class GrpcEventQueue {
     using namespace std::chrono_literals;
     LOG_INFO("%s: Entering Loop", log_name_.c_str());
     while (!context->IsCancelled()) {
-      // Wait for 500 ms so that cancellation can be caught in amortized 250 ms latency
-      if (pending_events_.wait_to_take(500ms)) {
+      // Wait for 100 ms so that cancellation can be caught in amortized 50 ms latency
+      if (pending_events_.wait_to_take(100ms)) {
         LOG_INFO("%s: Got event from queue", log_name_.c_str());
         writer->Write(pending_events_.take());
       }
