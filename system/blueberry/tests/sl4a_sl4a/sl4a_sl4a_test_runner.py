d49c7291f2e52bf5f28889598fc60f61436d6389
Martin Brabham <optedoblivion@google.com>
2022-06-28 01:39:58 +0000

    Blueberry2: Add SL4A + SL4A LeAdvertisingTest
    Bug: 236737993
    Test: system/gd/cert/run --clean --sl4a_sl4a LeAdvertisingTest
    Tag: #stability
    Ignore-AOSP-First: Need in tm immediately
    Change-Id: I974ce9e7bfcb0b26dd9d6d2862166152fa03c1df

--- system/blueberry/tests/sl4a_sl4a/sl4a_sl4a_test_runner.py
+++ system/blueberry/tests/sl4a_sl4a/sl4a_sl4a_test_runner.py
@@ -14,6 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from blueberry.tests.sl4a_sl4a.advertising.le_advertising import LeAdvertisingTest
 from blueberry.tests.sl4a_sl4a.gatt.gatt_connect_test import GattConnectTest
 from blueberry.tests.sl4a_sl4a.gatt.gatt_connect_with_irk_test import GattConnectWithIrkTest
 from blueberry.tests.sl4a_sl4a.gatt.gatt_notify_test import GattNotifyTest
@@ -26,6 +27,7 @@ ALL_TESTS = [
     GattConnectTest,
     GattConnectWithIrkTest,
     GattNotifyTest,
+    LeAdvertisingTest,
     OobPairingTest,
 ]
 
