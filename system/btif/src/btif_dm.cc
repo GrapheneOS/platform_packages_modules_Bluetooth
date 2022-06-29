dc8078abe546202e9ad91e3c7af5baca4b42861c
Rahul Sabnis <rahulsabnis@google.com>
2022-06-29 18:17:59 +0000

    Revert^2 "Queue discovery and device inquiry while bonding"
    3da8e931820f59d34e2f8b81e87d002cec809eda
    
    Bug: 237352700
    Test: Manual
    Change-Id: I4699b7d73383b8b09ad1e3292d9dc2673f145736

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -537,6 +537,7 @@ static void bond_state_changed(bt_status_t status, const RawAddress& bd_addr,
     pairing_cb.bd_addr = bd_addr;
   } else {
     pairing_cb = {};
+    bta_dm_execute_queued_request();
   }
 }
 
@@ -1436,6 +1437,7 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
         // Both SDP and bonding are done, clear pairing control block in case
         // it is not already cleared
         pairing_cb = {};
+        bta_dm_execute_queued_request();
       }
 
       if (p_data->disc_res.num_uuids != 0) {
@@ -1906,10 +1908,16 @@ static void bta_energy_info_cb(tBTM_BLE_TX_TIME_MS tx_time,
 void btif_dm_start_discovery(void) {
   BTIF_TRACE_EVENT("%s", __func__);
 
+  if (bta_dm_is_search_request_queued()) {
+    LOG_INFO("%s skipping start discovery because a request is queued",
+             __func__);
+    return;
+  }
+
   /* Will be enabled to true once inquiry busy level has been received */
   btif_dm_inquiry_in_progress = false;
   /* find nearby devices */
-  BTA_DmSearch(btif_dm_search_devices_evt);
+  BTA_DmSearch(btif_dm_search_devices_evt, is_bonding_or_sdp());
 }
 
 /*******************************************************************************
@@ -2389,7 +2397,8 @@ void btif_dm_get_remote_services(RawAddress remote_addr, const int transport) {
   BTIF_TRACE_EVENT("%s: transport=%d, remote_addr=%s", __func__, transport,
                    remote_addr.ToString().c_str());
 
-  BTA_DmDiscover(remote_addr, btif_dm_search_services_evt, transport);
+  BTA_DmDiscover(remote_addr, btif_dm_search_services_evt, transport,
+                 remote_addr != pairing_cb.bd_addr && is_bonding_or_sdp());
 }
 
 void btif_dm_enable_service(tBTA_SERVICE_ID service_id, bool enable) {
