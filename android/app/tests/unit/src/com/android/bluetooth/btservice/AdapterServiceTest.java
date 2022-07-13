bf9246b080ee80380d3fc5f20258294a69f81391
Bart Sears <bsears@google.com>
2022-07-13 17:00:30 +0000

    Revert "Fixes a bug in the BluetoothDevice#getIdentityAddress API where it"
    This reverts commit 448c2830336794d350b1c7c4f62c3e406b172e32.
    
    Reason for revert: b/238847789
    
    Test: Clean revert of CL
    
    Change-Id: I30941e8b9344e9dbaacaec5ca2c2046d149ed839

--- android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -853,22 +853,19 @@ public class AdapterServiceTest {
 
     @Test
     public void testAddressConsolidation() {
-        // Verify that we return null when the device property for this address does not exist
-        Assert.assertNull(mAdapterService.getIdentityAddress(TEST_BT_ADDR_1));
-
-        // Create device property
+        // Create device properties
         RemoteDevices remoteDevices = mAdapterService.getRemoteDevices();
         remoteDevices.addDeviceProperties(Utils.getBytesFromAddress((TEST_BT_ADDR_1)));
-
-        // Verify that we return null when the identity address is not known
-        Assert.assertNull(mAdapterService.getIdentityAddress(TEST_BT_ADDR_1));
+        String identityAddress = mAdapterService.getIdentityAddress(TEST_BT_ADDR_1);
+        Assert.assertEquals(identityAddress, TEST_BT_ADDR_1);
 
         // Trigger address consolidate callback
         remoteDevices.addressConsolidateCallback(Utils.getBytesFromAddress(TEST_BT_ADDR_1),
                 Utils.getBytesFromAddress(TEST_BT_ADDR_2));
 
         // Verify we can get correct identity address
-        Assert.assertEquals(TEST_BT_ADDR_2, mAdapterService.getIdentityAddress(TEST_BT_ADDR_1));
+        identityAddress = mAdapterService.getIdentityAddress(TEST_BT_ADDR_1);
+        Assert.assertEquals(identityAddress, TEST_BT_ADDR_2);
     }
 
     private static byte[] getMetricsSalt(HashMap<String, HashMap<String, String>> adapterConfig) {
