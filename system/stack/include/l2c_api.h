miyu-xu/packages-modules-Bluetooth

l2cap: Notify upper layer about credit based conn request collision
When Android handles local Credit Based Connection request, the
incoming requests are rejected. With this patch, upper layer will know
about collision and can handle it later on if needed

Bug: 236216322
Test: atest BluetoothInstrumentationTests
Test: PTS
Tag: #stability
Change-Id: Iebaee7ecfa4b1c330fd42e5755d5e43b6a334aab

system/stack/include/l2c_api.h
@@ -287,6 +287,14 @@ typedef void(tL2CA_CREDIT_BASED_CONNECT_IND_CB)(const RawAddress& bdaddr,
                                                 uint16_t psm, uint16_t peer_mtu,
                                                 uint8_t identifier);
 
+/* Collision Indication callback prototype. Used to notify upper layer that
+ * remote devices sent Credit Based Connection Request but it was rejected due
+ * to ongoing local request. Upper layer might want to sent another request when
+ * local request is completed. Parameters are:
+ *              BD Address of remote
+ */
+typedef void(tL2CA_CREDIT_BASED_COLLISION_IND)(const RawAddress& bdaddr);
+
 /* Credit based connection confirmation callback prototype. Parameters are
  *              BD Address of remote
  *              Connected Local CIDs
@@ -328,6 +336,7 @@ typedef struct {
   tL2CA_CREDIT_BASED_CONNECT_CFM_CB* pL2CA_CreditBasedConnectCfm_Cb;
   tL2CA_CREDIT_BASED_RECONFIG_COMPLETED_CB*
       pL2CA_CreditBasedReconfigCompleted_Cb;
+  tL2CA_CREDIT_BASED_COLLISION_IND* pL2CA_CreditBasedCollisionInd_Cb;
 } tL2CAP_APPL_INFO;
 
 /* Define the structure that applications use to create or accept

GrapheneOS/platform_packages_modules_Bluetooth

Merged-In: Iebaee7ecfa4b1c330fd42e5755d5e43b6a334aab
(cherry picked from commit 7fc203dcb6421ccbe9372b950435102208116509)

system/stack/include/l2c_api.h
@@ -287,6 +287,14 @@ typedef void(tL2CA_CREDIT_BASED_CONNECT_IND_CB)(const RawAddress& bdaddr,
                                                 uint16_t psm, uint16_t peer_mtu,
                                                 uint8_t identifier);
 
+/* Collision Indication callback prototype. Used to notify upper layer that
+ * remote devices sent Credit Based Connection Request but it was rejected due
+ * to ongoing local request. Upper layer might want to sent another request when
+ * local request is completed. Parameters are:
+ *              BD Address of remote
+ */
+typedef void(tL2CA_CREDIT_BASED_COLLISION_IND_CB)(const RawAddress& bdaddr);
+
 /* Credit based connection confirmation callback prototype. Parameters are
  *              BD Address of remote
  *              Connected Local CIDs
@@ -328,6 +336,7 @@ typedef struct {
   tL2CA_CREDIT_BASED_CONNECT_CFM_CB* pL2CA_CreditBasedConnectCfm_Cb;
   tL2CA_CREDIT_BASED_RECONFIG_COMPLETED_CB*
       pL2CA_CreditBasedReconfigCompleted_Cb;
+  tL2CA_CREDIT_BASED_COLLISION_IND_CB* pL2CA_CreditBasedCollisionInd_Cb;
 } tL2CAP_APPL_INFO;
 
 /* Define the structure that applications use to create or accept