miyu-xu/packages-modules-Bluetooth

floss: Add client dbus bindings for socket manager
Bug: 233123287
Tag: #floss
Test: Build only
Change-Id: I243db01a30da7afbaac6086e1782ce5799a400b8

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -1,7 +1,10 @@
 //! D-Bus proxy implementations of the APIs.
 
-use bt_topshim::btif::{BtDeviceType, BtPropertyType, BtSspVariant, BtTransport, Uuid128Bit};
+use bt_topshim::btif::{
+    BtDeviceType, BtPropertyType, BtSspVariant, BtStatus, BtTransport, Uuid, Uuid128Bit,
+};
 use bt_topshim::profiles::gatt::GattStatus;
+use bt_topshim::profiles::socket::SocketType;
 
 use btstack::bluetooth::{
     BluetoothDevice, IBluetooth, IBluetoothCallback, IBluetoothConnectionCallback,
@@ -11,6 +14,10 @@ use btstack::bluetooth_gatt::{
     GattWriteRequestStatus, GattWriteType, IBluetoothGatt, IBluetoothGattCallback,
     IScannerCallback, LePhy, ScanFilter, ScanSettings,
 };
+use btstack::socket_manager::{
+    BluetoothServerSocket, BluetoothSocket, CallbackId, IBluetoothSocketManager,
+    IBluetoothSocketManagerCallbacks, SocketId, SocketResult,
+};
 use btstack::RPCProxy;
 
 use btstack::suspend::{ISuspend, ISuspendCallback, SuspendType};
@@ -19,7 +26,9 @@ use btstack::uuid::Profile;
 use dbus::arg::RefArg;
 use dbus::nonblock::SyncConnection;
 
-use dbus_projection::{dbus_generated, impl_dbus_arg_enum, ClientDBusProxy, DisconnectWatcher};
+use dbus_projection::{
+    dbus_generated, impl_dbus_arg_enum, impl_dbus_arg_from_into, ClientDBusProxy, DisconnectWatcher,
+};
 
 use dbus_macros::{
     dbus_method, dbus_propmap, generate_dbus_exporter, generate_dbus_interface_client,
@@ -31,7 +40,7 @@ use manager_service::iface_bluetooth_manager::{
 
 use num_traits::{FromPrimitive, ToPrimitive};
 
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 use std::sync::Arc;
 
 use crate::dbus_arg::{DBusArg, DBusArgError, RefArgToRust};
@@ -43,13 +52,27 @@ fn make_object_path(idx: i32, name: &str) -> dbus::Path {
 impl_dbus_arg_enum!(BtDeviceType);
 impl_dbus_arg_enum!(BtPropertyType);
 impl_dbus_arg_enum!(BtSspVariant);
+impl_dbus_arg_enum!(BtStatus);
 impl_dbus_arg_enum!(BtTransport);
 impl_dbus_arg_enum!(GattStatus);
 impl_dbus_arg_enum!(GattWriteRequestStatus);
 impl_dbus_arg_enum!(GattWriteType);
 impl_dbus_arg_enum!(LePhy);
 impl_dbus_arg_enum!(Profile);
+impl_dbus_arg_enum!(SocketType);
 impl_dbus_arg_enum!(SuspendType);
+impl_dbus_arg_from_into!(Uuid, Vec<u8>);
+
+impl RefArgToRust for Uuid {
+    type RustType = Vec<u8>;
+
+    fn ref_arg_to_rust(
+        arg: &(dyn dbus::arg::RefArg + 'static),
+        name: String,
+    ) -> Result<Self::RustType, Box<dyn std::error::Error>> {
+        <Vec<u8> as RefArgToRust>::ref_arg_to_rust(arg, name)
+    }
+}
 
 // Represents Uuid128Bit as an array in D-Bus.
 impl DBusArg for Uuid128Bit {
@@ -800,6 +823,184 @@ impl IBluetoothGattCallback for IBluetoothGattCallbackDBus {
 
     #[dbus_method("OnServiceChanged")]
     fn on_service_changed(&self, addr: String) {}
+}
+
+#[dbus_propmap(BluetoothServerSocket)]
+pub struct BluetoothServerSocketDBus {
+    id: SocketId,
+    sock_type: SocketType,
+    flags: i32,
+    psm: Option<i32>,
+    channel: Option<i32>,
+    name: Option<String>,
+    uuid: Option<Uuid>,
+}
+
+#[dbus_propmap(BluetoothSocket)]
+pub struct BluetoothSocketDBus {
+    id: SocketId,
+    remote_device: BluetoothDevice,
+    sock_type: SocketType,
+    flags: i32,
+    fd: Option<std::fs::File>,
+    port: i32,
+    uuid: Option<Uuid>,
+    max_rx_size: i32,
+    max_tx_size: i32,
+}
+
+#[dbus_propmap(SocketResult)]
+pub struct SocketResultDBus {
+    status: BtStatus,
+    id: u64,
+}
+
+pub(crate) struct BluetoothSocketManagerDBus {
+    client_proxy: ClientDBusProxy,
+}
+
+#[allow(dead_code)]
+impl BluetoothSocketManagerDBus {
+    pub(crate) fn new(conn: Arc<SyncConnection>, index: i32) -> Self {
+        BluetoothSocketManagerDBus {
+            client_proxy: ClientDBusProxy::new(
+                conn.clone(),
+                String::from("org.chromium.bluetooth"),
+                make_object_path(index, "adapter"),
+                String::from("org.chromium.bluetooth.SocketManager"),
+            ),
+        }
+    }
+}
+
+#[generate_dbus_interface_client]
+impl IBluetoothSocketManager for BluetoothSocketManagerDBus {
+    #[dbus_method("RegisterCallback")]
+    fn register_callback(
+        &mut self,
+        callback: Box<dyn IBluetoothSocketManagerCallbacks + Send>,
+    ) -> CallbackId {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingInsecureL2capChannel")]
+    fn listen_using_insecure_l2cap_channel(&mut self, callback: CallbackId) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingL2capChannel")]
+    fn listen_using_l2cap_channel(&mut self, callback: CallbackId) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingInsecureRfcommWithServiceRecord")]
+    fn listen_using_insecure_rfcomm_with_service_record(
+        &mut self,
+        callback: CallbackId,
+        name: String,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingRfcommWithServiceRecord")]
+    fn listen_using_rfcomm_with_service_record(
+        &mut self,
+        callback: CallbackId,
+        name: String,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateInsecureL2capChannel")]
+    fn create_insecure_l2cap_channel(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        psm: i32,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateL2capChannel")]
+    fn create_l2cap_channel(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        psm: i32,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateInsecureRfcommSocketToServiceRecord")]
+    fn create_insecure_rfcomm_socket_to_service_record(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateRfcommSocketToServiceRecord")]
+    fn create_rfcomm_socket_to_service_record(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("Accept")]
+    fn accept(&mut self, callback: CallbackId, id: SocketId, timeout_ms: Option<u32>) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("Close")]
+    fn close(&mut self, callback: CallbackId, id: SocketId) -> BtStatus {
+        dbus_generated!()
+    }
+}
+
+#[allow(dead_code)]
+struct IBluetoothSocketManagerCallbacksDBus {}
+
+impl RPCProxy for IBluetoothSocketManagerCallbacksDBus {
+    // Placeholder implementations just to satisfy impl RPCProxy requirements.
+    fn register_disconnect(&mut self, _f: Box<dyn Fn(u32) + Send>) -> u32 {
+        0
+    }
+    fn get_object_id(&self) -> String {
+        String::from("")
+    }
+    fn unregister(&mut self, _id: u32) -> bool {
+        false
+    }
+    fn export_for_rpc(self: Box<Self>) {}
+}
+#[generate_dbus_exporter(export_socket_callback_dbus_intf, "org.chromium.bluetooth.SocketCallback")]
+impl IBluetoothSocketManagerCallbacks for IBluetoothSocketManagerCallbacksDBus {
+    #[dbus_method("IncomingSocketReady")]
+    fn incoming_socket_ready(&mut self, socket: BluetoothServerSocket, status: BtStatus) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("IncomingSocketClosed")]
+    fn incoming_socket_closed(&mut self, listener_id: SocketId, reason: BtStatus) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("HandleIncomingConnection")]
+    fn handle_incoming_connection(&mut self, listener_id: SocketId, connection: BluetoothSocket) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("OutgoingConnectionResult")]
+    fn outgoing_connection_result(&mut self, result: BtStatus, socket: Option<BluetoothSocket>) {
+        dbus_generated!()
+    }
 }
 
 pub(crate) struct SuspendDBus {

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -1,7 +1,10 @@
 //! D-Bus proxy implementations of the APIs.
 
-use bt_topshim::btif::{BtDeviceType, BtPropertyType, BtSspVariant, BtTransport, Uuid128Bit};
+use bt_topshim::btif::{
+    BtDeviceType, BtPropertyType, BtSspVariant, BtStatus, BtTransport, Uuid, Uuid128Bit,
+};
 use bt_topshim::profiles::gatt::GattStatus;
+use bt_topshim::profiles::socket::SocketType;
 
 use btstack::bluetooth::{
     BluetoothDevice, IBluetooth, IBluetoothCallback, IBluetoothConnectionCallback,
@@ -11,6 +14,10 @@ use btstack::bluetooth_gatt::{
     GattWriteRequestStatus, GattWriteType, IBluetoothGatt, IBluetoothGattCallback,
     IScannerCallback, LePhy, ScanFilter, ScanSettings,
 };
+use btstack::socket_manager::{
+    BluetoothServerSocket, BluetoothSocket, CallbackId, IBluetoothSocketManager,
+    IBluetoothSocketManagerCallbacks, SocketId, SocketResult,
+};
 use btstack::RPCProxy;
 
 use btstack::suspend::{ISuspend, ISuspendCallback, SuspendType};
@@ -19,7 +26,9 @@ use btstack::uuid::Profile;
 use dbus::arg::RefArg;
 use dbus::nonblock::SyncConnection;
 
-use dbus_projection::{dbus_generated, impl_dbus_arg_enum, ClientDBusProxy, DisconnectWatcher};
+use dbus_projection::{
+    dbus_generated, impl_dbus_arg_enum, impl_dbus_arg_from_into, ClientDBusProxy, DisconnectWatcher,
+};
 
 use dbus_macros::{
     dbus_method, dbus_propmap, generate_dbus_exporter, generate_dbus_interface_client,
@@ -31,7 +40,7 @@ use manager_service::iface_bluetooth_manager::{
 
 use num_traits::{FromPrimitive, ToPrimitive};
 
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 use std::sync::Arc;
 
 use crate::dbus_arg::{DBusArg, DBusArgError, RefArgToRust};
@@ -43,13 +52,27 @@ fn make_object_path(idx: i32, name: &str) -> dbus::Path {
 impl_dbus_arg_enum!(BtDeviceType);
 impl_dbus_arg_enum!(BtPropertyType);
 impl_dbus_arg_enum!(BtSspVariant);
+impl_dbus_arg_enum!(BtStatus);
 impl_dbus_arg_enum!(BtTransport);
 impl_dbus_arg_enum!(GattStatus);
 impl_dbus_arg_enum!(GattWriteRequestStatus);
 impl_dbus_arg_enum!(GattWriteType);
 impl_dbus_arg_enum!(LePhy);
 impl_dbus_arg_enum!(Profile);
+impl_dbus_arg_enum!(SocketType);
 impl_dbus_arg_enum!(SuspendType);
+impl_dbus_arg_from_into!(Uuid, Vec<u8>);
+
+impl RefArgToRust for Uuid {
+    type RustType = Vec<u8>;
+
+    fn ref_arg_to_rust(
+        arg: &(dyn dbus::arg::RefArg + 'static),
+        name: String,
+    ) -> Result<Self::RustType, Box<dyn std::error::Error>> {
+        <Vec<u8> as RefArgToRust>::ref_arg_to_rust(arg, name)
+    }
+}
 
 // Represents Uuid128Bit as an array in D-Bus.
 impl DBusArg for Uuid128Bit {
@@ -844,6 +867,192 @@ impl IBluetoothGattCallback for IBluetoothGattCallbackDBus {
 
     #[dbus_method("OnServiceChanged")]
     fn on_service_changed(&self, addr: String) {}
+}
+
+#[dbus_propmap(BluetoothServerSocket)]
+pub struct BluetoothServerSocketDBus {
+    id: SocketId,
+    sock_type: SocketType,
+    flags: i32,
+    psm: Option<i32>,
+    channel: Option<i32>,
+    name: Option<String>,
+    uuid: Option<Uuid>,
+}
+
+#[dbus_propmap(BluetoothSocket)]
+pub struct BluetoothSocketDBus {
+    id: SocketId,
+    remote_device: BluetoothDevice,
+    sock_type: SocketType,
+    flags: i32,
+    fd: Option<std::fs::File>,
+    port: i32,
+    uuid: Option<Uuid>,
+    max_rx_size: i32,
+    max_tx_size: i32,
+}
+
+#[dbus_propmap(SocketResult)]
+pub struct SocketResultDBus {
+    status: BtStatus,
+    id: u64,
+}
+
+pub(crate) struct BluetoothSocketManagerDBus {
+    client_proxy: ClientDBusProxy,
+}
+
+impl BluetoothSocketManagerDBus {
+    pub(crate) fn _new(conn: Arc<SyncConnection>, index: i32) -> Self {
+        BluetoothSocketManagerDBus {
+            client_proxy: ClientDBusProxy::new(
+                conn.clone(),
+                String::from("org.chromium.bluetooth"),
+                make_object_path(index, "adapter"),
+                String::from("org.chromium.bluetooth.SocketManager"),
+            ),
+        }
+    }
+}
+
+#[generate_dbus_interface_client]
+impl IBluetoothSocketManager for BluetoothSocketManagerDBus {
+    #[dbus_method("RegisterCallback")]
+    fn register_callback(
+        &mut self,
+        callback: Box<dyn IBluetoothSocketManagerCallbacks + Send>,
+    ) -> CallbackId {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingInsecureL2capChannel")]
+    fn listen_using_insecure_l2cap_channel(&mut self, callback: CallbackId) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingL2capChannel")]
+    fn listen_using_l2cap_channel(&mut self, callback: CallbackId) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingInsecureRfcommWithServiceRecord")]
+    fn listen_using_insecure_rfcomm_with_service_record(
+        &mut self,
+        callback: CallbackId,
+        name: String,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("ListenUsingRfcommWithServiceRecord")]
+    fn listen_using_rfcomm_with_service_record(
+        &mut self,
+        callback: CallbackId,
+        name: String,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateInsecureL2capChannel")]
+    fn create_insecure_l2cap_channel(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        psm: i32,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateL2capChannel")]
+    fn create_l2cap_channel(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        psm: i32,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateInsecureRfcommSocketToServiceRecord")]
+    fn create_insecure_rfcomm_socket_to_service_record(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("CreateRfcommSocketToServiceRecord")]
+    fn create_rfcomm_socket_to_service_record(
+        &mut self,
+        callback: CallbackId,
+        device: BluetoothDevice,
+        uuid: Uuid,
+    ) -> SocketResult {
+        dbus_generated!()
+    }
+
+    #[dbus_method("Accept")]
+    fn accept(&mut self, callback: CallbackId, id: SocketId, timeout_ms: Option<u32>) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("Close")]
+    fn close(&mut self, callback: CallbackId, id: SocketId) -> BtStatus {
+        dbus_generated!()
+    }
+}
+
+struct IBluetoothSocketManagerCallbacksDBus {}
+
+impl RPCProxy for IBluetoothSocketManagerCallbacksDBus {
+    // Placeholder implementations just to satisfy impl RPCProxy requirements.
+    fn register_disconnect(&mut self, _f: Box<dyn Fn(u32) + Send>) -> u32 {
+        0
+    }
+    fn get_object_id(&self) -> String {
+        String::from("")
+    }
+    fn unregister(&mut self, _id: u32) -> bool {
+        false
+    }
+    fn export_for_rpc(self: Box<Self>) {}
+}
+
+#[generate_dbus_exporter(export_socket_callback_dbus_intf, "org.chromium.bluetooth.SocketCallback")]
+impl IBluetoothSocketManagerCallbacks for IBluetoothSocketManagerCallbacksDBus {
+    #[dbus_method("OnIncomingSocketReady")]
+    fn on_incoming_socket_ready(&mut self, socket: BluetoothServerSocket, status: BtStatus) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("OnIncomingSocketClosed")]
+    fn on_incoming_socket_closed(&mut self, listener_id: SocketId, reason: BtStatus) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("OnHandleIncomingConnection")]
+    fn on_handle_incoming_connection(
+        &mut self,
+        listener_id: SocketId,
+        connection: BluetoothSocket,
+    ) {
+        dbus_generated!()
+    }
+
+    #[dbus_method("OnOutgoingConnectionResult")]
+    fn on_outgoing_connection_result(
+        &mut self,
+        connecting_id: SocketId,
+        result: BtStatus,
+        socket: Option<BluetoothSocket>,
+    ) {
+        dbus_generated!()
+    }
 }
 
 pub(crate) struct SuspendDBus {