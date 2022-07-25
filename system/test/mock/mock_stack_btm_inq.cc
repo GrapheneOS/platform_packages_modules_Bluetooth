c48ab912e47f864998e38515db19b01f9f800740
Chris Manton <cmanton@google.com>
2022-07-25 17:10:52 +0000

    Revert "Add btm_cb.notify_when_complete_cb when inquiry is cancelled"
    This reverts commit 31a662d2affbc2e60de5a5c7ba140b2e31c49e8e.
    
    Bug: 235293624
    Tag: #refactor
    Test: manual & atest CtsBluetoothTestCases
    Reason for revert: Breaks
    
    Merged-In: I1a9505fabd581201117ec54f711c4120a092afb7
    Merged-In: I60bae5d51b6128489964f7ed32c4f9f2605471a0
    Change-Id: I60bae5d51b6128489964f7ed32c4f9f2605471a0
    (cherry picked from commit b88a389715fd8f8e298b2d4267ef9b46ad401850)
    Merged-In: I60bae5d51b6128489964f7ed32c4f9f2605471a0

--- system/test/mock/mock_stack_btm_inq.cc
+++ system/test/mock/mock_stack_btm_inq.cc
@@ -142,9 +142,6 @@ void BTM_AddEirService(uint32_t* p_eir_uuid, uint16_t uuid16) {
   mock_function_count_map[__func__]++;
 }
 void BTM_CancelInquiry(void) { mock_function_count_map[__func__]++; }
-void BTM_CancelInquiryNotifyWhenComplete(std::function<void()> cb) {
-  mock_function_count_map[__func__]++;
-}
 void BTM_EnableInterlacedInquiryScan() { mock_function_count_map[__func__]++; }
 void BTM_EnableInterlacedPageScan() { mock_function_count_map[__func__]++; }
 void btm_clr_inq_db(const RawAddress* p_bda) {
