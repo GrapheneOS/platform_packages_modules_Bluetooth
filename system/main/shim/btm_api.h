7c8d319582f8c90e53478a9187692c588c667d6a
Bill Chang <billdir@google.com>
2022-08-09 04:32:06 +0000

    Revert "Reconfigure Address policy on last bond removed"
    This reverts commit a4675b98df5af25ace2f323610089709c78f884e.
    
    Reason for revert: broken test b/241860519
    
    Test: SystemAppCheck#system_app_native_crash
    
    Change-Id: I66359f65ad56b0161434015e2c29fb9f710789e6

--- system/main/shim/btm_api.h
+++ system/main/shim/btm_api.h
@@ -1902,15 +1902,6 @@ tBTM_STATUS BTM_SetDefaultEventMask(void);
  *******************************************************************************/
 tBTM_STATUS BTM_SetEventFilterInquiryResultAllDevices(void);
 
-/*******************************************************************************
- *
- * Function         BTM_BleResetId
- *
- * Description      Resets the local BLE keys
- *
- *******************************************************************************/
-tBTM_STATUS BTM_BleResetId(void);
-
 /**
  * Send remote name request to GD shim Name module
  */
--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -32,7 +32,6 @@
 #include "main/shim/l2c_api.h"
 #include "main/shim/shim.h"
 #include "osi/include/allocator.h"
-#include "osi/include/properties.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/security_device_record.h"
@@ -56,11 +55,6 @@ extern bool btm_ble_init_pseudo_addr(tBTM_SEC_DEV_REC* p_dev_rec,
                                      const RawAddress& new_pseudo_addr);
 extern void gatt_notify_phy_updated(tGATT_STATUS status, uint16_t handle,
                                     uint8_t tx_phy, uint8_t rx_phy);
-
-
-#ifndef PROPERTY_BLE_PRIVACY_ENABLED
-#define PROPERTY_BLE_PRIVACY_ENABLED "bluetooth.core.gap.le.privacy.enabled"
-#endif
 
 /******************************************************************************/
 /* External Function to be called by other modules                            */
@@ -88,7 +82,7 @@ void BTM_SecAddBleDevice(const RawAddress& bd_addr, tBT_DEVICE_TYPE dev_type,
     p_dev_rec->conn_params.peripheral_latency = BTM_BLE_CONN_PARAM_UNDEF;
 
     LOG_DEBUG("Device added, handle=0x%x, p_dev_rec=%p, bd_addr=%s",
-              p_dev_rec->ble_hci_handle, p_dev_rec, PRIVATE_ADDRESS(bd_addr));
+              p_dev_rec->ble_hci_handle, p_dev_rec, bd_addr.ToString().c_str());
   }
 
   memset(p_dev_rec->sec_bd_name, 0, sizeof(tBTM_BD_NAME));
@@ -2064,7 +2058,6 @@ static void btm_ble_reset_id_impl(const Octet16& rand1, const Octet16& rand2) {
   /* proceed generate ER */
   btm_cb.devcb.ble_encryption_key_value = rand2;
   btm_notify_new_key(BTM_BLE_KEY_TYPE_ER);
-  BTM_BleConfigPrivacy(btm_cb.ble_ctr_cb.privacy_mode != BTM_PRIVACY_NONE);
 }
 
 struct reset_id_data {
