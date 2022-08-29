miyu-xu/packages-modules-Bluetooth

Metric: Log extended smp pairing events
Include transport and complete information in extended enums.

Tag: #refactor
Bug: 236677941
Test: atest gd/cert/run
Change-Id: I6e527d8ad8b9dfa91b8abf67a9faa0fb11a513fb

system/gd/os/android/metrics.cc
@@ -232,7 +232,7 @@ void LogMetricReadTxPowerLevelResult(
 }
 
 void LogMetricSmpPairingEvent(
-    const Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {
+    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {
   int metric_id = 0;
   if (!address.IsEmpty()) {
     metric_id = MetricIdManager::GetInstance().AllocateId(address);

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/os/android/metrics.cc
@@ -232,7 +232,7 @@ void LogMetricReadTxPowerLevelResult(
 }
 
 void LogMetricSmpPairingEvent(
-    const Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {
+    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint16_t smp_fail_reason) {
   int metric_id = 0;
   if (!address.IsEmpty()) {
     metric_id = MetricIdManager::GetInstance().AllocateId(address);