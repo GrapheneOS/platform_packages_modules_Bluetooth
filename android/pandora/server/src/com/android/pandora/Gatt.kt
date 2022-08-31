miyu-xu/packages-modules-Bluetooth

[Pandora] Implement HID-over-GATT tests
All tests implemented! The only incomplete MMI is _mmi_1, which needs
the UIAutomation framework (as we need to check whether the mouse
moves).

Test: atest pts-bot:HOGP/RH
Bug: 239986609
Change-Id: I53c5d79ca78b3a2c93594f3d8cdcd0fb9b26735e

android/pandora/server/src/com/android/pandora/Gatt.kt
@@ -213,6 +213,33 @@ class Gatt(private val context: Context) : GATTImplBase() {
     }
   }
 
+  override fun writeAttribute(
+    request: WriteAttributeRequest,
+    responseObserver: StreamObserver<WriteAttributeResponse>
+  ) {
+    grpcUnary(mScope, responseObserver) {
+      val gattInstance = GattInstance.get(request.connection.cookie.toByteArray().decodeToString())
+      if (!gattInstance.servicesDiscovered() && !gattInstance.mGatt.discoverServices()) {
+        Log.e(TAG, "Error on discovering services for $gattInstance")
+        throw Status.UNKNOWN.asException()
+      } else {
+        gattInstance.waitForDiscoveryEnd()
+      }
+      for (service in gattInstance.mGatt.services.orEmpty()) {
+        for (characteristic in service.characteristics) {
+          for (descriptor in characteristic.descriptors) {
+            if (descriptor.instanceId == request.handle) {
+              Log.e(TAG, "descriptor.ID = ${descriptor.instanceId}")
+              gattInstance.mGatt.writeDescriptor(descriptor, request.value.toByteArray())
+              return@grpcUnary WriteAttributeResponse.getDefaultInstance()
+            }
+          }
+        }
+      }
+      error("could not find corresponding descriptor for ${request.handle}")
+    }
+  }
+
   /**
    * Discovers services, then returns characteristic with given handle.
    * BluetoothGatt API is package-private so we have to redefine it here.

GrapheneOS/platform_packages_modules_Bluetooth

android/pandora/server/src/com/android/pandora/Gatt.kt
@@ -99,6 +99,33 @@ class Gatt(private val context: Context) : GATTImplBase() {
     }
   }
 
+  override fun writeCharacteristicDescriptorFromHandle(
+    request: WriteCharacteristicDescriptorFromHandleRequest,
+    responseObserver: StreamObserver<WriteCharacteristicDescriptorFromHandleResponse>
+  ) {
+    grpcUnary(mScope, responseObserver) {
+      val gattInstance = GattInstance.get(request.connection.cookie.toByteArray().decodeToString())
+      if (!gattInstance.servicesDiscovered() && !gattInstance.mGatt.discoverServices()) {
+        Log.e(TAG, "Error on discovering services for $gattInstance")
+        throw Status.UNKNOWN.asException()
+      } else {
+        gattInstance.waitForDiscoveryEnd()
+      }
+      for (service in gattInstance.mGatt.services.orEmpty()) {
+        for (characteristic in service.characteristics) {
+          for (descriptor in characteristic.descriptors) {
+            if (descriptor.instanceId == request.handle) {
+              Log.e(TAG, "descriptor.ID = ${descriptor.instanceId}")
+              gattInstance.mGatt.writeDescriptor(descriptor, request.value.toByteArray())
+              return@grpcUnary WriteCharacteristicDescriptorFromHandleResponse.getDefaultInstance()
+            }
+          }
+        }
+      }
+      error("could not find corresponding descriptor for ${request.handle}")
+    }
+  }
+
   override fun discoverServiceByUuid(
     request: DiscoverServiceByUuidRequest,
     responseObserver: StreamObserver<DiscoverServicesResponse>