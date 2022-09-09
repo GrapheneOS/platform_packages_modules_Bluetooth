miyu-xu/packages-modules-Bluetooth

Topshim API: Add SecurityTest framework.
Add RemoveBond API to Topshim proto, AdapterClient and Facade Service.

Bug: 234756905
Test: system/gd/cert/run --clean --topshim SecurityTest
Tag: #stability
Change-Id: Ibfbb4f4da1e3dd5374f87f2c1cc1ba46b6e604c0

system/blueberry/tests/topshim/lib/adapter_client.py
@@ -106,6 +106,9 @@ class AdapterClient():
     async def set_event_filter_inquiry_result_all_devices(self):
         await self.__adapter_stub.SetEventFilterInquiryResultAllDevices(empty_proto.Empty())
 
+    async def remove_bond(self, adress):
+        await self.__adapter_stub.RemoveBond(facade_pb2.RemoveBondRequest(address=address))
+
 
 class A2dpAutomationHelper():
     """Invoke gRPC on topshim for A2DP testing"""

GrapheneOS/platform_packages_modules_Bluetooth

system/blueberry/tests/topshim/lib/adapter_client.py
@@ -111,6 +111,9 @@ class AdapterClient():
     async def allow_wake_by_hid(self):
         await self.__adapter_stub.AllowWakeByHid(empty_proto.Empty())
 
+    async def remove_bond(self, address):
+        await self.__adapter_stub.RemoveBond(facade_pb2.RemoveBondRequest(address=address))
+
 
 class A2dpAutomationHelper():
     """Invoke gRPC on topshim for A2DP testing"""