miyu-xu/packages-modules-Bluetooth

leaudio: Extend storage for leaudio device
With this patch we store PACs and ASE information for bonded devices.
This will improve stream creation time after reconnection (BT OFF/ON)
and allows to configure complete set even not all devices are connected.
Following entiers will be added to bt_config.conf e.g:

SinkSupportedContextType = 4095
SourceSupportedContextType = 3
SinkPacsBin = [binary]
SourcePacsBin = [binary]
AsesBin = [binary]
SinkAudioLocation = 2
SourceAudioLocation = 2

Bug: 225020659
Test: atest BluetoothInstrumentationTests
Test: atest bluetooth_le_audio_client_test
Test: Manual audio stream creation after Bluetooth Off/On
Tag: #feature

Change-Id: Ic05ca8179d3913eefdc10074ffff6e4ad793e12b

system/bta/test/common/btif_storage_mock.cc
@@ -33,6 +33,37 @@ void btif_storage_set_leaudio_autoconnect(RawAddress const& addr,
   btif_storage_interface->AddLeaudioAutoconnect(addr, autoconnect);
 }
 
+void btif_storage_leaudio_update_pacs_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeaudioUpdatePacs(addr);
+}
+
+void btif_storage_leaudio_update_ase_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeaudioUpdateAses(addr);
+}
+
+void btif_storage_leaudio_update_handles_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeaudioUpdateHandles(addr);
+}
+
+void btif_storage_set_leaudio_audio_location(const RawAddress& addr,
+                                             uint32_t sink_location,
+                                             uint32_t source_location) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->SetLeaudioLocations(addr, sink_location,
+                                              source_location);
+}
+
+void btif_storage_set_leaudio_supported_context_types(
+    const RawAddress& addr, uint16_t sink_supported_context_type,
+    uint16_t source_supported_context_type) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->SetLeaudioContexts(addr, sink_supported_context_type,
+                                             source_supported_context_type);
+}
+
 void btif_storage_remove_leaudio(RawAddress const& addr) {
   LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
   btif_storage_interface->RemoveLeaudio(addr);

system/bta/test/common/btif_storage_mock.h
@@ -27,6 +27,14 @@ class BtifStorageInterface {
  public:
   virtual void AddLeaudioAutoconnect(RawAddress const& addr,
                                      bool autoconnect) = 0;
+  virtual void LeaudioUpdatePacs(RawAddress const& addr) = 0;
+  virtual void LeaudioUpdateAses(RawAddress const& addr) = 0;
+  virtual void LeaudioUpdateHandles(RawAddress const& addr) = 0;
+  virtual void SetLeaudioLocations(RawAddress const& addr,
+                                   uint32_t sink_location,
+                                   uint32_t source_location) = 0;
+  virtual void SetLeaudioContexts(RawAddress const& addr, uint16_t sink_context,
+                                  uint16_t source_context) = 0;
   virtual void RemoveLeaudio(RawAddress const& addr) = 0;
   virtual void AddLeaudioHasDevice(const RawAddress& address,
                                    std::vector<uint8_t> presets_bin,
@@ -50,6 +58,18 @@ class MockBtifStorageInterface : public BtifStorageInterface {
  public:
   MOCK_METHOD((void), AddLeaudioAutoconnect,
               (RawAddress const& addr, bool autoconnect), (override));
+  MOCK_METHOD((void), LeaudioUpdatePacs, (RawAddress const& addr), (override));
+  MOCK_METHOD((void), LeaudioUpdateAses, (RawAddress const& addr), (override));
+  MOCK_METHOD((void), LeaudioUpdateHandles, (RawAddress const& addr),
+              (override));
+  MOCK_METHOD((void), SetLeaudioLocations,
+              (RawAddress const& addr, uint32_t sink_location,
+               uint32_t source_location),
+              (override));
+  MOCK_METHOD((void), SetLeaudioContexts,
+              (RawAddress const& addr, uint16_t sink_context,
+               uint16_t source_context),
+              (override));
   MOCK_METHOD((void), RemoveLeaudio, (RawAddress const& addr), (override));
   MOCK_METHOD((void), AddLeaudioHasDevice,
               (const RawAddress& address, std::vector<uint8_t> presets_bin,

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 242645864
Test: Manual audio stream creation
Merged-In: Ic05ca8179d3913eefdc10074ffff6e4ad793e12b
(cherry picked from commit d3ceab9998a6994a420b6455e17770efa531c4f3)

system/bta/test/common/btif_storage_mock.cc
@@ -33,6 +33,37 @@ void btif_storage_set_leaudio_autoconnect(RawAddress const& addr,
   btif_storage_interface->AddLeaudioAutoconnect(addr, autoconnect);
 }
 
+void btif_storage_leaudio_update_pacs_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeAudioUpdatePacs(addr);
+}
+
+void btif_storage_leaudio_update_ase_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeAudioUpdateAses(addr);
+}
+
+void btif_storage_leaudio_update_handles_bin(const RawAddress& addr) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->LeAudioUpdateHandles(addr);
+}
+
+void btif_storage_set_leaudio_audio_location(const RawAddress& addr,
+                                             uint32_t sink_location,
+                                             uint32_t source_location) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->SetLeAudioLocations(addr, sink_location,
+                                              source_location);
+}
+
+void btif_storage_set_leaudio_supported_context_types(
+    const RawAddress& addr, uint16_t sink_supported_context_type,
+    uint16_t source_supported_context_type) {
+  LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
+  btif_storage_interface->SetLeAudioContexts(addr, sink_supported_context_type,
+                                             source_supported_context_type);
+}
+
 void btif_storage_remove_leaudio(RawAddress const& addr) {
   LOG_ASSERT(btif_storage_interface) << "Mock storage module not set!";
   btif_storage_interface->RemoveLeaudio(addr);

system/bta/test/common/btif_storage_mock.h
@@ -27,6 +27,14 @@ class BtifStorageInterface {
  public:
   virtual void AddLeaudioAutoconnect(RawAddress const& addr,
                                      bool autoconnect) = 0;
+  virtual void LeAudioUpdatePacs(RawAddress const& addr) = 0;
+  virtual void LeAudioUpdateAses(RawAddress const& addr) = 0;
+  virtual void LeAudioUpdateHandles(RawAddress const& addr) = 0;
+  virtual void SetLeAudioLocations(RawAddress const& addr,
+                                   uint32_t sink_location,
+                                   uint32_t source_location) = 0;
+  virtual void SetLeAudioContexts(RawAddress const& addr, uint16_t sink_context,
+                                  uint16_t source_context) = 0;
   virtual void RemoveLeaudio(RawAddress const& addr) = 0;
   virtual void AddLeaudioHasDevice(const RawAddress& address,
                                    std::vector<uint8_t> presets_bin,
@@ -50,6 +58,18 @@ class MockBtifStorageInterface : public BtifStorageInterface {
  public:
   MOCK_METHOD((void), AddLeaudioAutoconnect,
               (RawAddress const& addr, bool autoconnect), (override));
+  MOCK_METHOD((void), LeAudioUpdatePacs, (RawAddress const& addr), (override));
+  MOCK_METHOD((void), LeAudioUpdateAses, (RawAddress const& addr), (override));
+  MOCK_METHOD((void), LeAudioUpdateHandles, (RawAddress const& addr),
+              (override));
+  MOCK_METHOD((void), SetLeAudioLocations,
+              (RawAddress const& addr, uint32_t sink_location,
+               uint32_t source_location),
+              (override));
+  MOCK_METHOD((void), SetLeAudioContexts,
+              (RawAddress const& addr, uint16_t sink_context,
+               uint16_t source_context),
+              (override));
   MOCK_METHOD((void), RemoveLeaudio, (RawAddress const& addr), (override));
   MOCK_METHOD((void), AddLeaudioHasDevice,
               (const RawAddress& address, std::vector<uint8_t> presets_bin,