66b626e15c3ca2114e5b8da9b8ca9018726be562
William Escande <wescande@google.com>
2022-10-05 23:54:01 -0700

    GD: use one source as init_flag
    Add ffi for needed method and remove duplicate parsing in c++
    
    Test: cargo test init_flags
    Bug: 251328572
    Tag: #refactor
    Change-Id: Id5f60e7ac2834e71783787b4c2af70c9581f5d40

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -4,16 +4,19 @@ mod ffi {
         fn load(flags: Vec<String>);
         fn set_all_for_testing();
 
-        fn sdp_serialization_is_enabled() -> bool;
-        fn gd_core_is_enabled() -> bool;
-        fn gd_security_is_enabled() -> bool;
-        fn gd_l2cap_is_enabled() -> bool;
+        fn btaa_hci_is_enabled() -> bool;
         fn gatt_robust_caching_client_is_enabled() -> bool;
         fn gatt_robust_caching_server_is_enabled() -> bool;
-        fn btaa_hci_is_enabled() -> bool;
-        fn gd_rust_is_enabled() -> bool;
+        fn gd_core_is_enabled() -> bool;
+        fn gd_l2cap_is_enabled() -> bool;
         fn gd_link_policy_is_enabled() -> bool;
+        fn gd_rust_is_enabled() -> bool;
+        fn gd_security_is_enabled() -> bool;
+        fn get_hci_adapter() -> i32;
         fn irk_rotation_is_enabled() -> bool;
+        fn is_debug_logging_enabled_for_tag(tag: &str) -> bool;
+        fn logging_debug_enabled_for_all_is_enabled() -> bool;
+        fn sdp_serialization_is_enabled() -> bool;
     }
 }
 
