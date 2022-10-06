90b2517650ffd113711a404ed2b4fec694d035fd
Jakub Tyszkowski <tyszkowski@google.com>
2022-10-06 12:50:05 +0000

    vc: Send control point request only if device is ready
    When trying to do control point operations, being connected
    is not enough as we also need to know the volume change counter.
    
    If a group API is used to set volume bofore the individual
    devices are reported as connected, we should not set it
    as we dont know the volume change counter yet. The group volume
    which could not be applied because of this, will be applied from
    the cache once we report the device as connected.
    
    Test: atest --host bluetooth_vc_test --no-bazel-mode
    Bug: 248475583
    Tag: #feature
    Change-Id: I27b462f7f55122cc7c38fb18908d52e86ea4b5d8

--- system/bta/vc/devices.h
+++ system/bta/vc/devices.h
@@ -137,6 +137,7 @@ class VolumeControlDevice {
   void EnqueueRemainingRequests(tGATT_IF gatt_if, GATT_READ_OP_CB chrc_read_cb,
                                 GATT_WRITE_OP_CB cccd_write_cb);
   bool VerifyReady(uint16_t handle);
+  bool IsReady() { return device_ready; }
 
  private:
   /*
