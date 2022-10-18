miyu-xu/packages-modules-Bluetooth

Floss: Implement basic bluetoothAdmin interface
Implement proto type of bluetoothAdmin and implement some basic
functions.

Bug: b:239470589
Test: run build.py --target test and test it along with the top
of this chain
Tag: #floss

Change-Id: I74a880cbe59c96308d1e5fa69c911dd0c4b6d73f

system/gd/rust/linux/service/src/main.rs
@@ -13,6 +13,7 @@ use syslog::{BasicLogger, Facility, Formatter3164};
 use bt_topshim::{btif::get_btinterface, topstack};
 use btstack::{
     bluetooth::{get_bt_dispatcher, Bluetooth, IBluetooth},
+    bluetooth_admin::BluetoothAdmin,
     bluetooth_gatt::BluetoothGatt,
     bluetooth_media::BluetoothMedia,
     socket_manager::BluetoothSocketManager,
@@ -102,6 +103,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Arc::new(Mutex::new(Box::new(BluetoothGatt::new(intf.clone(), tx.clone()))));
     let bluetooth_media =
         Arc::new(Mutex::new(Box::new(BluetoothMedia::new(tx.clone(), intf.clone()))));
+    let bluetooth_admin = Arc::new(Mutex::new(Box::new(BluetoothAdmin::new())));
     let bluetooth = Arc::new(Mutex::new(Box::new(Bluetooth::new(
         tx.clone(),
         intf.clone(),
@@ -237,6 +239,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             intf.lock().unwrap().initialize(get_bt_dispatcher(tx.clone()), init_flags);
 
             bluetooth_media.lock().unwrap().set_adapter(bluetooth.clone());
+            bluetooth_admin.lock().unwrap().set_adapter(bluetooth.clone());
 
             let mut bluetooth = bluetooth.lock().unwrap();
             bluetooth.init_profiles();

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 239470589

system/gd/rust/linux/service/src/main.rs
@@ -21,6 +21,7 @@ use btstack::{
     battery_provider_manager::BatteryProviderManager,
     battery_service::BatteryService,
     bluetooth::{get_bt_dispatcher, Bluetooth, IBluetooth},
+    bluetooth_admin::BluetoothAdmin,
     bluetooth_gatt::BluetoothGatt,
     bluetooth_media::BluetoothMedia,
     socket_manager::BluetoothSocketManager,
@@ -125,6 +126,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         battery_provider_manager.clone(),
         tx.clone(),
     ))));
+    let bluetooth_admin = Arc::new(Mutex::new(Box::new(BluetoothAdmin::new())));
     let bluetooth = Arc::new(Mutex::new(Box::new(Bluetooth::new(
         tx.clone(),
         intf.clone(),
@@ -288,6 +290,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             let adapter = bluetooth.clone();
             bluetooth_media.lock().unwrap().set_adapter(adapter.clone());
+            bluetooth_admin.lock().unwrap().set_adapter(adapter.clone());
 
             let mut bluetooth = bluetooth.lock().unwrap();
             bluetooth.init_profiles();