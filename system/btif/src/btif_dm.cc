miyu-xu/packages-modules-Bluetooth

floss: Send event on connection failure
This can be used to tell the upper layers that connection fails,
therefore they can provide better feedback to the user. Additionally
floss will also use it for metrics.

Bug: 240782154
Tag: #floss
Test: Verify btadapter print warn "Connection to <address> failed"
      when connection failed.
Change-Id: Id2ec0d71bf5392fdc8314349296d71a6af1e76ba

system/btif/src/btif_dm.cc
@@ -1822,11 +1822,19 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
           (int)p_data->link_up.transport_link_type, HCI_SUCCESS);
       break;
 
+    case BTA_DM_LINK_UP_FAILED_EVT:
+      invoke_acl_state_changed_cb(
+          BT_STATUS_FAIL, p_data->link_up_failed.bd_addr,
+          BT_ACL_STATE_CONNECTED, p_data->link_up_failed.transport_link_type,
+          p_data->link_up_failed.status);
+      break;
+
     case BTA_DM_LINK_DOWN_EVT:
       bd_addr = p_data->link_down.bd_addr;
       btm_set_bond_type_dev(p_data->link_down.bd_addr,
                             tBTM_SEC_DEV_REC::BOND_TYPE_UNKNOWN);
       btif_av_acl_disconnected(bd_addr);
+
       invoke_acl_state_changed_cb(
           BT_STATUS_SUCCESS, bd_addr, BT_ACL_STATE_DISCONNECTED,
           (int)p_data->link_down.transport_link_type,

GrapheneOS/platform_packages_modules_Bluetooth

system/btif/src/btif_dm.cc
@@ -1828,11 +1828,19 @@ static void btif_dm_upstreams_evt(uint16_t event, char* p_param) {
           (int)p_data->link_up.transport_link_type, HCI_SUCCESS);
       break;
 
+    case BTA_DM_LINK_UP_FAILED_EVT:
+      invoke_acl_state_changed_cb(
+          BT_STATUS_FAIL, p_data->link_up_failed.bd_addr,
+          BT_ACL_STATE_DISCONNECTED, p_data->link_up_failed.transport_link_type,
+          p_data->link_up_failed.status);
+      break;
+
     case BTA_DM_LINK_DOWN_EVT:
       bd_addr = p_data->link_down.bd_addr;
       btm_set_bond_type_dev(p_data->link_down.bd_addr,
                             tBTM_SEC_DEV_REC::BOND_TYPE_UNKNOWN);
       btif_av_acl_disconnected(bd_addr);
+
       invoke_acl_state_changed_cb(
           BT_STATUS_SUCCESS, bd_addr, BT_ACL_STATE_DISCONNECTED,
           (int)p_data->link_down.transport_link_type,