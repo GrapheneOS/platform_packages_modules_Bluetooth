miyu-xu/packages-modules-Bluetooth

RootCanal: Move link key notification to pairing
* Move link_key_notification to pairing because they are not a part of
  pairing and pairing info is required to determin key type later.
* Properly return comb key in legacy pairing

Tag: #refactor
Test: atest liblmp_tests --host
Bug: 235777894
Change-Id: Iaee68ebe5d1525072e99bc1a7a55cfe5865d3a9b

tools/rootcanal/lmp/src/procedure/secure_simple_pairing.rs
@@ -6,7 +6,7 @@ use num_traits::{FromPrimitive, ToPrimitive};
 
 use crate::either::Either;
 use crate::packets::{hci, lmp};
-use crate::procedure::Context;
+use crate::procedure::{authentication, Context};
 
 use crate::num_hci_command_packets;
 
@@ -458,6 +458,22 @@ pub async fn initiate(ctx: &impl Context) -> Result<(), ()> {
         .build(),
     );
 
+    // Link Key Calculation
+    let link_key = [0; 16];
+    let auth_result = authentication::send_challenge(ctx, 0, link_key).await;
+    authentication::receive_challenge(ctx, link_key).await;
+
+    if auth_result.is_ok() {
+        ctx.send_hci_event(
+            hci::LinkKeyNotificationBuilder {
+                bd_addr: ctx.peer_address(),
+                key_type: hci::KeyType::AuthenticatedP192,
+                link_key,
+            }
+            .build(),
+        );
+    }
+
     Ok(())
 }
 
@@ -616,6 +632,22 @@ pub async fn respond(ctx: &impl Context, request: lmp::IoCapabilityReqPacket) ->
         .build(),
     );
 
+    // Link Key Calculation
+    let link_key = [0; 16];
+    authentication::receive_challenge(ctx, link_key).await;
+    let auth_result = authentication::send_challenge(ctx, 0, link_key).await;
+
+    if auth_result.is_ok() {
+        ctx.send_hci_event(
+            hci::LinkKeyNotificationBuilder {
+                bd_addr: ctx.peer_address(),
+                key_type: hci::KeyType::AuthenticatedP192,
+                link_key,
+            }
+            .build(),
+        );
+    }
+
     Ok(())
 }
 
@@ -639,7 +671,7 @@ mod tests {
             assert!(size < limit)
         }
 
-        assert_max_size(procedure, 250);
+        assert_max_size(procedure, 300);
     }
 
     #[test]

GrapheneOS/platform_packages_modules_Bluetooth

  auth and pairing info is required to determine key type later.
Bug: 245578454
Ignore-AOSP-First: cherry-pick from AOSP
Merged-In: Iaee68ebe5d1525072e99bc1a7a55cfe5865d3a9b
(cherry picked from commit a2c262dd2db5969558f5f8dcb1e214c6d29107f2)

tools/rootcanal/lmp/src/procedure/secure_simple_pairing.rs
@@ -6,7 +6,7 @@ use num_traits::{FromPrimitive, ToPrimitive};
 
 use crate::either::Either;
 use crate::packets::{hci, lmp};
-use crate::procedure::Context;
+use crate::procedure::{authentication, Context};
 
 use crate::num_hci_command_packets;
 
@@ -458,6 +458,23 @@ pub async fn initiate(ctx: &impl Context) -> Result<(), ()> {
         .build(),
     );
 
+    // Link Key Calculation
+    let link_key = [0; 16];
+    let auth_result = authentication::send_challenge(ctx, 0, link_key).await;
+    authentication::receive_challenge(ctx, link_key).await;
+
+    if auth_result.is_err() {
+        return Err(());
+    }
+    ctx.send_hci_event(
+        hci::LinkKeyNotificationBuilder {
+            bd_addr: ctx.peer_address(),
+            key_type: hci::KeyType::AuthenticatedP192,
+            link_key,
+        }
+        .build(),
+    );
+
     Ok(())
 }
 
@@ -616,6 +633,23 @@ pub async fn respond(ctx: &impl Context, request: lmp::IoCapabilityReqPacket) ->
         .build(),
     );
 
+    // Link Key Calculation
+    let link_key = [0; 16];
+    authentication::receive_challenge(ctx, link_key).await;
+    let auth_result = authentication::send_challenge(ctx, 0, link_key).await;
+
+    if auth_result.is_err() {
+        return Err(());
+    }
+    ctx.send_hci_event(
+        hci::LinkKeyNotificationBuilder {
+            bd_addr: ctx.peer_address(),
+            key_type: hci::KeyType::AuthenticatedP192,
+            link_key,
+        }
+        .build(),
+    );
+
     Ok(())
 }
 
@@ -639,7 +673,7 @@ mod tests {
             assert!(size < limit)
         }
 
-        assert_max_size(procedure, 250);
+        assert_max_size(procedure, 300);
     }
 
     #[test]