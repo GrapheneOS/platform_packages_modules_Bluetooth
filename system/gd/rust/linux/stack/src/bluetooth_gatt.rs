30bacb0a33516f92cd9e2fa8840fe8cea3825729
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2022-10-28 15:09:08 -0700

    floss: Fix disconnect order and gatt cache
    Disconnect was removing the client before trying to access that client
    id to send callbacks (hence we were never seeing gatt disconnect
    callbacks). Also updates the gatt cache directory to one that is
    accessible on ChromeOS/Linux.
    
    Bug: 247605554
    Tag: #floss
    Test: Manual test on ChromeOS
    Change-Id: I932e2ae623b6ef572d18b7af9885799a3ec44810

--- system/gd/rust/linux/stack/src/bluetooth_gatt.rs
+++ system/gd/rust/linux/stack/src/bluetooth_gatt.rs
@@ -1658,22 +1658,17 @@ impl BtifGattClientCallbacks for BluetoothGatt {
         client_id: i32,
         addr: RawAddress,
     ) {
-        self.context_map.remove_connection(client_id, conn_id);
         let client = self.context_map.get_by_client_id(client_id);
         if let Some(c) = client {
             let cbid = c.cbid;
             self.context_map.get_callback_from_callback_id(cbid).and_then(
                 |cb: &mut GattClientCallback| {
-                    cb.on_client_connection_state(
-                        status,
-                        client_id,
-                        status == GattStatus::Success,
-                        addr.to_string(),
-                    );
+                    cb.on_client_connection_state(status, client_id, false, addr.to_string());
                     Some(())
                 },
             );
         }
+        self.context_map.remove_connection(client_id, conn_id);
     }
 
     fn search_complete_cb(&mut self, conn_id: i32, _status: GattStatus) {
