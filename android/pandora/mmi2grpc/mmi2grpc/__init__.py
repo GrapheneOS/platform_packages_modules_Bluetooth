b6c6d7ea96c722480d44f7f0976aaa7892449ad0
Rahul Arya <aryarahul@google.com>
2022-11-08 01:43:09 +0000

    [Pandora] First HFP tests involving calls
    Implemented the basic MMIs, and set the feature bits for SCO/eSCO
    appropriately. More CLs will follow, these are just the first few.
    
    Bug: 245578454
    Test: atest pts-bot:HFP
    Ignore-AOSP-First: Cherry-picked from AOSP
    Merged-In: I70826afe16f504f7e0ea5f095e01e6cde38dd79f
    Change-Id: I70826afe16f504f7e0ea5f095e01e6cde38dd79f

--- android/pandora/mmi2grpc/mmi2grpc/__init__.py
+++ android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -63,6 +63,7 @@ class IUT:
         self.pandora_server_port = int(args[0]) if len(args) > 0 else PANDORA_SERVER_PORT
         self.rootcanal_control_port = int(args[1]) if len(args) > 1 else ROOTCANAL_CONTROL_PORT
         self.modem_simulator_port = int(args[2]) if len(args) > 2 else MODEM_SIMULATOR_PORT
+
         self.test = test
         self.rootcanal = None
         self.modem = None
@@ -89,7 +90,7 @@ class IUT:
 
         # Note: we don't keep a single gRPC channel instance in the IUT class
         # because reset is allowed to close the gRPC server.
-        with grpc.insecure_channel(f'localhost:{self.pandora_server_port}') as channel:
+        with grpc.insecure_channel(f"localhost:{self.pandora_server_port}") as channel:
             self._retry(Host(channel).HardReset)(wait_for_ready=True)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
@@ -123,7 +124,7 @@ class IUT:
                     if tries >= MAX_RETRIES:
                         raise
                     else:
-                        print(f'Retry {func.__name__}: {tries}/{MAX_RETRIES}')
+                        print(f"Retry {func.__name__}: {tries}/{MAX_RETRIES}")
                         time.sleep(1)
 
         return wrapper
@@ -137,7 +138,7 @@ class IUT:
         mut_address = None
 
         def read_local_address():
-            with grpc.insecure_channel(f'localhost:{self.pandora_server_port}') as channel:
+            with grpc.insecure_channel(f"localhost:{self.pandora_server_port}") as channel:
                 nonlocal mut_address
                 mut_address = self._retry(Host(channel).ReadLocalAddress)(wait_for_ready=True).address
 
@@ -150,8 +151,16 @@ class IUT:
         else:
             return mut_address
 
-    def interact(self, pts_address: bytes, profile: str, test: str, interaction: str, description: str, style: str,
-                 **kwargs) -> str:
+    def interact(
+            self,
+            pts_address: bytes,
+            profile: str,
+            test: str,
+            interaction: str,
+            description: str,
+            style: str,
+            **kwargs,
+    ) -> str:
         """Routes MMI calls to corresponding profile proxy.
 
         Args:
@@ -162,70 +171,78 @@ class IUT:
             description: MMI description.
             style: MMI popup style, unused for now.
         """
-        print(f'{profile} mmi: {interaction}', file=sys.stderr)
+        print(f"{profile} mmi: {interaction}", file=sys.stderr)
 
         # Handles A2DP and AVDTP MMIs.
-        if profile in ('A2DP', 'AVDTP'):
+        if profile in ("A2DP", "AVDTP"):
             if not self._a2dp:
-                self._a2dp = A2DPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._a2dp = A2DPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._a2dp.interact(test, interaction, description, pts_address)
         # Handles AVRCP and AVCTP MMIs.
-        if profile in ('AVRCP', 'AVCTP'):
+        if profile in ("AVRCP", "AVCTP"):
             if not self._avrcp:
-                self._avrcp = AVRCPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._avrcp = AVRCPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._avrcp.interact(test, interaction, description, pts_address)
         # Handles GATT MMIs.
-        if profile in ('GATT'):
+        if profile in ("GATT"):
             if not self._gatt:
-                self._gatt = GATTProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._gatt = GATTProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._gatt.interact(test, interaction, description, pts_address)
         # Handles GAP MMIs.
-        if profile in ('GAP'):
+        if profile in ("GAP"):
             if not self._gap:
-                self._gap = GAPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._gap = GAPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._gap.interact(test, interaction, description, pts_address)
         # Handles HFP MMIs.
-        if profile in ('HFP'):
+        if profile in ("HFP"):
             if not self._hfp:
-                self._hfp = HFPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._hfp = HFPProxy(
+                    test,
+                    grpc.insecure_channel(f"localhost:{self.pandora_server_port}"),
+                    self.rootcanal,
+                    self.modem,
+                )
             return self._hfp.interact(test, interaction, description, pts_address)
         # Handles HID MMIs.
-        if profile in ('HID'):
+        if profile in ("HID"):
             if not self._hid:
-                self._hid = HIDProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'), self.rootcanal)
+                self._hid = HIDProxy(
+                    grpc.insecure_channel(f"localhost:{self.pandora_server_port}"),
+                    self.rootcanal,
+                )
             return self._hid.interact(test, interaction, description, pts_address)
         # Handles HOGP MMIs.
-        if profile in ('HOGP'):
+        if profile in ("HOGP"):
             if not self._hogp:
-                self._hogp = HOGPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._hogp = HOGPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._hogp.interact(test, interaction, description, pts_address)
         # Instantiates L2CAP proxy and reroutes corresponding MMIs to it.
-        if profile in ('L2CAP'):
+        if profile in ("L2CAP"):
             if not self._l2cap:
-                self._l2cap = L2CAPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._l2cap = L2CAPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._l2cap.interact(test, interaction, description, pts_address)
         # Handles RFCOMM MMIs.
-        if profile in ('RFCOMM'):
+        if profile in ("RFCOMM"):
             if not self._rfcomm:
-                self._rfcomm = RFCOMMProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._rfcomm = RFCOMMProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._rfcomm.interact(test, interaction, description, pts_address)
         # Handles SDP MMIs.
-        if profile in ('SDP'):
+        if profile in ("SDP"):
             if not self._sdp:
-                self._sdp = SDPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._sdp = SDPProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._sdp.interact(test, interaction, description, pts_address)
         # Handles SM MMIs.
-        if profile in ('SM'):
+        if profile in ("SM"):
             if not self._sm:
-                self._sm = SMProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+                self._sm = SMProxy(grpc.insecure_channel(f"localhost:{self.pandora_server_port}"))
             return self._sm.interact(test, interaction, description, pts_address)
 
         # Handles unsupported profiles.
         code = format_proxy(profile, interaction, description)
-        error_msg = (f'Missing {profile} proxy and mmi: {interaction}\n'
-                     f'Create a {profile.lower()}.py in mmi2grpc/:\n\n{code}\n'
-                     f'Then, instantiate the corresponding proxy in __init__.py\n'
-                     f'Finally, create a {profile.lower()}.proto in proto/pandora/'
-                     f'and generate the corresponding interface.')
+        error_msg = (f"Missing {profile} proxy and mmi: {interaction}\n"
+                     f"Create a {profile.lower()}.py in mmi2grpc/:\n\n{code}\n"
+                     f"Then, instantiate the corresponding proxy in __init__.py\n"
+                     f"Finally, create a {profile.lower()}.proto in proto/pandora/"
+                     f"and generate the corresponding interface.")
 
         assert False, error_msg
