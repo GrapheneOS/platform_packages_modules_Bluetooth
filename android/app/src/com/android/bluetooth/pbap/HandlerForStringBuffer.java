miyu-xu/packages-modules-Bluetooth

Pull out an inner class HandlerForStringBuffer
This CL merges two identical inner classes into one:
- BluetoothPbapVcardManager.HandlerForStringBuffer
- BluetoothPbapSimVcardManager.HandlerForStringBuffer

And add test for it.

Bug: 237467631
Test: atest HandlerForStringBufferTest
Change-Id: I8060e2762638725d3b310c7ced1fc61c77a28198

android/app/src/com/android/bluetooth/pbap/HandlerForStringBuffer.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.pbap;
+
+import android.util.Log;
+
+import com.android.obex.Operation;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Handler to emit vCards to PCE.
+ */
+public class HandlerForStringBuffer {
+    private static final String TAG = "HandlerForStringBuffer";
+
+    private final Operation mOperation;
+    private final String mOwnerVCard;
+
+    private OutputStream mOutputStream;
+
+    public HandlerForStringBuffer(Operation op, String ownerVCard) {
+        mOperation = op;
+        mOwnerVCard = ownerVCard;
+        if (BluetoothPbapService.VERBOSE) {
+            Log.v(TAG, "ownerVCard \n " + mOwnerVCard);
+        }
+    }
+
+    public boolean onInit() {
+        try {
+            mOutputStream = mOperation.openOutputStream();
+            if (mOwnerVCard != null) {
+                return write(mOwnerVCard);
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "openOutputStream failed", e);
+        }
+        return false;
+    }
+
+    public boolean onEntryCreated(String vcard) {
+        return write(vcard);
+    }
+
+    public void onTerminate() {
+        boolean result = BluetoothPbapObexServer.closeStream(mOutputStream, mOperation);
+        if (BluetoothPbapService.VERBOSE) {
+            if (result) {
+                Log.v(TAG, "closeStream succeeded!");
+            } else {
+                Log.v(TAG, "closeStream failed!");
+            }
+        }
+    }
+
+    private boolean write(String vCard) {
+        try {
+            if (vCard != null) {
+                mOutputStream.write(vCard.getBytes());
+                return true;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "write failed", e);
+        }
+        return false;
+    }
+}

GrapheneOS/platform_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/pbap/HandlerForStringBuffer.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.pbap;
+
+import android.util.Log;
+
+import com.android.obex.Operation;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Handler to emit vCards to PCE.
+ */
+public class HandlerForStringBuffer {
+    private static final String TAG = "HandlerForStringBuffer";
+
+    private final Operation mOperation;
+    private final String mOwnerVCard;
+
+    private OutputStream mOutputStream;
+
+    public HandlerForStringBuffer(Operation op, String ownerVCard) {
+        mOperation = op;
+        mOwnerVCard = ownerVCard;
+        if (BluetoothPbapService.VERBOSE) {
+            Log.v(TAG, "ownerVCard \n " + mOwnerVCard);
+        }
+    }
+
+    public boolean init() {
+        try {
+            mOutputStream = mOperation.openOutputStream();
+            if (mOwnerVCard != null) {
+                return writeVCard(mOwnerVCard);
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "openOutputStream failed", e);
+        }
+        return false;
+    }
+
+    public boolean writeVCard(String vCard) {
+        try {
+            if (vCard != null) {
+                mOutputStream.write(vCard.getBytes());
+                return true;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "write failed", e);
+        }
+        return false;
+    }
+
+    public void terminate() {
+        boolean result = BluetoothPbapObexServer.closeStream(mOutputStream, mOperation);
+        if (BluetoothPbapService.VERBOSE) {
+            if (result) {
+                Log.v(TAG, "closeStream succeeded!");
+            } else {
+                Log.v(TAG, "closeStream failed!");
+            }
+        }
+    }
+}