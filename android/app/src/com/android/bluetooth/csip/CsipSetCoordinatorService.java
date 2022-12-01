05abc814fb5f4db1f0e8fae714a5a502e139c1de
Łukasz Rymanowski <rlukasz@google.com>
2022-12-01 13:35:51 +0000

    BluetoothServices: On unbond disconnected services if needed
    Without this patch, service state machines might end up in connected
    state even for already unbonded devices
    
    Bug: 242645864
    Tag: #feature
    Test: atest BluetoothInstrumenatationTests
    Merged-In: I16a83e5bc741ed7c0978000cd27bdbe7238d5c63
    Change-Id: I16a83e5bc741ed7c0978000cd27bdbe7238d5c63
    (cherry picked from commit 3cc77584b8d2a36119738f45d117cc0417fd2124)

--- android/app/src/com/android/bluetooth/csip/CsipSetCoordinatorService.java
+++ android/app/src/com/android/bluetooth/csip/CsipSetCoordinatorService.java
@@ -906,6 +906,8 @@ public class CsipSetCoordinatorService extends ProfileService {
                 return;
             }
             if (sm.getConnectionState() != BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "Disconnecting device because it was unbonded.");
+                disconnect(device);
                 return;
             }
             removeStateMachine(device);
