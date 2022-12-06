miyu-xu/packages-modules-Bluetooth

SDP: TEST Handle race condition in HID SDP
Add race condition test

Bug: 246022810
Test: atest net_test_stack_sdp
Tag: #feature
Change-Id: I97ca9cff76e6249f61c7ad2ab2fbf30941780e71

system/stack/test/sdp/stack_sdp_test.cc
@@ -144,3 +144,35 @@ TEST_F(StackSdpMainTest, sdp_service_search_request_queuing) {
   ASSERT_EQ(p_ccb1->con_state, SDP_STATE_IDLE);
   ASSERT_EQ(p_ccb2->con_state, SDP_STATE_IDLE);
 }
+
+void sdp_cb(tSDP_RESULT result) {
+  if (result == SDP_SUCCESS) {
+    ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, nullptr));
+  }
+}
+
+TEST_F(StackSdpMainTest, sdp_service_search_request_queuing_race_condition) {
+  // start first request
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, sdp_cb));
+  const int cid1 = L2CA_ConnectReq2_cid;
+  tCONN_CB* p_ccb1 = find_ccb(cid1, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb1, nullptr);
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONN_SETUP);
+
+  tL2CAP_CFG_INFO cfg;
+  sdp_cb.reg_info.pL2CA_ConfigCfm_Cb(p_ccb1->connection_id, 0, &cfg);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONNECTED);
+
+  sdp_disconnect(p_ccb1, SDP_SUCCESS);
+  sdp_cb.reg_info.pL2CA_DisconnectCfm_Cb(p_ccb1->connection_id, 0, &cfg);
+
+  const int cid2 = L2CA_ConnectReq2_cid;
+  ASSERT_NEQ(cid1, cid2);  // The callback a queued a new request
+  tCONN_CB* p_ccb2 = find_ccb(cid2, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb2, nullptr);
+  // If race condition, this will be stuck in PEND
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONN_SETUP);
+
+  sdp_disconnect(p_ccb2, SDP_SUCCESS);
+}

GrapheneOS/platform_packages_modules_Bluetooth

Merged-In: I97ca9cff76e6249f61c7ad2ab2fbf30941780e71
(cherry picked from commit a7c5ec63863775746575dd07c78db96cfad27b49)

system/stack/test/sdp/stack_sdp_test.cc
@@ -140,3 +140,35 @@ TEST_F(StackSdpMainTest, sdp_service_search_request_queuing) {
   ASSERT_EQ(p_ccb1->con_state, SDP_STATE_IDLE);
   ASSERT_EQ(p_ccb2->con_state, SDP_STATE_IDLE);
 }
+
+void sdp_callback(tSDP_RESULT result) {
+  if (result == SDP_SUCCESS) {
+    ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, nullptr));
+  }
+}
+
+TEST_F(StackSdpMainTest, sdp_service_search_request_queuing_race_condition) {
+  // start first request
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, sdp_callback));
+  const int cid1 = L2CA_ConnectReq2_cid;
+  tCONN_CB* p_ccb1 = find_ccb(cid1, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb1, nullptr);
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONN_SETUP);
+
+  tL2CAP_CFG_INFO cfg;
+  sdp_cb.reg_info.pL2CA_ConfigCfm_Cb(p_ccb1->connection_id, 0, &cfg);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONNECTED);
+
+  sdp_disconnect(p_ccb1, SDP_SUCCESS);
+  sdp_cb.reg_info.pL2CA_DisconnectCfm_Cb(p_ccb1->connection_id, 0);
+
+  const int cid2 = L2CA_ConnectReq2_cid;
+  ASSERT_NE(cid1, cid2);  // The callback a queued a new request
+  tCONN_CB* p_ccb2 = find_ccb(cid2, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb2, nullptr);
+  // If race condition, this will be stuck in PEND
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONN_SETUP);
+
+  sdp_disconnect(p_ccb2, SDP_SUCCESS);
+}