miyu-xu/packages-modules-Bluetooth

headless: Fix fcntl descriptor swap
Bug: 257972065
Test: gd/cert/run
Tag: #refactor
BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines

Change-Id: I7baf85639b0fe6df0d0b49126c4270fa968449fc

system/test/headless/main.cc
@@ -113,7 +113,8 @@ class Main : public HeadlessTest<int> {
   }
 
   int Run() override {
-    console_fd = fcntl(STDERR_FILENO, F_DUPFD_CLOEXEC);
+    console_fd = fcntl(STDERR_FILENO, F_DUPFD, STDERR_FILENO);
+    ASSERT(console_fd != -1);
     if (options_.close_stderr_) {
       fclose(stderr);
     }

GrapheneOS/platform_packages_modules_Bluetooth

system/test/headless/main.cc
@@ -114,7 +114,8 @@ class Main : public HeadlessTest<int> {
   }
 
   int Run() override {
-    console_fd = fcntl(STDERR_FILENO, F_DUPFD_CLOEXEC);
+    console_fd = fcntl(STDERR_FILENO, F_DUPFD_CLOEXEC, STDERR_FILENO);
+    ASSERT(console_fd != -1);
     if (options_.close_stderr_) {
       fclose(stderr);
     }