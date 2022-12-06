cb89c36426d2b9c4fa69590057ecfedddae5c240
Chris Manton <cmanton@google.com>
2022-12-05 17:25:51 -0800

    Add missing stack dependencies and fix test linker
    The net_test_btif_stack reference both the TestMockStack and the
    libbt-stack. This shouldn't be the case
    
    Bug: 248126637
    Test: atest net_test_btif_stack net_test_stack_btu net_test_stack_acl
    Test: atest net_test_bta net_test_stack_smp net_test_stack_gatt_native
    Test: atest net_test_stack_gatt_sr_hash_native net_test_stack_hid
    Test: atest net_test_stack_gatt net_test_stack_l2cap net_test_main_shim
    Change-Id: I7e4fafd63d132cc2353943db304bc852018051c6

---
+++ system/test/mock/mock_stack_btm_hfp_msbc_encoder.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+/*
+ * Generated mock file from original source file
+ *   Functions generated:3
+ *
+ *  mockcify.pl ver 0.5.1
+ */
+
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <string>
+
+extern std::map<std::string, int> mock_function_count_map;
+
+// Original included files, if any
+// NOTE: Since this is a mock file with mock definitions some number of
+//       include files may not be required.  The include-what-you-use
+//       still applies, but crafting proper inclusion is out of scope
+//       for this effort.  This compilation unit may compile as-is, or
+//       may need attention to prune from (or add to ) the inclusion set.
+#include <cstring>
+
+#include "embdrv/sbc/encoder/include/sbc_encoder.h"
+#include "hfp_msbc_encoder.h"
+#include "osi/include/log.h"
+
+// Original usings
+
+// Mocked compile conditionals, if any
+
+namespace test {
+namespace mock {
+namespace stack_btm_hfp_msbc_encoder {
+
+// Shared state between mocked functions and tests
+// Name: hfp_msbc_encode_frames
+// Params: int16_t* input, uint8_t* output
+// Return: uint32_t
+struct hfp_msbc_encode_frames {
+  static uint32_t return_value;
+  std::function<uint32_t(int16_t* input, uint8_t* output)> body{
+      [](int16_t* input, uint8_t* output) { return return_value; }};
+  uint32_t operator()(int16_t* input, uint8_t* output) {
+    return body(input, output);
+  };
+};
+extern struct hfp_msbc_encode_frames hfp_msbc_encode_frames;
+
+// Name: hfp_msbc_encoder_cleanup
+// Params: void
+// Return: void
+struct hfp_msbc_encoder_cleanup {
+  std::function<void(void)> body{[](void) {}};
+  void operator()(void) { body(); };
+};
+extern struct hfp_msbc_encoder_cleanup hfp_msbc_encoder_cleanup;
+
+// Name: hfp_msbc_encoder_init
+// Params: void
+// Return: void
+struct hfp_msbc_encoder_init {
+  std::function<void(void)> body{[](void) {}};
+  void operator()(void) { body(); };
+};
+extern struct hfp_msbc_encoder_init hfp_msbc_encoder_init;
+
+}  // namespace stack_btm_hfp_msbc_encoder
+}  // namespace mock
+}  // namespace test
+
+// END mockcify generation
