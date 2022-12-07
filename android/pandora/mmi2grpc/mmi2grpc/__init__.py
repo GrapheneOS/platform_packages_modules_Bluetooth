miyu-xu/packages-modules-Bluetooth

Pandora: Implement RFCOMM/
RFCOMM/DEVA/
RFCOMM/DEVB/
RFCOMM/DEVA-DEVB/

There are three tests that are still skipped.

Bug: 240483035
Test: atest pts-bot:RFCOMM/ -v
Change-Id: I7df1d38f1a5501a42030d6dfeff5560eb1124fe2

android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -27,6 +27,7 @@ from mmi2grpc.avrcp import AVRCPProxy
 from mmi2grpc.gatt import GATTProxy
 from mmi2grpc.hfp import HFPProxy
 from mmi2grpc.hogp import HOGPProxy
+from mmi2grpc.rfcomm import RFCOMMProxy
 from mmi2grpc.sdp import SDPProxy
 from mmi2grpc.sm import SMProxy
 from mmi2grpc._helpers import format_proxy
@@ -61,6 +62,7 @@ class IUT:
         self._avrcp = None
         self._gatt = None
         self._hfp = None
+        self._rfcomm = None
         self._sdp = None
         self._sm = None
         self._hogp = None
@@ -77,6 +79,7 @@ class IUT:
         self._avrcp = None
         self._gatt = None
         self._hfp = None
+        self._rfcomm = None
         self._sdp = None
         self._sm = None
         self._hogp = None
@@ -154,6 +157,11 @@ class IUT:
             if not self._hfp:
                 self._hfp = HFPProxy(grpc.insecure_channel(f'localhost:{self.port}'))
             return self._hfp.interact(test, interaction, description, pts_address)
+        # Handles RFCOMM MMIs.
+        if profile in ('RFCOMM'):
+            if not self._rfcomm:
+                self._rfcomm = RFCOMMProxy(grpc.insecure_channel(f'localhost:{self.port}'))
+            return self._rfcomm.interact(test, interaction, description, pts_address)
         # Handles SDP MMIs.
         if profile in ('SDP'):
             if not self._sdp:

GrapheneOS/platform_packages_modules_Bluetooth

There are two tests that are still skipped.
I couldn't find the IXIT value referenced in the test spec,
which would allow the tests to pass:
The role of DevA/DevB taken on by the IUT does not
matter to achieve a pass verdict for some test cases
for this Protocol as indicated in the test purpose and is
specified in the test case identifier for role agnostic
tests as detailed in Table 4.1. The role of the IUT/Lower
Tester for these tests is specified in the IXIT [7] in
order to enable the correct test environment conditions
to provoke the Lower Tester.
Since we don't support hooking up a physical serial_port,
TSPC_SPP_2_1 is set to false, which disables
RFCOMM/DEVA-DEVB/RFC/BV-14-C
Bug: 245578454
Ignore-AOSP-First: Cherry-picked from AOSP
Merged-In: I7df1d38f1a5501a42030d6dfeff5560eb1124fe2

android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -30,6 +30,7 @@ from mmi2grpc.hfp import HFPProxy
 from mmi2grpc.hid import HIDProxy
 from mmi2grpc.hogp import HOGPProxy
 from mmi2grpc.l2cap import L2CAPProxy
+from mmi2grpc.rfcomm import RFCOMMProxy
 from mmi2grpc.sdp import SDPProxy
 from mmi2grpc.sm import SMProxy
 from mmi2grpc._helpers import format_proxy
@@ -71,6 +72,7 @@ class IUT:
         self._hid = None
         self._hogp = None
         self._l2cap = None
+        self._rfcomm = None
         self._sdp = None
         self._sm = None
 
@@ -96,6 +98,7 @@ class IUT:
         self._l2cap = None
         self._hid = None
         self._hogp = None
+        self._rfcomm = None
         self._sdp = None
         self._sm = None
 
@@ -192,6 +195,11 @@ class IUT:
             if not self._l2cap:
                 self._l2cap = L2CAPProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
             return self._l2cap.interact(test, interaction, description, pts_address)
+        # Handles RFCOMM MMIs.
+        if profile in ('RFCOMM'):
+            if not self._rfcomm:
+                self._rfcomm = RFCOMMProxy(grpc.insecure_channel(f'localhost:{self.pandora_server_port}'))
+            return self._rfcomm.interact(test, interaction, description, pts_address)
         # Handles SDP MMIs.
         if profile in ('SDP'):
             if not self._sdp: