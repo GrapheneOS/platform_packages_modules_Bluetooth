d3a66dfb51f68ddb6c437dd3298825850861154b
Kihong Seong <kihongs@google.com>
2022-12-09 06:22:32 +0000

    Add ConvoContactInfoTest
    Bug: 237467631
    Test: atest ConvoContactInfoTest
    Change-Id: I9a5348cf9a812aabc043c45c9fd1736a912c5f91

--- android/app/src/com/android/bluetooth/map/BluetoothMapContentObserver.java
+++ android/app/src/com/android/bluetooth/map/BluetoothMapContentObserver.java
@@ -536,7 +536,8 @@ public class BluetoothMapContentObserver {
         this.mFolders = folderStructure;
     }
 
-    private static class ConvoContactInfo {
+    @VisibleForTesting
+    static class ConvoContactInfo {
         public int mConvoColConvoId = -1;
         public int mConvoColLastActivity = -1;
         public int mConvoColName = -1;
