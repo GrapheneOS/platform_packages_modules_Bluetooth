miyu-xu/packages-modules-Bluetooth

Add ability to find discoverable devices.
Added tests for the same.

Bug: 261616658
Test: mma -j $(nproc)
Test: ./build.py
Test: system/gd/cert/run --clean --topshim
Tag: #floss
Change-Id: I6252c6759937d263a2ae9a161df78083115860cf

system/blueberry/tests/topshim/adapter/adapter_test.py
@@ -53,11 +53,25 @@ class AdapterTest(TopshimBaseTest):
     def test_start_discovery(self):
         state = self.dut().toggle_discovery(True)
         assertThat(state).isEqualTo("Started")
+        # Reset device discovering to Stopped
+        self.dut().toggle_discovery(False)
 
     def test_cancel_discovery(self):
         self.dut().toggle_discovery(True)
         state = self.dut().toggle_discovery(False)
         assertThat(state).isEqualTo("Stopped")
+
+    def test_find_device_device_available(self):
+        self.dut().enable_inquiry_scan()
+        self.cert().enable_inquiry_scan()
+        self.dut().toggle_discovery(True)
+        device = self.dut().find_device()
+        assertThat(device).isNotNone()
+        # Reset DUT device discovering and scanning to None
+        self.dut().disable_page_scan()
+        self.dut().toggle_discovery(False)
+        # Reset CERT device to not discoverable
+        self.cert().disable_page_scan()
 
 
 if __name__ == "__main__":

system/blueberry/tests/topshim/lib/topshim_device.py
@@ -188,6 +188,25 @@ class TopshimDevice(AsyncClosable):
         f = self.__post(self.__adapter.toggle_discovery(is_start))
         return self.__post(f)
 
+    def find_device(self):
+        """
+        Attempts to find discoverable devices when discovery is toggled on.
+
+        @return a list of properties of found device.
+        """
+        f = self.__post(self.__adapter.find_device())
+
+        async def waiter(f):
+            try:
+                property = await f
+                return list(property[1:-1].strip().split(","))
+            except:
+                # The future `f` has a timeout after 2s post which it is cancelled.
+                print("No device was found. Timed out.")
+            return None
+
+        return self.__post(waiter(f))
+
     async def __adapter_properties_waiter(self, f):
         data = await f
         data_list = data.split(" :: ")

GrapheneOS/platform_packages_modules_Bluetooth

system/blueberry/tests/topshim/adapter/adapter_test.py
@@ -57,6 +57,18 @@ class AdapterTest(TopshimBaseTest):
         state = self.dut().toggle_discovery(False)
         assertThat(state).isEqualTo("Stopped")
 
+    def test_find_device_device_available(self):
+        self.dut().enable_inquiry_scan()
+        self.cert().enable_inquiry_scan()
+        self.dut().toggle_discovery(True)
+        device_addr = self.dut().find_device()
+        assertThat(device_addr).isNotNone()
+        # Reset DUT device discovering and scanning to None
+        self.dut().disable_page_scan()
+        self.dut().toggle_discovery(False)
+        # Reset CERT device to not discoverable
+        self.cert().disable_page_scan()
+
 
 if __name__ == "__main__":
     test_runner.main()

system/blueberry/tests/topshim/lib/topshim_device.py
@@ -209,3 +209,22 @@ class TopshimDevice(AsyncClosable):
             return params["state"].data[0]
 
         return self.__post(waiter(f))
+
+    def find_device(self):
+        """
+        Attempts to find discoverable devices when discovery is toggled on.
+
+        @return a list of properties of found device.
+        """
+        f = self.__post(self.__adapter.find_device())
+
+        async def waiter(f):
+            try:
+                params = await f
+                return params["BdAddr"].data[0]
+            except:
+                # The future `f` has a timeout after 2s post which it is cancelled.
+                print("No device was found. Timed out.")
+            return None
+
+        return self.__post(waiter(f))