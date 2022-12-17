miyu-xu/packages-modules-Bluetooth

Add APIs to toggle Adapter discoverability between none, connectable and connectable/discoverable.
Added associated tests to validate the same.

Bug: 261602425
Test: mma -j $(nproc)
Test: ./build.py
Test: system/gd/cert/run --clean --topshim
Tag: #floss
Change-Id: I6fd70f06ba606404842a8bee58e8e1cf2e044635

system/blueberry/tests/topshim/adapter/adapter_test.py
@@ -26,8 +26,23 @@ class AdapterTest(TopshimBaseTest):
     def test_verify_adapter_started(self):
         print("Adapter is verified when test starts")
 
+    def test_enable_inquiry_scan(self):
+        status, properties = self.dut().enable_inquiry_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(len(properties)).isEqualTo(1)
+        assertThat(properties[0]).isEqualTo("AdapterScanMode(ConnectableDiscoverable)")
+
     def test_enable_page_scan(self):
-        self.dut().enable_page_scan()
+        status, properties = self.dut().enable_page_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(len(properties)).isEqualTo(1)
+        assertThat(properties[0]).isEqualTo("AdapterScanMode(Connectable)")
+
+    def test_disable_page_scan(self):
+        status, properties = self.dut().disable_page_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(len(properties)).isEqualTo(1)
+        assertThat(properties[0]).isEqualTo("AdapterScanMode(None_)")
 
     def test_set_local_io_caps(self):
         status, properties = self.dut().set_local_io_caps(3)

GrapheneOS/platform_packages_modules_Bluetooth

system/blueberry/tests/topshim/adapter/adapter_test.py
@@ -26,8 +26,20 @@ class AdapterTest(TopshimBaseTest):
     def test_verify_adapter_started(self):
         print("Adapter is verified when test starts")
 
+    def test_enable_inquiry_scan(self):
+        status, discovery_mode = self.dut().enable_inquiry_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(discovery_mode).isEqualTo("ConnectableDiscoverable")
+
     def test_enable_page_scan(self):
-        self.dut().enable_page_scan()
+        status, discovery_mode = self.dut().enable_page_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(discovery_mode).isEqualTo("Connectable")
+
+    def test_disable_page_scan(self):
+        status, discovery_mode = self.dut().disable_page_scan()
+        assertThat(status).isEqualTo("Success")
+        assertThat(discovery_mode).isEqualTo("None_")
 
     def test_set_local_io_caps(self):
         status, caps = self.dut().set_local_io_caps(3)