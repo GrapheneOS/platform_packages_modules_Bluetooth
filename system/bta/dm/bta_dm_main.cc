813269e06e4ad81a829d8c98babf00dbdc6da3f3
Chris Manton <cmanton@google.com>
2022-12-16 17:27:48 -0800

    Move bta::dm:: accessor functions to proper file
    Bug: 224999611
    Test: gd/cert/run
    Tag: #refactor
    
    Change-Id: Ia55816f9fc646a46e89d6dd3698ae62d8d43e1d9

--- system/bta/dm/bta_dm_main.cc
+++ system/bta/dm/bta_dm_main.cc
@@ -47,9 +47,6 @@ tBTA_DM_DI_CB bta_dm_di_cb;
  ******************************************************************************/
 void bta_dm_search_sm_disable() { bta_sys_deregister(BTA_ID_DM_SEARCH); }
 
-void bta_dm_search_set_state(uint8_t state) { bta_dm_search_cb.state = state; }
-uint8_t bta_dm_search_get_state() { return bta_dm_search_cb.state; }
-
 /*******************************************************************************
  *
  * Function         bta_dm_search_sm_execute
