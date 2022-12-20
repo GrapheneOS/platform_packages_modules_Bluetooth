miyu-xu/packages-modules-Bluetooth

[PTS-Bot]: Added 7 OPP/SR test cases
Added opp.py file
Updated _android.proto and AndroidInternal.kt files
Added AcceptIncomingFile function in AndroidInternal.kt using uiautomator.

Test: atest pts-bot:OPP -v
Change-Id: If2ab94f8033035ddf7b40a1a5acdc58b8f272acc

android/pandora/server/src/com/android/pandora/AndroidInternal.kt
@@ -23,6 +23,10 @@ import io.grpc.stub.StreamObserver
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import androidx.test.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -35,8 +39,13 @@ private const val TAG = "PandoraAndroidInternal"
 class AndroidInternal(context: Context) : AndroidImplBase() {
 
   private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+  private val ACCEPT_BTN_TXT = "ACCEPT"
+  private val INCOMING_FILE_TITLE = "Incoming file"
+  private val WAIT_TIMEOUT = 2000L
+
   private val bluetoothManager = context.getSystemService(BluetoothManager::class.java)!!
   private val bluetoothAdapter = bluetoothManager.adapter
+  private var device: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
   fun deinit() {
     scope.cancel()
@@ -58,6 +67,14 @@ class AndroidInternal(context: Context) : AndroidImplBase() {
         AccessType.ACCESS_SIM -> bluetoothDevice.setSimAccessPermission(BluetoothDevice.ACCESS_ALLOWED)
         else -> {}
       }
+      Empty.getDefaultInstance()
+    }
+  }
+
+  override fun acceptIncomingFile(request: Empty, responseObserver: StreamObserver<Empty>) {
+    grpcUnary<Empty>(scope, responseObserver) {
+      device.wait(Until.findObject(By.text(INCOMING_FILE_TITLE)), WAIT_TIMEOUT).click()
+      device.wait(Until.findObject(By.text(ACCEPT_BTN_TXT)), WAIT_TIMEOUT).click()
       Empty.getDefaultInstance()
     }
   }

GrapheneOS/platform_packages_modules_Bluetooth

android/pandora/server/src/com/android/pandora/AndroidInternal.kt
@@ -28,6 +28,10 @@ import android.net.Uri
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import androidx.test.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -40,10 +44,15 @@ private const val TAG = "PandoraAndroidInternal"
 class AndroidInternal(val context: Context) : AndroidImplBase() {
 
   private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+  private val INCOMING_FILE_ACCEPT_BTN = "ACCEPT"
+  private val INCOMING_FILE_TITLE = "Incoming file"
+  private val INCOMING_FILE_WAIT_TIMEOUT = 2000L
+
   private val bluetoothManager = context.getSystemService(BluetoothManager::class.java)!!
   private val bluetoothAdapter = bluetoothManager.adapter
   private var telephonyManager = context.getSystemService(TelephonyManager::class.java)
   private val DEFAULT_MESSAGE_LEN = 130
+  private var device: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
   fun deinit() {
     scope.cancel()
@@ -77,6 +86,14 @@ class AndroidInternal(val context: Context) : AndroidImplBase() {
       val avdPhoneNumber = telephonyManager.getLine1Number()
 
       smsManager.sendTextMessage(avdPhoneNumber, avdPhoneNumber, generateAlphanumericString(DEFAULT_MESSAGE_LEN), null, null)
+      Empty.getDefaultInstance()
+    }
+  }
+
+  override fun acceptIncomingFile(request: Empty, responseObserver: StreamObserver<Empty>) {
+    grpcUnary<Empty>(scope, responseObserver) {
+      device.wait(Until.findObject(By.text(INCOMING_FILE_TITLE)), INCOMING_FILE_WAIT_TIMEOUT).click()
+      device.wait(Until.findObject(By.text(INCOMING_FILE_ACCEPT_BTN)), INCOMING_FILE_WAIT_TIMEOUT).click()
       Empty.getDefaultInstance()
     }
   }