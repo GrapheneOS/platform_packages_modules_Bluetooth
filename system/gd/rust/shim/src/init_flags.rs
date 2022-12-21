miyu-xu/packages-modules-Bluetooth

BT5.3: Periodic Advtising adi -- feature flag
Bug: 248126637
Tag: #feature
Test: Manual end-to-end testing (need 5.3 support in controller)
Change-Id: Ic3da3d9d1592ec2e6ada689cd6fea564947766ae

system/gd/rust/shim/src/init_flags.rs
@@ -22,6 +22,7 @@ mod ffi {
         fn pass_phy_update_callback_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
+        fn sydney_5_3_gatt_periodic_advertising_adi_is_enabled() -> bool;
     }
 }
 

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/shim/src/init_flags.rs
@@ -23,6 +23,7 @@ mod ffi {
         fn leaudio_targeted_announcement_reconnection_mode_is_enabled() -> bool;
         fn logging_debug_enabled_for_all_is_enabled() -> bool;
         fn pass_phy_update_callback_is_enabled() -> bool;
+        fn periodic_advertising_adi_is_enabled() -> bool;
         fn queue_l2cap_coc_while_encrypting_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;