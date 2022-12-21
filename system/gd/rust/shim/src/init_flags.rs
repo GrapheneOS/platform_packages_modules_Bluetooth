miyu-xu/packages-modules-Bluetooth

BT5.3: Set min encryption -- feature flag
Bug: 248126637
Tag: #feature
Test: Manual end-to-end testing (need 5.3 support in controller)
Change-Id: I95987cafd0a25ea42b29a7adb9096441836dc13f

system/gd/rust/shim/src/init_flags.rs
@@ -23,6 +23,7 @@ mod ffi {
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
         fn sydney_5_3_gatt_periodic_advertising_adi_is_enabled() -> bool;
+        fn sydney_5_3_set_min_encryption_is_enabled() -> bool;
     }
 }
 

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/shim/src/init_flags.rs
@@ -28,6 +28,7 @@ mod ffi {
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
         fn sdp_skip_rnr_if_known_is_enabled() -> bool;
+        fn set_min_encryption_is_enabled() -> bool;
         fn trigger_advertising_callbacks_on_first_resume_after_pause_is_enabled() -> bool;
     }
 }