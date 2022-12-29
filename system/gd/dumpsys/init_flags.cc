miyu-xu/packages-modules-Bluetooth

BT5.3 Subrating -- feature flags
Bug: 248126637
Test: build
Change-Id: Idca8b425dc907b6194c891549a6d011a5700690d

system/gd/dumpsys/init_flags.cc
@@ -51,6 +51,7 @@ flatbuffers::Offset<bluetooth::common::InitFlagsData> bluetooth::dumpsys::InitFl
   builder.add_pass_phy_update_callback_is_enabled(initFlags::pass_phy_update_callback_is_enabled());
   builder.add_redact_log_is_enabled(initFlags::redact_log_is_enabled());
   builder.add_sdp_serialization_is_enabled(initFlags::sdp_serialization_is_enabled());
+  builder.add_sydney_5_3_subrating_is_enabled(initFlags::sydney_5_3_subrating_is_enabled());
 
   return builder.Finish();
 }

system/gd/hci/controller.cc
@@ -156,7 +156,8 @@ struct Controller::impl {
           handler->BindOnceOn(this, &Controller::impl::le_set_host_feature_handler));
     }
 
-    if (is_supported(OpCode::LE_SET_HOST_FEATURE) && module_.SupportsBleConnectionSubratingHost()) {
+    if (common::init_flags::sydney_5_3_subrating_is_enabled() && is_supported(OpCode::LE_SET_HOST_FEATURE) &&
+        module_.SupportsBleConnectionSubratingHost()) {
       hci_->EnqueueCommand(
           LeSetHostFeatureBuilder::Create(LeHostFeatureBits::CONNECTION_SUBRATING_HOST_SUPPORT, Enable::ENABLED),
           handler->BindOnceOn(this, &Controller::impl::le_set_host_feature_handler));

system/gd/rust/shim/src/init_flags.rs
@@ -22,6 +22,7 @@ mod ffi {
         fn pass_phy_update_callback_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
+        fn sydney_5_3_subrating_is_enabled() -> bool;
     }
 }
 

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/dumpsys/init_flags.cc
@@ -61,6 +61,7 @@ flatbuffers::Offset<bluetooth::common::InitFlagsData> bluetooth::dumpsys::InitFl
   builder.add_sdp_serialization_is_enabled(initFlags::sdp_serialization_is_enabled());
   builder.add_sdp_skip_rnr_if_known_is_enabled(initFlags::sdp_skip_rnr_if_known_is_enabled());
   builder.add_set_min_encryption_is_enabled(bluetooth::common::init_flags::set_min_encryption_is_enabled());
+  builder.add_subrating_is_enabled(initFlags::subrating_is_enabled());
   builder.add_trigger_advertising_callbacks_on_first_resume_after_pause_is_enabled(
       initFlags::trigger_advertising_callbacks_on_first_resume_after_pause_is_enabled());
 

system/gd/hci/controller.cc
@@ -164,7 +164,8 @@ struct Controller::impl {
           handler->BindOnceOn(this, &Controller::impl::le_set_host_feature_handler));
     }
 
-    if (is_supported(OpCode::LE_SET_HOST_FEATURE) && module_.SupportsBleConnectionSubrating()) {
+    if (common::init_flags::subrating_is_enabled() && is_supported(OpCode::LE_SET_HOST_FEATURE) &&
+        module_.SupportsBleConnectionSubrating()) {
       hci_->EnqueueCommand(
           LeSetHostFeatureBuilder::Create(
               LeHostFeatureBits::CONNECTION_SUBRATING_HOST_SUPPORT, Enable::ENABLED),

system/gd/rust/shim/src/init_flags.rs
@@ -29,6 +29,7 @@ mod ffi {
         fn sdp_serialization_is_enabled() -> bool;
         fn sdp_skip_rnr_if_known_is_enabled() -> bool;
         fn set_min_encryption_is_enabled() -> bool;
+        fn subrating_is_enabled() -> bool;
         fn trigger_advertising_callbacks_on_first_resume_after_pause_is_enabled() -> bool;
     }
 }