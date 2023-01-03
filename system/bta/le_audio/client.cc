GrapheneOS/platform_packages_modules_Bluetooth

commit 9914d0e7b19536e83df1d180cc9482d14d57cc44

le_audio: Don't trigger connection when device is not disconnected
LE Audio device should not be triggered to connect when device is in
invalid state. Having device in invalid state may cause problems with
setting its state as connecting e.g. when device is already connected.

Bug: 260166914
Tag: #feature
Test: test bluetooth_le_audio_test bluetooth_le_audio_client_test
Change-Id: Ifd29256477651a29e45b980f7398aa9e038aa37b

system/bta/le_audio/client.cc
@@ -991,8 +991,17 @@ class LeAudioClientImpl : public LeAudioClient {
     LeAudioDevice* leAudioDevice = leAudioDevices_.FindByAddress(address);
     if (!leAudioDevice) {
       leAudioDevices_.Add(address, DeviceConnectState::CONNECTING_BY_USER);
-
     } else {
+      if (leAudioDevice->GetConnectionState() !=
+          DeviceConnectState::DISCONNECTED) {
+        LOG_ERROR(
+            "Device %s is in invalid state: %s",
+            leAudioDevice->address_.ToString().c_str(),
+            bluetooth::common::ToString(leAudioDevice->GetConnectionState())
+                .c_str());
+
+        return;
+      }
       leAudioDevice->SetConnectionState(DeviceConnectState::CONNECTING_BY_USER);
 
       le_audio::MetricsCollector::Get()->OnConnectionStateChanged(

commit f3829a2cb9c276377c848f9c5eea8bb7607f6c13

leaudio: Fix reconnection
This is regression after:

9914d0e7b1 le_audio: Don't trigger connection when device is not disconnected

If LeAudio set is in the autoconnect mode, and first devices has been
found, Java layer triggers connection to other devices.
Without this fix, device would stay in the autoconnect mode instead of
trying to do direct connection.

Important for Targeted annoucement reconnection mode

Bug: 260166914
Bug: 264225824
Test: manual
Tag: #feature
Change-Id: I4ed36163623075a43f3b9e5078e301c5ee71ac63

system/bta/le_audio/client.cc
@@ -1108,13 +1108,12 @@ class LeAudioClientImpl : public LeAudioClient {
     if (!leAudioDevice) {
       leAudioDevices_.Add(address, DeviceConnectState::CONNECTING_BY_USER);
     } else {
-      if (leAudioDevice->GetConnectionState() !=
-          DeviceConnectState::DISCONNECTED) {
-        LOG_ERROR(
-            "Device %s is in invalid state: %s",
-            leAudioDevice->address_.ToString().c_str(),
-            bluetooth::common::ToString(leAudioDevice->GetConnectionState())
-                .c_str());
+      auto current_connect_state = leAudioDevice->GetConnectionState();
+      if ((current_connect_state == DeviceConnectState::CONNECTED) ||
+          (current_connect_state == DeviceConnectState::CONNECTING_BY_USER)) {
+        LOG_ERROR("Device %s is in invalid state: %s",
+                  leAudioDevice->address_.ToString().c_str(),
+                  bluetooth::common::ToString(current_connect_state).c_str());
 
         return;
       }