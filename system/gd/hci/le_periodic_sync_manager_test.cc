c46cf9be09470038f36eed583aa43f99d055afee
William Escande <wescande@google.com>
2023-01-07 22:58:36 -0800

    Ensure future are reset before being set
    This avoid async flakiness when test assert the future is null before
    enqueue next packet
    
    Test: atest --host
    Bug: 264572812
    Bug: 265213364
    Merged-In: Iac9e2f0722db5d6d4c148358d96bb1df1f00aebf
    (cherry picked from commit 0c7e0b3e4865271493adbd2c3d75e59302f508a1)
    Change-Id: I3209a9d2fa4a0fcec400a99b58ad7d741f522b85

--- system/gd/hci/le_periodic_sync_manager_test.cc
+++ system/gd/hci/le_periodic_sync_manager_test.cc
@@ -45,8 +45,9 @@ class TestLeScanningInterface : public LeScanningInterface {
     command_queue_.push(std::move(command));
     command_complete_callbacks.push_back(std::move(on_complete));
     if (command_promise_ != nullptr) {
-      command_promise_->set_value();
-      command_promise_.reset();
+      std::promise<void>* prom = command_promise_.release();
+      prom->set_value();
+      delete prom;
     }
   }
 
@@ -56,8 +57,9 @@ class TestLeScanningInterface : public LeScanningInterface {
     command_queue_.push(std::move(command));
     command_status_callbacks.push_back(std::move(on_status));
     if (command_promise_ != nullptr) {
-      command_promise_->set_value();
-      command_promise_.reset();
+      std::promise<void>* prom = command_promise_.release();
+      prom->set_value();
+      delete prom;
     }
   }
 
