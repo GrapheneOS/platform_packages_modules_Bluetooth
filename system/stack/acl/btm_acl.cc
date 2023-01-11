5e38ce6bf77cef373dbe836567a3bde97aa92889
Brian Delwiche <delwiche@google.com>
2023-01-11 00:27:45 +0000

    Reject encryption drop in Common Criteria mode
    For NCIS certification, we need to drop the connection or
    reestablish encryption after receiving a command to disable link
    layer encryption on an encrypted link.  However, dropping the
    connection for all devices breaks compatibility during role switch
    with devices running Bluetooth 2.1 or earlier, a category including
    many cars still in the field.
    
    Add a check forcing connections to drop in this case, conditioned on
    Common Criteria mode..
    
    Bug: 251436534
    Test: atest: bluetooth, lab validation forthcoming
    Tag: #security
    Ignore-AOSP-First: Security
    Change-Id: I94654ebeb16774643107ee41473725cfae3764ab
    (cherry picked from commit on googleplex-android-review.googlesource.com host: 1ee290b87b23c30d4389c2de283e01f72b2db17a)
    Merged-In: I94654ebeb16774643107ee41473725cfae3764ab

--- system/stack/acl/btm_acl.cc
+++ system/stack/acl/btm_acl.cc
@@ -50,6 +50,7 @@
 #include "main/shim/dumpsys.h"
 #include "main/shim/l2c_api.h"
 #include "main/shim/shim.h"
+#include "os/parameter_provider.h"
 #include "osi/include/allocator.h"
 #include "osi/include/log.h"
 #include "osi/include/osi.h"  // UNUSED_ATTR
@@ -628,6 +629,23 @@ void btm_acl_encrypt_change(uint16_t handle, uint8_t status,
   if (p == nullptr) {
     LOG_WARN("Unable to find active acl");
     return;
+  }
+
+  /* Common Criteria mode only: if we are trying to drop encryption on an
+   * encrypted connection, drop the connection */
+  if (bluetooth::os::ParameterProvider::IsCommonCriteriaMode()) {
+    if (p->is_encrypted && !encr_enable) {
+      LOG(ERROR)
+          << __func__
+          << " attempting to decrypt encrypted connection, disconnecting. "
+             "handle: "
+          << loghex(handle);
+
+      acl_disconnect_from_handle(handle, HCI_ERR_HOST_REJECT_SECURITY,
+                                 "stack::btu::btu_hcif::read_drop_encryption "
+                                 "Connection Already Encrypted");
+      return;
+    }
   }
 
   p->is_encrypted = encr_enable;
