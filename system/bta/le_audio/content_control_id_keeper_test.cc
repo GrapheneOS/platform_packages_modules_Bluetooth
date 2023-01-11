miyu-xu/packages-modules-Bluetooth

LeAudio: Allow GMCS to control the Live context
With this change, the generic MCS declares control
over the "Live" context. This can be used by the recorder app
to implement the media session controller.

This adds support to Content Control Id Keeper to hold
multiple contexts under the same CCID value, and by this allows
a single control service to declare control over multiple audio
contexts. This information is then used in stream establishment so
that the remote would know which control service to use for stream
control.

Additionally it
 - removes 'virtual' declaration as Pimpl patterm is used for
   compile time function call dispatch
 - disambiguates native API by using the proper type for a single
   context type
 - at the same time adds support for multiple contexts at the higher
   Java layer (resulting the native code to add proper entry for each
   individual context)
 - improves robustness by verifying context parameter for a valid value
 - improves robustness for multple calls with the same client UUID
   (previously we were leaking ccid values from the ccid pool)
 - moves the helper function used for getting CCID list for a compound
   context types into the CCID keeper
 - adds java code unit tests
 - adds native code unit tests

Bug: 262488856
Tag: #feature
Test: atest ContentControlIdKeeperTest bluetooth_le_audio_test bluetooth_test_broadcaster --no-bazel-mode
Change-Id: Iadcd75023c71b8ee76950633c7c779dc5ce2e6d1

system/bta/le_audio/content_control_id_keeper_test.cc
@@ -1,0 +1,126 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+#include "content_control_id_keeper.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "le_audio_types.h"
+
+namespace le_audio {
+
+TEST(ContentControlIdKeeperTest, testStart) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testMultipleSetGetOneCcid) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_one = 1;
+
+  ccid_keeper->SetCcid(
+      types::LeAudioContextType::MEDIA | types::LeAudioContextType::ALERTS,
+      ccid_one);
+  ASSERT_EQ(ccid_one, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(ccid_one, ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+
+  auto media_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(1lu, media_ccids.size());
+  ASSERT_NE(media_ccids.end(),
+            std::find(media_ccids.begin(), media_ccids.end(), ccid_one));
+
+  auto allerts_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::ALERTS));
+  ASSERT_EQ(1lu, allerts_ccids.size());
+  ASSERT_NE(allerts_ccids.end(),
+            std::find(allerts_ccids.begin(), allerts_ccids.end(), ccid_one));
+
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(1lu, all_ccids.size());
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_one));
+
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testMultipleSetGetMultipleCcids) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_two = 2;
+  int ccid_three = 3;
+
+  ccid_keeper->SetCcid(types::LeAudioContextType::MEDIA, ccid_two);
+  ASSERT_EQ(ccid_two, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ccid_keeper->SetCcid(types::LeAudioContextType::ALERTS, ccid_three);
+  ASSERT_EQ(ccid_three,
+            ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+
+  auto media_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(1lu, media_ccids.size());
+  ASSERT_NE(media_ccids.end(),
+            std::find(media_ccids.begin(), media_ccids.end(), ccid_two));
+
+  auto allerts_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::ALERTS));
+  ASSERT_EQ(1lu, allerts_ccids.size());
+  ASSERT_NE(allerts_ccids.end(),
+            std::find(allerts_ccids.begin(), allerts_ccids.end(), ccid_three));
+
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(2lu, all_ccids.size());
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_two));
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_three));
+
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testStop) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_two = 2;
+  int ccid_three = 3;
+  ccid_keeper->SetCcid(types::LeAudioContextType::MEDIA, ccid_two);
+  ccid_keeper->SetCcid(types::LeAudioContextType::ALERTS, ccid_three);
+
+  ccid_keeper->Stop();
+
+  // Check if it all got erased on Stop
+  ASSERT_EQ(-1, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(-1, ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(0lu, all_ccids.size());
+}
+
+}  // namespace le_audio

GrapheneOS/platform_packages_modules_Bluetooth

 - removes 'virtual' declaration as Pimpl design pattern is used for
   and complex context type
 - properly support multiple contexts at the top and native layer
   resulting in proper entries for each individual context
 - improves robustness by validating context input parameter
 - moves the helper function used for getting CCID list for the complex

system/bta/le_audio/content_control_id_keeper_test.cc
@@ -1,0 +1,126 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+#include "content_control_id_keeper.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "le_audio_types.h"
+
+namespace le_audio {
+
+TEST(ContentControlIdKeeperTest, testStart) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testMultipleSetGetOneCcid) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_one = 1;
+
+  ccid_keeper->SetCcid(
+      types::LeAudioContextType::MEDIA | types::LeAudioContextType::ALERTS,
+      ccid_one);
+  ASSERT_EQ(ccid_one, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(ccid_one, ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+
+  auto media_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(1LU, media_ccids.size());
+  ASSERT_NE(media_ccids.end(),
+            std::find(media_ccids.begin(), media_ccids.end(), ccid_one));
+
+  auto allerts_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::ALERTS));
+  ASSERT_EQ(1LU, allerts_ccids.size());
+  ASSERT_NE(allerts_ccids.end(),
+            std::find(allerts_ccids.begin(), allerts_ccids.end(), ccid_one));
+
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(1LU, all_ccids.size());
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_one));
+
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testMultipleSetGetMultipleCcids) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_two = 2;
+  int ccid_three = 3;
+
+  ccid_keeper->SetCcid(types::LeAudioContextType::MEDIA, ccid_two);
+  ASSERT_EQ(ccid_two, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ccid_keeper->SetCcid(types::LeAudioContextType::ALERTS, ccid_three);
+  ASSERT_EQ(ccid_three,
+            ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+
+  auto media_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(1LU, media_ccids.size());
+  ASSERT_NE(media_ccids.end(),
+            std::find(media_ccids.begin(), media_ccids.end(), ccid_two));
+
+  auto allerts_ccids = ccid_keeper->GetAllCcids(
+      types::AudioContexts(types::LeAudioContextType::ALERTS));
+  ASSERT_EQ(1LU, allerts_ccids.size());
+  ASSERT_NE(allerts_ccids.end(),
+            std::find(allerts_ccids.begin(), allerts_ccids.end(), ccid_three));
+
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(2LU, all_ccids.size());
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_two));
+  ASSERT_NE(all_ccids.end(),
+            std::find(all_ccids.begin(), all_ccids.end(), ccid_three));
+
+  ccid_keeper->Stop();
+}
+
+TEST(ContentControlIdKeeperTest, testStop) {
+  auto ccid_keeper = ContentControlIdKeeper::GetInstance();
+  ASSERT_NE(nullptr, ccid_keeper);
+  ccid_keeper->Start();
+
+  int ccid_two = 2;
+  int ccid_three = 3;
+  ccid_keeper->SetCcid(types::LeAudioContextType::MEDIA, ccid_two);
+  ccid_keeper->SetCcid(types::LeAudioContextType::ALERTS, ccid_three);
+
+  ccid_keeper->Stop();
+
+  // Check if it all got erased on Stop
+  ASSERT_EQ(-1, ccid_keeper->GetCcid(types::LeAudioContextType::MEDIA));
+  ASSERT_EQ(-1, ccid_keeper->GetCcid(types::LeAudioContextType::ALERTS));
+  auto all_ccids = ccid_keeper->GetAllCcids(types::LeAudioContextType::MEDIA |
+                                            types::LeAudioContextType::ALERTS);
+  ASSERT_EQ(0LU, all_ccids.size());
+}
+
+}  // namespace le_audio