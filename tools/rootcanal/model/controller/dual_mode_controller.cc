miyu-xu/packages-modules-Bluetooth

RootCanal: Pass RSSI information in device Receive / Send methods
Test: m root-canal
Change-Id: I1fff099890032e51aaaa087176df09ffb8ce3e68

tools/rootcanal/model/controller/dual_mode_controller.cc
@@ -42,8 +42,8 @@ std::string DualModeController::GetTypeString() const {
 }
 
 void DualModeController::IncomingPacket(
-    model::packets::LinkLayerPacketView incoming) {
-  link_layer_controller_.IncomingPacket(incoming);
+    model::packets::LinkLayerPacketView incoming, int8_t rssi) {
+  link_layer_controller_.IncomingPacket(incoming, rssi);
 }
 
 void DualModeController::TimerTick() { link_layer_controller_.TimerTick(); }
@@ -83,8 +83,8 @@ DualModeController::DualModeController(const std::string& properties_filename,
 
   link_layer_controller_.RegisterRemoteChannel(
       [this](std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-             Phy::Type phy_type) {
-        this->SendLinkLayerPacket(packet, phy_type);
+             Phy::Type phy_type, uint8_t tx_power) {
+        this->SendLinkLayerPacket(packet, phy_type, tx_power);
       });
 
   std::array<uint8_t, 64> supported_commands{0};

tools/rootcanal/model/controller/dual_mode_controller_python3.cc
@@ -77,8 +77,7 @@ class BaseController : public DualModeController {
 
   virtual void SendLinkLayerPacket(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      Phy::Type phy_type_) override {
-    (void)phy_type_;
+      Phy::Type /*phy_type*/, uint8_t /*tx_power*/) override {
     auto bytes = std::make_shared<std::vector<uint8_t>>();
     bluetooth::packet::BitInserter inserter(*bytes);
     bytes->reserve(packet->size());
@@ -212,11 +211,11 @@ PYBIND11_MODULE(lib_rootcanal_python3, m) {
         }
       });
 
-  // Implement method BaseController.receive_hci which
+  // Implement method BaseController.send_ll which
   // injects LL packets into the controller as if sent over the air.
   basic_controller.def(
       "send_ll", [](std::shared_ptr<rootcanal::BaseController> controller,
-                    py::bytes data) {
+                    py::bytes data, int rssi) {
         std::string data_str = data;
         std::shared_ptr<std::vector<uint8_t>> bytes =
             std::make_shared<std::vector<uint8_t>>(data_str.begin(),
@@ -230,7 +229,7 @@ PYBIND11_MODULE(lib_rootcanal_python3, m) {
           std::cerr << "Dropping malformed LL packet" << std::endl;
           return;
         }
-        controller->IncomingPacket(std::move(packet));
+        controller->IncomingPacket(std::move(packet), rssi);
       });
 }
 

tools/rootcanal/model/controller/link_layer_controller.h
@@ -141,13 +141,11 @@ class LinkLayerController {
  private:
   void SendDisconnectionCompleteEvent(uint16_t handle, ErrorCode reason);
 
-  void IncomingPacketWithRssi(model::packets::LinkLayerPacketView incoming,
-                              uint8_t rssi);
-
  public:
   const Address& GetAddress() const;
 
-  void IncomingPacket(model::packets::LinkLayerPacketView incoming);
+  void IncomingPacket(model::packets::LinkLayerPacketView incoming,
+                      int8_t rssi);
 
   void TimerTick();
 
@@ -180,9 +178,9 @@ class LinkLayerController {
           send_iso);
 
   void RegisterRemoteChannel(
-      const std::function<void(
-          std::shared_ptr<model::packets::LinkLayerPacketBuilder>, Phy::Type)>&
-          send_to_remote);
+      const std::function<
+          void(std::shared_ptr<model::packets::LinkLayerPacketBuilder>,
+               Phy::Type, uint8_t)>& send_to_remote);
 
   // Set the callbacks for scheduling tasks.
   void RegisterTaskScheduler(
@@ -542,12 +540,11 @@ class LinkLayerController {
 
  protected:
   void SendLinkLayerPacket(
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
+      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      uint8_t tx_power = 0);
   void SendLeLinkLayerPacket(
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
-  void SendLeLinkLayerPacketWithRssi(
-      Address source_address, Address destination_address, uint8_t rssi,
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
+      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      uint8_t tx_power = 0);
 
   void IncomingAclPacket(model::packets::LinkLayerPacketView incoming);
   void IncomingScoPacket(model::packets::LinkLayerPacketView incoming);
@@ -903,7 +900,7 @@ class LinkLayerController {
 
   // Callback to send packets to remote devices.
   std::function<void(std::shared_ptr<model::packets::LinkLayerPacketBuilder>,
-                     Phy::Type phy_type)>
+                     Phy::Type phy_type, uint8_t tx_power)>
       send_to_remote_;
 
   uint32_t oob_id_{1};

tools/rootcanal/model/devices/device.cc
@@ -50,19 +50,19 @@ void Device::UnregisterPhyLayer(Phy::Type phy_type, uint32_t factory_id) {
 
 void Device::SendLinkLayerPacket(
     std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-    Phy::Type phy_type) {
+    Phy::Type phy_type, uint8_t tx_power) {
   for (auto phy : phy_layers_) {
     if (phy != nullptr && phy->GetType() == phy_type) {
-      phy->Send(packet);
+      phy->Send(packet, tx_power);
     }
   }
 }
 
 void Device::SendLinkLayerPacket(model::packets::LinkLayerPacketView packet,
-                                 Phy::Type phy_type) {
+                                 Phy::Type phy_type, uint8_t tx_power) {
   for (auto phy : phy_layers_) {
     if (phy != nullptr && phy->GetType() == phy_type) {
-      phy->Send(packet);
+      phy->Send(packet, tx_power);
     }
   }
 }

tools/rootcanal/model/devices/device.h
@@ -58,14 +58,15 @@ class Device {
 
   void UnregisterPhyLayer(Phy::Type phy_type, uint32_t factory_id);
 
-  virtual void IncomingPacket(model::packets::LinkLayerPacketView packet){};
+  virtual void IncomingPacket(model::packets::LinkLayerPacketView packet,
+                              int8_t rssi){};
 
   virtual void SendLinkLayerPacket(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      Phy::Type phy_type);
+      Phy::Type phy_type, uint8_t tx_power = 0);
 
   virtual void SendLinkLayerPacket(model::packets::LinkLayerPacketView packet,
-                                   Phy::Type phy_type);
+                                   Phy::Type phy_type, uint8_t tx_power = 0);
 
   virtual void Close();
 

tools/rootcanal/model/setup/phy_layer_factory.cc
@@ -28,7 +28,7 @@ Phy::Type PhyLayerFactory::GetType() const { return phy_type_; }
 uint32_t PhyLayerFactory::GetFactoryId() const { return factory_id_; }
 
 std::shared_ptr<PhyLayer> PhyLayerFactory::GetPhyLayer(
-    const std::function<void(model::packets::LinkLayerPacketView)>&
+    const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
         device_receive,
     uint32_t device_id) {
   std::shared_ptr<PhyLayer> new_phy = std::make_shared<PhyLayerImpl>(
@@ -57,7 +57,7 @@ void PhyLayerFactory::UnregisterAllPhyLayers() {
 
 void PhyLayerFactory::Send(
     const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-    uint32_t id, [[maybe_unused]] uint32_t device_id) {
+    uint32_t id, [[maybe_unused]] uint32_t device_id, uint8_t tx_power) {
   // Convert from a Builder to a View
   auto bytes = std::make_shared<std::vector<uint8_t>>();
   bluetooth::packet::BitInserter i(*bytes);
@@ -69,14 +69,25 @@ void PhyLayerFactory::Send(
       model::packets::LinkLayerPacketView::Create(packet_view);
   ASSERT(link_layer_packet_view.IsValid());
 
-  Send(link_layer_packet_view, id, device_id);
+  Send(link_layer_packet_view, id, device_id, tx_power);
+}
+
+int8_t PhyLayerFactory::ComputeRssi(uint32_t sender_id, uint32_t receiver_id,
+                                    uint8_t tx_power) {
+  // Perform no RSSI computation by default.
+  // Clients overriding this function should use the TX power and
+  // positional information to derive correct device-to-device RSSI.
+  static uint8_t rssi = 0;
+  rssi = (rssi + 5) % 128;
+  return -rssi;
 }
 
 void PhyLayerFactory::Send(model::packets::LinkLayerPacketView packet,
-                           uint32_t id, [[maybe_unused]] uint32_t device_id) {
+                           uint32_t id, [[maybe_unused]] uint32_t device_id,
+                           uint8_t tx_power) {
   for (const auto& phy : phy_layers_) {
     if (id != phy->GetId()) {
-      phy->Receive(packet);
+      phy->Receive(packet, ComputeRssi(device_id, phy->GetId(), tx_power));
     }
   }
 }
@@ -109,7 +120,7 @@ std::string PhyLayerFactory::ToString() const {
 
 PhyLayerImpl::PhyLayerImpl(
     Phy::Type phy_type, uint32_t id,
-    const std::function<void(model::packets::LinkLayerPacketView)>&
+    const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
         device_receive,
     uint32_t device_id, PhyLayerFactory* factory)
     : PhyLayer(phy_type, id, device_receive, device_id), factory_(factory) {}
@@ -117,12 +128,14 @@ PhyLayerImpl::PhyLayerImpl(
 PhyLayerImpl::~PhyLayerImpl() {}
 
 void PhyLayerImpl::Send(
-    const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet) {
-  factory_->Send(packet, GetId(), GetDeviceId());
+    const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+    uint8_t tx_power) {
+  factory_->Send(packet, GetId(), GetDeviceId(), tx_power);
 }
 
-void PhyLayerImpl::Send(model::packets::LinkLayerPacketView packet) {
-  factory_->Send(packet, GetId(), GetDeviceId());
+void PhyLayerImpl::Send(model::packets::LinkLayerPacketView packet,
+                        uint8_t tx_power) {
+  factory_->Send(packet, GetId(), GetDeviceId(), tx_power);
 }
 
 void PhyLayerImpl::Unregister() { factory_->UnregisterPhyLayer(GetId()); }
@@ -131,8 +144,9 @@ bool PhyLayerImpl::IsFactoryId(uint32_t id) {
   return factory_->GetFactoryId() == id;
 }
 
-void PhyLayerImpl::Receive(model::packets::LinkLayerPacketView packet) {
-  transmit_to_device_(packet);
+void PhyLayerImpl::Receive(model::packets::LinkLayerPacketView packet,
+                           int8_t rssi) {
+  transmit_to_device_(packet, rssi);
 }
 
 void PhyLayerImpl::TimerTick() {}

tools/rootcanal/model/setup/phy_layer_factory.h
@@ -39,7 +39,7 @@ class PhyLayerFactory {
   uint32_t GetFactoryId() const;
 
   std::shared_ptr<PhyLayer> GetPhyLayer(
-      const std::function<void(model::packets::LinkLayerPacketView)>&
+      const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
           device_receive,
       uint32_t device_id);
 
@@ -54,11 +54,15 @@ class PhyLayerFactory {
  protected:
   virtual void Send(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      uint32_t phy_id, uint32_t device_id);
-  virtual void Send(
-      model::packets::LinkLayerPacketView packet,
-      uint32_t phy_id, uint32_t device_id);
+      uint32_t phy_id, uint32_t device_id, uint8_t tx_power);
+  virtual void Send(model::packets::LinkLayerPacketView packet, uint32_t phy_id,
+                    uint32_t device_id, uint8_t tx_power);
   std::list<std::shared_ptr<PhyLayer>> phy_layers_;
+
+  // Compute the RSSI for a packet sent from one device to the other
+  // with the specified TX power.
+  virtual int8_t ComputeRssi(uint32_t sender_id, uint32_t receiver_id,
+                             uint8_t tx_power);
 
  private:
   Phy::Type phy_type_;
@@ -69,15 +73,17 @@ class PhyLayerFactory {
 class PhyLayerImpl : public PhyLayer {
  public:
   PhyLayerImpl(Phy::Type phy_type, uint32_t id,
-               const std::function<void(model::packets::LinkLayerPacketView)>&
-                   device_receive,
+               const std::function<void(model::packets::LinkLayerPacketView,
+                                        int8_t)>& device_receive,
                uint32_t device_id, PhyLayerFactory* factory);
   ~PhyLayerImpl() override;
 
-  void Send(
-      std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet) override;
-  void Send(model::packets::LinkLayerPacketView packet) override;
-  void Receive(model::packets::LinkLayerPacketView packet) override;
+  void Send(std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+            uint8_t tx_power) override;
+  void Send(model::packets::LinkLayerPacketView packet,
+            uint8_t tx_power) override;
+  void Receive(model::packets::LinkLayerPacketView packet,
+               int8_t rssi) override;
   void Unregister() override;
   bool IsFactoryId(uint32_t factory_id) override;
   void TimerTick() override;

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 264630489

tools/rootcanal/model/controller/dual_mode_controller.cc
@@ -42,8 +42,8 @@ std::string DualModeController::GetTypeString() const {
 }
 
 void DualModeController::IncomingPacket(
-    model::packets::LinkLayerPacketView incoming) {
-  link_layer_controller_.IncomingPacket(incoming);
+    model::packets::LinkLayerPacketView incoming, int8_t rssi) {
+  link_layer_controller_.IncomingPacket(incoming, rssi);
 }
 
 void DualModeController::TimerTick() { link_layer_controller_.TimerTick(); }
@@ -83,8 +83,8 @@ DualModeController::DualModeController(const std::string& properties_filename,
 
   link_layer_controller_.RegisterRemoteChannel(
       [this](std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-             Phy::Type phy_type) {
-        this->SendLinkLayerPacket(packet, phy_type);
+             Phy::Type phy_type, int8_t tx_power) {
+        this->SendLinkLayerPacket(packet, phy_type, tx_power);
       });
 
   std::array<uint8_t, 64> supported_commands{0};

tools/rootcanal/model/controller/dual_mode_controller_python3.cc
@@ -77,8 +77,7 @@ class BaseController : public DualModeController {
 
   virtual void SendLinkLayerPacket(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      Phy::Type phy_type_) override {
-    (void)phy_type_;
+      Phy::Type /*phy_type*/, int8_t /*tx_power*/) override {
     auto bytes = std::make_shared<std::vector<uint8_t>>();
     bluetooth::packet::BitInserter inserter(*bytes);
     bytes->reserve(packet->size());
@@ -212,11 +211,11 @@ PYBIND11_MODULE(lib_rootcanal_python3, m) {
         }
       });
 
-  // Implement method BaseController.receive_hci which
+  // Implement method BaseController.send_ll which
   // injects LL packets into the controller as if sent over the air.
   basic_controller.def(
       "send_ll", [](std::shared_ptr<rootcanal::BaseController> controller,
-                    py::bytes data) {
+                    py::bytes data, int rssi) {
         std::string data_str = data;
         std::shared_ptr<std::vector<uint8_t>> bytes =
             std::make_shared<std::vector<uint8_t>>(data_str.begin(),
@@ -230,7 +229,7 @@ PYBIND11_MODULE(lib_rootcanal_python3, m) {
           std::cerr << "Dropping malformed LL packet" << std::endl;
           return;
         }
-        controller->IncomingPacket(std::move(packet));
+        controller->IncomingPacket(std::move(packet), rssi);
       });
 }
 

tools/rootcanal/model/controller/link_layer_controller.h
@@ -141,13 +141,11 @@ class LinkLayerController {
  private:
   void SendDisconnectionCompleteEvent(uint16_t handle, ErrorCode reason);
 
-  void IncomingPacketWithRssi(model::packets::LinkLayerPacketView incoming,
-                              uint8_t rssi);
-
  public:
   const Address& GetAddress() const;
 
-  void IncomingPacket(model::packets::LinkLayerPacketView incoming);
+  void IncomingPacket(model::packets::LinkLayerPacketView incoming,
+                      int8_t rssi);
 
   void TimerTick();
 
@@ -180,9 +178,9 @@ class LinkLayerController {
           send_iso);
 
   void RegisterRemoteChannel(
-      const std::function<void(
-          std::shared_ptr<model::packets::LinkLayerPacketBuilder>, Phy::Type)>&
-          send_to_remote);
+      const std::function<
+          void(std::shared_ptr<model::packets::LinkLayerPacketBuilder>,
+               Phy::Type, int8_t)>& send_to_remote);
 
   // Set the callbacks for scheduling tasks.
   void RegisterTaskScheduler(
@@ -542,12 +540,11 @@ class LinkLayerController {
 
  protected:
   void SendLinkLayerPacket(
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
+      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      int8_t tx_power = 0);
   void SendLeLinkLayerPacket(
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
-  void SendLeLinkLayerPacketWithRssi(
-      Address source_address, Address destination_address, uint8_t rssi,
-      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet);
+      std::unique_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      int8_t tx_power = 0);
 
   void IncomingAclPacket(model::packets::LinkLayerPacketView incoming);
   void IncomingScoPacket(model::packets::LinkLayerPacketView incoming);
@@ -903,7 +900,7 @@ class LinkLayerController {
 
   // Callback to send packets to remote devices.
   std::function<void(std::shared_ptr<model::packets::LinkLayerPacketBuilder>,
-                     Phy::Type phy_type)>
+                     Phy::Type phy_type, int8_t tx_power)>
       send_to_remote_;
 
   uint32_t oob_id_{1};

tools/rootcanal/model/devices/device.cc
@@ -50,19 +50,19 @@ void Device::UnregisterPhyLayer(Phy::Type phy_type, uint32_t factory_id) {
 
 void Device::SendLinkLayerPacket(
     std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-    Phy::Type phy_type) {
+    Phy::Type phy_type, int8_t tx_power) {
   for (auto phy : phy_layers_) {
     if (phy != nullptr && phy->GetType() == phy_type) {
-      phy->Send(packet);
+      phy->Send(packet, tx_power);
     }
   }
 }
 
 void Device::SendLinkLayerPacket(model::packets::LinkLayerPacketView packet,
-                                 Phy::Type phy_type) {
+                                 Phy::Type phy_type, int8_t tx_power) {
   for (auto phy : phy_layers_) {
     if (phy != nullptr && phy->GetType() == phy_type) {
-      phy->Send(packet);
+      phy->Send(packet, tx_power);
     }
   }
 }

tools/rootcanal/model/devices/device.h
@@ -58,14 +58,15 @@ class Device {
 
   void UnregisterPhyLayer(Phy::Type phy_type, uint32_t factory_id);
 
-  virtual void IncomingPacket(model::packets::LinkLayerPacketView packet){};
+  virtual void IncomingPacket(model::packets::LinkLayerPacketView packet,
+                              int8_t rssi){};
 
   virtual void SendLinkLayerPacket(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      Phy::Type phy_type);
+      Phy::Type phy_type, int8_t tx_power = 0);
 
   virtual void SendLinkLayerPacket(model::packets::LinkLayerPacketView packet,
-                                   Phy::Type phy_type);
+                                   Phy::Type phy_type, int8_t tx_power = 0);
 
   virtual void Close();
 

tools/rootcanal/model/setup/phy_layer_factory.cc
@@ -28,7 +28,7 @@ Phy::Type PhyLayerFactory::GetType() const { return phy_type_; }
 uint32_t PhyLayerFactory::GetFactoryId() const { return factory_id_; }
 
 std::shared_ptr<PhyLayer> PhyLayerFactory::GetPhyLayer(
-    const std::function<void(model::packets::LinkLayerPacketView)>&
+    const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
         device_receive,
     uint32_t device_id) {
   std::shared_ptr<PhyLayer> new_phy = std::make_shared<PhyLayerImpl>(
@@ -57,7 +57,7 @@ void PhyLayerFactory::UnregisterAllPhyLayers() {
 
 void PhyLayerFactory::Send(
     const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-    uint32_t id, [[maybe_unused]] uint32_t device_id) {
+    uint32_t id, [[maybe_unused]] uint32_t device_id, int8_t tx_power) {
   // Convert from a Builder to a View
   auto bytes = std::make_shared<std::vector<uint8_t>>();
   bluetooth::packet::BitInserter i(*bytes);
@@ -69,14 +69,25 @@ void PhyLayerFactory::Send(
       model::packets::LinkLayerPacketView::Create(packet_view);
   ASSERT(link_layer_packet_view.IsValid());
 
-  Send(link_layer_packet_view, id, device_id);
+  Send(link_layer_packet_view, id, device_id, tx_power);
+}
+
+int8_t PhyLayerFactory::ComputeRssi(uint32_t sender_id, uint32_t receiver_id,
+                                    int8_t tx_power) {
+  // Perform no RSSI computation by default.
+  // Clients overriding this function should use the TX power and
+  // positional information to derive correct device-to-device RSSI.
+  static uint8_t rssi = 0;
+  rssi = (rssi + 5) % 128;
+  return static_cast<int8_t>(-rssi);
 }
 
 void PhyLayerFactory::Send(model::packets::LinkLayerPacketView packet,
-                           uint32_t id, [[maybe_unused]] uint32_t device_id) {
+                           uint32_t id, [[maybe_unused]] uint32_t device_id,
+                           int8_t tx_power) {
   for (const auto& phy : phy_layers_) {
     if (id != phy->GetId()) {
-      phy->Receive(packet);
+      phy->Receive(packet, ComputeRssi(device_id, phy->GetId(), tx_power));
     }
   }
 }
@@ -109,7 +120,7 @@ std::string PhyLayerFactory::ToString() const {
 
 PhyLayerImpl::PhyLayerImpl(
     Phy::Type phy_type, uint32_t id,
-    const std::function<void(model::packets::LinkLayerPacketView)>&
+    const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
         device_receive,
     uint32_t device_id, PhyLayerFactory* factory)
     : PhyLayer(phy_type, id, device_receive, device_id), factory_(factory) {}
@@ -117,12 +128,14 @@ PhyLayerImpl::PhyLayerImpl(
 PhyLayerImpl::~PhyLayerImpl() {}
 
 void PhyLayerImpl::Send(
-    const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet) {
-  factory_->Send(packet, GetId(), GetDeviceId());
+    const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+    int8_t tx_power) {
+  factory_->Send(packet, GetId(), GetDeviceId(), tx_power);
 }
 
-void PhyLayerImpl::Send(model::packets::LinkLayerPacketView packet) {
-  factory_->Send(packet, GetId(), GetDeviceId());
+void PhyLayerImpl::Send(model::packets::LinkLayerPacketView packet,
+                        int8_t tx_power) {
+  factory_->Send(packet, GetId(), GetDeviceId(), tx_power);
 }
 
 void PhyLayerImpl::Unregister() { factory_->UnregisterPhyLayer(GetId()); }
@@ -131,8 +144,9 @@ bool PhyLayerImpl::IsFactoryId(uint32_t id) {
   return factory_->GetFactoryId() == id;
 }
 
-void PhyLayerImpl::Receive(model::packets::LinkLayerPacketView packet) {
-  transmit_to_device_(packet);
+void PhyLayerImpl::Receive(model::packets::LinkLayerPacketView packet,
+                           int8_t rssi) {
+  transmit_to_device_(packet, rssi);
 }
 
 void PhyLayerImpl::TimerTick() {}

tools/rootcanal/model/setup/phy_layer_factory.h
@@ -39,7 +39,7 @@ class PhyLayerFactory {
   uint32_t GetFactoryId() const;
 
   std::shared_ptr<PhyLayer> GetPhyLayer(
-      const std::function<void(model::packets::LinkLayerPacketView)>&
+      const std::function<void(model::packets::LinkLayerPacketView, int8_t)>&
           device_receive,
       uint32_t device_id);
 
@@ -54,11 +54,15 @@ class PhyLayerFactory {
  protected:
   virtual void Send(
       std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
-      uint32_t phy_id, uint32_t device_id);
-  virtual void Send(
-      model::packets::LinkLayerPacketView packet,
-      uint32_t phy_id, uint32_t device_id);
+      uint32_t phy_id, uint32_t device_id, int8_t tx_power);
+  virtual void Send(model::packets::LinkLayerPacketView packet, uint32_t phy_id,
+                    uint32_t device_id, int8_t tx_power);
   std::list<std::shared_ptr<PhyLayer>> phy_layers_;
+
+  // Compute the RSSI for a packet sent from one device to the other
+  // with the specified TX power.
+  virtual int8_t ComputeRssi(uint32_t sender_id, uint32_t receiver_id,
+                             int8_t tx_power);
 
  private:
   Phy::Type phy_type_;
@@ -69,15 +73,17 @@ class PhyLayerFactory {
 class PhyLayerImpl : public PhyLayer {
  public:
   PhyLayerImpl(Phy::Type phy_type, uint32_t id,
-               const std::function<void(model::packets::LinkLayerPacketView)>&
-                   device_receive,
+               const std::function<void(model::packets::LinkLayerPacketView,
+                                        int8_t)>& device_receive,
                uint32_t device_id, PhyLayerFactory* factory);
   ~PhyLayerImpl() override;
 
-  void Send(
-      std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet) override;
-  void Send(model::packets::LinkLayerPacketView packet) override;
-  void Receive(model::packets::LinkLayerPacketView packet) override;
+  void Send(std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+            int8_t tx_power) override;
+  void Send(model::packets::LinkLayerPacketView packet,
+            int8_t tx_power) override;
+  void Receive(model::packets::LinkLayerPacketView packet,
+               int8_t rssi) override;
   void Unregister() override;
   bool IsFactoryId(uint32_t factory_id) override;
   void TimerTick() override;