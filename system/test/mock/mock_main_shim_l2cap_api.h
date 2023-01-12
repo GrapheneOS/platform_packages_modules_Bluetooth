0faf0ef92b1265efdf5d1b0ef5322643a3af1cc4
William Escande <wescande@google.com>
2023-01-11 23:24:54 -0800

    Mock static initialization order 06
    cpp static order initialization is not guarantee.
    In order to avoid the mess, we need to have a lazy init schema.
    Replacing all mock_main extern with the correct header
    ```
    's|extern std::map<std::string, int> mock_function_count_map;|#include "test/common/mock_functions.h"|'
    ```
    
    Bug: 265217208
    Test: atest --host
    Change-Id: I3a174f6c9438e16f8cfff6a4d3fcbe71bb78d0d9

--- system/test/mock/mock_main_shim_l2cap_api.h
+++ system/test/mock/mock_main_shim_l2cap_api.h
@@ -26,8 +26,6 @@
 #include <map>
 #include <string>
 
-extern std::map<std::string, int> mock_function_count_map;
-
 // Original included files, if any
 // NOTE: Since this is a mock file with mock definitions some number of
 //       include files may not be required.  The include-what-you-use
@@ -54,6 +52,7 @@ extern std::map<std::string, int> mock_function_count_map;
 #include "stack/include/btm_api.h"
 #include "stack/include/gatt_api.h"
 #include "stack/include/sco_hci_link_interface.h"
+#include "test/common/mock_functions.h"
 #include "types/ble_address_with_type.h"
 #include "types/raw_address.h"
 
