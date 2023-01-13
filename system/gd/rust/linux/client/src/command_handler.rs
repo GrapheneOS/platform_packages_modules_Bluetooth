miyu-xu/packages-modules-Bluetooth

Floss: Export HID reports methods to D-Bus layer
This exports HID GetReport, SetReport and SendData to the D-Bus layer
for the purpose of qualification.

Bug: 264642033
Test: Pass HID11/HOS/HCT/BV-01-C, HID11/HOS/HCT/BV-02-C and
HID11/HOS/HIT/BV-02-C
Tag: #floss

Change-Id: Icb5de4c71afed7f694591edc73c43ff1036a2dcd

system/gd/rust/linux/client/src/command_handler.rs
@@ -9,6 +9,7 @@ use crate::callbacks::{BtGattCallback, BtGattServerCallback};
 use crate::ClientContext;
 use crate::{console_red, console_yellow, print_error, print_info};
 use bt_topshim::btif::{BtConnectionState, BtStatus, BtTransport};
+use bt_topshim::profiles::hid_host::BthhReportType;
 use bt_topshim::profiles::{gatt::LePhy, ProfileConnectionState};
 use btstack::bluetooth::{BluetoothDevice, IBluetooth, IBluetoothQA};
 use btstack::bluetooth_gatt::{GattWriteType, IBluetoothGatt, ScanSettings, ScanType};
@@ -211,6 +212,18 @@ fn build_commands() -> HashMap<String, CommandOption> {
             ],
             description: String::from("Socket manager utilities."),
             function_pointer: CommandHandler::cmd_socket,
+        },
+    );
+    command_options.insert(
+        String::from("hid"),
+        CommandOption {
+            rules: vec![
+                String::from("hid get-report <address> <Input|Output|Feature> <report_id>"),
+                String::from("hid set-report <address> <Input|Output|Feature> <report_value>"),
+                String::from("hid send-data <address> <data>"),
+            ],
+            description: String::from("Socket manager utilities."),
+            function_pointer: CommandHandler::cmd_hid,
         },
     );
     command_options.insert(
@@ -1384,6 +1397,64 @@ impl CommandHandler {
                 }
             }
 
+            _ => return Err(CommandError::InvalidArgs),
+        };
+
+        Ok(())
+    }
+
+    fn cmd_hid(&mut self, args: &Vec<String>) -> CommandResult {
+        if !self.context.lock().unwrap().adapter_ready {
+            return Err(self.adapter_not_ready());
+        }
+
+        let command = get_arg(args, 0)?;
+
+        match &command[..] {
+            "get-report" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let report_type = match &get_arg(args, 2)?[..] {
+                    "Input" => BthhReportType::InputReport,
+                    "Output" => BthhReportType::OutputReport,
+                    "Feature" => BthhReportType::FeatureReport,
+                    _ => {
+                        return Err("Failed to parse report type".into());
+                    }
+                };
+                let report_id = String::from(get_arg(args, 3)?)
+                    .parse::<u8>()
+                    .or(Err("Failed parsing report_id"))?;
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().get_report(
+                    addr,
+                    report_type,
+                    report_id,
+                );
+            }
+            "set-report" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let report_type = match &get_arg(args, 2)?[..] {
+                    "Input" => BthhReportType::InputReport,
+                    "Output" => BthhReportType::OutputReport,
+                    "Feature" => BthhReportType::FeatureReport,
+                    _ => {
+                        return Err("Failed to parse report type".into());
+                    }
+                };
+                let report_value = String::from(get_arg(args, 3)?);
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().set_report(
+                    addr,
+                    report_type,
+                    report_value,
+                );
+            }
+            "send-data" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let data = String::from(get_arg(args, 2)?);
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().send_data(addr, data);
+            }
             _ => return Err(CommandError::InvalidArgs),
         };
 

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -5,6 +5,7 @@ use bt_topshim::btif::{
     BtTransport, Uuid, Uuid128Bit,
 };
 use bt_topshim::profiles::gatt::{AdvertisingStatus, GattStatus, LePhy};
+use bt_topshim::profiles::hid_host::BthhReportType;
 use bt_topshim::profiles::socket::SocketType;
 use bt_topshim::profiles::ProfileConnectionState;
 
@@ -75,6 +76,7 @@ impl_dbus_arg_enum!(SocketType);
 impl_dbus_arg_enum!(SuspendMode);
 impl_dbus_arg_enum!(SuspendType);
 impl_dbus_arg_from_into!(Uuid, Vec<u8>);
+impl_dbus_arg_enum!(BthhReportType);
 
 impl RefArgToRust for Uuid {
     type RustType = Vec<u8>;
@@ -635,6 +637,24 @@ impl IBluetoothQA for BluetoothQADBus {
     fn set_connectable(&mut self, mode: bool) -> bool {
         dbus_generated!()
     }
+
+    #[dbus_method("GetReport")]
+    fn get_report(&mut self, addr: String, report_type: BthhReportType, report_id: u8) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SetReport")]
+    fn set_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report: String,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SendData")]
+    fn send_data(&mut self, addr: String, data: String) -> BtStatus;
 }
 
 #[dbus_propmap(AdapterWithEnabled)]

system/gd/rust/linux/service/src/iface_bluetooth.rs
@@ -5,6 +5,8 @@ use bt_topshim::btif::{
 use bt_topshim::profiles::socket::SocketType;
 use bt_topshim::profiles::ProfileConnectionState;
 
+use bt_topshim::profiles::hid_host::BthhReportType;
+
 use btstack::bluetooth::{
     Bluetooth, BluetoothDevice, IBluetooth, IBluetoothCallback, IBluetoothConnectionCallback,
     IBluetoothQA,
@@ -591,6 +593,8 @@ impl ISuspendCallback for SuspendCallbackDBus {
     }
 }
 
+impl_dbus_arg_enum!(BthhReportType);
+
 #[allow(dead_code)]
 struct IBluetoothQADBus {}
 
@@ -608,6 +612,26 @@ impl IBluetoothQA for IBluetoothQADBus {
 
     #[dbus_method("SetConnectable")]
     fn set_connectable(&mut self, mode: bool) -> bool {
+        dbus_generated!()
+    }
+
+    #[dbus_method("GetReport")]
+    fn get_report(&mut self, addr: String, report_type: BthhReportType, report_id: u8) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SetReport")]
+    fn set_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report: String,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SendData")]
+    fn send_data(&mut self, addr: String, data: String) -> BtStatus {
         dbus_generated!()
     }
 }

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/linux/client/src/command_handler.rs
@@ -9,6 +9,7 @@ use crate::callbacks::{BtGattCallback, BtGattServerCallback};
 use crate::ClientContext;
 use crate::{console_red, console_yellow, print_error, print_info};
 use bt_topshim::btif::{BtConnectionState, BtStatus, BtTransport};
+use bt_topshim::profiles::hid_host::BthhReportType;
 use bt_topshim::profiles::{gatt::LePhy, ProfileConnectionState};
 use btstack::bluetooth::{BluetoothDevice, IBluetooth, IBluetoothQA};
 use btstack::bluetooth_gatt::{GattWriteType, IBluetoothGatt, ScanSettings, ScanType};
@@ -211,6 +212,18 @@ fn build_commands() -> HashMap<String, CommandOption> {
             ],
             description: String::from("Socket manager utilities."),
             function_pointer: CommandHandler::cmd_socket,
+        },
+    );
+    command_options.insert(
+        String::from("hid"),
+        CommandOption {
+            rules: vec![
+                String::from("hid get-report <address> <Input|Output|Feature> <report_id>"),
+                String::from("hid set-report <address> <Input|Output|Feature> <report_value>"),
+                String::from("hid send-data <address> <data>"),
+            ],
+            description: String::from("Socket manager utilities."),
+            function_pointer: CommandHandler::cmd_hid,
         },
     );
     command_options.insert(
@@ -1384,6 +1397,64 @@ impl CommandHandler {
                 }
             }
 
+            _ => return Err(CommandError::InvalidArgs),
+        };
+
+        Ok(())
+    }
+
+    fn cmd_hid(&mut self, args: &Vec<String>) -> CommandResult {
+        if !self.context.lock().unwrap().adapter_ready {
+            return Err(self.adapter_not_ready());
+        }
+
+        let command = get_arg(args, 0)?;
+
+        match &command[..] {
+            "get-report" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let report_type = match &get_arg(args, 2)?[..] {
+                    "Input" => BthhReportType::InputReport,
+                    "Output" => BthhReportType::OutputReport,
+                    "Feature" => BthhReportType::FeatureReport,
+                    _ => {
+                        return Err("Failed to parse report type".into());
+                    }
+                };
+                let report_id = String::from(get_arg(args, 3)?)
+                    .parse::<u8>()
+                    .or(Err("Failed parsing report_id"))?;
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().get_hid_report(
+                    addr,
+                    report_type,
+                    report_id,
+                );
+            }
+            "set-report" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let report_type = match &get_arg(args, 2)?[..] {
+                    "Input" => BthhReportType::InputReport,
+                    "Output" => BthhReportType::OutputReport,
+                    "Feature" => BthhReportType::FeatureReport,
+                    _ => {
+                        return Err("Failed to parse report type".into());
+                    }
+                };
+                let report_value = String::from(get_arg(args, 3)?);
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().set_hid_report(
+                    addr,
+                    report_type,
+                    report_value,
+                );
+            }
+            "send-data" => {
+                let addr = String::from(get_arg(args, 1)?);
+                let data = String::from(get_arg(args, 2)?);
+
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().send_hid_data(addr, data);
+            }
             _ => return Err(CommandError::InvalidArgs),
         };
 

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -5,6 +5,7 @@ use bt_topshim::btif::{
     BtTransport, Uuid, Uuid128Bit,
 };
 use bt_topshim::profiles::gatt::{AdvertisingStatus, GattStatus, LePhy};
+use bt_topshim::profiles::hid_host::BthhReportType;
 use bt_topshim::profiles::socket::SocketType;
 use bt_topshim::profiles::ProfileConnectionState;
 
@@ -75,6 +76,7 @@ impl_dbus_arg_enum!(SocketType);
 impl_dbus_arg_enum!(SuspendMode);
 impl_dbus_arg_enum!(SuspendType);
 impl_dbus_arg_from_into!(Uuid, Vec<u8>);
+impl_dbus_arg_enum!(BthhReportType);
 
 impl RefArgToRust for Uuid {
     type RustType = Vec<u8>;
@@ -635,6 +637,29 @@ impl IBluetoothQA for BluetoothQADBus {
     fn set_connectable(&mut self, mode: bool) -> bool {
         dbus_generated!()
     }
+
+    #[dbus_method("GetHIDReport")]
+    fn get_hid_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report_id: u8,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SetHIDReport")]
+    fn set_hid_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report: String,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SendHIDData")]
+    fn send_hid_data(&mut self, addr: String, data: String) -> BtStatus;
 }
 
 #[dbus_propmap(AdapterWithEnabled)]

system/gd/rust/linux/service/src/iface_bluetooth.rs
@@ -5,6 +5,8 @@ use bt_topshim::btif::{
 use bt_topshim::profiles::socket::SocketType;
 use bt_topshim::profiles::ProfileConnectionState;
 
+use bt_topshim::profiles::hid_host::BthhReportType;
+
 use btstack::bluetooth::{
     Bluetooth, BluetoothDevice, IBluetooth, IBluetoothCallback, IBluetoothConnectionCallback,
     IBluetoothQA,
@@ -591,6 +593,8 @@ impl ISuspendCallback for SuspendCallbackDBus {
     }
 }
 
+impl_dbus_arg_enum!(BthhReportType);
+
 #[allow(dead_code)]
 struct IBluetoothQADBus {}
 
@@ -608,6 +612,31 @@ impl IBluetoothQA for IBluetoothQADBus {
 
     #[dbus_method("SetConnectable")]
     fn set_connectable(&mut self, mode: bool) -> bool {
+        dbus_generated!()
+    }
+
+    #[dbus_method("GetHIDReport")]
+    fn get_hid_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report_id: u8,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SetHIDReport")]
+    fn set_hid_report(
+        &mut self,
+        addr: String,
+        report_type: BthhReportType,
+        report: String,
+    ) -> BtStatus {
+        dbus_generated!()
+    }
+
+    #[dbus_method("SendHIDData")]
+    fn send_hid_data(&mut self, addr: String, data: String) -> BtStatus {
         dbus_generated!()
     }
 }