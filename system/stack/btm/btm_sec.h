miyu-xu/packages-modules-Bluetooth

[Non-Discoverable Mode] Selectively hide Device Name characteristic
When in non-discoverable mode, this characteristic should not be
readable (5.3 Vol 3C 12.1 Device Name Characteristic)

Bug: 254314964
Test: compiles
Change-Id: I4f9441eb099269e2c99a45ff7e505d71ed76d914

system/stack/btm/btm_sec.h
@@ -97,6 +97,7 @@ bool BTM_IsEncrypted(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsLinkKeyAuthed(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsLinkKeyKnown(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsAuthenticated(const RawAddress& bd_addr, tBT_TRANSPORT transport);
+bool BTM_IsDiscovering(const RawAddress& bd_addr);
 
 /*******************************************************************************
  *

system/test/mock/mock_stack_btm_sec.cc
@@ -67,6 +67,10 @@ bool BTM_IsAuthenticated(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
   mock_function_count_map[__func__]++;
   return false;
 }
+bool BTM_IsDiscovering(const RawAddress& bd_addr) {
+  mock_function_count_map[__func__]++;
+  return false;
+}
 bool BTM_IsEncrypted(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
   mock_function_count_map[__func__]++;
   return false;

GrapheneOS/platform_packages_modules_Bluetooth

system/stack/btm/btm_sec.h
@@ -97,6 +97,7 @@ bool BTM_IsEncrypted(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsLinkKeyAuthed(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsLinkKeyKnown(const RawAddress& bd_addr, tBT_TRANSPORT transport);
 bool BTM_IsAuthenticated(const RawAddress& bd_addr, tBT_TRANSPORT transport);
+bool BTM_CanReadDiscoverableCharacteristics(const RawAddress& bd_addr);
 
 /*******************************************************************************
  *

system/test/mock/mock_stack_btm_sec.cc
@@ -67,6 +67,10 @@ bool BTM_IsAuthenticated(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
   mock_function_count_map[__func__]++;
   return false;
 }
+bool BTM_CanReadDiscoverableCharacteristics(const RawAddress& bd_addr) {
+  mock_function_count_map[__func__]++;
+  return false;
+}
 bool BTM_IsEncrypted(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
   mock_function_count_map[__func__]++;
   return false;