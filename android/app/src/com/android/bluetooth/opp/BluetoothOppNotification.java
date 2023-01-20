88cbe29f3f9ac59ec9f0b0484c3124d6b90d8bd8
My Name <thebestguy@google.com>
2023-01-20 06:33:18 +0000

    Cherry-pick ag/21002020 to aosp
    Test: atest BluetoothInstrumentationTests
    Bug: 237467631
    Tag: #refactor
    Change-Id: Ia1765bbd024e86a8482f15a93234933edd32024a

--- android/app/src/com/android/bluetooth/opp/BluetoothOppNotification.java
+++ android/app/src/com/android/bluetooth/opp/BluetoothOppNotification.java
@@ -61,7 +61,7 @@ import java.util.HashMap;
  * where there is an ongoing transfer, incoming transfer need confirm and
  * complete (successful or failed) transfer.
  */
-class BluetoothOppNotification {
+public class BluetoothOppNotification {
     private static final String TAG = "BluetoothOppNotification";
     private static final boolean V = Constants.VERBOSE;
 
@@ -152,7 +152,7 @@ class BluetoothOppNotification {
      * @param ctx The context to use to obtain access to the Notification
      *            Service
      */
-    BluetoothOppNotification(Context ctx) {
+    public BluetoothOppNotification(Context ctx) {
         mContext = ctx;
         mNotificationMgr = mContext.getSystemService(NotificationManager.class);
         mNotificationChannel = new NotificationChannel(OPP_NOTIFICATION_CHANNEL,
