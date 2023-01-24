miyu-xu/packages-modules-Bluetooth

Bt: Add support for interop conf feature
Add support for interop conf feature.

Submitted on behalf of a third-party:
Qualcomm Innovation Center, The Linux Foundation, Broadcom Corporation,
The Android Open Source Project, Google, Inc
License rights, if any, to the submission are granted solely by the
copyright owner of such submission under its applicable intellectual
property.
/*
* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
* Copyright (c) 2016-2017, 2020, The Linux Foundation. All rights reserved.
* Copyright 2014-2015 Google, Inc
* Copyright 2009-2012 Broadcom Corporation
* Copyright (C) 2012 The Android Open Source Project
* Licensed under the Apache License, Version 2.0 (the "License");
* you may not use this file except in compliance with the License.
* You may obtain a copy of the License at
*
*      http://www.apache.org/licenses/LICENSE-2.0
*
* Unless required by applicable law or agreed to in writing, software
* distributed under the License is distributed on an "AS IS" BASIS,
* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
* See the License for the specific language governing permissions and
* limitations under the License.
*/
Third Party code includes additions/modifications from Qualcomm Innovation Center, Inc

Change-Id: I959ee79fe74d0b22a26b3df1633ee88670d396d1

android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016-2017 The Linux Foundation. All rights reserved
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -109,6 +110,7 @@ import com.android.bluetooth.bass_client.BassClientService;
 import com.android.bluetooth.btservice.RemoteDevices.DeviceProperties;
 import com.android.bluetooth.btservice.activityattribution.ActivityAttributionService;
 import com.android.bluetooth.btservice.bluetoothkeystore.BluetoothKeystoreService;
+import com.android.bluetooth.btservice.InteropUtil.InteropFeature;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
 import com.android.bluetooth.btservice.storage.MetadataDatabase;
 import com.android.bluetooth.csip.CsipSetCoordinatorService;
@@ -4918,6 +4920,10 @@ public class AdapterService extends Service {
         return mAdapterProperties.isA2dpOffloadEnabled();
     }
 
+    public String[] getWhitelistedMediaPlayers() {
+        return mAdapterProperties.getWhitelistedMediaPlayers();
+    }
+
     private BluetoothActivityEnergyInfo reportActivityInfo() {
         if (mAdapterProperties.getState() != BluetoothAdapter.STATE_ON
                 || !mAdapterProperties.isActivityAndEnergyReportingSupported()) {
@@ -5611,6 +5617,35 @@ public class AdapterService extends Service {
         mRemoteDevices.updateBatteryLevel(device, batteryLevel);
     }
 
+    public boolean interopMatchAddr(InteropFeature feature, String address) {
+         return interopMatchAddrNative(feature.name(), address);
+     }
+
+     public boolean interopMatchName(InteropFeature feature, String name) {
+         return interopMatchNameNative(feature.name(), name);
+     }
+
+     public boolean interopMatchAddrOrName(InteropFeature feature, String address) {
+         return interopMatchAddrOrNameNative(feature.name(), address);
+     }
+
+     public void interopDatabaseAddAddr(InteropFeature feature,
+             String address, int length) {
+         interopDatabaseAddRemoveAddrNative(true, feature.name(), address, length);
+     }
+
+     public void interopDatabaseRemoveAddr(InteropFeature feature, String address) {
+         interopDatabaseAddRemoveAddrNative(false, feature.name(), address, 0);
+     }
+
+     public void interopDatabaseAddName(InteropFeature feature, String name) {
+         interopDatabaseAddRemoveNameNative(true, feature.name(), name);
+     }
+
+     public void interopDatabaseRemoveName(InteropFeature feature, String name) {
+         interopDatabaseAddRemoveNameNative(false, feature.name(), name);
+     }
+
     static native void classInitNative();
 
     native boolean initNative(boolean startRestricted, boolean isCommonCriteriaMode,
@@ -5705,6 +5740,14 @@ public class AdapterService extends Service {
     /*package*/ native void requestMaximumTxDataLengthNative(byte[] address);
 
     private native boolean allowLowLatencyAudioNative(boolean allowed, byte[] address);
+
+    private native boolean interopMatchAddrNative(String feature_name, String address);
+    private native boolean interopMatchNameNative(String feature_name, String name);
+    private native boolean interopMatchAddrOrNameNative(String feature_name, String address);
+    private native void interopDatabaseAddRemoveAddrNative(boolean do_add,
+            String feature_name, String address, int length);
+    private native void interopDatabaseAddRemoveNameNative(boolean do_add,
+            String feature_name, String name);
 
     // Returns if this is a mock object. This is currently used in testing so that we may not call
     // System.exit() while finalizing the object. Otherwise GC of mock objects unfortunately ends up

GrapheneOS/platform_packages_modules_Bluetooth

Snapshot of:
558ee40545537cac75f561c6b35c21ec388218a5
3e76e1c8460e566e37eb2e6fa2116804d967e4e7
5229383750c1278c7bd3d940d59c5c50a18a1053
5752baca5abdfa5f30e803a67827f119fbe03339
4a63a13ebe0b8576a30fe1d5efc1b6d99ebab28f
2a7911429a78d33de0570335fac08ef9469a3621
73c5720caabd944c11906192298c858e2e586885
2c3b5e9ac41b4bbb946387334e7e0b0a4c32c65e
d907b0cf64f1eecdd87b96c09980d89ccf9fc897
a7d8799123a10cfc5a32bc8aafdd938166f912db
dbd88bfc3f8a63c8df24571205413a1170e947e2
319d2310997dd53f835e1729aa513bef6839c32e
7489832bbf0482ac19bd67d9446f8b0323c850d9
deb8aa88ab3df0f11981311beb50281c0cef1f43
050e9ea9d75d5a576aedff7e0a155b5b98a582ae
9a080f23406f101f76956b8226a5956e0e875062
d1a2484905c10821dfb5dfa7bd5579e5ff5f307e
f726e3467f570f8c28d9ea9d5f8c4040445e7791
272d58799f73c044aadc9daf74991e39e7b93bff
c67929e52961254985b93835e0834de2c0be40a8
2e43f9bb24046c88fc443f801d4e8ad5fe048a58
5c48da0d4631157f7cd6541363758c6d83b36691
01922f885641365887b4f1900355ef35cb816d2e
3dfd8a30897ea1c5d4b4548be820a96cbfb79f3d
ae79a1e066e9ae7c7359c5f2a19a6fb0485a2217
a85581f04f0d97d769405d400f4a0aa44f17e3a2
a1234fd3d1063c8cc31da64be1ac373112c4d987
c778c918e8249693d0bc5f483299396a83dc19e3
9419b00e227ec087eb6c79d33a6be3d2e25b32b9
502ddc2b9d115eaf46e113569d0720bb977d058b
Test: atest net_test_device
Bug: 252903680
Ignore-AOSP-First: Prevent merge conflict
(cherry picked from commit c935977bb382a5c570f5273b7404c3beb6537d89)

android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2016-2017 The Linux Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +109,7 @@ import com.android.bluetooth.a2dp.A2dpService;
 import com.android.bluetooth.a2dpsink.A2dpSinkService;
 import com.android.bluetooth.bas.BatteryService;
 import com.android.bluetooth.bass_client.BassClientService;
+import com.android.bluetooth.btservice.InteropUtil.InteropFeature;
 import com.android.bluetooth.btservice.RemoteDevices.DeviceProperties;
 import com.android.bluetooth.btservice.activityattribution.ActivityAttributionService;
 import com.android.bluetooth.btservice.bluetoothkeystore.BluetoothKeystoreService;
@@ -5468,6 +5470,10 @@ public class AdapterService extends Service {
         return mAdapterProperties.isA2dpOffloadEnabled();
     }
 
+    public String[] getAllowlistedMediaPlayers() {
+        return mAdapterProperties.getAllowlistedMediaPlayers();
+    }
+
     @VisibleForTesting
     BluetoothActivityEnergyInfo reportActivityInfo() {
         if (mAdapterProperties.getState() != BluetoothAdapter.STATE_ON
@@ -6246,6 +6252,35 @@ public class AdapterService extends Service {
         mRemoteDevices.updateBatteryLevel(device, batteryLevel);
     }
 
+    public boolean interopMatchAddr(InteropFeature feature, String address) {
+        return interopMatchAddrNative(feature.name(), address);
+    }
+
+    public boolean interopMatchName(InteropFeature feature, String name) {
+        return interopMatchNameNative(feature.name(), name);
+    }
+
+    public boolean interopMatchAddrOrName(InteropFeature feature, String address) {
+        return interopMatchAddrOrNameNative(feature.name(), address);
+    }
+
+    public void interopDatabaseAddAddr(InteropFeature feature,
+            String address, int length) {
+        interopDatabaseAddRemoveAddrNative(true, feature.name(), address, length);
+    }
+
+    public void interopDatabaseRemoveAddr(InteropFeature feature, String address) {
+        interopDatabaseAddRemoveAddrNative(false, feature.name(), address, 0);
+    }
+
+    public void interopDatabaseAddName(InteropFeature feature, String name) {
+        interopDatabaseAddRemoveNameNative(true, feature.name(), name);
+    }
+
+    public void interopDatabaseRemoveName(InteropFeature feature, String name) {
+        interopDatabaseAddRemoveNameNative(false, feature.name(), name);
+    }
+
     static native void classInitNative();
 
     native boolean initNative(boolean startRestricted, boolean isCommonCriteriaMode,
@@ -6342,6 +6377,14 @@ public class AdapterService extends Service {
     private native boolean allowLowLatencyAudioNative(boolean allowed, byte[] address);
 
     private native void metadataChangedNative(byte[] address, int key, byte[] value);
+
+    private native boolean interopMatchAddrNative(String featureName, String address);
+    private native boolean interopMatchNameNative(String featureName, String name);
+    private native boolean interopMatchAddrOrNameNative(String featureName, String address);
+    private native void interopDatabaseAddRemoveAddrNative(boolean doAdd,
+            String featureName, String address, int length);
+    private native void interopDatabaseAddRemoveNameNative(boolean doAdd,
+            String featureBame, String name);
 
     // Returns if this is a mock object. This is currently used in testing so that we may not call
     // System.exit() while finalizing the object. Otherwise GC of mock objects unfortunately ends up