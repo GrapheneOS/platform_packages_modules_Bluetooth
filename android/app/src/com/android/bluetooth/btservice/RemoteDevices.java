miyu-xu/packages-modules-Bluetooth

Bluetooth Audio Policy APIs and HFP support integration
Bluetooth Audio Policy feature support from HFP

Bug: 235541516
Test: manual
Change-Id: I5fd6c7959a85104f2bb5e31809a0e250c2dd13d0

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -23,6 +23,7 @@ import android.annotation.RequiresPermission;
 import android.app.admin.SecurityLog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothAssignedNumbers;
+import android.bluetooth.BluetoothAudioPolicy;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
@@ -314,6 +315,7 @@ final class RemoteDevices {
         @VisibleForTesting int mBondState;
         @VisibleForTesting int mDeviceType;
         @VisibleForTesting ParcelUuid[] mUuids;
+        private BluetoothAudioPolicy mAudioPolicy;
 
         DeviceProperties() {
             mBondState = BluetoothDevice.BOND_NONE;
@@ -594,6 +596,14 @@ final class RemoteDevices {
             synchronized (mObject) {
                 this.mIsCoordinatedSetMember = isCoordinatedSetMember;
             }
+        }
+
+        public void setAudioPolicy(BluetoothAudioPolicy policies) {
+            mAudioPolicy = policies;
+        }
+
+        public BluetoothAudioPolicy getAudioPolicy() {
+            return mAudioPolicy;
         }
     }
 

GrapheneOS/platform_packages_modules_Bluetooth

HFP android specific command protocol to implement android features.
Used this protocol to support the Audio Policy feature from the HFP
protocol. BluetoothDevice is the endpoint to invoke the feature and
later BLE Audio will also support it.
Cherry picked from wear OS
Dirty because of conflict + hide the API
Bug: 263323082
Bug: 253059988
Test: atest BluetoothInstrumentationTests
Tag: #feature
Merged-In: I5fd6c7959a85104f2bb5e31809a0e250c2dd13d0
(cherry picked from commit cb85f8cb0da7680b9251bbbca7ce12e78008037d)
Change-Id: I5d2d8f4ab71c8ed4f2f2997a7e16fc6b863bc5ae

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -22,6 +22,7 @@ import static android.Manifest.permission.BLUETOOTH_SCAN;
 import android.app.admin.SecurityLog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothAssignedNumbers;
+import android.bluetooth.BluetoothAudioPolicy;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
@@ -312,6 +313,7 @@ final class RemoteDevices {
         @VisibleForTesting int mBondState;
         @VisibleForTesting int mDeviceType;
         @VisibleForTesting ParcelUuid[] mUuids;
+        private BluetoothAudioPolicy mAudioPolicy;
 
         DeviceProperties() {
             mBondState = BluetoothDevice.BOND_NONE;
@@ -498,6 +500,14 @@ final class RemoteDevices {
             synchronized (mObject) {
                 return mIsCoordinatedSetMember;
             }
+        }
+
+        public void setHfAudioPolicyForRemoteAg(BluetoothAudioPolicy policies) {
+            mAudioPolicy = policies;
+        }
+
+        public BluetoothAudioPolicy getHfAudioPolicyForRemoteAg() {
+            return mAudioPolicy;
         }
     }
 