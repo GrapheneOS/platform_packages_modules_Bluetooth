miyu-xu/packages-modules-Bluetooth

Make L2CAP credit config modifiable
Value is overridable by build time system properties:
  bluetooth.l2cap.le.credit_default.value
  bluetooth.l2cap.le.credit_threshold.value

Bug: 263323082
Bug: 174290912
Test: Manual test
Change-Id: Ic27f572e73527bdf1af47e3781372043f8e310be
(cherry picked from commit 5fa1707fd2d7ed5d040ab8aa2c35d07a3ce920fa)

system/stack/eatt/eatt_impl.h
@@ -153,7 +153,8 @@ struct eatt_impl {
     tL2CAP_LE_CFG_INFO local_coc_cfg = {
         .mtu = eatt_dev->rx_mtu_,
         .mps = eatt_dev->rx_mps_ < max_mps ? eatt_dev->rx_mps_ : max_mps,
-        .credits = L2CAP_LE_CREDIT_DEFAULT};
+        .credits = get_l2cap_le_credit_default(),
+    };
 
     if (!L2CA_ConnectCreditBasedRsp(bda, identifier, lcids, L2CAP_CONN_OK,
                                     &local_coc_cfg))
@@ -586,7 +587,7 @@ struct eatt_impl {
     tL2CAP_LE_CFG_INFO local_coc_cfg = {
         .mtu = eatt_dev->rx_mtu_,
         .mps = eatt_dev->rx_mps_,
-        .credits = L2CAP_LE_CREDIT_DEFAULT,
+        .credits = get_l2cap_le_credit_default(),
         .number_of_channels = num_of_channels,
     };
 

system/stack/l2cap/l2c_ble.cc
@@ -1008,7 +1008,7 @@ void l2cble_process_sig_cmd(tL2C_LCB* p_lcb, uint8_t* p, uint16_t pkt_len) {
       p_ccb->local_conn_cfg.mtu = L2CAP_SDU_LENGTH_LE_MAX;
       p_ccb->local_conn_cfg.mps =
           controller_get_interface()->get_acl_data_size_ble();
-      p_ccb->local_conn_cfg.credits = L2CAP_LE_CREDIT_DEFAULT,
+      p_ccb->local_conn_cfg.credits = get_l2cap_le_credit_default(),
 
       p_ccb->peer_conn_cfg.mtu = mtu;
       p_ccb->peer_conn_cfg.mps = mps;

system/stack/test/stack_l2cap_test.cc
@@ -94,7 +94,7 @@ class StackL2capChannelTest : public StackL2capTest {
               .result = 0,
               .mtu = 100,
               .mps = 100,
-              .credits = L2CAP_LE_CREDIT_DEFAULT,
+              .credits = get_l2cap_le_credit_default(),
               .number_of_channels = L2CAP_CREDIT_BASED_MAX_CIDS,
           },
       .peer_conn_cfg =
@@ -103,7 +103,7 @@ class StackL2capChannelTest : public StackL2capTest {
               .result = 0,
               .mtu = 100,
               .mps = 100,
-              .credits = L2CAP_LE_CREDIT_DEFAULT,
+              .credits = get_l2cap_le_credit_default(),
               .number_of_channels = L2CAP_CREDIT_BASED_MAX_CIDS,
           },
       .is_first_seg = false,

GrapheneOS/platform_packages_modules_Bluetooth

(cherry picked from commit 4f221c977f4c2a4329cd0679e23541c3b672a6f0)
Merged-In: Ic27f572e73527bdf1af47e3781372043f8e310be
Change-Id: Iaced125567764ab5c9d373f10fea6417c589f3e0

system/stack/eatt/eatt_impl.h
@@ -153,7 +153,8 @@ struct eatt_impl {
     tL2CAP_LE_CFG_INFO local_coc_cfg = {
         .mtu = eatt_dev->rx_mtu_,
         .mps = eatt_dev->rx_mps_ < max_mps ? eatt_dev->rx_mps_ : max_mps,
-        .credits = L2CAP_LE_CREDIT_DEFAULT};
+        .credits = L2CA_LeCreditDefault(),
+    };
 
     if (!L2CA_ConnectCreditBasedRsp(bda, identifier, lcids, L2CAP_CONN_OK,
                                     &local_coc_cfg))
@@ -584,7 +585,7 @@ struct eatt_impl {
     tL2CAP_LE_CFG_INFO local_coc_cfg = {
         .mtu = eatt_dev->rx_mtu_,
         .mps = eatt_dev->rx_mps_,
-        .credits = L2CAP_LE_CREDIT_DEFAULT,
+        .credits = L2CA_LeCreditDefault(),
         .number_of_channels = num_of_channels,
     };
 

system/stack/l2cap/l2c_ble.cc
@@ -989,7 +989,7 @@ void l2cble_process_sig_cmd(tL2C_LCB* p_lcb, uint8_t* p, uint16_t pkt_len) {
       p_ccb->local_conn_cfg.mtu = L2CAP_SDU_LENGTH_LE_MAX;
       p_ccb->local_conn_cfg.mps =
           controller_get_interface()->get_acl_data_size_ble();
-      p_ccb->local_conn_cfg.credits = L2CAP_LE_CREDIT_DEFAULT,
+      p_ccb->local_conn_cfg.credits = L2CA_LeCreditDefault(),
 
       p_ccb->peer_conn_cfg.mtu = mtu;
       p_ccb->peer_conn_cfg.mps = mps;

system/stack/test/stack_l2cap_test.cc
@@ -96,7 +96,7 @@ class StackL2capChannelTest : public StackL2capTest {
               .result = 0,
               .mtu = 100,
               .mps = 100,
-              .credits = L2CAP_LE_CREDIT_DEFAULT,
+              .credits = L2CA_LeCreditDefault(),
               .number_of_channels = L2CAP_CREDIT_BASED_MAX_CIDS,
           },
       .peer_conn_cfg =
@@ -105,7 +105,7 @@ class StackL2capChannelTest : public StackL2capTest {
               .result = 0,
               .mtu = 100,
               .mps = 100,
-              .credits = L2CAP_LE_CREDIT_DEFAULT,
+              .credits = L2CA_LeCreditDefault(),
               .number_of_channels = L2CAP_CREDIT_BASED_MAX_CIDS,
           },
       .is_first_seg = false,