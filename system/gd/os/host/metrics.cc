miyu-xu/packages-modules-Bluetooth

Metric: Log extended smp pairing events
Include transport and complete information in extended enums.

Tag: #refactor
Bug: 236677941
Test: atest gd/cert/run
Change-Id: I6e527d8ad8b9dfa91b8abf67a9faa0fb11a513fb

system/gd/os/host/metrics.cc
@@ -96,7 +96,7 @@ void LogMetricSdpAttribute(
     const char* attribute_value) {}
 
 void LogMetricSmpPairingEvent(
-    const Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {}
+    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {}
 
 void LogMetricA2dpPlaybackEvent(const Address& address, int playback_state, int audio_coding_mode) {}
 

system/gd/os/metrics.h
@@ -166,7 +166,10 @@ void LogMetricReadTxPowerLevelResult(
  * @param smp_fail_reason SMP pairing failure reason code from SMP spec
  */
 void LogMetricSmpPairingEvent(
-    const hci::Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason);
+    const hci::Address& address,
+    uint16_t smp_cmd,
+    android::bluetooth::DirectionEnum direction,
+    uint8_t smp_fail_reason);
 
 /**
  * Logs there is an event related Bluetooth classic pairing

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 263323082
(cherry picked from commit 2f74a3c29e801cced2160be3b55266f167b3b202)
Merged-In: I6e527d8ad8b9dfa91b8abf67a9faa0fb11a513fb

system/gd/os/host/metrics.cc
@@ -96,7 +96,7 @@ void LogMetricSdpAttribute(
     const char* attribute_value) {}
 
 void LogMetricSmpPairingEvent(
-    const Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason) {}
+    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint16_t smp_fail_reason) {}
 
 void LogMetricA2dpPlaybackEvent(const Address& address, int playback_state, int audio_coding_mode) {}
 

system/gd/os/metrics.h
@@ -166,7 +166,10 @@ void LogMetricReadTxPowerLevelResult(
  * @param smp_fail_reason SMP pairing failure reason code from SMP spec
  */
 void LogMetricSmpPairingEvent(
-    const hci::Address& address, uint8_t smp_cmd, android::bluetooth::DirectionEnum direction, uint8_t smp_fail_reason);
+    const hci::Address& address,
+    uint16_t smp_cmd,
+    android::bluetooth::DirectionEnum direction,
+    uint16_t smp_fail_reason);
 
 /**
  * Logs there is an event related Bluetooth classic pairing