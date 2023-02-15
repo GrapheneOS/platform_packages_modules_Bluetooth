miyu-xu/packages-modules-Bluetooth

add getAdvertisementServiceData as system api
This targets to expose ASHA service data to settingLib

Test: manual
Tag: #feature
Bug: 265027285
Change-Id: I44850f17ff9248173d9782b2d54037fec13b766c

framework/java/android/bluetooth/BluetoothHearingAid.java
@@ -34,6 +34,8 @@ import android.content.AttributionSource;
 import android.content.Context;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -61,20 +63,184 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     private static final boolean VDBG = false;
 
     /**
-     * Intent used to broadcast the change in connection state of the Hearing Aid
-     * profile. Please note that in the binaural case, there will be two different LE devices for
-     * the left and right side and each device will have their own connection state changes.S
+     * This class provides the APIs to get device's advertisement data. The advertisement data might
+     * be incomplete or not available.
+     *
+     * <p><a
+     * href=https://source.android.com/docs/core/connect/bluetooth/asha#advertisements-for-asha-gatt-service>
+     * documentation can be found here</a>
+     *
+     * @hide
+     */
+    @SystemApi
+    public static final class AdvertisementServiceData implements Parcelable {
+        private static final String TAG = "AdvertisementData";
+
+        private final int mCapability;
+        private final int mTruncatedHiSyncId;
+
+        /**
+         * Construct AdvertisementServiceData
+         *
+         * @param capability hearing aid's capability
+         * @param truncatedHiSyncId truncated HiSyncId
+         * @hide
+         */
+        public AdvertisementServiceData(int capability, int truncatedHiSyncId) {
+            if (DBG) {
+                Log.d(TAG, "capability:" + capability + " truncatedHiSyncId:" + truncatedHiSyncId);
+            }
+            mCapability = capability;
+            mTruncatedHiSyncId = truncatedHiSyncId;
+        }
+
+        /**
+         * Get the mode of the device based on its advertisement data.
+         *
+         * @return the {@code MODE_MONAURAL}, {@code MODE_BINAURAL} of the device, or {@code
+         *     MODE_UNKNOWN} if one is not available.
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        @DeviceMode
+        public int getDeviceMode() {
+            if (VDBG) log("getDeviceMode()");
+            return mCapability & 1;
+        }
+
+        /**
+         * Get the side of the device based on its advertisement data.
+         *
+         * @return the {@code SIDE_LEFT}, {@code SIDE_RIGHT} of the device, or {@code SIDE_UNKNOWN}
+         *     if one is not available.
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        @DeviceSide
+        public int getDeviceSide() {
+            if (VDBG) log("getDeviceSide()");
+            return (mCapability >> 1) & 1;
+        }
+
+        /**
+         * Check if {@link BluetoothHearingAid} marks itself as CSIP supported based on its
+         * advertisement data.
+         *
+         * @return true or false
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public boolean isCsipSupported() {
+            if (VDBG) log("isCsipSupported()");
+            return ((mCapability >> 2) & 1) != 0;
+        }
+
+        /**
+         * Get the truncated HiSyncId of the device based on its advertisement data.
+         *
+         * @return truncated HiSyncId.
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public int getTruncatedHiSyncId() {
+            if (VDBG) log("getTruncatedHiSyncId: " + mTruncatedHiSyncId);
+            return mTruncatedHiSyncId;
+        }
+
+        /**
+         * Check if another {@link AdvertisementServiceData} is likely a pair with current one.
+         * There is a possibility of a collision on truncated HiSyncId which leads to falsely
+         * identifying as a pair.
+         *
+         * @param data another device's {@link AdvertisementServiceData} .
+         * @return true of false
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public boolean isLikelySamePairWith(@Nullable AdvertisementServiceData data) {
+            if (VDBG) log("isLikelySamePairWith()");
+            if (data == null) {
+                return false;
+            }
+
+            boolean bothSupportCsip = isCsipSupported() && data.isCsipSupported();
+            boolean isDifferentSide =
+                    (getDeviceSide() != SIDE_UNKNOWN && data.getDeviceSide() != SIDE_UNKNOWN)
+                            && (getDeviceSide() != data.getDeviceSide());
+            boolean isSameTruncatedHiSyncId = mTruncatedHiSyncId == data.mTruncatedHiSyncId;
+            return bothSupportCsip && isDifferentSide && isSameTruncatedHiSyncId;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(@NonNull Parcel dest, int flags) {
+            dest.writeInt(mCapability);
+            dest.writeInt(mTruncatedHiSyncId);
+        }
+
+        public static final @NonNull Parcelable.Creator<AdvertisementServiceData> CREATOR =
+                new Parcelable.Creator<AdvertisementServiceData>() {
+                    public AdvertisementServiceData createFromParcel(Parcel in) {
+                        return new AdvertisementServiceData(in);
+                    }
+
+                    public AdvertisementServiceData[] newArray(int size) {
+                        return new AdvertisementServiceData[size];
+                    }
+                };
+
+        private AdvertisementServiceData(@NonNull Parcel in) {
+            in.setDataPosition(0);
+            mCapability = in.readInt();
+            mTruncatedHiSyncId = in.readInt();
+        }
+    }
+
+    /**
+     * Intent used to broadcast the change in connection state of the Hearing Aid profile. Please
+     * note that in the binaural case, there will be two different LE devices for the left and right
+     * side and each device will have their own connection state changes.S
      *
      * <p>This intent will have 3 extras:
+     *
      * <ul>
-     * <li> {@link #EXTRA_STATE} - The current state of the profile. </li>
-     * <li> {@link #EXTRA_PREVIOUS_STATE}- The previous state of the profile.</li>
-     * <li> {@link BluetoothDevice#EXTRA_DEVICE} - The remote device. </li>
+     *   <li>{@link #EXTRA_STATE} - The current state of the profile.
+     *   <li>{@link #EXTRA_PREVIOUS_STATE}- The previous state of the profile.
+     *   <li>{@link BluetoothDevice#EXTRA_DEVICE} - The remote device.
      * </ul>
      *
-     * <p>{@link #EXTRA_STATE} or {@link #EXTRA_PREVIOUS_STATE} can be any of
-     * {@link #STATE_DISCONNECTED}, {@link #STATE_CONNECTING},
-     * {@link #STATE_CONNECTED}, {@link #STATE_DISCONNECTING}.
+     * <p>{@link #EXTRA_STATE} or {@link #EXTRA_PREVIOUS_STATE} can be any of {@link
+     * #STATE_DISCONNECTED}, {@link #STATE_CONNECTING}, {@link #STATE_CONNECTED}, {@link
+     * #STATE_DISCONNECTING}.
      */
     @RequiresLegacyBluetoothPermission
     @RequiresBluetoothConnectPermission
@@ -725,9 +891,47 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     }
 
     /**
+     * Get ASHA device's advertisement service data.
+     *
+     * @param device discovered Bluetooth device
+     * @return {@link AdvertisementServiceData}
+     * @hide
+     */
+    @SystemApi
+    @RequiresPermission(
+            allOf = {
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+            })
+    public @Nullable AdvertisementServiceData getAdvertisementServiceData(
+            @NonNull BluetoothDevice device) {
+        if (DBG) {
+            log("getAdvertisementServiceData()");
+        }
+        final IBluetoothHearingAid service = getService();
+        AdvertisementServiceData result = null;
+        if (service == null || !isEnabled() || isValidDevice(device)) {
+            Log.w(TAG, "Proxy not attached to service");
+            if (DBG) {
+                log(Log.getStackTraceString(new Throwable()));
+            }
+        } else {
+            try {
+                final SynchronousResultReceiver<AdvertisementServiceData> recv =
+                        SynchronousResultReceiver.get();
+                service.getAdvertisementServiceData(device, mAttributionSource, recv);
+                result = recv.awaitResultNoInterrupt(getSyncTimeout()).getValue(null);
+            } catch (RemoteException | TimeoutException e) {
+                Log.e(TAG, e.toString() + "\n" + Log.getStackTraceString(new Throwable()));
+            }
+        }
+        return result;
+    }
+
+    /**
      * Get the side of the device.
      *
-     * TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
+     * <p>TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
      * Need to change to formal call in next bluetooth release.
      *
      * @param device Bluetooth device.
@@ -743,7 +947,7 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     /**
      * Get the mode of the device.
      *
-     * TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
+     * <p>TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
      * Need to change to formal call in next bluetooth release.
      *
      * @param device Bluetooth device

GrapheneOS/platform_packages_modules_Bluetooth

framework/java/android/bluetooth/BluetoothHearingAid.java
@@ -34,6 +34,8 @@ import android.content.AttributionSource;
 import android.content.Context;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -61,20 +63,183 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     private static final boolean VDBG = false;
 
     /**
-     * Intent used to broadcast the change in connection state of the Hearing Aid
-     * profile. Please note that in the binaural case, there will be two different LE devices for
-     * the left and right side and each device will have their own connection state changes.S
+     * This class provides the APIs to get device's advertisement data. The advertisement data might
+     * be incomplete or not available.
+     *
+     * <p><a
+     * href=https://source.android.com/docs/core/connect/bluetooth/asha#advertisements-for-asha-gatt-service>
+     * documentation can be found here</a>
+     *
+     * @hide
+     */
+    @SystemApi
+    public static final class AdvertisementServiceData implements Parcelable {
+        private static final String TAG = "AdvertisementData";
+
+        private final int mCapability;
+        private final int mTruncatedHiSyncId;
+
+        /**
+         * Construct AdvertisementServiceData.
+         *
+         * @param capability hearing aid's capability
+         * @param truncatedHiSyncId truncated HiSyncId
+         * @hide
+         */
+        public AdvertisementServiceData(int capability, int truncatedHiSyncId) {
+            if (DBG) {
+                Log.d(TAG, "capability:" + capability + " truncatedHiSyncId:" + truncatedHiSyncId);
+            }
+            mCapability = capability;
+            mTruncatedHiSyncId = truncatedHiSyncId;
+        }
+
+        /**
+         * Get the mode of the device based on its advertisement data.
+         *
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        @DeviceMode
+        public int getDeviceMode() {
+            if (VDBG) log("getDeviceMode()");
+            return (mCapability >> 1) & 1;
+        }
+
+        private AdvertisementServiceData(@NonNull Parcel in) {
+            in.setDataPosition(0);
+            mCapability = in.readInt();
+            mTruncatedHiSyncId = in.readInt();
+        }
+
+        /**
+         * Get the side of the device based on its advertisement data.
+         *
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        @DeviceSide
+        public int getDeviceSide() {
+            if (VDBG) log("getDeviceSide()");
+            return mCapability & 1;
+        }
+
+        /**
+         * Check if {@link BluetoothHearingAid} marks itself as CSIP supported based on its
+         * advertisement data.
+         *
+         * @return {@code true} when CSIP is supported, {@code false} otherwise
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public boolean isCsipSupported() {
+            if (VDBG) log("isCsipSupported()");
+            return ((mCapability >> 2) & 1) != 0;
+        }
+
+        /**
+         * Get the truncated HiSyncId of the device based on its advertisement data.
+         *
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public int getTruncatedHiSyncId() {
+            if (VDBG) log("getTruncatedHiSyncId: " + mTruncatedHiSyncId);
+            return mTruncatedHiSyncId;
+        }
+
+        /**
+         * Check if another {@link AdvertisementServiceData} is likely a pair with current one.
+         * There is a possibility of a collision on truncated HiSyncId which leads to falsely
+         * identified as a pair.
+         *
+         * @param data another device's {@link AdvertisementServiceData}
+         * @return {@code true} if the devices are a likely pair, {@code false} otherwise
+         * @hide
+         */
+        @RequiresPermission(
+                allOf = {
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+                })
+        @SystemApi
+        public boolean isInPairWith(@Nullable AdvertisementServiceData data) {
+            if (VDBG) log("isInPairWith()");
+            if (data == null) {
+                return false;
+            }
+
+            boolean bothSupportCsip = isCsipSupported() && data.isCsipSupported();
+            boolean isDifferentSide =
+                    (getDeviceSide() != SIDE_UNKNOWN && data.getDeviceSide() != SIDE_UNKNOWN)
+                            && (getDeviceSide() != data.getDeviceSide());
+            boolean isSameTruncatedHiSyncId = mTruncatedHiSyncId == data.mTruncatedHiSyncId;
+            return bothSupportCsip && isDifferentSide && isSameTruncatedHiSyncId;
+        }
+
+        /**
+         * @hide
+         */
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(@NonNull Parcel dest, int flags) {
+            dest.writeInt(mCapability);
+            dest.writeInt(mTruncatedHiSyncId);
+        }
+
+        public static final @NonNull Parcelable.Creator<AdvertisementServiceData> CREATOR =
+                new Parcelable.Creator<AdvertisementServiceData>() {
+                    public AdvertisementServiceData createFromParcel(Parcel in) {
+                        return new AdvertisementServiceData(in);
+                    }
+
+                    public AdvertisementServiceData[] newArray(int size) {
+                        return new AdvertisementServiceData[size];
+                    }
+                };
+
+    }
+
+    /**
+     * Intent used to broadcast the change in connection state of the Hearing Aid profile. Please
+     * note that in the binaural case, there will be two different LE devices for the left and right
+     * side and each device will have their own connection state changes.S
      *
      * <p>This intent will have 3 extras:
+     *
      * <ul>
-     * <li> {@link #EXTRA_STATE} - The current state of the profile. </li>
-     * <li> {@link #EXTRA_PREVIOUS_STATE}- The previous state of the profile.</li>
-     * <li> {@link BluetoothDevice#EXTRA_DEVICE} - The remote device. </li>
+     *   <li>{@link #EXTRA_STATE} - The current state of the profile.
+     *   <li>{@link #EXTRA_PREVIOUS_STATE}- The previous state of the profile.
+     *   <li>{@link BluetoothDevice#EXTRA_DEVICE} - The remote device.
      * </ul>
      *
-     * <p>{@link #EXTRA_STATE} or {@link #EXTRA_PREVIOUS_STATE} can be any of
-     * {@link #STATE_DISCONNECTED}, {@link #STATE_CONNECTING},
-     * {@link #STATE_CONNECTED}, {@link #STATE_DISCONNECTING}.
+     * <p>{@link #EXTRA_STATE} or {@link #EXTRA_PREVIOUS_STATE} can be any of {@link
+     * #STATE_DISCONNECTED}, {@link #STATE_CONNECTING}, {@link #STATE_CONNECTED}, {@link
+     * #STATE_DISCONNECTING}.
      */
     @RequiresLegacyBluetoothPermission
     @RequiresBluetoothConnectPermission
@@ -725,9 +890,47 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     }
 
     /**
+     * Get ASHA device's advertisement service data.
+     *
+     * @param device discovered Bluetooth device
+     * @return {@link AdvertisementServiceData}
+     * @hide
+     */
+    @SystemApi
+    @RequiresPermission(
+            allOf = {
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_PRIVILEGED,
+            })
+    public @Nullable AdvertisementServiceData getAdvertisementServiceData(
+            @NonNull BluetoothDevice device) {
+        if (DBG) {
+            log("getAdvertisementServiceData()");
+        }
+        final IBluetoothHearingAid service = getService();
+        AdvertisementServiceData result = null;
+        if (service == null || !isEnabled() || isValidDevice(device)) {
+            Log.w(TAG, "Proxy not attached to service");
+            if (DBG) {
+                log(Log.getStackTraceString(new Throwable()));
+            }
+        } else {
+            try {
+                final SynchronousResultReceiver<AdvertisementServiceData> recv =
+                        SynchronousResultReceiver.get();
+                service.getAdvertisementServiceData(device, mAttributionSource, recv);
+                result = recv.awaitResultNoInterrupt(getSyncTimeout()).getValue(null);
+            } catch (RemoteException | TimeoutException e) {
+                Log.e(TAG, e.toString() + "\n" + Log.getStackTraceString(new Throwable()));
+            }
+        }
+        return result;
+    }
+
+    /**
      * Get the side of the device.
      *
-     * TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
+     * <p>TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
      * Need to change to formal call in next bluetooth release.
      *
      * @param device Bluetooth device.
@@ -743,7 +946,7 @@ public final class BluetoothHearingAid implements BluetoothProfile {
     /**
      * Get the mode of the device.
      *
-     * TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
+     * <p>TODO(b/231901542): Used by internal only to improve hearing aids experience in short-term.
      * Need to change to formal call in next bluetooth release.
      *
      * @param device Bluetooth device