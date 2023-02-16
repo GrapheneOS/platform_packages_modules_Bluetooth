1093aee165b8f078edff8094b6f7ea0d11e50d43
Rahul Arya <aryarahul@google.com>
2023-02-16 01:46:47 +0000

    Fix issue where HIDH gets stuck after HIDD toggles
    Bug: 243458209
    Test: CtsVerifier
    Change-Id: I85b60f4bece97e1526b1234c71357a57d47d279e

--- system/gd/rust/common/src/init_flags.rs
+++ system/gd/rust/common/src/init_flags.rs
@@ -257,6 +257,7 @@ init_flags!(
         bta_dm_clear_conn_id_on_client_close = true,
         btm_dm_flush_discovery_queue_on_search_cancel,
         clear_hidd_interrupt_cid_on_disconnect = true,
+        delay_hidh_cleanup_until_hidh_ready_start = true,
         device_iot_config_logging,
         dynamic_avrcp_version_enhancement = true,
         finite_att_timeout = true,
