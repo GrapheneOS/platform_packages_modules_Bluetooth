miyu-xu/packages-modules-Bluetooth

PBAP: Dynamic Version Upgrade for PBAP 1.2 supporting remote
This change stores version of remote device if it supports
PBAP 1.2. While sending SDP response, remote device support
will be checked in conf file and if entry for remote device
is found for 1.2 support, device will send 1.2 SDP response.

Submitted on behalf of a third-party:
Qualcomm Innovation Center, The Linux Foundation, Broadcom Corporation,
The Android Open Source Project, Google, Inc
License rights, if any, to the submission are granted solely by the
copyright owner of such submission under its applicable intellectual
property.
/*
* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
* Copyright 2014 Google, Inc
* Copyright 2003-2014 Broadcom Corporation
* Copyright (c) 2014 The Android Open Source Project
* Licensed under the Apache License, Version 2.0 (the "License");
* you may not use this file except in compliance with the License.
* You may obtain a copy of the License at
*
*      http://www.apache.org/licenses/LICENSE-2.0
*
* Unless required by applicable law or agreed to in writing, software
* distributed under the License is distributed on an "AS IS" BASIS,
* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
* See the License for the specific language governing permissions and
* limitations under the License.
*/
Third Party code includes additions/modifications from Qualcomm Innovation Center, Inc

Change-Id: Idf4edbc4b39506855ecf6245a96eca5706082256

system/stack/sdp/sdp_db.cc
@@ -1,5 +1,6 @@
 /******************************************************************************
  *
+ *  Copyright (C) 2017, The Linux Foundation. All rights reserved.
  *  Copyright 1999-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -352,7 +353,7 @@ bool SDP_DeleteRecord(uint32_t handle) {
  ******************************************************************************/
 bool SDP_AddAttribute(uint32_t handle, uint16_t attr_id, uint8_t attr_type,
                       uint32_t attr_len, uint8_t* p_val) {
-  uint16_t xx, yy, zz;
+  uint16_t zz;
   tSDP_RECORD* p_rec = &sdp_cb.server_db.record[0];
 
   if (sdp_cb.trace_level >= BT_TRACE_LEVEL_DEBUG) {
@@ -400,66 +401,88 @@ bool SDP_AddAttribute(uint32_t handle, uint16_t attr_id, uint8_t attr_type,
   /* Find the record in the database */
   for (zz = 0; zz < sdp_cb.server_db.num_records; zz++, p_rec++) {
     if (p_rec->record_handle == handle) {
-      tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
-
-      /* Found the record. Now, see if the attribute already exists */
-      for (xx = 0; xx < p_rec->num_attributes; xx++, p_attr++) {
-        /* The attribute exists. replace it */
-        if (p_attr->id == attr_id) {
-          SDP_DeleteAttribute(handle, attr_id);
-          break;
-        }
-        if (p_attr->id > attr_id) break;
-      }
-
-      if (p_rec->num_attributes == SDP_MAX_REC_ATTR) return (false);
-
-      /* If not found, see if we can allocate a new entry */
-      if (xx == p_rec->num_attributes)
-        p_attr = &p_rec->attribute[p_rec->num_attributes];
-      else {
-        /* Since the attributes are kept in sorted order, insert ours here */
-        for (yy = p_rec->num_attributes; yy > xx; yy--)
-          p_rec->attribute[yy] = p_rec->attribute[yy - 1];
-      }
-
-      p_attr->id = attr_id;
-      p_attr->type = attr_type;
-      p_attr->len = attr_len;
-
-      if (p_rec->free_pad_ptr + attr_len >= SDP_MAX_PAD_LEN) {
-        /* do truncate only for text string type descriptor */
-        if (attr_type == TEXT_STR_DESC_TYPE) {
-          SDP_TRACE_WARNING(
-              "SDP_AddAttribute: attr_len:%d too long. truncate to (%d)",
-              attr_len, SDP_MAX_PAD_LEN - p_rec->free_pad_ptr);
-
-          attr_len = SDP_MAX_PAD_LEN - p_rec->free_pad_ptr;
-          p_val[SDP_MAX_PAD_LEN - p_rec->free_pad_ptr - 1] = '\0';
-        } else
-          attr_len = 0;
-      }
-
-      if ((attr_len > 0) && (p_val != 0)) {
-        p_attr->len = attr_len;
-        memcpy(&p_rec->attr_pad[p_rec->free_pad_ptr], p_val, (size_t)attr_len);
-        p_attr->value_ptr = &p_rec->attr_pad[p_rec->free_pad_ptr];
-        p_rec->free_pad_ptr += attr_len;
-      } else if ((attr_len == 0 &&
-                  p_attr->len !=
-                      0) || /* if truncate to 0 length, simply don't add */
-                 p_val == 0) {
-        SDP_TRACE_ERROR(
-            "SDP_AddAttribute fail, length exceed maximum: ID %d: attr_len:%d ",
-            attr_id, attr_len);
-        p_attr->id = p_attr->type = p_attr->len = 0;
-        return (false);
-      }
-      p_rec->num_attributes++;
-      return (true);
+      return SDP_AddAttributeToRecord (p_rec, attr_id, attr_type, attr_len, p_val);
     }
   }
   return (false);
+}
+
+/*******************************************************************************
+ *
+ * Function         SDP_AddAttributeToRecord
+ *
+ * Description      This function is called to add an attribute to a record.
+ *                  This would be through the SDP database maintenance API.
+ *                  If the attribute already exists in the record, it is
+ *                  replaced with the new value.
+ *
+ * NOTE             Attribute values must be passed as a Big Endian stream.
+ *
+ * Returns          true if added OK, else false
+ *
+ ******************************************************************************/
+bool SDP_AddAttributeToRecord (tSDP_RECORD *p_rec, uint16_t attr_id,
+                                     uint8_t attr_type, uint32_t attr_len,
+                                     uint8_t *p_val)
+{
+    uint16_t xx, yy;
+    tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
+
+    /* Found the record. Now, see if the attribute already exists */
+    for (xx = 0; xx < p_rec->num_attributes; xx++, p_attr++) {
+      /* The attribute exists. replace it */
+      if (p_attr->id == attr_id) {
+        SDP_DeleteAttributeFromRecord(p_rec, attr_id);
+        break;
+      }
+      if (p_attr->id > attr_id) break;
+    }
+
+    if (p_rec->num_attributes >= SDP_MAX_REC_ATTR) return (false);
+
+    /* If not found, see if we can allocate a new entry */
+    if (xx == p_rec->num_attributes)
+      p_attr = &p_rec->attribute[p_rec->num_attributes];
+    else {
+      /* Since the attributes are kept in sorted order, insert ours here */
+      for (yy = p_rec->num_attributes; yy > xx; yy--)
+        p_rec->attribute[yy] = p_rec->attribute[yy - 1];
+    }
+
+    p_attr->id = attr_id;
+    p_attr->type = attr_type;
+    p_attr->len = attr_len;
+
+    if (p_rec->free_pad_ptr + attr_len >= SDP_MAX_PAD_LEN) {
+      /* do truncate only for text string type descriptor */
+      if (attr_type == TEXT_STR_DESC_TYPE) {
+        SDP_TRACE_WARNING(
+            "SDP_AddAttributeToRecord: attr_len:%d too long. truncate to (%d)",
+            attr_len, SDP_MAX_PAD_LEN - p_rec->free_pad_ptr);
+
+        attr_len = SDP_MAX_PAD_LEN - p_rec->free_pad_ptr;
+        p_val[SDP_MAX_PAD_LEN - p_rec->free_pad_ptr - 1] = '\0';
+      } else
+        attr_len = 0;
+    }
+
+    if ((attr_len > 0) && (p_val != 0)) {
+      p_attr->len = attr_len;
+      memcpy(&p_rec->attr_pad[p_rec->free_pad_ptr], p_val, (size_t)attr_len);
+      p_attr->value_ptr = &p_rec->attr_pad[p_rec->free_pad_ptr];
+      p_rec->free_pad_ptr += attr_len;
+    } else if ((attr_len == 0 &&
+                p_attr->len !=
+                    0) || /* if truncate to 0 length, simply don't add */
+               p_val == 0) {
+      SDP_TRACE_ERROR(
+          "SDP_AddAttributeToRecord fail, length exceed maximum: ID %d: attr_len:%d ",
+          attr_id, attr_len);
+      p_attr->id = p_attr->type = p_attr->len = 0;
+      return (false);
+    }
+    p_rec->num_attributes++;
+    return (true);
 }
 
 /*******************************************************************************
@@ -683,6 +706,72 @@ bool SDP_AddProfileDescriptorList(uint32_t handle, uint16_t profile_uuid,
   return result;
 }
 
+bool SDP_AddProfileDescriptorListUuid128(uint32_t handle, uint8_t *profile_uuid,
+                                  uint16_t version) {
+  uint8_t* p;
+  bool result;
+  uint8_t* p_buff = (uint8_t*)osi_malloc(sizeof(uint8_t) * SDP_MAX_ATTR_LEN);
+
+  p = p_buff + 2;
+
+  /* First, build the profile descriptor list. This consists of a data element
+   * sequence. */
+  /* The sequence consists of profile's UUID and version number  */
+  UINT8_TO_BE_STREAM(p, (UUID_DESC_TYPE << 3) | SIZE_SIXTEEN_BYTES);
+  ARRAY_TO_BE_STREAM(p, profile_uuid, 16);
+
+  UINT8_TO_BE_STREAM(p, (UINT_DESC_TYPE << 3) | SIZE_TWO_BYTES);
+  UINT16_TO_BE_STREAM(p, version);
+
+  /* Add in type and length fields */
+  *p_buff = (uint8_t)((DATA_ELE_SEQ_DESC_TYPE << 3) | SIZE_IN_NEXT_BYTE);
+  *(p_buff + 1) = (uint8_t)(p - (p_buff + 2));
+
+  result =
+      SDP_AddAttribute(handle, ATTR_ID_BT_PROFILE_DESC_LIST,
+                       DATA_ELE_SEQ_DESC_TYPE, (uint32_t)(p - p_buff), p_buff);
+  osi_free(p_buff);
+  return result;
+}
+/*******************************************************************************
+ *
+ * Function         SDP_AddProfileDescriptorListToRecord
+ *
+ * Description      This function is called to add a profile descriptor list to
+ *                  a record. This would be through the SDP database maintenance
+ *                  API. If the version already exists in the record, it is
+ *                  replaced with the new one.
+ *
+ * Returns          true if added OK, else false
+ *
+ ******************************************************************************/
+bool SDP_AddProfileDescriptorListToRecord (tSDP_RECORD* prec, uint16_t profile_uuid,
+                                                  uint16_t version)
+{
+  uint8_t* p;
+  bool result;
+  uint8_t* p_buff = (uint8_t *)osi_malloc(sizeof(uint8_t) * SDP_MAX_ATTR_LEN);
+
+  p = p_buff + 2;
+
+  /* First, build the profile descriptor list. This consists of a data element sequence. */
+  /* The sequence consists of profile's UUID and version number  */
+  UINT8_TO_BE_STREAM  (p, (UUID_DESC_TYPE << 3) | SIZE_TWO_BYTES);
+  UINT16_TO_BE_STREAM (p, profile_uuid);
+
+  UINT8_TO_BE_STREAM  (p, (UINT_DESC_TYPE << 3) | SIZE_TWO_BYTES);
+  UINT16_TO_BE_STREAM (p, version);
+
+  /* Add in type and length fields */
+  *p_buff = (uint8_t) ((DATA_ELE_SEQ_DESC_TYPE << 3) | SIZE_IN_NEXT_BYTE);
+  *(p_buff+1) = (uint8_t) (p - (p_buff+2));
+
+  result = SDP_AddAttributeToRecord (prec, ATTR_ID_BT_PROFILE_DESC_LIST,
+       DATA_ELE_SEQ_DESC_TYPE, (uint32_t) (p - p_buff), p_buff);
+  osi_free(p_buff);
+  return result;
+}
+
 /*******************************************************************************
  *
  * Function         SDP_AddLanguageBaseAttrIDList
@@ -767,48 +856,80 @@ bool SDP_AddServiceClassIdList(uint32_t handle, uint16_t num_services,
  *
  ******************************************************************************/
 bool SDP_DeleteAttribute(uint32_t handle, uint16_t attr_id) {
+  uint16_t xx;
   tSDP_RECORD* p_rec = &sdp_cb.server_db.record[0];
-  uint8_t* pad_ptr;
-  uint32_t len; /* Number of bytes in the entry */
 
   /* Find the record in the database */
-  for (uint16_t record_index = 0; record_index < sdp_cb.server_db.num_records; record_index++, p_rec++) {
+  for (xx = 0; xx < sdp_cb.server_db.num_records; xx++, p_rec++) {
     if (p_rec->record_handle == handle) {
-      tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
+      SDP_TRACE_API("Deleting attr_id 0x%04x for handle 0x%x",
+          attr_id, handle);
+      if (SDP_DeleteAttributeFromRecord (p_rec, attr_id))
+        return (true);
+    }
+  }
+  /* If here, not found */
+  return (false);
+}
 
-      SDP_TRACE_API("Deleting attr_id 0x%04x for handle 0x%x", attr_id, handle);
-      /* Found it. Now, find the attribute */
-      for (uint16_t attribute_index = 0; attribute_index < p_rec->num_attributes; attribute_index++, p_attr++) {
-        if (p_attr->id == attr_id) {
-          pad_ptr = p_attr->value_ptr;
-          len = p_attr->len;
+/*******************************************************************************
+ *
+ * Function         SDP_DeleteAttributeFromRecord
+ *
+ * Description      This function is called to delete an attribute from a
+ *                  record. This would be through the SDP database maintenance
+ *                  API.
+ *
+ * Returns          true if deleted OK, else false if not found
+ *
+ ******************************************************************************/
 
-          if (len) {
-            for (uint16_t zz = 0; zz < p_rec->num_attributes; zz++) {
-              if (p_rec->attribute[zz].value_ptr > pad_ptr)
-                p_rec->attribute[zz].value_ptr -= len;
-            }
-          }
+bool SDP_DeleteAttributeFromRecord (tSDP_RECORD* p_rec, uint16_t attr_id)
+{
+  uint16_t xx, yy;
+  tSDP_ATTRIBUTE * p_attr = &p_rec->attribute[0];
+  uint8_t *pad_ptr;
+  uint32_t len;                        /* Number of bytes in the entry */
 
-          /* Found it. Shift everything up one */
-          p_rec->num_attributes--;
-
-          for (uint16_t zz = attribute_index; zz < p_rec->num_attributes; zz++, p_attr++) {
-            *p_attr = *(p_attr + 1);
-          }
-
-          /* adjust attribute values if needed */
-          if (len) {
-            uint16_t last_attribute_to_adjust =
-                (p_rec->free_pad_ptr - ((pad_ptr + len) - &p_rec->attr_pad[0]));
-            for (uint16_t zz = 0; zz < last_attribute_to_adjust; zz++, pad_ptr++) {
-              *pad_ptr = *(pad_ptr + len);
-            }
-            p_rec->free_pad_ptr -= len;
-          }
-          return (true);
+  /* Found it. Now, find the attribute */
+  for (xx = 0; xx < p_rec->num_attributes; xx++, p_attr++) {
+    if (p_attr->id == attr_id) {
+      pad_ptr = p_attr->value_ptr;
+      len = p_attr->len;
+      if (p_rec->free_pad_ptr + p_attr->len >= SDP_MAX_PAD_LEN) {
+        SDP_TRACE_ERROR("Deleting attr_id 0x%04x len %d exceeds 600",
+            attr_id, len);
+        if (p_attr->type == TEXT_STR_DESC_TYPE) {
+          len = SDP_MAX_PAD_LEN - p_rec->free_pad_ptr;
+        } else {
+          len = 0;
         }
       }
+      if (len) {
+        for (yy = 0; yy < p_rec->num_attributes; yy++) {
+          if( p_rec->attribute[yy].value_ptr > pad_ptr ) {
+              p_rec->attribute[yy].value_ptr -= len;
+          }
+        }
+      }
+
+      /* Found it. Shift everything up one */
+      p_rec->num_attributes--;
+
+      for (yy = xx; yy < p_rec->num_attributes; yy++, p_attr++) {
+        *p_attr = *(p_attr + 1);
+      }
+
+      /* adjust attribute values if needed */
+      if (len) {
+        xx = (p_rec->free_pad_ptr - ((pad_ptr+len) -
+                                  &p_rec->attr_pad[0]));
+        for( yy=0; yy<xx; yy++, pad_ptr++) {
+          *pad_ptr = *(pad_ptr+len);
+        }
+        p_rec->free_pad_ptr -= len;
+      }
+      return (true);
     }
   }
   /* If here, not found */

GrapheneOS/platform_packages_modules_Bluetooth

Snapshot of:
72f658a69d62794bf25f99fece089ccf3666750e
6f313c106a71a12eb87e005a41213a54cda425da
e909f6e5ef0ba5f56a3ee7ede5b86dc3f668c564
6f66c22f26a7cd3fdc4cd44d8c8bb47af0a629b3
009034241686da41a474bedf225d9f7af8595640
680251a750a7b3e46de6cd71a6396f272dd91e51
a156ef66075ad88f605304ec929389fabed236bf
7ad3706865c28c08330023d7921dbc43fbb1111a
b79f5b83684d3605b3ae44430bbff12f5c09f4c5
9cc899b5529da7504ede4810ec8f1f47c01b8f42
Test: atest net_test_stack_sdp
Tag: #feature
Bug: 252902891
(cherry picked from commit ea3614eb6b8aff18075271788a3d1b9b7e8dfe60)

system/stack/sdp/sdp_db.cc
@@ -1,5 +1,6 @@
 /******************************************************************************
  *
+ *  Copyright (C) 2017, The Linux Foundation.
  *  Copyright 1999-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -352,7 +353,7 @@ bool SDP_DeleteRecord(uint32_t handle) {
  ******************************************************************************/
 bool SDP_AddAttribute(uint32_t handle, uint16_t attr_id, uint8_t attr_type,
                       uint32_t attr_len, uint8_t* p_val) {
-  uint16_t xx, yy, zz;
+  uint16_t zz;
   tSDP_RECORD* p_rec = &sdp_cb.server_db.record[0];
 
   if (p_val == nullptr) {
@@ -405,7 +406,6 @@ bool SDP_AddAttribute(uint32_t handle, uint16_t attr_id, uint8_t attr_type,
   /* Find the record in the database */
   for (zz = 0; zz < sdp_cb.server_db.num_records; zz++, p_rec++) {
     if (p_rec->record_handle == handle) {
-      tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
 
       // error out early, no need to look up
       if (p_rec->free_pad_ptr >= SDP_MAX_PAD_LEN) {
@@ -414,62 +414,87 @@ bool SDP_AddAttribute(uint32_t handle, uint16_t attr_id, uint8_t attr_type,
         return (false);
       }
 
-      /* Found the record. Now, see if the attribute already exists */
-      for (xx = 0; xx < p_rec->num_attributes; xx++, p_attr++) {
-        /* The attribute exists. replace it */
-        if (p_attr->id == attr_id) {
-          SDP_DeleteAttribute(handle, attr_id);
-          break;
-        }
-        if (p_attr->id > attr_id) break;
-      }
-
-      if (p_rec->num_attributes == SDP_MAX_REC_ATTR) return (false);
-
-      /* If not found, see if we can allocate a new entry */
-      if (xx == p_rec->num_attributes)
-        p_attr = &p_rec->attribute[p_rec->num_attributes];
-      else {
-        /* Since the attributes are kept in sorted order, insert ours here */
-        for (yy = p_rec->num_attributes; yy > xx; yy--)
-          p_rec->attribute[yy] = p_rec->attribute[yy - 1];
-      }
-
-      p_attr->id = attr_id;
-      p_attr->type = attr_type;
-      p_attr->len = attr_len;
-
-      if (p_rec->free_pad_ptr + attr_len >= SDP_MAX_PAD_LEN) {
-        /* do truncate only for text string type descriptor */
-        if (attr_type == TEXT_STR_DESC_TYPE) {
-          SDP_TRACE_WARNING(
-              "SDP_AddAttribute: attr_len:%d too long. truncate to (%d)",
-              attr_len, SDP_MAX_PAD_LEN - p_rec->free_pad_ptr);
-
-          attr_len = SDP_MAX_PAD_LEN - p_rec->free_pad_ptr;
-          p_val[SDP_MAX_PAD_LEN - p_rec->free_pad_ptr - 1] = '\0';
-        } else
-          attr_len = 0;
-      }
-
-      if (attr_len > 0) {
-        p_attr->len = attr_len;
-        memcpy(&p_rec->attr_pad[p_rec->free_pad_ptr], p_val, (size_t)attr_len);
-        p_attr->value_ptr = &p_rec->attr_pad[p_rec->free_pad_ptr];
-        p_rec->free_pad_ptr += attr_len;
-      } else if (attr_len == 0 && p_attr->len != 0) {
-        /* if truncate to 0 length, simply don't add */
-        SDP_TRACE_ERROR(
-            "SDP_AddAttribute fail, length exceed maximum: ID %d: attr_len:%d ",
-            attr_id, attr_len);
-        p_attr->id = p_attr->type = p_attr->len = 0;
-        return (false);
-      }
-      p_rec->num_attributes++;
-      return (true);
+      return SDP_AddAttributeToRecord(p_rec, attr_id, attr_type, attr_len,
+                                      p_val);
     }
   }
   return (false);
+}
+
+/*******************************************************************************
+ *
+ * Function         SDP_AddAttributeToRecord
+ *
+ * Description      This function is called to add an attribute to a record.
+ *                  This would be through the SDP database maintenance API.
+ *                  If the attribute already exists in the record, it is
+ *                  replaced with the new value.
+ *
+ * NOTE             Attribute values must be passed as a Big Endian stream.
+ *
+ * Returns          true if added OK, else false
+ *
+ ******************************************************************************/
+bool SDP_AddAttributeToRecord(tSDP_RECORD* p_rec, uint16_t attr_id,
+                              uint8_t attr_type, uint32_t attr_len,
+                              uint8_t* p_val) {
+  uint16_t xx, yy;
+  tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
+
+  /* Found the record. Now, see if the attribute already exists */
+  for (xx = 0; xx < p_rec->num_attributes; xx++, p_attr++) {
+    /* The attribute exists. replace it */
+    if (p_attr->id == attr_id) {
+      SDP_DeleteAttributeFromRecord(p_rec, attr_id);
+      break;
+    }
+    if (p_attr->id > attr_id) break;
+  }
+
+  if (p_rec->num_attributes >= SDP_MAX_REC_ATTR) return (false);
+
+  /* If not found, see if we can allocate a new entry */
+  if (xx == p_rec->num_attributes)
+    p_attr = &p_rec->attribute[p_rec->num_attributes];
+  else {
+    /* Since the attributes are kept in sorted order, insert ours here */
+    for (yy = p_rec->num_attributes; yy > xx; yy--)
+      p_rec->attribute[yy] = p_rec->attribute[yy - 1];
+  }
+
+  p_attr->id = attr_id;
+  p_attr->type = attr_type;
+  p_attr->len = attr_len;
+
+  if (p_rec->free_pad_ptr + attr_len >= SDP_MAX_PAD_LEN) {
+    /* do truncate only for text string type descriptor */
+    if (attr_type == TEXT_STR_DESC_TYPE) {
+      SDP_TRACE_WARNING(
+          "SDP_AddAttributeToRecord: attr_len:%d too long. truncate to (%d)",
+          attr_len, SDP_MAX_PAD_LEN - p_rec->free_pad_ptr);
+
+      attr_len = SDP_MAX_PAD_LEN - p_rec->free_pad_ptr;
+      p_val[SDP_MAX_PAD_LEN - p_rec->free_pad_ptr - 1] = '\0';
+    } else
+      attr_len = 0;
+  }
+
+  if (attr_len > 0) {
+    p_attr->len = attr_len;
+    memcpy(&p_rec->attr_pad[p_rec->free_pad_ptr], p_val, (size_t)attr_len);
+    p_attr->value_ptr = &p_rec->attr_pad[p_rec->free_pad_ptr];
+    p_rec->free_pad_ptr += attr_len;
+  } else if (attr_len == 0 && p_attr->len != 0) {
+    /* if truncate to 0 length, simply don't add */
+    SDP_TRACE_ERROR(
+        "SDP_AddAttributeToRecord fail, length exceed maximum: ID %d: "
+        "attr_len:%d ",
+        attr_id, attr_len);
+    p_attr->id = p_attr->type = p_attr->len = 0;
+    return (false);
+  }
+  p_rec->num_attributes++;
+  return (true);
 }
 
 /*******************************************************************************
@@ -695,6 +720,47 @@ bool SDP_AddProfileDescriptorList(uint32_t handle, uint16_t profile_uuid,
 
 /*******************************************************************************
  *
+ * Function         SDP_AddProfileDescriptorListToRecord
+ *
+ * Description      This function is called to add a profile descriptor list to
+ *                  a record. This would be through the SDP database maintenance
+ *                  API. If the version already exists in the record, it is
+ *                  replaced with the new one.
+ *
+ * Returns          true if added OK, else false
+ *
+ ******************************************************************************/
+bool SDP_AddProfileDescriptorListToRecord(tSDP_RECORD* prec,
+                                          uint16_t profile_uuid,
+                                          uint16_t version) {
+  uint8_t* p;
+  bool result;
+  uint8_t* p_buff = (uint8_t*)osi_malloc(sizeof(uint8_t) * SDP_MAX_ATTR_LEN);
+
+  p = p_buff + 2;
+
+  /* First, build the profile descriptor list. This consists of a data element
+   * sequence. */
+  /* The sequence consists of profile's UUID and version number  */
+  UINT8_TO_BE_STREAM(p, (UUID_DESC_TYPE << 3) | SIZE_TWO_BYTES);
+  UINT16_TO_BE_STREAM(p, profile_uuid);
+
+  UINT8_TO_BE_STREAM(p, (UINT_DESC_TYPE << 3) | SIZE_TWO_BYTES);
+  UINT16_TO_BE_STREAM(p, version);
+
+  /* Add in type and length fields */
+  *p_buff = (uint8_t)((DATA_ELE_SEQ_DESC_TYPE << 3) | SIZE_IN_NEXT_BYTE);
+  *(p_buff + 1) = (uint8_t)(p - (p_buff + 2));
+
+  result = SDP_AddAttributeToRecord(prec, ATTR_ID_BT_PROFILE_DESC_LIST,
+                                    DATA_ELE_SEQ_DESC_TYPE,
+                                    (uint32_t)(p - p_buff), p_buff);
+  osi_free(p_buff);
+  return result;
+}
+
+/*******************************************************************************
+ *
  * Function         SDP_AddLanguageBaseAttrIDList
  *
  * Description      This function is called to add a language base attr list to
@@ -778,47 +844,67 @@ bool SDP_AddServiceClassIdList(uint32_t handle, uint16_t num_services,
  ******************************************************************************/
 bool SDP_DeleteAttribute(uint32_t handle, uint16_t attr_id) {
   tSDP_RECORD* p_rec = &sdp_cb.server_db.record[0];
-  uint8_t* pad_ptr;
-  uint32_t len; /* Number of bytes in the entry */
 
   /* Find the record in the database */
   for (uint16_t record_index = 0; record_index < sdp_cb.server_db.num_records; record_index++, p_rec++) {
     if (p_rec->record_handle == handle) {
-      tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
-
       SDP_TRACE_API("Deleting attr_id 0x%04x for handle 0x%x", attr_id, handle);
-      /* Found it. Now, find the attribute */
-      for (uint16_t attribute_index = 0; attribute_index < p_rec->num_attributes; attribute_index++, p_attr++) {
-        if (p_attr->id == attr_id) {
-          pad_ptr = p_attr->value_ptr;
-          len = p_attr->len;
+      return SDP_DeleteAttributeFromRecord(p_rec, attr_id);
+    }
+  }
+  /* If here, not found */
+  return (false);
+}
 
-          if (len) {
-            for (uint16_t zz = 0; zz < p_rec->num_attributes; zz++) {
-              if (p_rec->attribute[zz].value_ptr > pad_ptr)
-                p_rec->attribute[zz].value_ptr -= len;
-            }
-          }
+/*******************************************************************************
+ *
+ * Function         SDP_DeleteAttributeFromRecord
+ *
+ * Description      This function is called to delete an attribute from a
+ *                  record. This would be through the SDP database maintenance
+ *                  API.
+ *
+ * Returns          true if deleted OK, else false if not found
+ *
+ ******************************************************************************/
 
-          /* Found it. Shift everything up one */
-          p_rec->num_attributes--;
+bool SDP_DeleteAttributeFromRecord(tSDP_RECORD* p_rec, uint16_t attr_id) {
+  tSDP_ATTRIBUTE* p_attr = &p_rec->attribute[0];
+  uint8_t* pad_ptr;
+  uint32_t len; /* Number of bytes in the entry */
 
-          for (uint16_t zz = attribute_index; zz < p_rec->num_attributes; zz++, p_attr++) {
-            *p_attr = *(p_attr + 1);
-          }
+  /* Found it. Now, find the attribute */
+  for (uint16_t attribute_index = 0; attribute_index < p_rec->num_attributes;
+       attribute_index++, p_attr++) {
+    if (p_attr->id == attr_id) {
+      pad_ptr = p_attr->value_ptr;
+      len = p_attr->len;
 
-          /* adjust attribute values if needed */
-          if (len) {
-            uint16_t last_attribute_to_adjust =
-                (p_rec->free_pad_ptr - ((pad_ptr + len) - &p_rec->attr_pad[0]));
-            for (uint16_t zz = 0; zz < last_attribute_to_adjust; zz++, pad_ptr++) {
-              *pad_ptr = *(pad_ptr + len);
-            }
-            p_rec->free_pad_ptr -= len;
-          }
-          return (true);
+      if (len) {
+        for (uint16_t zz = 0; zz < p_rec->num_attributes; zz++) {
+          if (p_rec->attribute[zz].value_ptr > pad_ptr)
+            p_rec->attribute[zz].value_ptr -= len;
         }
       }
+
+      /* Found it. Shift everything up one */
+      p_rec->num_attributes--;
+
+      for (uint16_t zz = attribute_index; zz < p_rec->num_attributes;
+           zz++, p_attr++) {
+        *p_attr = *(p_attr + 1);
+      }
+
+      /* adjust attribute values if needed */
+      if (len) {
+        uint16_t last_attribute_to_adjust =
+            (p_rec->free_pad_ptr - ((pad_ptr + len) - &p_rec->attr_pad[0]));
+        for (uint16_t zz = 0; zz < last_attribute_to_adjust; zz++, pad_ptr++) {
+          *pad_ptr = *(pad_ptr + len);
+        }
+        p_rec->free_pad_ptr -= len;
+      }
+      return (true);
     }
   }
   /* If here, not found */