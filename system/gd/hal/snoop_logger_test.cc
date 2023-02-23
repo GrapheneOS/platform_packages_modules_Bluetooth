fc995c6a7cf6bbf26216348cc66d7c34d6678de2
Michał Narajowski <narajn@google.com>
2023-02-23 11:05:05 +0000

    snoop_logger: Fix leaking socket FDs in tests
    Bug: 270516354
    Tag: #feature
    Test: atest bluetooth_test_gd
    Change-Id: I820b263956f4f1f09eae4b258f03192c62f87c28

--- system/gd/hal/snoop_logger_test.cc
+++ system/gd/hal/snoop_logger_test.cc
@@ -1383,6 +1383,7 @@ TEST_F(SnoopLoggerModuleTest, socket_disabled_connect_fail_test) {
   ASSERT_TRUE(ret != 0);
 
   test_registry->StopAll();
+  close(socket_fd);
 }
 
 TEST_F(SnoopLoggerModuleTest, default_socket_enabled_capture_recv_test) {
@@ -1432,6 +1433,7 @@ TEST_F(SnoopLoggerModuleTest, default_socket_enabled_capture_recv_test) {
   ASSERT_TRUE(std::memcmp(recv_buf3, kHfpAtNrec0.data(), kHfpAtNrec0.size()) == 0);
 
   test_registry->StopAll();
+  close(socket_fd);
 }
 
 TEST_F(SnoopLoggerModuleTest, custom_socket_register_enabled_capture_recv_test) {
@@ -1491,6 +1493,7 @@ TEST_F(SnoopLoggerModuleTest, custom_socket_register_enabled_capture_recv_test) 
   ASSERT_TRUE(std::memcmp(recv_buf3, kHfpAtNrec0.data(), kHfpAtNrec0.size()) == 0);
 
   test_registry->StopAll();
+  close(socket_fd);
 }
 
 TEST_F(SnoopLoggerModuleTest, custom_socket_interface_register_logging_disabled_test) {
