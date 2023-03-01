miyu-xu/packages-modules-Bluetooth

BluetoothMetrics: Extract popular device names using Bloomfilter and hash
Bug: 264937355
Test: atest BluetoothInstrumentationTests
Change-Id: I0b9982eac34c1005904d7edb54f04ef0c16d35d8

android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -696,6 +696,7 @@ class AdapterProperties {
         if (state == BluetoothProfile.STATE_CONNECTING) {
             BluetoothStatsLog.write(BluetoothStatsLog.BLUETOOTH_DEVICE_NAME_REPORTED,
                     mService.getMetricId(device), device.getName());
+            MetricsLogger.getInstance().logBluetoothDeviceName(device.getName());
         }
         Log.d(TAG,
                 "PROFILE_CONNECTION_STATE_CHANGE: profile=" + profile + ", device=" + device + ", "

GrapheneOS/platform_packages_modules_Bluetooth

(cherry picked from commit d8d4a78801e6fed30306bb8b297086cefd63f32d)
Merged-In: I0b9982eac34c1005904d7edb54f04ef0c16d35d8

android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -714,6 +714,7 @@ class AdapterProperties {
         if (state == BluetoothProfile.STATE_CONNECTING) {
             BluetoothStatsLog.write(BluetoothStatsLog.BLUETOOTH_DEVICE_NAME_REPORTED,
                     mService.getMetricId(device), device.getName());
+            MetricsLogger.getInstance().logSanitizedBluetoothDeviceName(device.getName());
         }
         Log.d(TAG,
                 "PROFILE_CONNECTION_STATE_CHANGE: profile=" + profile + ", device=" + device + ", "