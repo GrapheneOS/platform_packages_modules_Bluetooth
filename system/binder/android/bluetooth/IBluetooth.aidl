miyu-xu/packages-modules-Bluetooth

BluetoothMetrics: Upload Bluetooth L2CAP CoC Client Connection Metrics
Bug: 266625932
Test: atest BluetoothInstrumentationTests
Change-Id: I7481369f74bf25e2586ff4860cc71f42b207297e

system/binder/android/bluetooth/IBluetooth.aidl
@@ -178,6 +178,9 @@ interface IBluetooth
     @JavaPassthrough(annotation="@android.annotation.RequiresNoPermission")
     IBluetoothSocketManager getSocketManager();
 
+    @JavaPassthrough(annotation="@android.annotation.RequiresNoPermission")
+    oneway void logL2capcocClientConnection(in BluetoothDevice device, int port, boolean isSecured, int result, long connectionLatencyNanos);
+
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(allOf={android.Manifest.permission.BLUETOOTH_CONNECT,android.Manifest.permission.BLUETOOTH_PRIVILEGED})")
     oneway void factoryReset(in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
 

GrapheneOS/platform_packages_modules_Bluetooth

system/binder/android/bluetooth/IBluetooth.aidl
@@ -180,6 +180,9 @@ interface IBluetooth
     @JavaPassthrough(annotation="@android.annotation.RequiresNoPermission")
     IBluetoothSocketManager getSocketManager();
 
+    @JavaPassthrough(annotation="@android.annotation.RequiresNoPermission")
+    oneway void logL2capcocClientConnection(in BluetoothDevice device, int port, boolean isSecured, int result, long connectionLatencyMillis);
+
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(allOf={android.Manifest.permission.BLUETOOTH_CONNECT,android.Manifest.permission.BLUETOOTH_PRIVILEGED})")
     oneway void factoryReset(in AttributionSource attributionSource, in SynchronousResultReceiver receiver);
 