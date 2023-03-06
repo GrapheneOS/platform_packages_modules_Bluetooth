miyu-xu/packages-modules-Bluetooth

Address API feedback for Bluetooth Distance Measurement
Bug: 269252708
Test: atest DistanceMeasurementParamsTest
Test: atest DistanceMeasurementResultTest
Test: atest DistanceMeasurementSessionTest
Change-Id: I238c84c462a90859d314737fca0046858011075e

android/app/src/com/android/bluetooth/gatt/DistanceMeasurementTracker.java
@@ -52,8 +52,8 @@ class DistanceMeasurementTracker {
         mIdentityAddress = identityAddress;
         mUuid = uuid;
         mFrequency = frequency;
-        mDuration = params.getDuration();
-        mMethod = params.getMethod();
+        mDuration = params.getDurationInSeconds();
+        mMethod = params.getMethodId();
         mCallback = callback;
     }
 

android/app/tests/unit/src/com/android/bluetooth/gatt/DistanceMeasurementTrackerTest.java
@@ -64,9 +64,9 @@ public class DistanceMeasurementTrackerTest {
         mUuid = UUID.randomUUID();
         mDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(IDENTITY_ADDRESS);
         mParams = new DistanceMeasurementParams.Builder(mDevice)
-                .setDuration(TIMEOUT_S)
+                .setDurationInSeconds(TIMEOUT_S)
                 .setFrequency(DistanceMeasurementParams.REPORT_FREQUENCY_LOW)
-                .setMethod(mMethod)
+                .setMethodId(mMethod)
                 .build();
         mTracker = new DistanceMeasurementTracker(
                 mDistanceMeasurementManager, mParams, IDENTITY_ADDRESS, mUuid, 1000, mCallback);

GrapheneOS/platform_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/gatt/DistanceMeasurementTracker.java
@@ -52,8 +52,8 @@ class DistanceMeasurementTracker {
         mIdentityAddress = identityAddress;
         mUuid = uuid;
         mFrequency = frequency;
-        mDuration = params.getDuration();
-        mMethod = params.getMethod();
+        mDuration = params.getDurationSeconds();
+        mMethod = params.getMethodId();
         mCallback = callback;
     }
 

android/app/tests/unit/src/com/android/bluetooth/gatt/DistanceMeasurementTrackerTest.java
@@ -64,9 +64,9 @@ public class DistanceMeasurementTrackerTest {
         mUuid = UUID.randomUUID();
         mDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(IDENTITY_ADDRESS);
         mParams = new DistanceMeasurementParams.Builder(mDevice)
-                .setDuration(TIMEOUT_S)
+                .setDurationSeconds(TIMEOUT_S)
                 .setFrequency(DistanceMeasurementParams.REPORT_FREQUENCY_LOW)
-                .setMethod(mMethod)
+                .setMethodId(mMethod)
                 .build();
         mTracker = new DistanceMeasurementTracker(
                 mDistanceMeasurementManager, mParams, IDENTITY_ADDRESS, mUuid, 1000, mCallback);