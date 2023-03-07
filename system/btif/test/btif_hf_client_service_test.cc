517203d1c9611e78f569a140405607802e107d21
William Escande <wescande@google.com>
2023-03-07 01:25:40 -0800

    Add hfp sysprop for profile version
    The `define` cannot be override on runtime and some target need to
    change the default version to use (Eg: Wear OS)
    
    Dirty cherry because of:
      * hfp dynamic version feature missing
      * invisalign
    
    Bug: 263323082
    Test: atest net_test_btif_hf_client_service net_test_bta
          net_test_btif_bta
    (cherry picked from https://android-review.googlesource.com/q/commit:fb9e4fac25fe3f6ef5e0b1822715fdf5c508b076)
    Merged-In: Iee020f8029dc5f82564fa28574914096a8e7cd75
    
    Change-Id: I16bd905974e9cf589b01bdd908763f3a2699a49b

--- system/btif/test/btif_hf_client_service_test.cc
+++ system/btif/test/btif_hf_client_service_test.cc
@@ -2,11 +2,26 @@
 #include <gtest/gtest.h>
 #include "bta_hfp_api.h"
 
+#ifdef OS_ANDROID
+#include <hfp.sysprop.h>
+#endif
+
 #undef LOG_TAG
 #include "btif/src/btif_hf_client.cc"
 
 static tBTA_HF_CLIENT_FEAT gFeatures;
 
+#define DEFAULT_BTA_HFP_VERSION HFP_VERSION_1_7
+int get_default_hfp_version() {
+#ifdef OS_ANDROID
+  static const int version =
+      android::sysprop::bluetooth::Hfp::version().value_or(
+          DEFAULT_BTA_HFP_VERSION);
+  return version;
+#else
+  return DEFAULT_BTA_HFP_VERSION;
+#endif
+}
 
 uint8_t btif_trace_level = BT_TRACE_LEVEL_WARNING;
 void LogMsg(uint32_t trace_set_mask, const char* fmt_str, ...) {}
@@ -41,5 +56,5 @@ TEST_F(BtifHfClientTest, test_btif_hf_cleint_service) {
 
   btif_hf_client_execute_service(enable);
   ASSERT_EQ((gFeatures & BTA_HF_CLIENT_FEAT_ESCO_S4) > 0,
-            BTA_HFP_VERSION >= HFP_VERSION_1_7);
+            get_default_hfp_version() >= HFP_VERSION_1_7);
 }
