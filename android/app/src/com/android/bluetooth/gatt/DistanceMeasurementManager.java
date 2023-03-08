fd084902579cab20a038eaa891922ab3836ed95d
Chih Wei Chang <aaronchang@google.com>
2023-03-08 02:13:37 +0000

    Revert "Address API feedback for Bluetooth Distance Measurement"
    Revert submission 2450382-Address API feedback for Bluetooth Distance Measurement
    
    Reason for revert: DroidMonitor: Potential culprit for Bug 272130885 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Reverted changes: /q/submissionid:2450382-Address+API+feedback+for+Bluetooth+Distance+Measurement
    
    Change-Id: I938bb17dabb3597f6204e64c1c364fd79be10f8f

--- android/app/src/com/android/bluetooth/gatt/DistanceMeasurementManager.java
+++ android/app/src/com/android/bluetooth/gatt/DistanceMeasurementManager.java
@@ -86,13 +86,13 @@ public class DistanceMeasurementManager {
     void startDistanceMeasurement(UUID uuid, DistanceMeasurementParams params,
             IDistanceMeasurementCallback callback) {
         Log.i(TAG, "startDistanceMeasurement device:" + params.getDevice().getAnonymizedAddress()
-                + ", method: " + params.getMethodId());
+                + ", method: " + params.getMethod());
         String identityAddress = mAdapterService.getIdentityAddress(
                 params.getDevice().getAddress());
         logd("Get identityAddress: " + params.getDevice().getAnonymizedAddress() + " => "
                 + BluetoothUtils.toAnonymizedAddress(identityAddress));
 
-        int frequencyValue = getFrequencyValue(params.getFrequency(), params.getMethodId());
+        int frequencyValue = getFrequencyValue(params.getFrequency(), params.getMethod());
         if (frequencyValue == -1) {
             invokeStartFail(callback, params.getDevice(),
                     BluetoothStatusCodes.ERROR_BAD_PARAMETERS);
@@ -102,7 +102,7 @@ public class DistanceMeasurementManager {
         DistanceMeasurementTracker tracker = new DistanceMeasurementTracker(
                 this, params, identityAddress, uuid, frequencyValue, callback);
 
-        switch (params.getMethodId()) {
+        switch (params.getMethod()) {
             case DistanceMeasurementMethod.DISTANCE_MEASUREMENT_METHOD_AUTO:
             case DistanceMeasurementMethod.DISTANCE_MEASUREMENT_METHOD_RSSI:
                 startRssiTracker(tracker);
@@ -140,7 +140,7 @@ public class DistanceMeasurementManager {
                 return stopRssiTracker(uuid, identityAddress, timeout);
             default:
                 Log.w(TAG, "stopDistanceMeasurement with invalid method:" + method);
-                return BluetoothStatusCodes.ERROR_DISTANCE_MEASUREMENT_INTERNAL;
+                return BluetoothStatusCodes.DISTANCE_MEASUREMENT_ERROR_INTERNAL;
         }
     }
 
@@ -149,7 +149,7 @@ public class DistanceMeasurementManager {
         HashSet<DistanceMeasurementTracker> set = mRssiTrackers.get(identityAddress);
         if (set == null) {
             Log.w(TAG, "Can't find rssi tracker");
-            return BluetoothStatusCodes.ERROR_DISTANCE_MEASUREMENT_INTERNAL;
+            return BluetoothStatusCodes.DISTANCE_MEASUREMENT_ERROR_INTERNAL;
         }
 
         for (DistanceMeasurementTracker tracker : set) {
