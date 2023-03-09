5a7584b370804f25a91283a931c64cd959fccd5b
Himanshu Rawat <rwt@google.com>
2023-03-09 21:54:50 +0000

    Don't remove the HOGP device if remote device does not have LTK.
    HOGP host removes the bonded device if encryption fails due to "Pin or Key Missing" error. This behavior was introduced for a remote control device which would frequently forget the LTK.
    
    Bug: 264516467
    Change-Id: If751f10b46aa5889629f64dc0199caefc831d5ed
    Test: Remove bond information from the peripheral device before reconnection

--- system/bta/hh/bta_hh_le.cc
+++ system/bta/hh/bta_hh_le.cc
@@ -80,7 +80,6 @@ static void bta_hh_le_add_dev_bg_conn(tBTA_HH_DEV_CB* p_cb, bool check_bond);
 static void bta_hh_process_cache_rpt(tBTA_HH_DEV_CB* p_cb,
                                      tBTA_HH_RPT_CACHE_ENTRY* p_rpt_cache,
                                      uint8_t num_rpt);
-extern void bta_dm_process_delete_key_RC_to_unpair(const RawAddress& bd_addr);
 
 static const char* bta_hh_le_rpt_name[4] = {"UNKNOWN", "INPUT", "OUTPUT",
                                             "FEATURE"};
@@ -1009,7 +1008,7 @@ void bta_hh_security_cmpl(tBTA_HH_DEV_CB* p_cb,
     LOG_ERROR("Received encryption failed status:%s btm_status:%s",
               bta_hh_status_text(p_cb->status).c_str(),
               btm_status_text(p_cb->btm_status).c_str());
-    bta_dm_process_delete_key_RC_to_unpair(p_cb->addr);
+    bta_hh_le_api_disc_act(p_cb);
   } else {
     LOG_ERROR("Encryption failed status:%s btm_status:%s",
               bta_hh_status_text(p_cb->status).c_str(),
