d0158456df40e3f701d3834e04d31f1e32ae4bb5
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2023-03-15 12:58:44 +0000

    mcs: Add dump of authorization state and CCC values
    This extends dumping of MCS data. Authorization state and CCC values for
    each characteristic is dump.
    
    Current dump of McpService:
    Profile: McpService
    Media Control Service  instance list:
    	MediaControlService instance:
    		Ccid = 2
    		Features:
    			PLAYER_NAME(BIT 1)
    			TRACK_CHANGED(BIT 4)
    			TRACK_TITLE(BIT 5)
    			TRACK_DURATION(BIT 6)
    			TRACK_POSITION(BIT 7)
    			PLAYBACK_SPEED(BIT 8)
    			SEEKING_SPEED(BIT 9)
    			PLAYING_ORDER(BIT 15)
    			PLAYING_ORDER_SUPPORTED(BIT 16)
    			MEDIA_STATE(BIT 17)
    			MEDIA_CONTROL_POINT(BIT 18)
    			MEDIA_CONTROL_POINT_OPCODES_SUPPORTED(BIT 19)
    			CONTENT_CONTROL_ID(BIT 22)
    			PLAYER_NAME(BIT 1)
    			TRACK_TITLE(BIT 5)
    			TRACK_DURATION(BIT 6)
    			TRACK_POSITION(BIT 7)
    			PLAYBACK_SPEED(BIT 8)
    			SEEKING_SPEED(BIT 9)
    			PLAYING_ORDER(BIT 15)
    			MEDIA_CONTROL_POINT_OPCODES_SUPPORTED(BIT 19)
    		Player name: YouTube Music
    		CurrentPlaybackState = PLAYING
    		CCC states for device: xx:xx:xx:xx:2C:08
    			Characteristic: MEDIA_STATE, value: NOTIFICATION
    			Characteristic: PLAYER_NAME, value: NOTIFICATION
    			Characteristic: MEDIA_CONTROL_POINT, value: NOTIFICATION
    			Characteristic: MEDIA_CONTROL_POINT_OPCODES_SUPPORTED, value: NOTIFICATION
    			Characteristic: TRACK_CHANGED, value: NOTIFICATION
    		CCC states for device: xx:xx:xx:xx:0F:2E
    			Characteristic: MEDIA_STATE, value: NOTIFICATION
    			Characteristic: PLAYER_NAME, value: NOTIFICATION
    			Characteristic: MEDIA_CONTROL_POINT, value: NOTIFICATION
    			Characteristic: MEDIA_CONTROL_POINT_OPCODES_SUPPORTED, value: NOTIFICATION
    			Characteristic: TRACK_CHANGED, value: NOTIFICATION
    		Device: XX:XX:XX:XX:2C:08, access: ACCESS_ALLOWED
    		Device: XX:XX:XX:XX:0F:2E, access: ACCESS_ALLOWED
    
    Tag: #feature
    Bug: 273530115
    Test: atest GoogleBluetoothInstrumentationTests
    Change-Id: Id373bf39defac11f345e9ef585cd77de6a0763a6

--- android/app/src/com/android/bluetooth/mcp/MediaControlGattService.java
+++ android/app/src/com/android/bluetooth/mcp/MediaControlGattService.java
@@ -149,6 +149,56 @@ public class MediaControlGattService implements MediaControlGattServiceInterface
     private LeAudioService mLeAudioService;
     private AdapterService mAdapterService;
 
+    private static String mcsUuidToString(UUID uuid) {
+        if (uuid.equals(UUID_PLAYER_NAME)) {
+            return "PLAYER_NAME";
+        } else if (uuid.equals(UUID_PLAYER_ICON_OBJ_ID)) {
+            return "PLAYER_ICON_OBJ_ID";
+        } else if (uuid.equals(UUID_PLAYER_ICON_URL)) {
+            return "PLAYER_ICON_URL";
+        } else if (uuid.equals(UUID_TRACK_CHANGED)) {
+            return "TRACK_CHANGED";
+        } else if (uuid.equals(UUID_TRACK_TITLE)) {
+            return "TRACK_TITLE";
+        } else if (uuid.equals(UUID_TRACK_DURATION)) {
+            return "TRACK_DURATION";
+        } else if (uuid.equals(UUID_TRACK_POSITION)) {
+            return "TRACK_POSITION";
+        } else if (uuid.equals(UUID_PLAYBACK_SPEED)) {
+            return "PLAYBACK_SPEED";
+        } else if (uuid.equals(UUID_SEEKING_SPEED)) {
+            return "SEEKING_SPEED";
+        } else if (uuid.equals(UUID_CURRENT_TRACK_SEGMENT_OBJ_ID)) {
+            return "CURRENT_TRACK_SEGMENT_OBJ_ID";
+        } else if (uuid.equals(UUID_CURRENT_TRACK_OBJ_ID)) {
+            return "CURRENT_TRACK_OBJ_ID";
+        } else if (uuid.equals(UUID_NEXT_TRACK_OBJ_ID)) {
+            return "NEXT_TRACK_OBJ_ID";
+        } else if (uuid.equals(UUID_CURRENT_GROUP_OBJ_ID)) {
+            return "CURRENT_GROUP_OBJ_ID";
+        } else if (uuid.equals(UUID_PARENT_GROUP_OBJ_ID)) {
+            return "PARENT_GROUP_OBJ_ID";
+        } else if (uuid.equals(UUID_PLAYING_ORDER)) {
+            return "PLAYING_ORDER";
+        } else if (uuid.equals(UUID_PLAYING_ORDER_SUPPORTED)) {
+            return "PLAYING_ORDER_SUPPORTED";
+        } else if (uuid.equals(UUID_MEDIA_STATE)) {
+            return "MEDIA_STATE";
+        } else if (uuid.equals(UUID_MEDIA_CONTROL_POINT)) {
+            return "MEDIA_CONTROL_POINT";
+        } else if (uuid.equals(UUID_MEDIA_CONTROL_POINT_OPCODES_SUPPORTED)) {
+            return "MEDIA_CONTROL_POINT_OPCODES_SUPPORTED";
+        } else if (uuid.equals(UUID_SEARCH_RESULT_OBJ_ID)) {
+            return "SEARCH_RESULT_OBJ_ID";
+        } else if (uuid.equals(UUID_SEARCH_CONTROL_POINT)) {
+            return "SEARCH_CONTROL_POINT";
+        } else if (uuid.equals(UUID_CONTENT_CONTROL_ID)) {
+            return "CONTENT_CONTROL_ID";
+        } else {
+            return "UNKNOWN(" + uuid + ")";
+        }
+    }
+
     private static class GattOpContext {
         public enum Operation {
             READ_CHARACTERISTIC,
@@ -1955,7 +2005,25 @@ public class MediaControlGattService implements MediaControlGattServiceInterface
     public void dump(StringBuilder sb) {
         sb.append("\tMediaControlService instance:");
         sb.append("\n\t\tCcid = " + mCcid);
-        sb.append("\n\t\tFeatures = " + String.format("0x%08X", mFeatures));
+        sb.append("\n\t\tFeatures:" + ServiceFeature.featuresToString(mFeatures, "\n\t\t\t"));
+
+        BluetoothGattCharacteristic characteristic =
+                mCharacteristics.get(CharId.PLAYER_NAME);
+        if (characteristic == null) {
+            sb.append("\n\t\tPlayer name: <No Player>");
+        } else {
+            sb.append("\n\t\tPlayer name: " + characteristic.getStringValue(0));
+        }
+
         sb.append("\n\t\tCurrentPlaybackState = " + mCurrentMediaState);
+        for (Map.Entry<String, HashMap<UUID, Short>> deviceEntry
+                : mCccDescriptorValues.entrySet()) {
+            sb.append("\n\t\tCCC states for device: " + "xx:xx:xx:xx:"
+                    + deviceEntry.getKey().substring(12));
+            for (Map.Entry<UUID, Short> entry : deviceEntry.getValue().entrySet()) {
+                sb.append("\n\t\t\tCharacteristic: " + mcsUuidToString(entry.getKey()) + ", value: "
+                        + Utils.cccIntToStr(entry.getValue()));
+            }
+        }
     }
 }
