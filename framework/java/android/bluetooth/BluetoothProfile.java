979f2e7984266b972a202251c1e1d95ee1fd3b15
Kyunglyul Hyun <klhyun@google.com>
2023-03-16 16:15:15 +0900

    Re-register callbacks when service is connected
    When a callback is registered to
    BluetoothHapClient, BluetoothLeAudio and BluetoothLeBroadcast,
    and their corresponding service is not available,
    they register callbacks when the service is available.
    
    Before this change, they did when BT is up, which could be
    before the service is available and it failed.
    
    This change fixes the timing by listening to the service connection.
    It also removes unnecessary broadcast receivers.
    
    Tag: #feature
    Bug: 267461828
    Test: atest BluetoothInstrumentationTests
    Change-Id: I12f49241b49fc2f793e7c2a25c66629aacd4c53f

--- framework/java/android/bluetooth/BluetoothProfile.java
+++ framework/java/android/bluetooth/BluetoothProfile.java
@@ -18,6 +18,7 @@ package android.bluetooth;
 
 import android.annotation.IntDef;
 import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.annotation.RequiresNoPermission;
 import android.annotation.SuppressLint;
 import android.annotation.SystemApi;
@@ -413,6 +414,33 @@ public interface BluetoothProfile {
          */
         @RequiresNoPermission
         void onServiceDisconnected(int profile);
+    }
+
+    /**
+     * A service listener that forwards methods calls to the given listener.
+     * This can be used to override specific method.
+     * @hide
+     */
+    class ForwardingServiceListener implements ServiceListener {
+        private final ServiceListener mListener;
+
+        ForwardingServiceListener(@Nullable ServiceListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+        public void onServiceConnected(int profile, BluetoothProfile proxy) {
+            if (mListener != null) {
+                mListener.onServiceConnected(profile, proxy);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(int profile) {
+            if (mListener != null) {
+                mListener.onServiceDisconnected(profile);
+            }
+        }
     }
 
     /**
