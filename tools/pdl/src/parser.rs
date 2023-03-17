miyu-xu/packages-modules-Bluetooth

pdl: Specialize Field::Fixed into FixedScalar and FixedEnum
There existed an implicit assumption that width, value and
enum_id, tag_id were mutually exclusive in the Field::Fixed
case.
This change makes the assumption explicit by modiyfing
the AST to add specialized cases.

Test: cargo build && cargo test
Change-Id: Ib51f61ad2cd39f8686ad9d94a6699cf2b3b318e7

tools/pdl/src/parser.rs
@@ -347,11 +347,19 @@ fn parse_field(node: Node<'_>, context: &Context) -> Result<ast::Field, String> 
                 let size_modifier = parse_size_modifier_opt(&mut children);
                 crate::ast::FieldDesc::Payload { size_modifier }
             }
-            Rule::fixed_field => {
-                let (tag_id, value) = parse_identifier_or_integer(&mut children)?;
-                let (enum_id, width) = parse_identifier_or_integer(&mut children)?;
-                crate::ast::FieldDesc::Fixed { enum_id, tag_id, width, value }
-            }
+            Rule::fixed_field => match children.next() {
+                Some(n) if n.as_rule() == Rule::integer => {
+                    let value = n.as_usize()?;
+                    let width = parse_integer(&mut children)?;
+                    crate::ast::FieldDesc::FixedScalar { width, value }
+                }
+                Some(n) if n.as_rule() == Rule::string => {
+                    let tag_id = n.as_string();
+                    let enum_id = parse_string(&mut children)?;
+                    crate::ast::FieldDesc::FixedEnum { enum_id, tag_id }
+                }
+                _ => unreachable!(),
+            },
             Rule::reserved_field => {
                 let width = parse_integer(&mut children)?;
                 crate::ast::FieldDesc::Reserved { width }

GrapheneOS/platform_packages_modules_Bluetooth

(cherry picked from https://android-review.googlesource.com/q/commit:36db1e2b61ec46e6b3e240796ff500294823acca)
Merged-In: Ib51f61ad2cd39f8686ad9d94a6699cf2b3b318e7

tools/pdl/src/parser.rs
@@ -357,11 +357,19 @@ fn parse_field(node: Node<'_>, context: &Context) -> Result<ast::Field, String> 
                 let size_modifier = parse_size_modifier_opt(&mut children);
                 crate::ast::FieldDesc::Payload { size_modifier }
             }
-            Rule::fixed_field => {
-                let (tag_id, value) = parse_identifier_or_integer(&mut children)?;
-                let (enum_id, width) = parse_identifier_or_integer(&mut children)?;
-                crate::ast::FieldDesc::Fixed { enum_id, tag_id, width, value }
-            }
+            Rule::fixed_field => match children.next() {
+                Some(n) if n.as_rule() == Rule::integer => {
+                    let value = n.as_usize()?;
+                    let width = parse_integer(&mut children)?;
+                    crate::ast::FieldDesc::FixedScalar { width, value }
+                }
+                Some(n) if n.as_rule() == Rule::identifier => {
+                    let tag_id = n.as_string();
+                    let enum_id = parse_identifier(&mut children)?;
+                    crate::ast::FieldDesc::FixedEnum { enum_id, tag_id }
+                }
+                _ => unreachable!(),
+            },
             Rule::reserved_field => {
                 let width = parse_integer(&mut children)?;
                 crate::ast::FieldDesc::Reserved { width }