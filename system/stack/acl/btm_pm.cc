miyu-xu/packages-modules-Bluetooth

Only add br_edr to power mode db
Power modes only work with classic/br_edr

Bug: 217587113
Tag: #refactor
Test: gd/cert/run
Change-Id: I2b28968159df873aabb2c12f7855beae89c6d653

system/stack/acl/btm_pm.cc
@@ -148,11 +148,18 @@ tBTM_STATUS BTM_PmRegister(uint8_t mask, uint8_t* p_pm_id,
 }
 
 void BTM_PM_OnConnected(uint16_t handle, const RawAddress& remote_bda) {
+  if (pm_mode_db.find(handle) != pm_mode_db.end()) {
+    LOG_ERROR("Overwriting power mode db entry handle:%hu peer:%s", handle,
+              ADDRESS_TO_LOGGABLE_CSTR(remote_bda));
+  }
   pm_mode_db[handle] = {};
   pm_mode_db[handle].Init(remote_bda, handle);
 }
 
 void BTM_PM_OnDisconnected(uint16_t handle) {
+  if (pm_mode_db.find(handle) == pm_mode_db.end()) {
+    LOG_ERROR("Erasing unknown power mode db entry handle:%hu", handle);
+  }
   pm_mode_db.erase(handle);
   if (handle == pm_pend_link) {
     pm_pend_link = 0;

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 262479565
(cherry picked from https://android-review.googlesource.com/q/commit:272684810473fe72988475eb307f49305b45904a)
Merged-In: I2b28968159df873aabb2c12f7855beae89c6d653
Bug: 263323082

system/stack/acl/btm_pm.cc
@@ -157,11 +157,18 @@ tBTM_STATUS BTM_PmRegister(uint8_t mask, uint8_t* p_pm_id,
 }
 
 void BTM_PM_OnConnected(uint16_t handle, const RawAddress& remote_bda) {
+  if (pm_mode_db.find(handle) != pm_mode_db.end()) {
+    LOG_ERROR("Overwriting power mode db entry handle:%hu peer:%s", handle,
+              PRIVATE_ADDRESS(remote_bda));
+  }
   pm_mode_db[handle] = {};
   pm_mode_db[handle].Init(remote_bda, handle);
 }
 
 void BTM_PM_OnDisconnected(uint16_t handle) {
+  if (pm_mode_db.find(handle) == pm_mode_db.end()) {
+    LOG_ERROR("Erasing unknown power mode db entry handle:%hu", handle);
+  }
   pm_mode_db.erase(handle);
   if (handle == pm_pend_link) {
     pm_pend_link = 0;