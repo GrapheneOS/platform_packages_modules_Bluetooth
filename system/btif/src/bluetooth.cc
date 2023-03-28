7c436bc0d1caae0de2527cdabc0ac06a35ba3431
Himanshu Rawat <rwt@google.com>
2023-03-28 21:08:06 +0000

    Disable HOGP only if it was previously enabled
    bta_hh_cleanup_disable() is called to cleanup after the HID host and
    HOGP host has been disabled. HOGP host must be disabled only if it was
    enabled in first place. This change also allows us to remove
    bta_hh_cleanup_disable callback from core interfaces. This interface
    was used by GATT client because the HOGP host would unregister from
    GATT even when it was not registered with GATT beforehand.
    
    
    Bug: 272153768
    Test: Test regression
    Change-Id: Iaff8495bbfcc1288b7011380474fa6a0eac302cc

--- system/btif/src/bluetooth.cc
+++ system/btif/src/bluetooth.cc
@@ -334,7 +334,6 @@ static bluetooth::core::CoreInterface* CreateInterfaceToProfiles() {
       .btif_hh_virtual_unplug = btif_hh_virtual_unplug,
       .bta_hh_read_ssr_param = bta_hh_read_ssr_param,
       .bta_hh_le_is_hh_gatt_if = bta_hh_le_is_hh_gatt_if,
-      .bta_hh_cleanup_disable = bta_hh_cleanup_disable,
 
       // AVDTP
       .btif_av_set_dynamic_audio_buffer_size =
