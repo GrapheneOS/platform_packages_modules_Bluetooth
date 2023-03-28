miyu-xu/packages-modules-Bluetooth

Floss: Add Bredr limited discoverable mode
Add API to configure device into Bredr limited discoverable mode.

Bug: 262485102
Test: Manually
Tag: #floss
Change-Id: I5408da1393df68289d26c47188f259878a840641

system/gd/rust/topshim/src/btif.rs
@@ -299,11 +299,34 @@ pub enum BtScanMode {
     None_,
     Connectable,
     ConnectableDiscoverable,
+    ConnectableLimitedDiscoverable,
 }
 
 impl From<bindings::bt_scan_mode_t> for BtScanMode {
     fn from(item: bindings::bt_scan_mode_t) -> Self {
         BtScanMode::from_u32(item).unwrap_or(BtScanMode::None_)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[repr(u32)]
+pub enum BtDiscMode {
+    // reference to system/stack/btm/neighbor_inquiry.h
+    NonDiscoverable = 0,
+    LimitedDiscoverable = 1,
+    GeneralDiscoverable = 2,
+}
+
+impl TryFrom<u32> for BtDiscMode {
+    type Error = &'static str;
+
+    fn try_from(num: u32) -> std::result::Result<Self, Self::Error> {
+        match num {
+            0 => Ok(BtDiscMode::NonDiscoverable),
+            1 => Ok(BtDiscMode::LimitedDiscoverable),
+            2 => Ok(BtDiscMode::GeneralDiscoverable),
+            _ => Err("Unknown discoverable mode."),
+        }
     }
 }
 

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/topshim/src/btif.rs
@@ -299,11 +299,33 @@ pub enum BtScanMode {
     None_,
     Connectable,
     ConnectableDiscoverable,
+    ConnectableLimitedDiscoverable,
 }
 
 impl From<bindings::bt_scan_mode_t> for BtScanMode {
     fn from(item: bindings::bt_scan_mode_t) -> Self {
         BtScanMode::from_u32(item).unwrap_or(BtScanMode::None_)
+    }
+}
+
+#[derive(Clone, Debug, FromPrimitive, ToPrimitive, PartialEq, PartialOrd)]
+#[repr(u32)]
+pub enum BtDiscMode {
+    // reference to system/stack/btm/neighbor_inquiry.h
+    NonDiscoverable = 0,
+    LimitedDiscoverable = 1,
+    GeneralDiscoverable = 2,
+}
+
+impl From<u32> for BtDiscMode {
+    fn from(num: u32) -> Self {
+        BtDiscMode::from_u32(num).unwrap_or(BtDiscMode::NonDiscoverable)
+    }
+}
+
+impl Into<u32> for BtDiscMode {
+    fn into(self) -> u32 {
+        self.to_u32().unwrap_or(0)
     }
 }
 