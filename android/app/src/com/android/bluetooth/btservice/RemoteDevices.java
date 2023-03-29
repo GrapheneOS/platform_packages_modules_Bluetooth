miyu-xu/packages-modules-Bluetooth

Add system properties to config pairing UI
Add and get audio codec priorities from the property below
ro.bluetooth.pairing_ui_package

Bug: 270032693
Test: manually test in local build
Change-Id: I359dba7eae531053653caa69eaf0cd6353255833

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -1062,8 +1062,11 @@ final class RemoteDevices {
                 // Send PAIRING_CANCEL intent to dismiss any dialog requesting bonding.
                 intent = new Intent(BluetoothDevice.ACTION_PAIRING_CANCEL);
                 intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
-                intent.setPackage(mAdapterService.getString(R.string.pairing_ui_package));
-                Utils.sendBroadcast(mAdapterService, intent, BLUETOOTH_CONNECT,
+                intent.setPackage(SystemProperties.get(
+                        "ro.bluetooth.pairing_ui_package",
+                        mAdapterService.getString(R.string.pairing_ui_package)));
+
+                mAdapterService.sendBroadcast(intent, BLUETOOTH_CONNECT,
                         Utils.getTempAllowlistBroadcastOptions());
             } else if (device.getBondState() == BluetoothDevice.BOND_NONE) {
                 String key = Utils.getAddressStringFromByte(address);

GrapheneOS/platform_packages_modules_Bluetooth

bluetooth.pairing_ui_package.name
(cherry picked from https://android-review.googlesource.com/q/commit:9687c012d1f4ddf9d6a008650c01eb249764f9e0)
Merged-In: I359dba7eae531053653caa69eaf0cd6353255833
Bug: 263323082

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -877,7 +877,9 @@ final class RemoteDevices {
                 // Send PAIRING_CANCEL intent to dismiss any dialog requesting bonding.
                 intent = new Intent(BluetoothDevice.ACTION_PAIRING_CANCEL);
                 intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
-                intent.setPackage(sAdapterService.getString(R.string.pairing_ui_package));
+                intent.setPackage(SystemProperties.get(
+                        Utils.PAIRING_UI_PROPERTY,
+                        sAdapterService.getString(R.string.pairing_ui_package)));
                 sAdapterService.sendBroadcast(intent, BLUETOOTH_CONNECT,
                         Utils.getTempAllowlistBroadcastOptions());
             }