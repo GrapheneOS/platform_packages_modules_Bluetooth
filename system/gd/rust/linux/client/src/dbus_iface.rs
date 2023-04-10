miyu-xu/packages-modules-Bluetooth

Floss: Introduce new QA interface
The new QA interface is based on message system so that we don't need to
include every struct.

Bug: 261143122
Test: buil`
Tag: #floss

Change-Id: Id27a32843d1bdb8ea20f4daa6eec14908633de64

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -30,6 +30,7 @@ use btstack::bluetooth_gatt::{
     ScanFilterPattern, ScanResult, ScanSettings, ScanType,
 };
 use btstack::bluetooth_media::IBluetoothTelephony;
+use btstack::bluetooth_qa::IBluetoothQA;
 use btstack::socket_manager::{
     BluetoothServerSocket, BluetoothSocket, CallbackId, IBluetoothSocketManager,
     IBluetoothSocketManagerCallbacks, SocketId, SocketResult,
@@ -2226,3 +2227,23 @@ impl IBluetoothTelephony for BluetoothTelephonyDBus {
         dbus_generated!()
     }
 }
+
+pub(crate) struct BluetoothQADBus {
+    client_proxy: ClientDBusProxy,
+}
+
+impl BluetoothQADBus {
+    pub(crate) fn new(conn: Arc<SyncConnection>, index: i32) -> BluetoothQADBus {
+        BluetoothQADBus {
+            client_proxy: ClientDBusProxy::new(
+                conn.clone(),
+                String::from("org.chromium.bluetooth"),
+                make_object_path(index, "qa"),
+                String::from("org.chromium.bluetooth.BluetoothQA"),
+            ),
+        }
+    }
+}
+
+#[generate_dbus_interface_client]
+impl IBluetoothQA for BluetoothQADBus {}

GrapheneOS/platform_packages_modules_Bluetooth

Test: build

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -30,6 +30,7 @@ use btstack::bluetooth_gatt::{
     ScanFilterPattern, ScanResult, ScanSettings, ScanType,
 };
 use btstack::bluetooth_media::IBluetoothTelephony;
+use btstack::bluetooth_qa::IBluetoothQA;
 use btstack::socket_manager::{
     BluetoothServerSocket, BluetoothSocket, CallbackId, IBluetoothSocketManager,
     IBluetoothSocketManagerCallbacks, SocketId, SocketResult,
@@ -2226,3 +2227,23 @@ impl IBluetoothTelephony for BluetoothTelephonyDBus {
         dbus_generated!()
     }
 }
+
+pub(crate) struct BluetoothQADBus {
+    _client_proxy: ClientDBusProxy,
+}
+
+impl BluetoothQADBus {
+    pub(crate) fn new(conn: Arc<SyncConnection>, index: i32) -> BluetoothQADBus {
+        BluetoothQADBus {
+            _client_proxy: ClientDBusProxy::new(
+                conn.clone(),
+                String::from("org.chromium.bluetooth"),
+                make_object_path(index, "qa"),
+                String::from("org.chromium.bluetooth.BluetoothQA"),
+            ),
+        }
+    }
+}
+
+#[generate_dbus_interface_client]
+impl IBluetoothQA for BluetoothQADBus {}