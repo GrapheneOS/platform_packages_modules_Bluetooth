e5a61a7ba772f4617b0280f29ef1b7b7bf5cb8c0
Rahul Arya <aryarahul@google.com>
2023-04-13 09:27:58 +0000

    [Connection Manager] Properly start/stop Rust thread
    We should start it once everything else has started, and
    stop it before everything else stops. We can simply kill the
    runtime when a Stop arrives - since all futures are meant to
    be cancellation safe, this won't leave us in a bad state.
    
    Test: AdapterServiceTest
    Bug: 272572974
    Change-Id: Ib76070887a43adec5878ceac3c33a27fba13e2eb

--- system/rust/src/core/mod.rs
+++ system/rust/src/core/mod.rs
@@ -6,25 +6,39 @@ pub mod shared_box;
 pub mod shared_mutex;
 pub mod uuid;
 
-use std::{rc::Rc, thread};
+use std::{pin::Pin, rc::Rc, thread};
 
 use bt_common::init_flags::rust_event_loop_is_enabled;
 use cxx::UniquePtr;
 
 use crate::{
     gatt::ffi::{AttTransportImpl, GattCallbacksImpl},
-    GlobalModuleRegistry,
+    GlobalModuleRegistry, MainThreadTxMessage, GLOBAL_MODULE_REGISTRY,
 };
 
-use self::ffi::GattServerCallbacks;
+use self::ffi::{future_ready, Future, GattServerCallbacks};
 
-fn init(gatt_server_callbacks: UniquePtr<GattServerCallbacks>) {
+fn start(
+    gatt_server_callbacks: UniquePtr<GattServerCallbacks>,
+    on_started: Pin<&'static mut Future>,
+) {
     if rust_event_loop_is_enabled() {
         thread::spawn(move || {
             GlobalModuleRegistry::start(
                 Rc::new(GattCallbacksImpl(gatt_server_callbacks)),
                 Rc::new(AttTransportImpl()),
+                || {
+                    future_ready(on_started);
+                },
             );
         });
     }
+}
+
+fn stop() {
+    let _ = GLOBAL_MODULE_REGISTRY
+        .try_lock()
+        .unwrap()
+        .as_ref()
+        .map(|registry| registry.task_tx.send(MainThreadTxMessage::Stop));
 }
