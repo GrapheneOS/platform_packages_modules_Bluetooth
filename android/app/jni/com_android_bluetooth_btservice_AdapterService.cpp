58802164ca5455fe86ce71ac9adefe0c89e4e12c
Hui Peng <phui@google.com>
2023-04-18 16:46:42 +0000

    Fix an OOB bug in set_data
    Plus move macros used in struct bt_oob_data_s
    to bluetooth.h
    
    Bug: 274722185
    Test: manual
    Ignore-AOSP-First: security
    Tag: #security
    Change-Id: Ie12feb4090a1eb88f5c9e097546f55a076839fb0

--- android/app/jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ android/app/jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -42,19 +42,6 @@ extern bt_interface_t bluetoothInterface;
 
 namespace android {
 // Both
-// OOB_ADDRESS_SIZE is 6 bytes address + 1 byte address type
-#define OOB_ADDRESS_SIZE 7
-#define OOB_C_SIZE 16
-#define OOB_R_SIZE 16
-#define OOB_NAME_MAX_SIZE 256
-// Classic
-#define OOB_DATA_LEN_SIZE 2
-#define OOB_COD_SIZE 3
-// LE
-#define OOB_TK_SIZE 16
-#define OOB_LE_FLAG_SIZE 1
-#define OOB_LE_ROLE_SIZE 1
-#define OOB_LE_APPEARANCE_SIZE 2
 
 #define TRANSPORT_AUTO 0
 #define TRANSPORT_BREDR 1
