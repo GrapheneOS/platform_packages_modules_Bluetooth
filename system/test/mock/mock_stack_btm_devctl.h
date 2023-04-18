bb61ff62dd27aa42d9c01de20c84e3db2ba2888e
Xuan Xing <xuanxing@google.com>
2023-04-18 21:29:19 +0000

    Adding fuzzing needed mock implementations.
    Bug: 187825293
    Test: local build
    Change-Id: Ied36888f901bb2f8345d086849346bcf74380ce1

---
+++ system/test/mock/mock_stack_btm_devctl.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <map>
+#include <string>
+
+#include "stack/btm/btm_dev.h"
+#include "stack/include/btm_api_types.h"
+#include "stack/include/btm_status.h"
+#include "test/common/mock_functions.h"
+#include "types/bt_transport.h"
+#include "types/raw_address.h"
+
+namespace test {
+namespace mock {
+namespace stack_btm_devctl {
+
+// Function state capture and return values, if needed
+struct BTM_IsDeviceUp {
+  std::function<bool()> body{[]() { return false; }};
+  bool operator()() { return body(); };
+};
+extern struct BTM_IsDeviceUp BTM_IsDeviceUp;
+
+}  // namespace stack_btm_devctl
+}  // namespace mock
+}  // namespace test
