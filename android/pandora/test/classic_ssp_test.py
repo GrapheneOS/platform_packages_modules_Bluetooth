1767a8ff3d4a78d44884d2e68dd348541a129c25
uael <uael@google.com>
2023-04-20 00:38:48 +0000

    avatar: update to latest bumble server
    This commit includes:
    * Update to bumble `PairingDelegate` move.
    * Update to the new way IO capability are overidden in the bumble server.
    * Fix invalid use of `TestAbortAll` instead of `TestAbortClass`
    * Normalize UUID string format accross Avatar.
    * Add missing `@rpc` decorator to Gatt servicer methods.
    * Remove now unecessary usage of `__repr__` for enums.
    * Use mobly asserts instead of `assert` for better error messages, keep
      `assert` where necessary for the linter to be happy.
    
    Bug: 278738537
    Bug: 279027405
    Test: avatar run
    Change-Id: I5a800cdd94c9f4d88683c7b81fcae663d8ff8938

--- android/pandora/test/classic_ssp_test.py
+++ android/pandora/test/classic_ssp_test.py
@@ -13,46 +13,31 @@
 # limitations under the License.
 
 import asyncio
-import bumble.device
 import itertools
+import logging
 
 from avatar import BumblePandoraDevice, PandoraDevice, PandoraDevices, asynchronous, parameterized
 from bumble.core import BT_BR_EDR_TRANSPORT
-from bumble.hci import HCI_CENTRAL_ROLE, HCI_PERIPHERAL_ROLE, Address as BumbleAddress
-from bumble.smp import PairingDelegate
+from bumble.hci import HCI_CENTRAL_ROLE, Address as BumbleAddress
+from bumble.pairing import PairingDelegate
 from concurrent import futures
 from contextlib import suppress
-from google.protobuf import any_pb2
-from mobly import base_test
-from mobly.asserts import assert_equal, assert_in, assert_is_not_none, fail
-from pandora.host_pb2 import PUBLIC, RANDOM, Connection, DataTypes, OwnAddressType
+from mobly import base_test, signals, test_runner
+from mobly.asserts import assert_equal  # type: ignore
+from mobly.asserts import assert_in  # type: ignore
+from mobly.asserts import assert_is_not_none  # type: ignore
+from mobly.asserts import fail  # type: ignore
+from pandora.host_pb2 import RANDOM, DataTypes, OwnAddressType
 from pandora.security_pb2 import LEVEL2, PairingEventAnswer
 from typing import NoReturn, Optional
 
 
-class BumbleAddressWrapper(BumbleAddress):
-    """Wrapper of Bumble Address class."""
-
-    def __init__(
-        self,
-        address: BumbleAddress.ANY,
-        address_type: int = BumbleAddress.PUBLIC_DEVICE_ADDRESS,
-        bytes_endian: str = 'little',
-    ):
-        if isinstance(address, bytes):
-            if bytes_endian == 'big':
-                address = bytes(reversed(address))
-            elif bytes_endian != 'little':
-                raise ValueError("byteorder must be either 'little' or 'big'")
-        super().__init__(address, address_type)
-
-
-class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
+class ClassicSspTest(base_test.BaseTestClass):  # type: ignore[misc]
     devices: Optional[PandoraDevices] = None
 
     # pandora devices.
     dut: PandoraDevice
-    ref: BumblePandoraDevice
+    ref: PandoraDevice
 
     def setup_class(self) -> None:
         self.devices = PandoraDevices(self)
@@ -61,7 +46,6 @@ class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
         # Enable BR/EDR mode for Bumble devices.
         for device in self.devices:
             if isinstance(device, BumblePandoraDevice):
-                device.config.setdefault('classic_enabled', True)
                 device.config.setdefault('classic_enabled', True)
 
     def teardown_class(self) -> None:
@@ -81,9 +65,10 @@ class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
         )
 
         scan = self.ref.aio.host.Scan(own_address_type=ref_address_type)
-        dut = await anext((x async for x in scan if b'pause cafe' in x.data.manufacturer_specific_data))  # pytype: disable=name-error
+        dut = await anext(
+            (x async for x in scan if b'pause cafe' in x.data.manufacturer_specific_data)
+        )  # pytype: disable=name-error
         scan.cancel()
-        assert dut
 
         (ref_dut_res, dut_ref_res) = await asyncio.gather(
             self.ref.aio.host.ConnectLE(own_address_type=ref_address_type, **dut.address_asdict()),
@@ -91,8 +76,8 @@ class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
         )
 
         advertisement.cancel()
-        ref_dut, dut_ref = ref_dut_res.connection, dut_ref_res.connection
-        assert ref_dut and dut_ref
+        ref_dut, _ = ref_dut_res.connection, dut_ref_res.connection
+        assert_is_not_none(ref_dut)
 
     async def handle_pairing_events(self) -> NoReturn:
         ref_pairing_stream = self.ref.aio.security.OnPairing()
@@ -106,57 +91,65 @@ class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
                 )
 
                 if dut_pairing_event.method_variant() in (
-                        'numeric_comparison',
-                        'just_works',
+                    'numeric_comparison',
+                    'just_works',
                 ):
-                    assert_in(
-                        ref_pairing_event.method_variant(),
-                        ('numeric_comparison', 'just_works'),
+                    assert_in(ref_pairing_event.method_variant(), ('numeric_comparison', 'just_works'))
+                    dut_pairing_stream.send_nowait(
+                        PairingEventAnswer(
+                            event=dut_pairing_event,
+                            confirm=True,
+                        )
                     )
-                    dut_pairing_stream.send_nowait(PairingEventAnswer(
-                        event=dut_pairing_event,
-                        confirm=True,
-                    ))
-                    ref_pairing_stream.send_nowait(PairingEventAnswer(
-                        event=ref_pairing_event,
-                        confirm=True,
-                    ))
+                    ref_pairing_stream.send_nowait(
+                        PairingEventAnswer(
+                            event=ref_pairing_event,
+                            confirm=True,
+                        )
+                    )
                 elif dut_pairing_event.method_variant() == 'passkey_entry_notification':
                     assert_equal(ref_pairing_event.method_variant(), 'passkey_entry_request')
                     ref_pairing_stream.send_nowait(
                         PairingEventAnswer(
                             event=ref_pairing_event,
                             passkey=dut_pairing_event.passkey_entry_notification,
-                        ))
+                        )
+                    )
                 elif dut_pairing_event.method_variant() == 'passkey_entry_request':
                     assert_equal(ref_pairing_event.method_variant(), 'passkey_entry_notification')
                     dut_pairing_stream.send_nowait(
                         PairingEventAnswer(
                             event=dut_pairing_event,
                             passkey=ref_pairing_event.passkey_entry_notification,
-                        ))
+                        )
+                    )
                 else:
-                    fail('unreachable')
+                    fail("")
 
         finally:
             ref_pairing_stream.cancel()
             dut_pairing_stream.cancel()
 
-    @parameterized(*itertools.product(
-        (PairingDelegate.NO_OUTPUT_NO_INPUT,),
-        (HCI_CENTRAL_ROLE,),
-        (RANDOM,),
-    ))  # type: ignore[misc]
+    @parameterized(
+        *itertools.product(
+            (PairingDelegate.NO_OUTPUT_NO_INPUT,),
+            (HCI_CENTRAL_ROLE,),
+            (RANDOM,),
+        )
+    )  # type: ignore[misc]
     @asynchronous
-    async def test_classic_pairing_incoming(self, ref_io_capability: int, ref_role: int,
-                                            ref_le_addr_type: OwnAddressType) -> None:
+    async def test_classic_pairing_incoming(
+        self, ref_io_capability: PairingDelegate.IoCapability, ref_role: int, ref_le_addr_type: OwnAddressType
+    ) -> None:
+        if not isinstance(self.ref, BumblePandoraDevice):
+            raise signals.TestSkip('Test require Bumble as reference device(s)')
+
         # override reference device IO capability
-        setattr(self.ref.device, 'io_capability', ref_io_capability)
+        self.ref.server_config.io_capability = ref_io_capability
 
         pairing = asyncio.create_task(self.handle_pairing_events())
 
-        if ref_le_addr_type is not None:
-            await self.connect_le(RANDOM, ref_le_addr_type)
+        await self.connect_le(RANDOM, ref_le_addr_type)
 
         (dut_ref_res, ref_dut_res) = await asyncio.gather(
             self.dut.aio.host.WaitConnection(address=self.ref.address),
@@ -169,10 +162,13 @@ class ClassicSspTests(base_test.BaseTestClass):  # type: ignore[misc]
         dut_ref = dut_ref_res.connection
         assert_is_not_none(ref_dut)
         assert_is_not_none(dut_ref)
+        assert ref_dut and dut_ref
 
         ref_dut_raw = self.ref.device.find_connection_by_bd_addr(
-            BumbleAddressWrapper(self.dut.address, bytes_endian='big'), BT_BR_EDR_TRANSPORT)
+            BumbleAddress(bytes(reversed(self.dut.address)), BumbleAddress.PUBLIC_DEVICE_ADDRESS), BT_BR_EDR_TRANSPORT
+        )
         assert_is_not_none(ref_dut_raw)
+        assert ref_dut_raw
 
         if ref_dut_raw.role != ref_role:
             await ref_dut_raw.switch_role(ref_role)
