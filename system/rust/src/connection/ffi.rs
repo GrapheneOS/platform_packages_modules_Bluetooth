a2a237901f39ab2af1ddfd45f3f19cee2eac5c54
Rahul Arya <aryarahul@google.com>
2023-04-21 21:49:16 +0000

    Revert^2 [Connection Manager] Forward disconnection callbacks
    Bug: 272572974
    Test: platinum tests
    
    This reverts commit b6fbc18a90c3d837e23fac3d31b39cb16eaab0ed.
    
    Change-Id: I168473c96f060fee655c868b57cb9ef510613bc1

--- system/rust/src/connection/ffi.rs
+++ system/rust/src/connection/ffi.rs
@@ -70,7 +70,7 @@ mod inner {
         fn on_le_connect_success(&self, address: AddressWithType);
         #[cxx_name = "OnLeConnectFail"]
         fn on_le_connect_fail(&self, address: AddressWithType, status: u8);
-        #[cxx_name = "OnDisconnect"]
+        #[cxx_name = "OnLeDisconnection"]
         fn on_disconnect(&self, address: AddressWithType);
     }
 
