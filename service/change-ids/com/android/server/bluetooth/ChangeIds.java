d2a8cbcbf5119bc2c907e43f66c32d050b2c9357
William Escande <wescande@google.com>
2023-04-24 17:07:55 -0700

    Extract Change-ids from BMS
    This allow the platform_compat_config to no longer parse the whole
    library.
    This follow the pattern used in the Bluetooth app.
    
    Test: No-op change
    Bug: 262605980
    Change-Id: Ib057df8e0ebdfa50ffeff4896d11920ea2a9f36e

---
+++ service/change-ids/com/android/server/bluetooth/ChangeIds.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.bluetooth;
+
+import android.compat.annotation.ChangeId;
+import android.compat.annotation.EnabledSince;
+
+/**
+ * All the {@link ChangeId} used in the Bluetooth service .
+ */
+class ChangeIds {
+    private ChangeIds(){}
+
+    /**
+     * Starting with {@link android.os.Build.VERSION_CODES#TIRAMISU}, applications are
+     * not allowed to enable/disable Bluetooth.
+     */
+    @ChangeId
+    @EnabledSince(targetSdkVersion = android.os.Build.VERSION_CODES.TIRAMISU)
+    static final long RESTRICT_ENABLE_DISABLE = 218493289L;
+}
+
