miyu-xu/packages-modules-Bluetooth

floss: btclient: Add listen-rfcomm and send-msc commands
Bug: 278505039
Tag: #floss
Test: RFCOMM/DEVA-DEVB/RFC/BV-21-C RFCOMM/DEVA-DEVB/RFC/BV-22-C
Change-Id: I5983b47aaa8871f0ff2957544c5ada2f987aeb2b

system/gd/rust/linux/client/src/command_handler.rs
@@ -16,6 +16,7 @@ use bt_topshim::profiles::{gatt::LePhy, ProfileConnectionState};
 use btstack::bluetooth::{BluetoothDevice, IBluetooth, IBluetoothQALegacy};
 use btstack::bluetooth_gatt::{GattWriteType, IBluetoothGatt, ScanSettings, ScanType};
 use btstack::bluetooth_media::IBluetoothTelephony;
+use btstack::bluetooth_qa::IBluetoothQA;
 use btstack::socket_manager::{IBluetoothSocketManager, SocketResult};
 use btstack::uuid::{Profile, UuidHelper, UuidWrapper};
 use manager_service::iface_bluetooth_manager::IBluetoothManager;
@@ -237,6 +238,8 @@ fn build_commands() -> HashMap<String, CommandOption> {
         CommandOption {
             rules: vec![
                 String::from("socket listen <auth-required> <Bredr|LE>"),
+                String::from("socket listen-rfcomm <csn>"),
+                String::from("socket send-msc <dlci> <addr>"),
                 String::from(
                     "socket connect <address> <l2cap|rfcomm> <psm|uuid> <auth-required> <Bredr|LE>",
                 ),
@@ -1415,6 +1418,39 @@ impl CommandHandler {
                 };
 
                 self.context.lock().unwrap().socket_test_schedule = Some(schedule);
+            }
+            "send-msc" => {
+                let dlci =
+                    String::from(get_arg(args, 1)?).parse::<u8>().or(Err("Failed parsing DLCI"))?;
+                let addr = String::from(get_arg(args, 2)?);
+                self.context
+                    .lock()
+                    .unwrap()
+                    .qa_dbus
+                    .as_mut()
+                    .unwrap()
+                    .rfcomm_start_control_request(dlci, addr);
+            }
+            "listen-rfcomm" => {
+                let csn = String::from(get_arg(args, 1)?)
+                    .parse::<i32>()
+                    .or(Err("Failed parsing Service Channel Number"))?;
+                let SocketResult { status, id } = self
+                    .context
+                    .lock()
+                    .unwrap()
+                    .socket_manager_dbus
+                    .as_mut()
+                    .unwrap()
+                    .listen_using_rfcomm(callback_id, Some(csn), None, None, None);
+                if status != BtStatus::Success {
+                    return Err(format!(
+                        "Failed to request for listening using rfcomm, status = {:?}",
+                        status,
+                    )
+                    .into());
+                }
+                print_info!("Requested for listening using rfcomm on socket {}", id);
             }
             "listen" => {
                 let auth_required = String::from(get_arg(args, 1)?)

system/gd/rust/linux/stack/src/socket_manager.rs
@@ -1141,6 +1141,27 @@ impl BluetoothSocketManager {
         self.listening.remove(&callback);
         self.callbacks.remove_callback(callback);
     }
+
+    // Send the MSC command to the peer. ONLY for Qualification use.
+    // libbluetooth auto starts the control request only when it is the client.
+    // This function allows the host to start the control request while as a server.
+    pub fn rfcomm_start_control_request(&mut self, dlci: u8, addr: String) {
+        match (|| -> Result<(), &str> {
+            let addr = RawAddress::from_string(addr)
+                .ok_or("Invalid address for starting control request")?;
+            let sock = self
+                .sock
+                .as_ref()
+                .ok_or("Socket Manager not initialized when starting control request")?;
+            if sock.control_req(dlci, addr) != BtStatus::Success {
+                return Err("Failed to start control request");
+            }
+            Ok(())
+        })() {
+            Ok(_) => {}
+            Err(msg) => log::warn!("{}", msg),
+        };
+    }
 }
 
 impl IBluetoothSocketManager for BluetoothSocketManager {

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/linux/client/src/command_handler.rs
@@ -246,6 +246,8 @@ fn build_commands() -> HashMap<String, CommandOption> {
         CommandOption {
             rules: vec![
                 String::from("socket listen <auth-required> <Bredr|LE>"),
+                String::from("socket listen-rfcomm <scn>"),
+                String::from("socket send-msc <dlci> <address>"),
                 String::from(
                     "socket connect <address> <l2cap|rfcomm> <psm|uuid> <auth-required> <Bredr|LE>",
                 ),
@@ -1460,6 +1462,33 @@ impl CommandHandler {
                 };
 
                 self.context.lock().unwrap().socket_test_schedule = Some(schedule);
+            }
+            "send-msc" => {
+                let dlci =
+                    String::from(get_arg(args, 1)?).parse::<u8>().or(Err("Failed parsing DLCI"))?;
+                let addr = String::from(get_arg(args, 2)?);
+                self.context.lock().unwrap().qa_dbus.as_mut().unwrap().rfcomm_send_msc(dlci, addr);
+            }
+            "listen-rfcomm" => {
+                let scn = String::from(get_arg(args, 1)?)
+                    .parse::<i32>()
+                    .or(Err("Failed parsing Service Channel Number"))?;
+                let SocketResult { status, id } = self
+                    .context
+                    .lock()
+                    .unwrap()
+                    .socket_manager_dbus
+                    .as_mut()
+                    .unwrap()
+                    .listen_using_rfcomm(callback_id, Some(scn), None, None, None);
+                if status != BtStatus::Success {
+                    return Err(format!(
+                        "Failed to request for listening using rfcomm, status = {:?}",
+                        status,
+                    )
+                    .into());
+                }
+                print_info!("Requested for listening using rfcomm on socket {}", id);
             }
             "listen" => {
                 let auth_required = String::from(get_arg(args, 1)?)

system/gd/rust/linux/stack/src/socket_manager.rs
@@ -1197,6 +1197,27 @@ impl BluetoothSocketManager {
         self.listening.remove(&callback);
         self.callbacks.remove_callback(callback);
     }
+
+    // Send MSC command to the peer. ONLY FOR QUALIFICATION USE.
+    // libbluetooth auto starts the control request only when it is the client.
+    // This function allows the host to start the control request while as a server.
+    pub fn rfcomm_send_msc(&mut self, dlci: u8, addr: String) {
+        match (|| -> Result<(), &str> {
+            let addr = RawAddress::from_string(addr)
+                .ok_or("Invalid address for starting control request")?;
+            let sock = self
+                .sock
+                .as_ref()
+                .ok_or("Socket Manager not initialized when starting control request")?;
+            if sock.send_msc(dlci, addr) != BtStatus::Success {
+                return Err("Failed to start control request");
+            }
+            Ok(())
+        })() {
+            Ok(_) => {}
+            Err(msg) => log::warn!("{}", msg),
+        };
+    }
 }
 
 impl IBluetoothSocketManager for BluetoothSocketManager {