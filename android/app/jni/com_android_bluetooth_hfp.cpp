miyu-xu/packages-modules-Bluetooth

Floss: Add tBTM_SCO_PKT_STATUS to HCI SCO
Add and use tBTM_SCO_PKT_STATUS to record the mSBC packets and
corresponding interfaces to expose SCO relevant information as debug
dump.

Bug: 272101425
Tag: #floss
Test: build
Change-Id: I2986e63920a14f7dc2405b7b970984d9797ab3a1

android/app/jni/com_android_bluetooth_hfp.cpp
@@ -409,6 +409,13 @@ class JniHeadsetCallbacks : bluetooth::headset::Callbacks {
     sCallbackEnv->CallVoidMethod(mCallbacksObj, method_onAtBia, service, roam,
                                  signal, battery, addr.get());
   }
+
+  void DebugDumpCallback(bool active, bool wbs, int total_num_decoded_frames,
+                         double pkt_loss_ratio, uint32_t begin_ts,
+                         uint32_t end_ts, const char* pkt_status_in_hex,
+                         const char* pkt_status_in_binary) override {
+    ALOGE("Not implemented and shouldn't be called");
+  }
 };
 
 static void classInitNative(JNIEnv* env, jclass clazz) {

system/include/hardware/bluetooth_headset_callbacks.h
@@ -204,6 +204,24 @@ class Callbacks {
    */
   virtual void AtBiaCallback(bool service, bool roam, bool signal, bool battery,
                              RawAddress* bd_addr) = 0;
+
+  /**
+   * Callback for DebugDump.
+   *
+   * @param active whether the SCO is active
+   * @param wbs whether it is under WBS, false for NBS.
+   * @param total_num_decoded_frames the number of WBS frames decoded.
+   * @param pkt_loss_ratio the ratio of lost frames
+   * @param begin_ts time of the packet status window starts in microseconds.
+   * @param end_ts time of the packet status window ends in microseconds.
+   * @param pkt_status_in_hex recorded WBS packets' status in hex string.
+   * @param pkt_status_in_binary recorde WBS packets' status in binary string.
+   */
+  virtual void DebugDumpCallback(bool active, bool wbs,
+                                 int total_num_decoded_frames,
+                                 double pkt_loss_ratio, uint32_t begin_ts,
+                                 uint32_t end_ts, const char* pkt_status_in_hex,
+                                 const char* pkt_status_in_binary) = 0;
 };
 
 }  // namespace headset

GrapheneOS/platform_packages_modules_Bluetooth

Change-Id: Idc9dbff03d56de898aaa6dc11576a4243ccf2964

android/app/jni/com_android_bluetooth_hfp.cpp
@@ -409,6 +409,13 @@ class JniHeadsetCallbacks : bluetooth::headset::Callbacks {
     sCallbackEnv->CallVoidMethod(mCallbacksObj, method_onAtBia, service, roam,
                                  signal, battery, addr.get());
   }
+
+  void DebugDumpCallback(bool active, bool wbs, int total_num_decoded_frames,
+                         double pkt_loss_ratio, uint64_t begin_ts,
+                         uint64_t end_ts, const char* pkt_status_in_hex,
+                         const char* pkt_status_in_binary) override {
+    ALOGE("Not implemented and shouldn't be called");
+  }
 };
 
 static void classInitNative(JNIEnv* env, jclass clazz) {

system/include/hardware/bluetooth_headset_callbacks.h
@@ -204,6 +204,24 @@ class Callbacks {
    */
   virtual void AtBiaCallback(bool service, bool roam, bool signal, bool battery,
                              RawAddress* bd_addr) = 0;
+
+  /**
+   * Callback for DebugDump.
+   *
+   * @param active whether the SCO is active
+   * @param wbs whether it is under WBS, false for NBS.
+   * @param total_num_decoded_frames the number of WBS frames decoded.
+   * @param pkt_loss_ratio the ratio of lost frames
+   * @param begin_ts time of the packet status window starts in microseconds.
+   * @param end_ts time of the packet status window ends in microseconds.
+   * @param pkt_status_in_hex recorded WBS packets' status in hex string.
+   * @param pkt_status_in_binary recorde WBS packets' status in binary string.
+   */
+  virtual void DebugDumpCallback(bool active, bool wbs,
+                                 int total_num_decoded_frames,
+                                 double pkt_loss_ratio, uint64_t begin_ts,
+                                 uint64_t end_ts, const char* pkt_status_in_hex,
+                                 const char* pkt_status_in_binary) = 0;
 };
 
 }  // namespace headset