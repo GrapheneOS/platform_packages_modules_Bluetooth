miyu-xu/packages-modules-Bluetooth

Floss: Add DBUS API |OnDevicePropertiesChanged|
Implements API so clients can watch for property changes for
a device.

Bug: 283998413
Tag: #floss
Test: Manual test with fast pair
Change-Id: I3ef4b225904f4554732f55be10859624c860b625

system/gd/rust/linux/client/src/callbacks.rs
@@ -119,6 +119,14 @@ impl BtCallback {
 impl IBluetoothCallback for BtCallback {
     fn on_adapter_property_changed(&mut self, _prop: BtPropertyType) {}
 
+    fn on_device_properties_changed(
+        &mut self,
+        device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+        print_info!("Bluetooth properties {:?} changed for {:?}", props, device);
+    }
+
     fn on_address_changed(&mut self, addr: String) {
         print_info!("Address changed to {}", &addr);
         self.context.lock().unwrap().adapter_address = Some(addr);

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -514,6 +514,14 @@ impl IBluetoothCallback for IBluetoothCallbackDBus {
     #[dbus_method("OnAdapterPropertyChanged")]
     fn on_adapter_property_changed(&mut self, prop: BtPropertyType) {}
 
+    #[dbus_method("OnDevicePropertiesChanged")]
+    fn on_device_properties_changed(
+        &mut self,
+        device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+    }
+
     #[dbus_method("OnAddressChanged")]
     fn on_address_changed(&mut self, addr: String) {}
 

system/gd/rust/linux/service/src/iface_bluetooth.rs
@@ -79,6 +79,14 @@ impl IBluetoothCallback for BluetoothCallbackDBus {
     fn on_adapter_property_changed(&mut self, prop: BtPropertyType) {
         dbus_generated!()
     }
+    #[dbus_method("OnDevicePropertiesChanged")]
+    fn on_device_properties_changed(
+        &mut self,
+        device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+        dbus_generated!()
+    }
     #[dbus_method("OnAddressChanged")]
     fn on_address_changed(&mut self, addr: String) {
         dbus_generated!()

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/rust/linux/client/src/callbacks.rs
@@ -119,6 +119,14 @@ impl BtCallback {
 impl IBluetoothCallback for BtCallback {
     fn on_adapter_property_changed(&mut self, _prop: BtPropertyType) {}
 
+    fn on_device_properties_changed(
+        &mut self,
+        remote_device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+        print_info!("Bluetooth properties {:?} changed for {:?}", props, remote_device);
+    }
+
     fn on_address_changed(&mut self, addr: String) {
         print_info!("Address changed to {}", &addr);
         self.context.lock().unwrap().adapter_address = Some(addr);

system/gd/rust/linux/client/src/dbus_iface.rs
@@ -514,6 +514,14 @@ impl IBluetoothCallback for IBluetoothCallbackDBus {
     #[dbus_method("OnAdapterPropertyChanged")]
     fn on_adapter_property_changed(&mut self, prop: BtPropertyType) {}
 
+    #[dbus_method("OnDevicePropertiesChanged")]
+    fn on_device_properties_changed(
+        &mut self,
+        remote_device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+    }
+
     #[dbus_method("OnAddressChanged")]
     fn on_address_changed(&mut self, addr: String) {}
 

system/gd/rust/linux/service/src/iface_bluetooth.rs
@@ -79,6 +79,14 @@ impl IBluetoothCallback for BluetoothCallbackDBus {
     fn on_adapter_property_changed(&mut self, prop: BtPropertyType) {
         dbus_generated!()
     }
+    #[dbus_method("OnDevicePropertiesChanged")]
+    fn on_device_properties_changed(
+        &mut self,
+        remote_device: BluetoothDevice,
+        props: Vec<BtPropertyType>,
+    ) {
+        dbus_generated!()
+    }
     #[dbus_method("OnAddressChanged")]
     fn on_address_changed(&mut self, addr: String) {
         dbus_generated!()