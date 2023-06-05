722192c98c4eaa7d44874d557cb98647f7d79d7c
Archie Pusaka <apusaka@google.com>
2023-06-05 14:24:52 +0800

    Floss: Fix bta_dm_remote_name_cmpl null pointer crash
    Bug: 285807216
    Test: None
    Change-Id: Ide25a2e3c09a6cb00334f8ebc94e57fc54cf1a52

--- system/bta/dm/bta_dm_act.cc
+++ system/bta/dm/bta_dm_act.cc
@@ -1157,7 +1157,7 @@ void bta_dm_remote_name_cmpl(const tBTA_DM_MSG* p_data) {
                      PRIVATE_NAME(remote_name_msg.bd_name)));
 
   tBTM_INQ_INFO* p_btm_inq_info = BTM_InqDbRead(remote_name_msg.bd_addr);
-  if (remote_name_msg.bd_name[0] != '\0' && bta_dm_search_cb.p_btm_inq_info) {
+  if (!bd_name_is_empty(remote_name_msg.bd_name) && p_btm_inq_info) {
     p_btm_inq_info->appl_knows_rem_name = true;
   }
 
