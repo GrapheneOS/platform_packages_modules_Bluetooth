c5b0cb95be32bd6c343eeb6a492829f86604504c
William Escande <wescande@google.com>
2023-06-06 00:55:04 -0700

    Move airplaneMode code into its listener
    Bug: 271963349
    Bug: 262605980
    Test: atest ServiceBluetoothTests | no-op change
    Change-Id: Ib793b8afbc4b9ae897c79b18e07d399255e6e981

--- service/src/com/android/server/bluetooth/BluetoothAirplaneModeListener.java
+++ service/src/com/android/server/bluetooth/BluetoothAirplaneModeListener.java
@@ -17,13 +17,16 @@
 package com.android.server.bluetooth;
 
 import android.annotation.RequiresPermission;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -68,6 +71,9 @@ class BluetoothAirplaneModeListener extends Handler {
     public static final int NOTIFICATION_SHOWN = 1;
     public static final int UNUSED = 0;
     public static final int USED = 1;
+
+    private static final int BLUETOOTH_OFF_APM = 0;
+    private static final int BLUETOOTH_ON_APM = 1;
 
     @VisibleForTesting static final int MAX_TOAST_COUNT = 10; // 10 times
 
@@ -279,11 +285,6 @@ class BluetoothAirplaneModeListener extends Handler {
                 && mAirplaneHelper.getSettingsSecureInt(WIFI_APM_STATE, 0) == 1;
     }
 
-    private boolean isFirstTimeNotification(String name) {
-        return mAirplaneHelper.getSettingsSecureInt(name, NOTIFICATION_NOT_SHOWN)
-                == NOTIFICATION_NOT_SHOWN;
-    }
-
     /** Helper method to send APM notification */
     public void sendApmNotification(String titleId, String messageId, String notificationState)
             throws PackageManager.NameNotFoundException {
@@ -304,11 +305,62 @@ class BluetoothAirplaneModeListener extends Handler {
     }
 
     /** Helper method to update whether user toggled Bluetooth in airplane mode */
-    public void updateBluetoothToggledTime() {
+    public void notifyUserToggledBluetooth(boolean isOn) {
+        if (!mIsAirplaneModeOn) {
+            // User not in Airplane mode, discard event
+            return;
+        }
         if (!mUserToggledBluetoothDuringApm) {
             mUserToggledBluetoothDuringApmWithinMinute =
                     SystemClock.elapsedRealtime() - mApmEnabledTime < 60000;
         }
         mUserToggledBluetoothDuringApm = true;
+        if (isApmEnhancementEnabled()) {
+            setSettingsSecureInt(BLUETOOTH_APM_STATE, isOn ? BLUETOOTH_ON_APM : BLUETOOTH_OFF_APM);
+            setSettingsSecureInt(APM_USER_TOGGLED_BLUETOOTH, USED);
+            if (isOn && isFirstTimeNotification(APM_BT_ENABLED_NOTIFICATION)) {
+                // waive WRITE_SECURE_SETTINGS permission check
+                final long callingIdentity = Binder.clearCallingIdentity();
+                try {
+                    sendApmNotification(
+                            "bluetooth_enabled_apm_title",
+                            "bluetooth_enabled_apm_message",
+                            APM_BT_ENABLED_NOTIFICATION);
+                } catch (Exception e) {
+                    Log.e(TAG, "APM enhancement BT enabled notification not shown");
+                } finally {
+                    Binder.restoreCallingIdentity(callingIdentity);
+                }
+            }
+        }
+    }
+
+    /** Return whether APM notification has been shown */
+    private boolean isFirstTimeNotification(String name) {
+        // waive WRITE_SECURE_SETTINGS permission check
+        final long callingIdentity = Binder.clearCallingIdentity();
+        try {
+            Context userContext =
+                    mContext.createContextAsUser(
+                            UserHandle.of(ActivityManager.getCurrentUser()), 0);
+            return mAirplaneHelper.getSettingsSecureInt(name, NOTIFICATION_NOT_SHOWN)
+                    == NOTIFICATION_NOT_SHOWN;
+        } finally {
+            Binder.restoreCallingIdentity(callingIdentity);
+        }
+    }
+
+    /** Set the Settings Secure Int value for foreground user */
+    private void setSettingsSecureInt(String name, int value) {
+        // waive WRITE_SECURE_SETTINGS permission check
+        final long callingIdentity = Binder.clearCallingIdentity();
+        try {
+            Context userContext =
+                    mContext.createContextAsUser(
+                            UserHandle.of(ActivityManager.getCurrentUser()), 0);
+            Settings.Secure.putInt(userContext.getContentResolver(), name, value);
+        } finally {
+            Binder.restoreCallingIdentity(callingIdentity);
+        }
     }
 }
