miyu-xu/packages-modules-Bluetooth

gd/rust: Migrate to the new pdl rust generator
Change-Id: I9e2fede3b37be9df1cef5e9ef13f0fccfd561bb1

system/gd/rust/stack/src/link/acl/core.rs
@@ -5,7 +5,7 @@ use crate::hci::{ControllerExports, EventRegistry};
 use crate::link::acl::fragment::{fragmenting_stream, Reassembler};
 use bt_common::Bluetooth::{self, Classic, Le};
 use bt_packets::hci::EventChild::{DisconnectionComplete, NumberOfCompletedPackets};
-use bt_packets::hci::{AclPacket, EventCode, EventPacket};
+use bt_packets::hci::{Acl, EventCode, Event};
 use bytes::Bytes;
 use futures::stream::{SelectAll, StreamExt};
 use gddi::{module, provides, Stoppable};
@@ -34,15 +34,15 @@ pub struct Connection {
     handle: u16,
     #[allow(dead_code)]
     requests: Sender<Request>,
-    pub evt_rx: Receiver<EventPacket>,
-    pub evt_tx: Sender<EventPacket>,
+    pub evt_rx: Receiver<Event>,
+    pub evt_tx: Sender<Event>,
 }
 
 struct ConnectionInternal {
     reassembler: Reassembler,
     bt: Bluetooth,
     close_tx: oneshot::Sender<()>,
-    evt_tx: Sender<EventPacket>,
+    evt_tx: Sender<Event>,
 }
 
 /// Manages rx and tx for open ACL connections
@@ -179,6 +179,6 @@ async fn provide_acl_dispatch(
     AclDispatch { requests: req_tx }
 }
 
-async fn consume(rx: &Arc<Mutex<Receiver<AclPacket>>>) -> Option<AclPacket> {
+async fn consume(rx: &Arc<Mutex<Receiver<Acl>>>) -> Option<Acl> {
     rx.lock().await.recv().await
 }

system/gd/rust/stack/src/link/acl/fragment.rs
@@ -4,7 +4,7 @@ use bt_common::Bluetooth;
 use bt_packets::hci::PacketBoundaryFlag::{
     ContinuingFragment, FirstAutomaticallyFlushable, FirstNonAutomaticallyFlushable,
 };
-use bt_packets::hci::{AclBuilder, AclChild, AclPacket, BroadcastFlag};
+use bt_packets::hci::{AclBuilder, AclChild, Acl, BroadcastFlag};
 use bytes::{Buf, Bytes, BytesMut};
 use futures::stream::{self, StreamExt};
 use log::{error, info, warn};
@@ -27,7 +27,7 @@ impl Reassembler {
     }
 
     /// Injest the packet and send out if fully reassembled
-    pub async fn on_packet(&mut self, packet: AclPacket) {
+    pub async fn on_packet(&mut self, packet: Acl) {
         let payload = match packet.specialize() {
             AclChild::Payload(payload) => payload,
             AclChild::None => {
@@ -94,7 +94,7 @@ pub fn fragmenting_stream(
     bt: Bluetooth,
     close_rx: oneshot::Receiver<()>,
 ) -> std::pin::Pin<
-    std::boxed::Box<dyn futures::Stream<Item = bt_packets::hci::AclPacket> + std::marker::Send>,
+    std::boxed::Box<dyn futures::Stream<Item = bt_packets::hci::Acl> + std::marker::Send>,
 > {
     rx.flat_map(move |data| {
         stream::iter(
@@ -113,7 +113,7 @@ pub fn fragmenting_stream(
                     }
                     .build()
                 })
-                .collect::<Vec<AclPacket>>(),
+                .collect::<Vec<Acl>>(),
         )
     })
     .take_until(close_rx)

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 284980692
Test: mmm packages/modules/Bluetooth/system/gd

system/gd/rust/stack/src/link/acl/core.rs
@@ -5,7 +5,7 @@ use crate::hci::{ControllerExports, EventRegistry};
 use crate::link::acl::fragment::{fragmenting_stream, Reassembler};
 use bt_common::Bluetooth::{self, Classic, Le};
 use bt_packets::hci::EventChild::{DisconnectionComplete, NumberOfCompletedPackets};
-use bt_packets::hci::{AclPacket, EventCode, EventPacket};
+use bt_packets::hci::{Acl, Event, EventCode};
 use bytes::Bytes;
 use futures::stream::{SelectAll, StreamExt};
 use gddi::{module, provides, Stoppable};
@@ -34,15 +34,15 @@ pub struct Connection {
     handle: u16,
     #[allow(dead_code)]
     requests: Sender<Request>,
-    pub evt_rx: Receiver<EventPacket>,
-    pub evt_tx: Sender<EventPacket>,
+    pub evt_rx: Receiver<Event>,
+    pub evt_tx: Sender<Event>,
 }
 
 struct ConnectionInternal {
     reassembler: Reassembler,
     bt: Bluetooth,
     close_tx: oneshot::Sender<()>,
-    evt_tx: Sender<EventPacket>,
+    evt_tx: Sender<Event>,
 }
 
 /// Manages rx and tx for open ACL connections
@@ -179,6 +179,6 @@ async fn provide_acl_dispatch(
     AclDispatch { requests: req_tx }
 }
 
-async fn consume(rx: &Arc<Mutex<Receiver<AclPacket>>>) -> Option<AclPacket> {
+async fn consume(rx: &Arc<Mutex<Receiver<Acl>>>) -> Option<Acl> {
     rx.lock().await.recv().await
 }

system/gd/rust/stack/src/link/acl/fragment.rs
@@ -4,7 +4,7 @@ use bt_common::Bluetooth;
 use bt_packets::hci::PacketBoundaryFlag::{
     ContinuingFragment, FirstAutomaticallyFlushable, FirstNonAutomaticallyFlushable,
 };
-use bt_packets::hci::{AclBuilder, AclChild, AclPacket, BroadcastFlag};
+use bt_packets::hci::{Acl, AclBuilder, AclChild, BroadcastFlag};
 use bytes::{Buf, Bytes, BytesMut};
 use futures::stream::{self, StreamExt};
 use log::{error, info, warn};
@@ -27,7 +27,7 @@ impl Reassembler {
     }
 
     /// Injest the packet and send out if fully reassembled
-    pub async fn on_packet(&mut self, packet: AclPacket) {
+    pub async fn on_packet(&mut self, packet: Acl) {
         let payload = match packet.specialize() {
             AclChild::Payload(payload) => payload,
             AclChild::None => {
@@ -94,7 +94,7 @@ pub fn fragmenting_stream(
     bt: Bluetooth,
     close_rx: oneshot::Receiver<()>,
 ) -> std::pin::Pin<
-    std::boxed::Box<dyn futures::Stream<Item = bt_packets::hci::AclPacket> + std::marker::Send>,
+    std::boxed::Box<dyn futures::Stream<Item = bt_packets::hci::Acl> + std::marker::Send>,
 > {
     rx.flat_map(move |data| {
         stream::iter(
@@ -113,7 +113,7 @@ pub fn fragmenting_stream(
                     }
                     .build()
                 })
-                .collect::<Vec<AclPacket>>(),
+                .collect::<Vec<Acl>>(),
         )
     })
     .take_until(close_rx)