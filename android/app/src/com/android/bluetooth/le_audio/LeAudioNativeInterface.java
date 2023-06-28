miyu-xu/packages-modules-Bluetooth

leaudio: Add health based action to java layer
Bug: 279233743
Test: atest LeAudioServiceTest
Test: atest BluetoothInstrumentationTests
Tag: #feature
Change-Id: I0a0db29ebc08d25b3dd50ee8cca8df8dac161b65

android/app/src/com/android/bluetooth/le_audio/LeAudioNativeInterface.java
@@ -219,6 +219,32 @@ public class LeAudioNativeInterface {
         sendMessageToService(event);
     }
 
+    @VisibleForTesting
+    void onHealthBaseRecommendationAction(byte[] address, int action) {
+        LeAudioStackEvent event =
+                new LeAudioStackEvent(LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_DEV_RECOMMENDATION);
+        event.device = getDevice(address);
+        event.valueInt1 = action;
+
+        if (DBG) {
+            Log.d(TAG, "onHealthBaseRecommendationAction: " + event);
+        }
+        sendMessageToService(event);
+    }
+
+    @VisibleForTesting
+    void onHealthBaseGroupRecommendationAction(int groupId, int action) {
+        LeAudioStackEvent event =
+                new LeAudioStackEvent(
+                        LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_GROUP_RECOMMENDATION);
+        event.valueInt1 = groupId;
+        event.valueInt2 = action;
+
+        if (DBG) {
+            Log.d(TAG, "onHealthBaseGroupRecommendationAction: " + event);
+        }
+        sendMessageToService(event);
+    }
     /**
      * Initializes the native interface.
      *

android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -1299,6 +1299,36 @@ public class LeAudioServiceTest {
         assertThat(intent).isNull();
     }
 
+    /**
+     * Test native interface health base action message handling. It does not much, just chects
+     * stack even and that service not crash
+     */
+    @Test
+    public void testHealthBaseDeviceAction() {
+        doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
+        connectTestDevice(mSingleDevice, testGroupId);
+
+        LeAudioStackEvent healthBaseDevAction =
+                new LeAudioStackEvent(LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_DEV_RECOMMENDATION);
+        healthBaseDevAction.device = mSingleDevice;
+        healthBaseDevAction.valueInt1 = LeAudioStackEvent.HEALTH_RECOMMENDATION_ACTION_DISABLE;
+        mService.messageFromNative(healthBaseDevAction);
+        assertThat(mService.mLeAudioNativeIsInitialized).isTrue();
+    }
+
+    @Test
+    public void testHealthBaseGroupAction() {
+        doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
+        connectTestDevice(mSingleDevice, testGroupId);
+
+        LeAudioStackEvent healthBaseGroupAction =
+                new LeAudioStackEvent(
+                        LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_GROUP_RECOMMENDATION);
+        healthBaseGroupAction.valueInt1 = testGroupId;
+        healthBaseGroupAction.valueInt2 = LeAudioStackEvent.HEALTH_RECOMMENDATION_ACTION_DISABLE;
+        mService.messageFromNative(healthBaseGroupAction);
+        assertThat(mService.mLeAudioNativeIsInitialized).isTrue();
+    }
 
     private void sendEventAndVerifyIntentForGroupStatusChanged(int groupId, int groupStatus) {
 

system/bta/le_audio/le_audio_client_test.cc
@@ -250,6 +250,11 @@ class MockAudioHalClientCallbacks
        std::vector<btle_audio_codec_config_t> input_selectable_codec_conf,
        std::vector<btle_audio_codec_config_t> output_selectable_codec_conf),
       (override));
+  MOCK_METHOD((void), OnHealthBaseRecommendationAction,
+              (const RawAddress& address, LeAudioHealthBasedAction action),
+              (override));
+  MOCK_METHOD((void), OnHealthBaseGroupRecommendationAction,
+              (int group_id, LeAudioHealthBasedAction action), (override));
 };
 
 class MockLeAudioSinkHalClient : public LeAudioSinkAudioHalClient {

system/include/hardware/bt_le_audio.h
@@ -142,6 +142,11 @@ class LeAudioClientCallbacks {
       btle_audio_codec_config_t output_codec_conf,
       std::vector<btle_audio_codec_config_t> input_selectable_codec_conf,
       std::vector<btle_audio_codec_config_t> output_selectable_codec_conf) = 0;
+
+  virtual void OnHealthBaseRecommendationAction(
+      const RawAddress& address, LeAudioHealthBasedAction action) = 0;
+  virtual void OnHealthBaseGroupRecommendationAction(
+      int group_id, LeAudioHealthBasedAction action) = 0;
 };
 
 class LeAudioClientInterface {

GrapheneOS/platform_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/le_audio/LeAudioNativeInterface.java
@@ -219,6 +219,32 @@ public class LeAudioNativeInterface {
         sendMessageToService(event);
     }
 
+    @VisibleForTesting
+    void onHealthBasedRecommendationAction(byte[] address, int action) {
+        LeAudioStackEvent event =
+                new LeAudioStackEvent(LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_DEV_RECOMMENDATION);
+        event.device = getDevice(address);
+        event.valueInt1 = action;
+
+        if (DBG) {
+            Log.d(TAG, "onHealthBasedRecommendationAction: " + event);
+        }
+        sendMessageToService(event);
+    }
+
+    @VisibleForTesting
+    void onHealthBasedGroupRecommendationAction(int groupId, int action) {
+        LeAudioStackEvent event =
+                new LeAudioStackEvent(
+                        LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_GROUP_RECOMMENDATION);
+        event.valueInt1 = groupId;
+        event.valueInt2 = action;
+
+        if (DBG) {
+            Log.d(TAG, "onHealthBasedGroupRecommendationAction: " + event);
+        }
+        sendMessageToService(event);
+    }
     /**
      * Initializes the native interface.
      *

android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -1299,6 +1299,36 @@ public class LeAudioServiceTest {
         assertThat(intent).isNull();
     }
 
+    /**
+     * Test native interface health base action message handling. It does not much, just chects
+     * stack even and that service not crash
+     */
+    @Test
+    public void testHealthBaseDeviceAction() {
+        doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
+        connectTestDevice(mSingleDevice, testGroupId);
+
+        LeAudioStackEvent healthBaseDevAction =
+                new LeAudioStackEvent(LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_DEV_RECOMMENDATION);
+        healthBaseDevAction.device = mSingleDevice;
+        healthBaseDevAction.valueInt1 = LeAudioStackEvent.HEALTH_RECOMMENDATION_ACTION_DISABLE;
+        mService.messageFromNative(healthBaseDevAction);
+        assertThat(mService.mLeAudioNativeIsInitialized).isTrue();
+    }
+
+    @Test
+    public void testHealthBasedGroupAction() {
+        doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
+        connectTestDevice(mSingleDevice, testGroupId);
+
+        LeAudioStackEvent healthBasedGroupAction =
+                new LeAudioStackEvent(
+                        LeAudioStackEvent.EVENT_TYPE_HEALTH_BASED_GROUP_RECOMMENDATION);
+        healthBasedGroupAction.valueInt1 = testGroupId;
+        healthBasedGroupAction.valueInt2 = LeAudioStackEvent.HEALTH_RECOMMENDATION_ACTION_DISABLE;
+        mService.messageFromNative(healthBasedGroupAction);
+        assertThat(mService.mLeAudioNativeIsInitialized).isTrue();
+    }
 
     private void sendEventAndVerifyIntentForGroupStatusChanged(int groupId, int groupStatus) {
 

system/bta/le_audio/le_audio_client_test.cc
@@ -250,6 +250,11 @@ class MockAudioHalClientCallbacks
        std::vector<btle_audio_codec_config_t> input_selectable_codec_conf,
        std::vector<btle_audio_codec_config_t> output_selectable_codec_conf),
       (override));
+  MOCK_METHOD((void), OnHealthBasedRecommendationAction,
+              (const RawAddress& address, LeAudioHealthBasedAction action),
+              (override));
+  MOCK_METHOD((void), OnHealthBasedGroupRecommendationAction,
+              (int group_id, LeAudioHealthBasedAction action), (override));
 };
 
 class MockLeAudioSinkHalClient : public LeAudioSinkAudioHalClient {

system/include/hardware/bt_le_audio.h
@@ -142,6 +142,11 @@ class LeAudioClientCallbacks {
       btle_audio_codec_config_t output_codec_conf,
       std::vector<btle_audio_codec_config_t> input_selectable_codec_conf,
       std::vector<btle_audio_codec_config_t> output_selectable_codec_conf) = 0;
+
+  virtual void OnHealthBasedRecommendationAction(
+      const RawAddress& address, LeAudioHealthBasedAction action) = 0;
+  virtual void OnHealthBasedGroupRecommendationAction(
+      int group_id, LeAudioHealthBasedAction action) = 0;
 };
 
 class LeAudioClientInterface {