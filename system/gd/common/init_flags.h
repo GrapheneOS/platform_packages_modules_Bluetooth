miyu-xu/packages-modules-Bluetooth

leaudio: Make use of leAudioHealthStatus module
Bug: 279233743
Test: atest bluetooth_le_audio_client_test
Test: atest BluetoothInstrumentationTests
Tag: #feature
Change-Id: Id12397823821fe4f6a38f812fe21e263bdc8c8d6

system/gd/common/init_flags.h
@@ -68,6 +68,10 @@ class InitFlags final {
     return init_flags::leaudio_targeted_announcement_reconnection_mode_is_enabled();
   }
 
+  inline static bool IsLeAudioHealthBaseActionsEnabled() {
+    return init_flags::leaudio_enable_health_status_actions_is_enabled();
+  }
+
   inline static bool UseRsiFromCachedInquiryResults() {
     return init_flags::use_rsi_from_cached_inqiry_results_is_enabled();
   }

system/gd/common/init_flags_test.cc
@@ -36,6 +36,12 @@ TEST(InitFlagsTest, test_leaudio_targeted_announcement_reconnection_mode) {
   ASSERT_TRUE(InitFlags::IsTargetedAnnouncementReconnectionMode());
 }
 
+TEST(InitFlagsTest, test_leaudio_enable_health_status_actions) {
+  const char* input[] = {"INIT_leaudio_enable_health_status_actions=true", nullptr};
+  InitFlags::Load(input);
+  ASSERT_TRUE(InitFlags::IsLeAudioHealthBaseActionsEnabled());
+}
+
 TEST(InitFlagsTest, test_enable_debug_logging_for_all) {
   const char* input[] = {"INIT_default_log_level=5", nullptr};
   InitFlags::Load(input);

system/gd/rust/shim/src/init_flags.rs
@@ -38,6 +38,7 @@ mod ffi {
         fn hfp_dynamic_version_is_enabled() -> bool;
         fn irk_rotation_is_enabled() -> bool;
         fn leaudio_targeted_announcement_reconnection_mode_is_enabled() -> bool;
+        fn leaudio_enable_health_status_actions_is_enabled() -> bool;
         fn pass_phy_update_callback_is_enabled() -> bool;
         fn pbap_pse_dynamic_version_upgrade_is_enabled() -> bool;
         fn periodic_advertising_adi_is_enabled() -> bool;

GrapheneOS/platform_packages_modules_Bluetooth

system/gd/common/init_flags.h
@@ -68,6 +68,10 @@ class InitFlags final {
     return init_flags::leaudio_targeted_announcement_reconnection_mode_is_enabled();
   }
 
+  inline static bool IsLeAudioHealthBasedActionsEnabled() {
+    return init_flags::leaudio_enable_health_based_actions_is_enabled();
+  }
+
   inline static bool UseRsiFromCachedInquiryResults() {
     return init_flags::use_rsi_from_cached_inqiry_results_is_enabled();
   }

system/gd/common/init_flags_test.cc
@@ -36,6 +36,12 @@ TEST(InitFlagsTest, test_leaudio_targeted_announcement_reconnection_mode) {
   ASSERT_TRUE(InitFlags::IsTargetedAnnouncementReconnectionMode());
 }
 
+TEST(InitFlagsTest, test_leaudio_enable_health_based_actions) {
+  const char* input[] = {"INIT_leaudio_enable_health_based_actions=true", nullptr};
+  InitFlags::Load(input);
+  ASSERT_TRUE(InitFlags::IsLeAudioHealthBasedActionsEnabled());
+}
+
 TEST(InitFlagsTest, test_enable_debug_logging_for_all) {
   const char* input[] = {"INIT_default_log_level=5", nullptr};
   InitFlags::Load(input);

system/gd/rust/shim/src/init_flags.rs
@@ -38,6 +38,7 @@ mod ffi {
         fn hfp_dynamic_version_is_enabled() -> bool;
         fn irk_rotation_is_enabled() -> bool;
         fn leaudio_targeted_announcement_reconnection_mode_is_enabled() -> bool;
+        fn leaudio_enable_health_based_actions_is_enabled() -> bool;
         fn pass_phy_update_callback_is_enabled() -> bool;
         fn pbap_pse_dynamic_version_upgrade_is_enabled() -> bool;
         fn periodic_advertising_adi_is_enabled() -> bool;