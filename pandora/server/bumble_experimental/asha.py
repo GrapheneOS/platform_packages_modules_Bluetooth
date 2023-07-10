544f98dbb934ecd5d0dd5c6071c75d5b7ce19195
Henri Chataing <henrichataing@google.com>
2023-07-10 11:54:08 +0000

    bumble_experimental/Asha: Test against dual registration of the ASHA service
    AshaService.Register did not check for previous registration;
    instead of creating a new service update the information for
    a previously registered AshaService instead
    
    Test: atest v2/pandora-prod-team/avatar
    Bug: 254077091
    Change-Id: Iab9153298f0f96a74bd680e1e71dd90437e64b34

--- pandora/server/bumble_experimental/asha.py
+++ pandora/server/bumble_experimental/asha.py
@@ -211,9 +211,12 @@ class AshaService(AshaServicer):
     @utils.rpc
     async def Register(self, request: RegisterRequest, context: grpc.ServicerContext) -> Empty:
         logging.info("Register")
-        # asha service from bumble profile
-        self.asha_service = AshaGattService(request.capability, request.hisyncid, self.device)
-        self.device.add_service(self.asha_service)  # type: ignore[no-untyped-call]
+        if self.asha_service:
+            self.asha_service.capability = request.capability
+            self.asha_service.hisyncid = request.hisyncid
+        else:
+            self.asha_service = AshaGattService(request.capability, request.hisyncid, self.device)
+            self.device.add_service(self.asha_service)  # type: ignore[no-untyped-call]
         return Empty()
 
     @utils.rpc
