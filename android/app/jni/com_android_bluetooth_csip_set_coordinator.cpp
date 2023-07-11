miyu-xu/packages-modules-Bluetooth

Improve logging for LE Audio profile connection
Add more logs for profile connection state change in JNI callbacks.

Bug: 290552681
Test: manual
Change-Id: I16049cd419208173b5e468125647ce53988bf85b

android/app/jni/com_android_bluetooth_csip_set_coordinator.cpp
@@ -75,7 +75,8 @@ class CsisClientCallbacksImpl : public CsisClientCallbacks {
 
   void OnConnectionState(const RawAddress& bd_addr,
                          ConnectionState state) override {
-    LOG(INFO) << __func__;
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);

android/app/jni/com_android_bluetooth_le_audio.cpp
@@ -137,7 +137,8 @@ class LeAudioClientCallbacksImpl : public LeAudioClientCallbacks {
 
   void OnConnectionState(ConnectionState state,
                          const RawAddress& bd_addr) override {
-    LOG(INFO) << __func__ << ", state:" << int(state);
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);

android/app/jni/com_android_bluetooth_vc.cpp
@@ -48,7 +48,8 @@ class VolumeControlCallbacksImpl : public VolumeControlCallbacks {
   ~VolumeControlCallbacksImpl() = default;
   void OnConnectionState(ConnectionState state,
                          const RawAddress& bd_addr) override {
-    LOG(INFO) << __func__;
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);

GrapheneOS/platform_packages_modules_Bluetooth

Test: atest BluetoothInstrumentationTests

android/app/jni/com_android_bluetooth_csip_set_coordinator.cpp
@@ -75,7 +75,8 @@ class CsisClientCallbacksImpl : public CsisClientCallbacks {
 
   void OnConnectionState(const RawAddress& bd_addr,
                          ConnectionState state) override {
-    LOG(INFO) << __func__;
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToRedactedStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);

android/app/jni/com_android_bluetooth_le_audio.cpp
@@ -137,7 +137,8 @@ class LeAudioClientCallbacksImpl : public LeAudioClientCallbacks {
 
   void OnConnectionState(ConnectionState state,
                          const RawAddress& bd_addr) override {
-    LOG(INFO) << __func__ << ", state:" << int(state);
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToRedactedStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);

android/app/jni/com_android_bluetooth_vc.cpp
@@ -48,7 +48,8 @@ class VolumeControlCallbacksImpl : public VolumeControlCallbacks {
   ~VolumeControlCallbacksImpl() = default;
   void OnConnectionState(ConnectionState state,
                          const RawAddress& bd_addr) override {
-    LOG(INFO) << __func__;
+    LOG(INFO) << __func__ << ", state:" << int(state)
+              << ", addr: " << bd_addr.ToRedactedStringForLogging();
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
     CallbackEnv sCallbackEnv(__func__);