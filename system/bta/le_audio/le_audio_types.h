miyu-xu/packages-modules-Bluetooth

LeAudio: Align context mixing policy
- bidirectional contexts are not mixed with each other
- bidirectional contexts from source metadata are not mixed with the old
  sink metadata contexts
- CONVERSATIONAL from the In Call preference callback call is not mixed
  with contexts from the metadata updates

Bug: 281686787
Bug: 285647765
Test: atest bluetooth_le_audio_test bluetooth_le_audio_client_test
Change-Id: Ib35accf37709b5aa23b92aa99e2cf5d0f737f397

system/bta/le_audio/le_audio_types.h
@@ -525,6 +525,20 @@ constexpr AudioContexts kLeAudioContextAllTypes =
     LeAudioContextType::NOTIFICATIONS | LeAudioContextType::RINGTONE |
     LeAudioContextType::ALERTS | LeAudioContextType::EMERGENCYALARM;
 
+constexpr AudioContexts kLeAudioContextAllBidir =
+    LeAudioContextType::GAME | LeAudioContextType::LIVE |
+    LeAudioContextType::CONVERSATIONAL | LeAudioContextType::VOICEASSISTANTS;
+
+constexpr AudioContexts kLeAudioContextAllRemoteSource =
+    LeAudioContextType::GAME | LeAudioContextType::LIVE |
+    LeAudioContextType::CONVERSATIONAL | LeAudioContextType::VOICEASSISTANTS;
+
+constexpr AudioContexts kLeAudioContextAllRemoteSinkOnly =
+    LeAudioContextType::UNSPECIFIED | LeAudioContextType::MEDIA |
+    LeAudioContextType::INSTRUCTIONAL | LeAudioContextType::SOUNDEFFECTS |
+    LeAudioContextType::NOTIFICATIONS | LeAudioContextType::RINGTONE |
+    LeAudioContextType::ALERTS | LeAudioContextType::EMERGENCYALARM;
+
 /* Print formaters for LTV data */
 std::string CodecCapabilitiesLtvFormat(const uint8_t& type,
                                        const std::vector<uint8_t>& value);

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/le_audio/le_audio_types.h
@@ -525,6 +525,20 @@ constexpr AudioContexts kLeAudioContextAllTypes =
     LeAudioContextType::NOTIFICATIONS | LeAudioContextType::RINGTONE |
     LeAudioContextType::ALERTS | LeAudioContextType::EMERGENCYALARM;
 
+constexpr AudioContexts kLeAudioContextAllBidir =
+    LeAudioContextType::GAME | LeAudioContextType::LIVE |
+    LeAudioContextType::CONVERSATIONAL | LeAudioContextType::VOICEASSISTANTS;
+
+constexpr AudioContexts kLeAudioContextAllRemoteSource =
+    LeAudioContextType::GAME | LeAudioContextType::LIVE |
+    LeAudioContextType::CONVERSATIONAL | LeAudioContextType::VOICEASSISTANTS;
+
+constexpr AudioContexts kLeAudioContextAllRemoteSinkOnly =
+    LeAudioContextType::MEDIA | LeAudioContextType::INSTRUCTIONAL |
+    LeAudioContextType::SOUNDEFFECTS | LeAudioContextType::NOTIFICATIONS |
+    LeAudioContextType::RINGTONE | LeAudioContextType::ALERTS |
+    LeAudioContextType::EMERGENCYALARM;
+
 /* Print formaters for LTV data */
 std::string CodecCapabilitiesLtvFormat(const uint8_t& type,
                                        const std::vector<uint8_t>& value);