miyu-xu/packages-modules-Bluetooth

Always initialize the AirplaneModeListener
Tests now need a provider since they are calling the settings
Some helper tests no longer work with the fake provider. They are
removed since this is not a meaningful test.

Bug: 271963349
Bug: 262605980
Test: atest ServiceBluetoothTests | no-op change
Change-Id: I9881edb1a89e65542c36eedc4670fa6a8a5f51a5

service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -214,7 +214,7 @@ class BluetoothManagerService {
 
     private BluetoothModeChangeHelper mBluetoothModeChangeHelper;
 
-    private BluetoothAirplaneModeListener mBluetoothAirplaneModeListener;
+    private final BluetoothAirplaneModeListener mBluetoothAirplaneModeListener;
 
     private BluetoothNotificationManager mBluetoothNotificationManager;
 
@@ -719,14 +719,9 @@ class BluetoothManagerService {
             mEnableExternal = true;
         }
 
-        String airplaneModeRadios =
-                Settings.Global.getString(mContentResolver, Settings.Global.AIRPLANE_MODE_RADIOS);
-        if (airplaneModeRadios == null
-                || airplaneModeRadios.contains(Settings.Global.RADIO_BLUETOOTH)) {
-            mBluetoothAirplaneModeListener =
-                    new BluetoothAirplaneModeListener(
-                            this, looper, mContext, mBluetoothNotificationManager);
-        }
+        mBluetoothAirplaneModeListener =
+                new BluetoothAirplaneModeListener(
+                        this, looper, mContext, mBluetoothNotificationManager);
 
         mHandler.post(
                 () ->
@@ -740,8 +735,7 @@ class BluetoothManagerService {
 
     /** Returns true if airplane mode is currently on */
     private boolean isAirplaneModeOn() {
-        return mBluetoothAirplaneModeListener != null
-            && mBluetoothAirplaneModeListener.isAirplaneModeOn();
+        return mBluetoothAirplaneModeListener.isAirplaneModeOn();
     }
 
     /** Returns true if satellite mode is turned on. */

GrapheneOS/platform_packages_modules_Bluetooth

service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -216,7 +216,7 @@ class BluetoothManagerService {
 
     private BluetoothModeChangeHelper mBluetoothModeChangeHelper;
 
-    private BluetoothAirplaneModeListener mBluetoothAirplaneModeListener;
+    private final BluetoothAirplaneModeListener mBluetoothAirplaneModeListener;
 
     private BluetoothNotificationManager mBluetoothNotificationManager;
 
@@ -732,14 +732,9 @@ class BluetoothManagerService {
             mEnableExternal = true;
         }
 
-        String airplaneModeRadios =
-                Settings.Global.getString(mContentResolver, Settings.Global.AIRPLANE_MODE_RADIOS);
-        if (airplaneModeRadios == null
-                || airplaneModeRadios.contains(Settings.Global.RADIO_BLUETOOTH)) {
-            mBluetoothAirplaneModeListener =
-                    new BluetoothAirplaneModeListener(
-                            this, mLooper, mContext, mBluetoothNotificationManager);
-        }
+        mBluetoothAirplaneModeListener =
+                new BluetoothAirplaneModeListener(
+                        this, mLooper, mContext, mBluetoothNotificationManager);
 
         if (!USE_NEW_SATELLITE_MODE) {
             mBluetoothSatelliteModeListener =
@@ -753,8 +748,7 @@ class BluetoothManagerService {
 
     /** Returns true if airplane mode is currently on */
     private boolean isAirplaneModeOn() {
-        return mBluetoothAirplaneModeListener != null
-            && mBluetoothAirplaneModeListener.isAirplaneModeOn();
+        return mBluetoothAirplaneModeListener.isAirplaneModeOn();
     }
 
     /** Returns true if satellite mode is turned on. */