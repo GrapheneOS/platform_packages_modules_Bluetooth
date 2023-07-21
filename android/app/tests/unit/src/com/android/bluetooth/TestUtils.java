d35e6a116aed2d7803a6de852d7bee31c34a5a66
Hieu Dang <thebestguy@google.com>
2023-07-21 05:46:34 +0000

    Add retry mechanism for flaky activity tests
    Bug: 292004801
    Test: atest BluetoothInstrumentationTests
    Tag: #refactor
    Change-Id: Idae0c05f41d8cd1ed159826b0214f2006ed0428a

--- android/app/tests/unit/src/com/android/bluetooth/TestUtils.java
+++ android/app/tests/unit/src/com/android/bluetooth/TestUtils.java
@@ -15,7 +15,6 @@
  */
 package com.android.bluetooth;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -31,6 +30,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.MessageQueue;
 import android.service.media.MediaBrowserService;
+import android.util.Log;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.rule.ServiceTestRule;
@@ -41,6 +41,9 @@ import com.android.bluetooth.btservice.AdapterService;
 import com.android.bluetooth.btservice.ProfileService;
 
 import org.junit.Assert;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.mockito.ArgumentCaptor;
 import org.mockito.internal.util.MockUtil;
 
@@ -62,6 +65,8 @@ public class TestUtils {
     private static final int SERVICE_TOGGLE_TIMEOUT_MS = 1000;    // 1s
 
     private static String sSystemScreenOffTimeout = "10000";
+
+    private static final String TAG = "BluetoothTestUtils";
 
     /**
      * Utility method to replace obj.fieldName with newValue where obj is of type c
@@ -397,19 +402,22 @@ public class TestUtils {
     public static void setUpUiTest() throws Exception {
         final UiDevice device = UiDevice.getInstance(
                 androidx.test.platform.app.InstrumentationRegistry.getInstrumentation());
-        // Turn on screen and unlock
-        device.wakeUp();
-        device.executeShellCommand("wm dismiss-keyguard");
-
         // Disable animation
         device.executeShellCommand("settings put global window_animation_scale 0.0");
         device.executeShellCommand("settings put global transition_animation_scale 0.0");
         device.executeShellCommand("settings put global animator_duration_scale 0.0");
 
-        // change device screen_off_timeout
+        // change device screen_off_timeout to 5 minutes
         sSystemScreenOffTimeout =
                 device.executeShellCommand("settings get system screen_off_timeout");
-        device.executeShellCommand("settings put system screen_off_timeout 30000");
+        device.executeShellCommand("settings put system screen_off_timeout 300000");
+
+        // Turn on screen and unlock
+        device.wakeUp();
+        device.executeShellCommand("wm dismiss-keyguard");
+
+        // Back to home screen, in case some dialog/activity is in front
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).pressHome();
     }
 
     public static void tearDownUiTest() throws Exception {
@@ -425,6 +433,48 @@ public class TestUtils {
         // restore screen_off_timeout
         device.executeShellCommand("settings put system screen_off_timeout "
                 + sSystemScreenOffTimeout);
+    }
+
+    public static class RetryTestRule implements TestRule {
+        private int retryCount = 5;
+
+        public RetryTestRule() {
+            this(5);
+        }
+
+        public RetryTestRule(int retryCount) {
+            this.retryCount = retryCount;
+        }
+
+        public Statement apply(Statement base, Description description) {
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    Throwable caughtThrowable = null;
+
+                    // implement retry logic here
+                    for (int i = 0; i < retryCount; i++) {
+                        try {
+                            base.evaluate();
+                            return;
+                        } catch (Throwable t) {
+                            caughtThrowable = t;
+                            Log.e(
+                                    TAG,
+                                    description.getDisplayName() + ": run " + (i + 1) + " failed",
+                                    t);
+                        }
+                    }
+                    Log.e(
+                            TAG,
+                            description.getDisplayName()
+                                    + ": giving up after "
+                                    + retryCount
+                                    + " failures");
+                    throw caughtThrowable;
+                }
+            };
+        }
     }
 
     /**
