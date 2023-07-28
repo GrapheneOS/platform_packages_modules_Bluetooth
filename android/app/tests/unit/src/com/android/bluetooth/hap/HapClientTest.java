miyu-xu/packages-modules-Bluetooth

Stop using intent from HAP profile to ActiveDeviceManager
Bug: 289879962
Test: atest BluetoothInstrumentationTest
Change-Id: Ibbf7ae3c48b5d388224f8b85401579173560dbb2

android/app/tests/unit/src/com/android/bluetooth/hap/HapClientTest.java
@@ -53,6 +53,7 @@ import androidx.test.runner.AndroidJUnit4;
 
 import com.android.bluetooth.TestUtils;
 import com.android.bluetooth.Utils;
+import com.android.bluetooth.btservice.ActiveDeviceManager;
 import com.android.bluetooth.btservice.AdapterService;
 import com.android.bluetooth.btservice.ServiceFactory;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
@@ -101,8 +102,8 @@ public class HapClientTest {
 
     @Mock
     private AdapterService mAdapterService;
-    @Mock
-    private DatabaseManager mDatabaseManager;
+    @Mock private ActiveDeviceManager mActiveDeviceManager;
+    @Mock private DatabaseManager mDatabaseManager;
     @Mock
     private HapClientNativeInterface mNativeInterface;
     @Mock
@@ -131,6 +132,7 @@ public class HapClientTest {
         HapClientStateMachine.sConnectTimeoutMs = TIMEOUT_MS;
 
         TestUtils.setAdapterService(mAdapterService);
+        doReturn(mActiveDeviceManager).when(mAdapterService).getActiveDeviceManager();
         doReturn(mDatabaseManager).when(mAdapterService).getDatabase();
         doReturn(true, false).when(mAdapterService).isStartedProfile(anyString());
 

GrapheneOS/platform_packages_modules_Bluetooth

android/app/tests/unit/src/com/android/bluetooth/hap/HapClientTest.java
@@ -86,8 +86,7 @@ public class HapClientTest {
     private final String mFlagDexmarker = System.getProperty("dexmaker.share_classloader", "false");
 
     private static final int TIMEOUT_MS = 1000;
-    @Rule
-    public final ServiceTestRule mServiceRule = new ServiceTestRule();
+    @Rule public final ServiceTestRule mServiceRule = new ServiceTestRule();
     private BluetoothAdapter mAdapter;
     private BluetoothDevice mDevice;
     private BluetoothDevice mDevice2;
@@ -99,20 +98,13 @@ public class HapClientTest {
     private HasIntentReceiver mHasIntentReceiver;
     private HashMap<BluetoothDevice, LinkedBlockingQueue<Intent>> mIntentQueue;
 
-    @Mock
-    private AdapterService mAdapterService;
-    @Mock
-    private DatabaseManager mDatabaseManager;
-    @Mock
-    private HapClientNativeInterface mNativeInterface;
-    @Mock
-    private ServiceFactory mServiceFactory;
-    @Mock
-    private CsipSetCoordinatorService mCsipService;
-    @Mock
-    private IBluetoothHapClientCallback mCallback;
-    @Mock
-    private Binder mBinder;
+    @Mock private AdapterService mAdapterService;
+    @Mock private DatabaseManager mDatabaseManager;
+    @Mock private HapClientNativeInterface mNativeInterface;
+    @Mock private ServiceFactory mServiceFactory;
+    @Mock private CsipSetCoordinatorService mCsipService;
+    @Mock private IBluetoothHapClientCallback mCallback;
+    @Mock private Binder mBinder;
 
     @Before
     public void setUp() throws Exception {