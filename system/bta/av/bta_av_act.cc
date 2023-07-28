miyu-xu/packages-modules-Bluetooth

Fix avdtp not start
[Root Cause]
When acp connect from peer, the pscb will use the wrong handle
 which maybe used by another peer, and then the BTA_AV_SST will
 be used by two pscb handle by the same current peer, and finally
 local cannot send start_req as the BTA_AV_SST state wrong. And
no sound output from the peer device.

[Solution]
Fix bta av layer pscb donot match with btif av layer btifavpeer

Bug: 288222049
Test: atest net_test_btif_stack

Change-Id: I37974843c604265ffc9098f480760d97ceb990df

system/bta/av/bta_av_act.cc
@@ -1510,6 +1510,7 @@ void bta_av_api_set_latency(tBTA_AV_DATA* p_data) {
  * @return the index for the free LCB entry to use or BTA_AV_NUM_LINKS
  * if no entry is found
  */
+extern const RawAddress& btif_av_find_by_handle(tBTA_AV_HNDL bta_handle);
 static uint8_t bta_av_find_lcb_index_by_scb_and_address(
     const RawAddress& peer_address) {
   APPL_TRACE_DEBUG("%s: peer_address: %s conn_lcb: 0x%x", __func__,
@@ -1541,6 +1542,12 @@ static uint8_t bta_av_find_lcb_index_by_scb_and_address(
       continue;
     }
     if (!p_scb->IsAssigned()) {
+      const RawAddress& btif_addr = btif_av_find_by_handle(p_scb->hndl);
+      if (!btif_addr.IsEmpty() && btif_addr != peer_address) {
+        APPL_TRACE_DEBUG("%s: btif_addr = %s, index=%d!",
+                         __func__, btif_addr.ToString().c_str(), index);
+        continue;
+      }
       return index;
     }
   }

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/av/bta_av_act.cc
@@ -70,6 +70,7 @@ extern bool btif_av_both_enable(void);
 extern bool btif_av_src_sink_coexist_enabled(void);
 extern bool btif_av_is_sink_enabled(void);
 extern bool btif_av_peer_is_connected_sink(const RawAddress& peer_address);
+extern const RawAddress& btif_av_find_by_handle(tBTA_AV_HNDL bta_handle);
 
 /*******************************************************************************
  *
@@ -1541,6 +1542,12 @@ static uint8_t bta_av_find_lcb_index_by_scb_and_address(
       continue;
     }
     if (!p_scb->IsAssigned()) {
+      const RawAddress& btif_addr = btif_av_find_by_handle(p_scb->hndl);
+      if (!btif_addr.IsEmpty() && btif_addr != peer_address) {
+        LOG_DEBUG("%s: btif_addr = %s, index=%d!",
+                         __func__, btif_addr.ToString().c_str(), index);
+        continue;
+      }
       return index;
     }
   }