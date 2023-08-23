49576764932e9203041d671bacc71fce21c399d4
Aritra Sen <aritrasen@google.com>
2023-08-23 09:00:42 +0000

    Remove 'queue_l2cap_coc_while_encrypting' flag since it has been enabled in prod bug-free since tm-qpr2-release.
    Bug: 290845993
    Tag: #stability
    Test: m -j $(nproc) ; Android should build without the flag as well.
    Change-Id: Icb19dfe9ca8d36bb9e95aeb477455adabc486e1c

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -43,7 +43,6 @@ mod ffi {
         fn pbap_pse_dynamic_version_upgrade_is_enabled() -> bool;
         fn periodic_advertising_adi_is_enabled() -> bool;
         fn private_gatt_is_enabled() -> bool;
-        fn queue_l2cap_coc_while_encrypting_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn rust_event_loop_is_enabled() -> bool;
         fn sco_codec_select_lc3_is_enabled() -> bool;
