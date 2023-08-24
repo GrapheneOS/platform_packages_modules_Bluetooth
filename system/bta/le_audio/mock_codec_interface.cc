miyu-xu/packages-modules-Bluetooth

LeAudio: Introduce codec wrapper interface
This extracts the encoding logic from client.cc,
for better code separation and maintainance.

Bug: 262481440
Test: atest bluetooth_le_audio_client_test bluetooth_le_audio_test bluetooth_test_broadcaster_state_machine bluetooth_test_broadcaster
Change-Id: Iee2d1a53c0a0955a8ff7bdd0e9e705ad17159cd9

system/bta/le_audio/mock_codec_interface.cc
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mock_codec_interface.h"
+
+namespace le_audio {
+
+struct CodecInterface::Impl : public MockCodecInterface {
+ public:
+  Impl(const types::LeAudioCodecId& codec_id) {
+    output_channel_data_.resize(1);
+  };
+  ~Impl() = default;
+
+  std::vector<int16_t>& getDecodedSamples() { return output_channel_data_; }
+  std::vector<int16_t> output_channel_data_;
+};
+
+CodecInterface::CodecInterface(const types::LeAudioCodecId& codec_id) {
+  impl = new Impl(codec_id);
+}
+CodecInterface::~CodecInterface() { delete impl; }
+bool CodecInterface::isReady() { return impl->isReady(); };
+CodecInterface::Status CodecInterface::initEncoder(
+    const LeAudioCodecConfiguration& pcm_config,
+    const LeAudioCodecConfiguration& codec_config) {
+  return impl->initEncoder(pcm_config, codec_config);
+}
+CodecInterface::Status CodecInterface::initDecoder(
+    const LeAudioCodecConfiguration& codec_config,
+    const LeAudioCodecConfiguration& pcm_config) {
+  return impl->initDecoder(codec_config, pcm_config);
+}
+std::vector<int16_t>& CodecInterface::getDecodedSamples() {
+  return impl->getDecodedSamples();
+}
+CodecInterface::Status CodecInterface::decode(uint8_t* data, uint16_t size) {
+  return impl->decode(data, size);
+}
+CodecInterface::Status CodecInterface::encode(const uint8_t* data, int stride,
+                                              uint16_t out_size,
+                                              std::vector<int16_t>* out_buffer,
+                                              uint16_t out_offset) {
+  return impl->encode(data, stride, out_size, out_buffer, out_offset);
+}
+void CodecInterface::cleanup() { return impl->cleanup(); }
+
+uint16_t CodecInterface::getNumOfSamplesPerChannel() {
+  return impl->getNumOfSamplesPerChannel();
+};
+uint8_t CodecInterface::getNumOfBytesPerSample() {
+  return impl->getNumOfBytesPerSample();
+};
+}  // namespace le_audio

system/bta/le_audio/mock_codec_interface.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "codec_interface.h"
+
+class MockCodecInterface {
+ public:
+  MockCodecInterface() = default;
+  MockCodecInterface(const MockCodecInterface&) = delete;
+  MockCodecInterface& operator=(const MockCodecInterface&) = delete;
+
+  virtual ~MockCodecInterface() = default;
+
+  MOCK_METHOD((le_audio::CodecInterface::Status), initEncoder,
+              (const le_audio::LeAudioCodecConfiguration& pcm_config,
+               const le_audio::LeAudioCodecConfiguration& codec_config));
+  MOCK_METHOD(le_audio::CodecInterface::Status, initDecoder,
+              (const le_audio::LeAudioCodecConfiguration& codec_config,
+               const le_audio::LeAudioCodecConfiguration& pcm_config));
+  MOCK_METHOD(le_audio::CodecInterface::Status, encode,
+              (const uint8_t* data, int stride, uint16_t out_size,
+               std::vector<int16_t>* out_buffer, uint16_t out_offset));
+  MOCK_METHOD(le_audio::CodecInterface::Status, decode,
+              (uint8_t * data, uint16_t size));
+  MOCK_METHOD((void), cleanup, ());
+  MOCK_METHOD((bool), isReady, ());
+  MOCK_METHOD((uint16_t), getNumOfSamplesPerChannel, ());
+  MOCK_METHOD((uint8_t), getNumOfBytesPerSample, ());
+  MOCK_METHOD((std::vector<int16_t>&), getDecodedSamples, ());
+};

GrapheneOS/platform_packages_modules_Bluetooth

Bug: 295972694
Bug: 262481609

system/bta/le_audio/mock_codec_interface.cc
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mock_codec_interface.h"
+
+namespace le_audio {
+
+struct CodecInterface::Impl : public MockCodecInterface {
+ public:
+  Impl(const types::LeAudioCodecId& codec_id) {
+    output_channel_data_.resize(1);
+  };
+  ~Impl() = default;
+
+  std::vector<int16_t>& GetDecodedSamples() { return output_channel_data_; }
+  std::vector<int16_t> output_channel_data_;
+};
+
+CodecInterface::CodecInterface(const types::LeAudioCodecId& codec_id) {
+  impl = new Impl(codec_id);
+}
+CodecInterface::~CodecInterface() { delete impl; }
+bool CodecInterface::IsReady() { return impl->IsReady(); };
+CodecInterface::Status CodecInterface::InitEncoder(
+    const LeAudioCodecConfiguration& pcm_config,
+    const LeAudioCodecConfiguration& codec_config) {
+  return impl->InitEncoder(pcm_config, codec_config);
+}
+CodecInterface::Status CodecInterface::InitDecoder(
+    const LeAudioCodecConfiguration& codec_config,
+    const LeAudioCodecConfiguration& pcm_config) {
+  return impl->InitDecoder(codec_config, pcm_config);
+}
+std::vector<int16_t>& CodecInterface::GetDecodedSamples() {
+  return impl->GetDecodedSamples();
+}
+CodecInterface::Status CodecInterface::Decode(uint8_t* data, uint16_t size) {
+  return impl->Decode(data, size);
+}
+CodecInterface::Status CodecInterface::Encode(const uint8_t* data, int stride,
+                                              uint16_t out_size,
+                                              std::vector<int16_t>* out_buffer,
+                                              uint16_t out_offset) {
+  return impl->Encode(data, stride, out_size, out_buffer, out_offset);
+}
+void CodecInterface::Cleanup() { return impl->Cleanup(); }
+
+uint16_t CodecInterface::GetNumOfSamplesPerChannel() {
+  return impl->GetNumOfSamplesPerChannel();
+};
+uint8_t CodecInterface::GetNumOfBytesPerSample() {
+  return impl->GetNumOfBytesPerSample();
+};
+}  // namespace le_audio

system/bta/le_audio/mock_codec_interface.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "codec_interface.h"
+
+class MockCodecInterface {
+ public:
+  MockCodecInterface() = default;
+  MockCodecInterface(const MockCodecInterface&) = delete;
+  MockCodecInterface& operator=(const MockCodecInterface&) = delete;
+
+  virtual ~MockCodecInterface() = default;
+
+  MOCK_METHOD((le_audio::CodecInterface::Status), InitEncoder,
+              (const le_audio::LeAudioCodecConfiguration& pcm_config,
+               const le_audio::LeAudioCodecConfiguration& codec_config));
+  MOCK_METHOD(le_audio::CodecInterface::Status, InitDecoder,
+              (const le_audio::LeAudioCodecConfiguration& codec_config,
+               const le_audio::LeAudioCodecConfiguration& pcm_config));
+  MOCK_METHOD(le_audio::CodecInterface::Status, Encode,
+              (const uint8_t* data, int stride, uint16_t out_size,
+               std::vector<int16_t>* out_buffer, uint16_t out_offset));
+  MOCK_METHOD(le_audio::CodecInterface::Status, Decode,
+              (uint8_t * data, uint16_t size));
+  MOCK_METHOD((void), Cleanup, ());
+  MOCK_METHOD((bool), IsReady, ());
+  MOCK_METHOD((uint16_t), GetNumOfSamplesPerChannel, ());
+  MOCK_METHOD((uint8_t), GetNumOfBytesPerSample, ());
+  MOCK_METHOD((std::vector<int16_t>&), GetDecodedSamples, ());
+};