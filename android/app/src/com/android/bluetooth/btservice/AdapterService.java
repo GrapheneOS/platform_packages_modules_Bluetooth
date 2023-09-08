2c9537460711d2fbefd848fcad09c5181ce27649
William Escande <wescande@google.com>
2023-09-08 16:06:55 -0700

    Use SynchronousResultReceiver when fetching Gatt
    Bug: 298530155
    Test: atest BluetoothInstrumentationTests
    Change-Id: I948d7b11776ee1a2f577dabba1e18dce6ee896c1

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -66,7 +66,6 @@ import android.bluetooth.IBluetooth;
 import android.bluetooth.IBluetoothActivityEnergyInfoListener;
 import android.bluetooth.IBluetoothCallback;
 import android.bluetooth.IBluetoothConnectionCallback;
-import android.bluetooth.IBluetoothGatt;
 import android.bluetooth.IBluetoothMetadataListener;
 import android.bluetooth.IBluetoothOobDataCallback;
 import android.bluetooth.IBluetoothPreferredAudioProfilesCallback;
@@ -5197,7 +5196,15 @@ public class AdapterService extends Service {
         }
 
         @Override
-        public IBluetoothGatt getBluetoothGatt() {
+        public void getBluetoothGatt(SynchronousResultReceiver receiver) {
+            try {
+                receiver.send(getBluetoothGatt());
+            } catch (RuntimeException e) {
+                receiver.propagateException(e);
+            }
+        }
+
+        private IBinder getBluetoothGatt() {
             AdapterService service = getService();
             if (service == null) {
                 return null;
@@ -6859,11 +6866,11 @@ public class AdapterService extends Service {
         return BluetoothStatusCodes.FEATURE_NOT_SUPPORTED;
     }
 
-    IBluetoothGatt getBluetoothGatt() {
+    IBinder getBluetoothGatt() {
         if (mGattService == null) {
             return null;
         }
-        return IBluetoothGatt.Stub.asInterface(((ProfileService) mGattService).getBinder());
+        return ((ProfileService) mGattService).getBinder();
     }
 
     void unregAllGattClient(AttributionSource source) {
