miyu-xu/packages-modules-Bluetooth

LeAudio: Rename codec configuration struct
Since this LTV types are defined as egeneral types
which are not LC3 specific, this struct is also not
LC3 specific.

Bug: 295972694
Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test
Change-Id: I51553313246ee04ea7582555ad1bf6ea8bf4cc92

system/audio_hal_interface/aidl/le_audio_software_aidl.cc
@@ -45,7 +45,7 @@ using ::bluetooth::audio::le_audio::LeAudioClientInterface;
 using ::bluetooth::audio::le_audio::StartRequestState;
 using ::bluetooth::audio::le_audio::StreamCallbacks;
 using ::le_audio::set_configurations::SetConfiguration;
-using ::le_audio::types::LeAudioLc3Config;
+using ::le_audio::types::LeAudioCodecConfigBase;
 
 static ChannelMode le_audio_channel_mode2audio_hal(uint8_t channels_count) {
   switch (channels_count) {
@@ -496,7 +496,7 @@ bool hal_ucast_capability_to_stack_format(
 
   stack_capability = {
       .id = ::le_audio::set_configurations::LeAudioCodecIdLc3,
-      .config = LeAudioLc3Config(
+      .config = LeAudioCodecConfigBase(
           {.sampling_frequency = sampling_freq_map[sample_rate_hz],
            .frame_duration = frame_duration_map[frame_duration_us],
            .audio_channel_allocation = audio_location_map[supported_channel],
@@ -550,7 +550,7 @@ bool hal_bcast_capability_to_stack_format(
 
   stack_capability = {
       .id = ::le_audio::set_configurations::LeAudioCodecIdLc3,
-      .config = LeAudioLc3Config(
+      .config = LeAudioCodecConfigBase(
           {.sampling_frequency = sampling_freq_map[sample_rate_hz],
            .frame_duration = frame_duration_map[frame_duration_us],
            .audio_channel_allocation = audio_location_map[supported_channel],

system/bta/le_audio/device_groups.cc
@@ -36,7 +36,7 @@ using types::BidirectionalPair;
 using types::CisType;
 using types::LeAudioContextType;
 
-using types::LeAudioLc3Config;
+using types::LeAudioCodecConfigBase;
 
 /* LeAudioDeviceGroup Class methods implementation */
 void LeAudioDeviceGroup::AddNode(
@@ -1282,7 +1282,8 @@ bool LeAudioDeviceGroup::IsAudioSetConfigurationSupported(
       /* TODO Make it no Lc3 specific */
       if (!CheckIfStrategySupported(
               strategy, audio_locations,
-              std::get<LeAudioLc3Config>(ent.codec.config).GetChannelCount(),
+              std::get<LeAudioCodecConfigBase>(ent.codec.config)
+                  .GetChannelCount(),
               device->GetLc3SupportedChannelCount(ent.direction))) {
         LOG_DEBUG(" insufficient device audio allocation: %lu",
                   audio_locations.to_ulong());

system/bta/le_audio/devices.cc
@@ -33,8 +33,8 @@ using le_audio::types::AudioContexts;
 using le_audio::types::AudioLocations;
 using le_audio::types::AudioStreamDataPathState;
 using le_audio::types::BidirectionalPair;
+using le_audio::types::LeAudioCodecConfigBase;
 using le_audio::types::LeAudioContextType;
-using le_audio::types::LeAudioLc3Config;
 
 namespace le_audio {
 std::ostream& operator<<(std::ostream& os, const DeviceConnectState& state) {
@@ -269,7 +269,7 @@ bool LeAudioDevice::ConfigureAses(
       ase->target_latency = ent.qos.target_latency;
       ase->codec_id = ent.codec.id;
       /* TODO: find better way to not use LC3 explicitly */
-      ase->codec_config = std::get<LeAudioLc3Config>(ent.codec.config);
+      ase->codec_config = std::get<LeAudioCodecConfigBase>(ent.codec.config);
 
       /*Let's choose audio channel allocation if not set */
       ase->codec_config.audio_channel_allocation =

system/bta/le_audio/devices_test.cc
@@ -400,7 +400,8 @@ class PublishedAudioCapabilitiesBuilder {
            uint8_t audio_channel_counts) {
     if (setting.id != LeAudioCodecIdLc3) return;
 
-    const LeAudioLc3Config config = std::get<LeAudioLc3Config>(setting.config);
+    const LeAudioCodecConfigBase config =
+        std::get<LeAudioCodecConfigBase>(setting.config);
 
     Add(setting.id, *config.sampling_frequency, *config.frame_duration,
         audio_channel_counts, *config.octets_per_codec_frame);

system/bta/le_audio/le_audio_set_configuration_provider_json.cc
@@ -206,7 +206,7 @@ struct AudioSetConfigurationProviderJson {
         .vendor_codec_id = flat_codec_id->vendor_codec_id(),
     });
 
-    /* Cache the types::LeAudioLc3Config type value */
+    /* Cache the types::LeAudioCodecConfigBase type value */
     uint8_t sampling_frequency = 0;
     uint8_t frame_duration = 0;
     uint32_t audio_channel_allocation = 0;
@@ -273,7 +273,7 @@ struct AudioSetConfigurationProviderJson {
       STREAM_TO_UINT8(codec_frames_blocks_per_sdu, ptr);
     }
 
-    codec.config = types::LeAudioLc3Config({
+    codec.config = types::LeAudioCodecConfigBase({
         .sampling_frequency = sampling_frequency,
         .frame_duration = frame_duration,
         .audio_channel_allocation = audio_channel_allocation,

system/bta/le_audio/le_audio_types.cc
@@ -42,7 +42,7 @@ using types::CodecLocation;
 using types::kLeAudioCodingFormatLC3;
 using types::kLeAudioDirectionSink;
 using types::kLeAudioDirectionSource;
-using types::LeAudioLc3Config;
+using types::LeAudioCodecConfigBase;
 
 static uint8_t min_req_devices_cnt(
     const AudioSetConfiguration* audio_set_conf) {
@@ -246,8 +246,9 @@ uint8_t get_num_of_devices_in_configuration(
   return min_req_devices_cnt(audio_set_conf);
 }
 
-static bool IsCodecConfigurationSupported(const types::LeAudioLtvMap& pacs,
-                                          const LeAudioLc3Config& lc3_config) {
+static bool IsCodecConfigurationSupported(
+    const types::LeAudioLtvMap& pacs,
+    const LeAudioCodecConfigBase& lc3_config) {
   const auto& reqs = lc3_config.GetAsLtvMap();
   uint8_t u8_req_val, u8_pac_val;
   uint16_t u16_req_val, u16_pac_val;
@@ -377,7 +378,7 @@ bool IsCodecCapabilitySettingSupported(
     case kLeAudioCodingFormatLC3:
       return IsCodecConfigurationSupported(
           pac.codec_spec_caps,
-          std::get<LeAudioLc3Config>(codec_capability_setting.config));
+          std::get<LeAudioCodecConfigBase>(codec_capability_setting.config));
     default:
       return false;
   }
@@ -386,7 +387,8 @@ bool IsCodecCapabilitySettingSupported(
 uint32_t CodecCapabilitySetting::GetConfigSamplingFrequency() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      return std::get<types::LeAudioLc3Config>(config).GetSamplingFrequencyHz();
+      return std::get<types::LeAudioCodecConfigBase>(config)
+          .GetSamplingFrequencyHz();
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -396,7 +398,8 @@ uint32_t CodecCapabilitySetting::GetConfigSamplingFrequency() const {
 uint32_t CodecCapabilitySetting::GetConfigDataIntervalUs() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      return std::get<types::LeAudioLc3Config>(config).GetFrameDurationUs();
+      return std::get<types::LeAudioCodecConfigBase>(config)
+          .GetFrameDurationUs();
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -417,10 +420,11 @@ uint8_t CodecCapabilitySetting::GetConfigBitsPerSample() const {
 uint8_t CodecCapabilitySetting::GetConfigChannelCount() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      LOG_DEBUG("count = %d",
-                static_cast<int>(
-                    std::get<types::LeAudioLc3Config>(config).channel_count));
-      return std::get<types::LeAudioLc3Config>(config).channel_count;
+      LOG_DEBUG(
+          "count = %d",
+          static_cast<int>(
+              std::get<types::LeAudioCodecConfigBase>(config).channel_count));
+      return std::get<types::LeAudioCodecConfigBase>(config).channel_count;
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -430,7 +434,7 @@ uint8_t CodecCapabilitySetting::GetConfigChannelCount() const {
 
 namespace types {
 /* Helper map for matching various frequency notations */
-const std::map<uint8_t, uint32_t> LeAudioLc3Config::sampling_freq_map = {
+const std::map<uint8_t, uint32_t> LeAudioCodecConfigBase::sampling_freq_map = {
     {codec_spec_conf::kLeAudioSamplingFreq8000Hz,
      LeAudioCodecConfiguration::kSampleRate8000},
     {codec_spec_conf::kLeAudioSamplingFreq16000Hz,
@@ -445,7 +449,7 @@ const std::map<uint8_t, uint32_t> LeAudioLc3Config::sampling_freq_map = {
      LeAudioCodecConfiguration::kSampleRate48000}};
 
 /* Helper map for matching various frame durations notations */
-const std::map<uint8_t, uint32_t> LeAudioLc3Config::frame_duration_map = {
+const std::map<uint8_t, uint32_t> LeAudioCodecConfigBase::frame_duration_map = {
     {codec_spec_conf::kLeAudioCodecFrameDur7500us,
      LeAudioCodecConfiguration::kInterval7500Us},
     {codec_spec_conf::kLeAudioCodecFrameDur10000us,
@@ -808,8 +812,9 @@ std::ostream& operator<<(std::ostream& os, const types::AseState& state) {
 }
 
 std::ostream& operator<<(std::ostream& os,
-                         const types::LeAudioLc3Config& config) {
-  os << " LeAudioLc3Config(SamplFreq=" << loghex(*config.sampling_frequency)
+                         const types::LeAudioCodecConfigBase& config) {
+  os << " LeAudioCodecConfigBase(SamplFreq="
+     << loghex(*config.sampling_frequency)
      << ", FrameDur=" << loghex(*config.frame_duration)
      << ", OctetsPerFrame=" << int(*config.octets_per_codec_frame)
      << ", CodecFramesBlocksPerSDU=" << int(*config.codec_frames_blocks_per_sdu)

GrapheneOS/platform_packages_modules_Bluetooth

system/audio_hal_interface/aidl/le_audio_software_aidl.cc
@@ -45,7 +45,7 @@ using ::bluetooth::audio::le_audio::LeAudioClientInterface;
 using ::bluetooth::audio::le_audio::StartRequestState;
 using ::bluetooth::audio::le_audio::StreamCallbacks;
 using ::le_audio::set_configurations::SetConfiguration;
-using ::le_audio::types::LeAudioLc3Config;
+using ::le_audio::types::LeAudioCoreCodecConfig;
 
 static ChannelMode le_audio_channel_mode2audio_hal(uint8_t channels_count) {
   switch (channels_count) {
@@ -502,7 +502,7 @@ bool hal_ucast_capability_to_stack_format(
 
   stack_capability = {
       .id = ::le_audio::set_configurations::LeAudioCodecIdLc3,
-      .config = LeAudioLc3Config(
+      .config = LeAudioCoreCodecConfig(
           {.sampling_frequency = sampling_freq_map[sample_rate_hz],
            .frame_duration = frame_duration_map[frame_duration_us],
            .audio_channel_allocation = audio_location_map[supported_channel],
@@ -556,7 +556,7 @@ bool hal_bcast_capability_to_stack_format(
 
   stack_capability = {
       .id = ::le_audio::set_configurations::LeAudioCodecIdLc3,
-      .config = LeAudioLc3Config(
+      .config = LeAudioCoreCodecConfig(
           {.sampling_frequency = sampling_freq_map[sample_rate_hz],
            .frame_duration = frame_duration_map[frame_duration_us],
            .audio_channel_allocation = audio_location_map[supported_channel],

system/bta/le_audio/device_groups.cc
@@ -37,7 +37,7 @@ using types::CisState;
 using types::CisType;
 using types::DataPathState;
 using types::LeAudioContextType;
-using types::LeAudioLc3Config;
+using types::LeAudioCoreCodecConfig;
 
 /* LeAudioDeviceGroup Class methods implementation */
 void LeAudioDeviceGroup::AddNode(
@@ -1293,7 +1293,8 @@ bool LeAudioDeviceGroup::IsAudioSetConfigurationSupported(
       /* TODO Make it no Lc3 specific */
       if (!CheckIfStrategySupported(
               strategy, audio_locations,
-              std::get<LeAudioLc3Config>(ent.codec.config).GetChannelCount(),
+              std::get<LeAudioCoreCodecConfig>(ent.codec.config)
+                  .GetChannelCount(),
               device->GetLc3SupportedChannelCount(ent.direction))) {
         LOG_DEBUG(" insufficient device audio allocation: %lu",
                   audio_locations.to_ulong());

system/bta/le_audio/devices.cc
@@ -35,7 +35,7 @@ using le_audio::types::BidirectionalPair;
 using le_audio::types::CisState;
 using le_audio::types::DataPathState;
 using le_audio::types::LeAudioContextType;
-using le_audio::types::LeAudioLc3Config;
+using le_audio::types::LeAudioCoreCodecConfig;
 
 namespace le_audio {
 std::ostream& operator<<(std::ostream& os, const DeviceConnectState& state) {
@@ -270,7 +270,7 @@ bool LeAudioDevice::ConfigureAses(
       ase->target_latency = ent.qos.target_latency;
       ase->codec_id = ent.codec.id;
       /* TODO: find better way to not use LC3 explicitly */
-      ase->codec_config = std::get<LeAudioLc3Config>(ent.codec.config);
+      ase->codec_config = std::get<LeAudioCoreCodecConfig>(ent.codec.config);
 
       /*Let's choose audio channel allocation if not set */
       ase->codec_config.audio_channel_allocation =

system/bta/le_audio/devices_test.cc
@@ -400,7 +400,8 @@ class PublishedAudioCapabilitiesBuilder {
            uint8_t audio_channel_counts) {
     if (setting.id != LeAudioCodecIdLc3) return;
 
-    const LeAudioLc3Config config = std::get<LeAudioLc3Config>(setting.config);
+    const LeAudioCoreCodecConfig config =
+        std::get<LeAudioCoreCodecConfig>(setting.config);
 
     Add(setting.id, *config.sampling_frequency, *config.frame_duration,
         audio_channel_counts, *config.octets_per_codec_frame);

system/bta/le_audio/le_audio_set_configuration_provider_json.cc
@@ -211,7 +211,7 @@ struct AudioSetConfigurationProviderJson {
         .vendor_codec_id = flat_codec_id->vendor_codec_id(),
     });
 
-    /* Cache the types::LeAudioLc3Config type value */
+    /* Cache the types::LeAudioCoreCodecConfig type value */
     uint8_t sampling_frequency = 0;
     uint8_t frame_duration = 0;
     uint32_t audio_channel_allocation = 0;
@@ -278,7 +278,7 @@ struct AudioSetConfigurationProviderJson {
       STREAM_TO_UINT8(codec_frames_blocks_per_sdu, ptr);
     }
 
-    codec.config = types::LeAudioLc3Config({
+    codec.config = types::LeAudioCoreCodecConfig({
         .sampling_frequency = sampling_frequency,
         .frame_duration = frame_duration,
         .audio_channel_allocation = audio_channel_allocation,

system/bta/le_audio/le_audio_types.cc
@@ -42,7 +42,7 @@ using types::CodecLocation;
 using types::kLeAudioCodingFormatLC3;
 using types::kLeAudioDirectionSink;
 using types::kLeAudioDirectionSource;
-using types::LeAudioLc3Config;
+using types::LeAudioCoreCodecConfig;
 
 static uint8_t min_req_devices_cnt(
     const AudioSetConfiguration* audio_set_conf) {
@@ -246,8 +246,9 @@ uint8_t get_num_of_devices_in_configuration(
   return min_req_devices_cnt(audio_set_conf);
 }
 
-static bool IsCodecConfigurationSupported(const types::LeAudioLtvMap& pacs,
-                                          const LeAudioLc3Config& lc3_config) {
+static bool IsCodecConfigurationSupported(
+    const types::LeAudioLtvMap& pacs,
+    const LeAudioCoreCodecConfig& lc3_config) {
   const auto& reqs = lc3_config.GetAsLtvMap();
   uint8_t u8_req_val, u8_pac_val;
   uint16_t u16_req_val, u16_pac_val;
@@ -377,7 +378,7 @@ bool IsCodecCapabilitySettingSupported(
     case kLeAudioCodingFormatLC3:
       return IsCodecConfigurationSupported(
           pac.codec_spec_caps,
-          std::get<LeAudioLc3Config>(codec_capability_setting.config));
+          std::get<LeAudioCoreCodecConfig>(codec_capability_setting.config));
     default:
       return false;
   }
@@ -386,7 +387,8 @@ bool IsCodecCapabilitySettingSupported(
 uint32_t CodecCapabilitySetting::GetConfigSamplingFrequency() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      return std::get<types::LeAudioLc3Config>(config).GetSamplingFrequencyHz();
+      return std::get<types::LeAudioCoreCodecConfig>(config)
+          .GetSamplingFrequencyHz();
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -396,7 +398,8 @@ uint32_t CodecCapabilitySetting::GetConfigSamplingFrequency() const {
 uint32_t CodecCapabilitySetting::GetConfigDataIntervalUs() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      return std::get<types::LeAudioLc3Config>(config).GetFrameDurationUs();
+      return std::get<types::LeAudioCoreCodecConfig>(config)
+          .GetFrameDurationUs();
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -417,10 +420,11 @@ uint8_t CodecCapabilitySetting::GetConfigBitsPerSample() const {
 uint8_t CodecCapabilitySetting::GetConfigChannelCount() const {
   switch (id.coding_format) {
     case kLeAudioCodingFormatLC3:
-      LOG_DEBUG("count = %d",
-                static_cast<int>(
-                    std::get<types::LeAudioLc3Config>(config).channel_count));
-      return std::get<types::LeAudioLc3Config>(config).channel_count;
+      LOG_DEBUG(
+          "count = %d",
+          static_cast<int>(
+              std::get<types::LeAudioCoreCodecConfig>(config).channel_count));
+      return std::get<types::LeAudioCoreCodecConfig>(config).channel_count;
     default:
       LOG_WARN(", invalid codec id: 0x%02x", id.coding_format);
       return 0;
@@ -430,7 +434,7 @@ uint8_t CodecCapabilitySetting::GetConfigChannelCount() const {
 
 namespace types {
 /* Helper map for matching various frequency notations */
-const std::map<uint8_t, uint32_t> LeAudioLc3Config::sampling_freq_map = {
+const std::map<uint8_t, uint32_t> LeAudioCoreCodecConfig::sampling_freq_map = {
     {codec_spec_conf::kLeAudioSamplingFreq8000Hz,
      LeAudioCodecConfiguration::kSampleRate8000},
     {codec_spec_conf::kLeAudioSamplingFreq16000Hz,
@@ -445,7 +449,7 @@ const std::map<uint8_t, uint32_t> LeAudioLc3Config::sampling_freq_map = {
      LeAudioCodecConfiguration::kSampleRate48000}};
 
 /* Helper map for matching various frame durations notations */
-const std::map<uint8_t, uint32_t> LeAudioLc3Config::frame_duration_map = {
+const std::map<uint8_t, uint32_t> LeAudioCoreCodecConfig::frame_duration_map = {
     {codec_spec_conf::kLeAudioCodecFrameDur7500us,
      LeAudioCodecConfiguration::kInterval7500Us},
     {codec_spec_conf::kLeAudioCodecFrameDur10000us,
@@ -811,8 +815,9 @@ std::ostream& operator<<(std::ostream& os, const types::AseState& state) {
 }
 
 std::ostream& operator<<(std::ostream& os,
-                         const types::LeAudioLc3Config& config) {
-  os << " LeAudioLc3Config(SamplFreq=" << loghex(*config.sampling_frequency)
+                         const types::LeAudioCoreCodecConfig& config) {
+  os << " LeAudioCoreCodecConfig(SamplFreq="
+     << loghex(*config.sampling_frequency)
      << ", FrameDur=" << loghex(*config.frame_duration)
      << ", OctetsPerFrame=" << int(*config.octets_per_codec_frame)
      << ", CodecFramesBlocksPerSDU=" << int(*config.codec_frames_blocks_per_sdu)