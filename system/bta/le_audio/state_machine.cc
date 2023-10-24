miyu-xu/packages-modules-Bluetooth

le_audio: Protect from unreliable autonomous disable operation
Both ASEs that are bonded with bi-directional ASEs should go to QoS
state once CIS is terminated. This change will guard for such
transition.

"If the server detects link loss of a CIS for an ASE in the Streaming
state or the Disabling state, the server shall immediately transition
that ASE to the QoS Configured state." ASCS 1.0 3.2
ASE state machine transitions.

Bug: 278621595
Tag: #feature
Test: atest bluetooth_le_audio_test
Change-Id: Ic6e91d63b9690771bd7da09f1a108a181369b861

system/bta/le_audio/state_machine.cc
@@ -112,6 +112,7 @@ using le_audio::types::LeAudioContextType;
 namespace {
 
 constexpr int linkQualityCheckInterval = 4000;
+constexpr int kAutonomousTransitionTimeoutMs = 5000;
 
 static void link_quality_cb(void* data) {
   // very ugly, but we need to pass just two bytes
@@ -939,6 +940,7 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
           LOG_WARN(
               "Group member disconnected during streaming. Cis handle 0x%04x",
               event->cis_conn_hdl);
+
           return;
         }
 
@@ -2039,6 +2041,12 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         SetAseState(leAudioDevice, ase,
                     AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED);
 
+        /* Remote may autonomously bring ASEs to QoS configured state */
+        if (group->GetTargetState() !=
+            AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+          ProcessAutonomousDisable(leAudioDevice, ase);
+        }
+
         /* Process the Disable Transition of the rest of group members if no
          * more ASE notifications has to come from this device. */
         if (leAudioDevice->IsReadyToSuspendStream()) ProcessGroupDisable(group);
@@ -2625,6 +2633,20 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
     }
   }
 
+  void ScheduleAutonomousOperationTimer(AseState target_state,
+                                        LeAudioDevice* leAudioDevice,
+                                        struct ase* ase) {
+    ase->autonomous_target_state_ = target_state;
+    alarm_set_on_mloop(
+        ase->autonomous_operation_timer_, kAutonomousTransitionTimeoutMs,
+        [](void* data) {
+          LeAudioDevice* leAudioDevice = static_cast<LeAudioDevice*>(data);
+          instance->state_machine_callbacks_
+              ->OnDeviceAutonomousStateTransitionTimeout(leAudioDevice);
+        },
+        leAudioDevice);
+  }
+
   void AseStateMachineProcessDisabling(
       struct le_audio::client_parser::ascs::ase_rsp_hdr& arh, struct ase* ase,
       LeAudioDeviceGroup* group, LeAudioDevice* leAudioDevice) {
@@ -2651,10 +2673,15 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         SetAseState(leAudioDevice, ase,
                     AseState::BTA_LE_AUDIO_ASE_STATE_DISABLING);
 
+        /* Remote may autonomously bring ASEs to QoS configured state */
+        if (group->GetTargetState() !=
+            AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+          ProcessAutonomousDisable(leAudioDevice, ase);
+        }
+
         /* Process the Disable Transition of the rest of group members if no
          * more ASE notifications has to come from this device. */
-        if (leAudioDevice->IsReadyToSuspendStream())
-          ProcessGroupDisable(group);
+        if (leAudioDevice->IsReadyToSuspendStream()) ProcessGroupDisable(group);
 
         break;
 
@@ -2820,6 +2847,32 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
                 ToString(group->GetState()).c_str(),
                 ToString(group->GetTargetState()).c_str());
       StopStream(group);
+    }
+  }
+
+  void ProcessAutonomousDisable(LeAudioDevice* leAudioDevice, struct ase* ase) {
+    auto bidirection_ase = leAudioDevice->GetAseToMatchBidirectionCis(ase);
+
+    /* ASE is not a part of bi-directional CIS */
+    if (!bidirection_ase) return;
+
+    /* ASE is already disabled */
+    if (bidirection_ase->state ==
+        AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+      /* Bi-direction ASEs are now disabled */
+      if ((ase->autonomous_target_state_ ==
+           AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) &&
+          alarm_is_scheduled(ase->autonomous_operation_timer_)) {
+        alarm_cancel(ase->autonomous_operation_timer_);
+      }
+      return;
+    }
+
+    /* Schedule alarm if first ASE is autonomously disabling */
+    if (!alarm_is_scheduled(bidirection_ase->autonomous_operation_timer_)) {
+      ScheduleAutonomousOperationTimer(
+          AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED, leAudioDevice,
+          bidirection_ase);
     }
   }
 };

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/le_audio/state_machine.cc
@@ -113,6 +113,7 @@ using le_audio::types::LeAudioContextType;
 namespace {
 
 constexpr int linkQualityCheckInterval = 4000;
+constexpr int kAutonomousTransitionTimeoutMs = 5000;
 
 static void link_quality_cb(void* data) {
   // very ugly, but we need to pass just two bytes
@@ -2075,6 +2076,12 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         SetAseState(leAudioDevice, ase,
                     AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED);
 
+        /* Remote may autonomously bring ASEs to QoS configured state */
+        if (group->GetTargetState() !=
+            AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+          ProcessAutonomousDisable(leAudioDevice, ase);
+        }
+
         /* Process the Disable Transition of the rest of group members if no
          * more ASE notifications has to come from this device. */
         if (leAudioDevice->IsReadyToSuspendStream()) ProcessGroupDisable(group);
@@ -2670,6 +2677,22 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
     }
   }
 
+  void ScheduleAutonomousOperationTimer(AseState target_state,
+                                        LeAudioDevice* leAudioDevice,
+                                        struct ase* ase) {
+    ase->autonomous_target_state_ = target_state;
+    ase->autonomous_operation_timer_ =
+        alarm_new("LeAudioAutonomousOperationTimeout");
+    alarm_set_on_mloop(
+        ase->autonomous_operation_timer_, kAutonomousTransitionTimeoutMs,
+        [](void* data) {
+          LeAudioDevice* leAudioDevice = static_cast<LeAudioDevice*>(data);
+          instance->state_machine_callbacks_
+              ->OnDeviceAutonomousStateTransitionTimeout(leAudioDevice);
+        },
+        leAudioDevice);
+  }
+
   void AseStateMachineProcessDisabling(
       struct le_audio::client_parser::ascs::ase_rsp_hdr& arh, struct ase* ase,
       LeAudioDeviceGroup* group, LeAudioDevice* leAudioDevice) {
@@ -2696,10 +2719,15 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         SetAseState(leAudioDevice, ase,
                     AseState::BTA_LE_AUDIO_ASE_STATE_DISABLING);
 
+        /* Remote may autonomously bring ASEs to QoS configured state */
+        if (group->GetTargetState() !=
+            AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+          ProcessAutonomousDisable(leAudioDevice, ase);
+        }
+
         /* Process the Disable Transition of the rest of group members if no
          * more ASE notifications has to come from this device. */
-        if (leAudioDevice->IsReadyToSuspendStream())
-          ProcessGroupDisable(group);
+        if (leAudioDevice->IsReadyToSuspendStream()) ProcessGroupDisable(group);
 
         break;
 
@@ -2893,6 +2921,34 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
                 ToString(group->GetState()).c_str(),
                 ToString(group->GetTargetState()).c_str());
       StopStream(group);
+    }
+  }
+
+  void ProcessAutonomousDisable(LeAudioDevice* leAudioDevice, struct ase* ase) {
+    auto bidirection_ase = leAudioDevice->GetAseToMatchBidirectionCis(ase);
+
+    /* ASE is not a part of bi-directional CIS */
+    if (!bidirection_ase) return;
+
+    /* ASE is already disabled */
+    if (bidirection_ase->state ==
+        AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) {
+      /* Bi-direction ASEs are now disabled */
+      if ((ase->autonomous_target_state_ ==
+           AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED) &&
+          alarm_is_scheduled(ase->autonomous_operation_timer_)) {
+        alarm_free(ase->autonomous_operation_timer_);
+        ase->autonomous_operation_timer_ = NULL;
+        ase->autonomous_target_state_ = AseState::BTA_LE_AUDIO_ASE_STATE_IDLE;
+      }
+      return;
+    }
+
+    /* Schedule alarm if first ASE is autonomously disabling */
+    if (!alarm_is_scheduled(bidirection_ase->autonomous_operation_timer_)) {
+      ScheduleAutonomousOperationTimer(
+          AseState::BTA_LE_AUDIO_ASE_STATE_QOS_CONFIGURED, leAudioDevice,
+          bidirection_ase);
     }
   }
 };