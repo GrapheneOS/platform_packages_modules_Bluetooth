miyu-xu/packages-modules-Bluetooth

Crypto: Always call aes_128 with Octet16
Bug: 301661850
Test: atest net_test_stack_smp
Change-Id: I2fb6873dd923121875c9d2bade5441311c685e64

system/stack/btm/btm_ble.cc
@@ -2079,16 +2079,18 @@ static void btm_notify_new_key(uint8_t key_type) {
 static void btm_ble_reset_id_impl(const Octet16& rand1, const Octet16& rand2) {
   /* Regenerate Identity Root */
   btm_cb.devcb.id_keys.ir = rand1;
-  uint8_t btm_ble_dhk_pt = 0x03;
+  Octet16 btm_ble_dhk_pt{};
+  btm_ble_dhk_pt[0] = 0x03;
 
   /* generate DHK= Eir({0x03, 0x00, 0x00 ...}) */
   btm_cb.devcb.id_keys.dhk =
-      crypto_toolbox::aes_128(btm_cb.devcb.id_keys.ir, &btm_ble_dhk_pt, 1);
+      crypto_toolbox::aes_128(btm_cb.devcb.id_keys.ir, btm_ble_dhk_pt);
 
-  uint8_t btm_ble_irk_pt = 0x01;
+  Octet16 btm_ble_irk_pt{};
+  btm_ble_irk_pt[0] = 0x01;
   /* IRK = D1(IR, 1) */
   btm_cb.devcb.id_keys.irk =
-      crypto_toolbox::aes_128(btm_cb.devcb.id_keys.ir, &btm_ble_irk_pt, 1);
+      crypto_toolbox::aes_128(btm_cb.devcb.id_keys.ir, btm_ble_irk_pt);
 
   btm_notify_new_key(BTM_BLE_KEY_TYPE_ID);
 

GrapheneOS/platform_packages_modules_Bluetooth

system/stack/btm/btm_ble.cc
@@ -2068,16 +2068,18 @@ static void btm_notify_new_key(uint8_t key_type) {
 static void btm_ble_reset_id_impl(const Octet16& rand1, const Octet16& rand2) {
   /* Regenerate Identity Root */
   btm_sec_cb.devcb.id_keys.ir = rand1;
-  uint8_t btm_ble_dhk_pt = 0x03;
+  Octet16 btm_ble_dhk_pt{};
+  btm_ble_dhk_pt[0] = 0x03;
 
   /* generate DHK= Eir({0x03, 0x00, 0x00 ...}) */
   btm_sec_cb.devcb.id_keys.dhk =
-      crypto_toolbox::aes_128(btm_sec_cb.devcb.id_keys.ir, &btm_ble_dhk_pt, 1);
+      crypto_toolbox::aes_128(btm_sec_cb.devcb.id_keys.ir, btm_ble_dhk_pt);
 
-  uint8_t btm_ble_irk_pt = 0x01;
+  Octet16 btm_ble_irk_pt{};
+  btm_ble_irk_pt[0] = 0x01;
   /* IRK = D1(IR, 1) */
   btm_sec_cb.devcb.id_keys.irk =
-      crypto_toolbox::aes_128(btm_sec_cb.devcb.id_keys.ir, &btm_ble_irk_pt, 1);
+      crypto_toolbox::aes_128(btm_sec_cb.devcb.id_keys.ir, btm_ble_irk_pt);
 
   btm_notify_new_key(BTM_BLE_KEY_TYPE_ID);
 