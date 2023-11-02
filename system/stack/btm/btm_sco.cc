0135d6ab35366186e38d664aa29fb0eb8f9a7692
En-Shuo Hsu <enshuo@google.com>
2023-11-02 16:22:43 -0700

    Clean noisy SCO debug log
    Meaningful log is flooded by the SCO log. Remove most of them to
    mitigate the issue.
    
    Replace them with a summary debug log printed when SCO gets
    disconnected.
    
    Bug: 301673369, 308457537
    Test: atest --host net_test_stack_btm --no-bazel-mode
    Change-Id: I05fa8af5ecab1439530822fed4844c92d7818641

--- system/stack/btm/btm_sco.cc
+++ system/stack/btm/btm_sco.cc
@@ -328,10 +328,7 @@ void btm_route_sco_data(bluetooth::hci::ScoView valid_packet) {
                         ? &bluetooth::audio::sco::swb::decode
                         : &bluetooth::audio::sco::wbs::decode;
       rc = decode(&decoded);
-      if (rc == 0) {
-        LOG_DEBUG("Failed to decode %s frames", codec.c_str());
-        break;
-      }
+      if (rc == 0) break;
 
       written += bluetooth::audio::sco::write(decoded, rc);
     }
@@ -1258,8 +1255,13 @@ void btm_sco_on_disconnected(uint16_t hci_handle, tHCI_REASON reason) {
       double packet_loss_ratio;
       if (fill_plc_stats(&num_decoded_frames, &packet_loss_ratio)) {
         const int16_t codec_id = sco_codec_type_to_id(codec_type);
+        const std::string codec = sco_codec_type_text(codec_type);
         log_hfp_audio_packet_loss_stats(bd_addr, num_decoded_frames,
                                         packet_loss_ratio, codec_id);
+        LOG_DEBUG(
+            "Stopped SCO codec:%s, num_decoded_frames:%d, "
+            "packet_loss_ratio:%lf",
+            codec.c_str(), num_decoded_frames, packet_loss_ratio);
       } else {
         LOG_WARN("Failed to get the packet loss stats");
       }
