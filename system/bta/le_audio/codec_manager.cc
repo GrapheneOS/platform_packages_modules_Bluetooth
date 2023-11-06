miyu-xu/packages-modules-Bluetooth

Move ConfigureDataPath out of ACL
Remove from ACL, since it has to do with SCO and ISO.

Use Gmock for the interface.

Update tests.

Bug: 304830775
Test: mma -j32
Change-Id: I1111fac4e90aa2137112a312ac524a74873a4ee8

system/bta/le_audio/codec_manager.cc
@@ -22,13 +22,13 @@
 #include "le_audio_set_configuration_provider.h"
 #include "osi/include/log.h"
 #include "osi/include/properties.h"
-#include "stack/acl/acl.h"
-#include "stack/include/acl_api.h"
+#include "stack/include/hcimsgs.h"
 
 namespace {
 
 using bluetooth::hci::iso_manager::kIsoDataPathHci;
 using bluetooth::hci::iso_manager::kIsoDataPathPlatformDefault;
+using bluetooth::legacy::hci::GetInterface;
 using le_audio::CodecManager;
 using le_audio::types::CodecLocation;
 
@@ -105,10 +105,10 @@ struct codec_manager_impl {
     }
 
     LOG_INFO("LeAudioCodecManagerImpl: configure_data_path for encode");
-    btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                            kIsoDataPathPlatformDefault, {});
-    btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                            kIsoDataPathPlatformDefault, {});
+    GetInterface().ConfigureDataPath(hci_data_direction::HOST_TO_CONTROLLER,
+                                     kIsoDataPathPlatformDefault, {});
+    GetInterface().ConfigureDataPath(hci_data_direction::CONTROLLER_TO_HOST,
+                                     kIsoDataPathPlatformDefault, {});
     SetCodecLocation(CodecLocation::ADSP);
   }
   void start(
@@ -118,10 +118,10 @@ struct codec_manager_impl {
   }
   ~codec_manager_impl() {
     if (GetCodecLocation() != CodecLocation::HOST) {
-      btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                              kIsoDataPathHci, {});
-      btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                              kIsoDataPathHci, {});
+      GetInterface().ConfigureDataPath(hci_data_direction::HOST_TO_CONTROLLER,
+                                       kIsoDataPathHci, {});
+      GetInterface().ConfigureDataPath(hci_data_direction::CONTROLLER_TO_HOST,
+                                       kIsoDataPathHci, {});
     }
     le_audio::AudioSetConfigurationProvider::Cleanup();
   }

system/stack/btm/btm_sco_hfp_hal_linux.cc
@@ -24,9 +24,10 @@
 #include "gd/common/init_flags.h"
 #include "osi/include/log.h"
 #include "osi/include/properties.h"
-#include "stack/acl/acl.h"
-#include "stack/include/acl_api.h"
+#include "stack/include/hcimsgs.h"
 #include "stack/include/sdpdefs.h"
+
+using bluetooth::legacy::hci::GetInterface;
 
 namespace hfp_hal_interface {
 namespace {
@@ -418,10 +419,10 @@ void set_codec_datapath(int codec_uuid) {
         break;
     }
 
-    btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                            OFFLOAD_DATAPATH, data);
-    btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                            OFFLOAD_DATAPATH, data);
+    GetInterface().ConfigureDataPath(hci_data_direction::CONTROLLER_TO_HOST,
+                                     OFFLOAD_DATAPATH, data);
+    GetInterface().ConfigureDataPath(hci_data_direction::HOST_TO_CONTROLLER,
+                                     OFFLOAD_DATAPATH, data);
   }
 }
 

system/stack/hcic/hcicmds.cc
@@ -1665,7 +1665,7 @@ void btsnd_hcic_vendor_spec_cmd(uint16_t opcode, uint8_t len, uint8_t* p_data,
                      base::Unretained(p_cmd_cplt_cback), v_opcode));
 }
 
-void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
+void btsnd_hcic_configure_data_path(hci_data_direction data_path_direction,
                                     uint8_t data_path_id,
                                     std::vector<uint8_t> vendor_config) {
   BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
@@ -1686,16 +1686,34 @@ void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
   btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
 }
 
-bluetooth::legacy::hci::Interface interface_ = {
-    // LINK_CONTROL
-    .StartInquiry = btsnd_hcic_inquiry,                   // OCF 0x0401
-    .InquiryCancel = btsnd_hcic_inq_cancel,               // OCF 0x0402
-    .Disconnect = btsnd_hcic_disconnect,                  // OCF 0x0406
-    .ChangeConnectionPacketType = btsnd_hcic_change_conn_type,  // OCF 0x040F,
-    .StartRoleSwitch = btsnd_hcic_switch_role,               // OCF 0x080B,
+namespace bluetooth::legacy::hci {
+class InterfaceImpl : public Interface {
+  void StartInquiry(const uint8_t* inq_lap, uint8_t duration,
+                    uint8_t response_cnt) const override {
+    btsnd_hcic_inquiry(inq_lap, duration, response_cnt);
+  }
+  void InquiryCancel() const override { btsnd_hcic_inq_cancel(); }
+  void Disconnect(uint16_t handle, uint8_t reason) const override {
+    btsnd_hcic_disconnect(handle, reason);
+  }
+  void ChangeConnectionPacketType(uint16_t handle,
+                                  uint16_t packet_types) const override {
+    btsnd_hcic_change_conn_type(handle, packet_types);
+  }
+  void StartRoleSwitch(const RawAddress& bd_addr, uint8_t role) const override {
+    btsnd_hcic_switch_role(bd_addr, role);
+  }
+  void ConfigureDataPath(hci_data_direction data_path_direction,
+                         uint8_t data_path_id,
+                         std::vector<uint8_t> vendor_config) const override {
+    btsnd_hcic_configure_data_path(data_path_direction, data_path_id,
+                                   vendor_config);
+  }
 };
 
-const bluetooth::legacy::hci::Interface&
-bluetooth::legacy::hci::GetInterface() {
-  return interface_;
+namespace {
+const InterfaceImpl interface_;
 }
+
+const Interface& GetInterface() { return interface_; }
+}  // namespace bluetooth::legacy::hci

system/stack/include/hcimsgs.h
@@ -22,6 +22,7 @@
 #include <base/functional/callback_forward.h>
 
 #include <cstdint>
+#include <vector>
 
 #include "device/include/esco_parameters.h"
 #include "stack/include/bt_lap.h"
@@ -33,26 +34,31 @@
 
 /* Message by message.... */
 
+enum hci_data_direction {
+  HOST_TO_CONTROLLER = 0,
+  CONTROLLER_TO_HOST = 1,
+};
+
 /* Disconnect */
-namespace bluetooth {
-namespace legacy {
-namespace hci {
-struct Interface {
-  // LINK_CONTROL 0x04xx
-  void (*StartInquiry)(const LAP inq_lap, uint8_t duration,
-                       uint8_t response_cnt);
-  void (*InquiryCancel)();
-  void (*Disconnect)(uint16_t handle, uint8_t reason);
-  void (*ChangeConnectionPacketType)(uint16_t handle, uint16_t packet_types);
-  void (*StartRoleSwitch)(const RawAddress& bd_addr, uint8_t role);
+namespace bluetooth::legacy::hci {
+class Interface {
+ public:
+  virtual void StartInquiry(const LAP inq_lap, uint8_t duration,
+                            uint8_t response_cnt) const = 0;
+  virtual void InquiryCancel() const = 0;
+  virtual void Disconnect(uint16_t handle, uint8_t reason) const = 0;
+  virtual void ChangeConnectionPacketType(uint16_t handle,
+                                          uint16_t packet_types) const = 0;
+  virtual void StartRoleSwitch(const RawAddress& bd_addr,
+                               uint8_t role) const = 0;
+  virtual void ConfigureDataPath(hci_data_direction data_path_direction,
+                                 uint8_t data_path_id,
+                                 std::vector<uint8_t> vendor_config) const = 0;
+  virtual ~Interface() = default;
 };
 
 const Interface& GetInterface();
-}  // namespace hci
-}  // namespace legacy
-}  // namespace bluetooth
-
-/* Disconnect */
+}  // namespace bluetooth::legacy::hci
 
 /* Add SCO Connection */
 void btsnd_hcic_add_SCO_conn(uint16_t handle, uint16_t packet_types);
@@ -516,7 +522,7 @@ void btsnd_hcic_ble_set_default_periodic_advertising_sync_transfer_params(
     uint16_t conn_handle, uint8_t mode, uint16_t skip, uint16_t sync_timeout,
     uint8_t cte_type, base::OnceCallback<void(uint8_t*, uint16_t)> cb);
 
-void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
+void btsnd_hcic_configure_data_path(hci_data_direction data_path_direction,
                                     uint8_t data_path_id,
                                     std::vector<uint8_t> vendor_config);
 

GrapheneOS/platform_packages_modules_Bluetooth

system/bta/le_audio/codec_manager.cc
@@ -22,13 +22,13 @@
 #include "le_audio_set_configuration_provider.h"
 #include "osi/include/log.h"
 #include "osi/include/properties.h"
-#include "stack/acl/acl.h"
-#include "stack/include/acl_api.h"
+#include "stack/include/hcimsgs.h"
 
 namespace {
 
 using bluetooth::hci::iso_manager::kIsoDataPathHci;
 using bluetooth::hci::iso_manager::kIsoDataPathPlatformDefault;
+using bluetooth::legacy::hci::GetInterface;
 using le_audio::CodecManager;
 using le_audio::types::CodecLocation;
 
@@ -105,10 +105,10 @@ struct codec_manager_impl {
     }
 
     LOG_INFO("LeAudioCodecManagerImpl: configure_data_path for encode");
-    btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                            kIsoDataPathPlatformDefault, {});
-    btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                            kIsoDataPathPlatformDefault, {});
+    GetInterface().ConfigureDataPath(hci_data_direction_t::HOST_TO_CONTROLLER,
+                                     kIsoDataPathPlatformDefault, {});
+    GetInterface().ConfigureDataPath(hci_data_direction_t::CONTROLLER_TO_HOST,
+                                     kIsoDataPathPlatformDefault, {});
     SetCodecLocation(CodecLocation::ADSP);
   }
   void start(
@@ -118,10 +118,10 @@ struct codec_manager_impl {
   }
   ~codec_manager_impl() {
     if (GetCodecLocation() != CodecLocation::HOST) {
-      btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                              kIsoDataPathHci, {});
-      btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                              kIsoDataPathHci, {});
+      GetInterface().ConfigureDataPath(hci_data_direction_t::HOST_TO_CONTROLLER,
+                                       kIsoDataPathHci, {});
+      GetInterface().ConfigureDataPath(hci_data_direction_t::CONTROLLER_TO_HOST,
+                                       kIsoDataPathHci, {});
     }
     le_audio::AudioSetConfigurationProvider::Cleanup();
   }

system/stack/btm/btm_sco_hfp_hal_linux.cc
@@ -24,9 +24,10 @@
 #include "gd/common/init_flags.h"
 #include "osi/include/log.h"
 #include "osi/include/properties.h"
-#include "stack/acl/acl.h"
-#include "stack/include/acl_api.h"
+#include "stack/include/hcimsgs.h"
 #include "stack/include/sdpdefs.h"
+
+using bluetooth::legacy::hci::GetInterface;
 
 namespace hfp_hal_interface {
 namespace {
@@ -418,10 +419,10 @@ void set_codec_datapath(int codec_uuid) {
         break;
     }
 
-    btm_configure_data_path(btm_data_direction::CONTROLLER_TO_HOST,
-                            OFFLOAD_DATAPATH, data);
-    btm_configure_data_path(btm_data_direction::HOST_TO_CONTROLLER,
-                            OFFLOAD_DATAPATH, data);
+    GetInterface().ConfigureDataPath(hci_data_direction_t::CONTROLLER_TO_HOST,
+                                     OFFLOAD_DATAPATH, data);
+    GetInterface().ConfigureDataPath(hci_data_direction_t::HOST_TO_CONTROLLER,
+                                     OFFLOAD_DATAPATH, data);
   }
 }
 

system/stack/hcic/hcicmds.cc
@@ -1665,7 +1665,7 @@ void btsnd_hcic_vendor_spec_cmd(uint16_t opcode, uint8_t len, uint8_t* p_data,
                      base::Unretained(p_cmd_cplt_cback), v_opcode));
 }
 
-void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
+void btsnd_hcic_configure_data_path(hci_data_direction_t data_path_direction,
                                     uint8_t data_path_id,
                                     std::vector<uint8_t> vendor_config) {
   BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
@@ -1686,16 +1686,34 @@ void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
   btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
 }
 
-bluetooth::legacy::hci::Interface interface_ = {
-    // LINK_CONTROL
-    .StartInquiry = btsnd_hcic_inquiry,                   // OCF 0x0401
-    .InquiryCancel = btsnd_hcic_inq_cancel,               // OCF 0x0402
-    .Disconnect = btsnd_hcic_disconnect,                  // OCF 0x0406
-    .ChangeConnectionPacketType = btsnd_hcic_change_conn_type,  // OCF 0x040F,
-    .StartRoleSwitch = btsnd_hcic_switch_role,               // OCF 0x080B,
+namespace bluetooth::legacy::hci {
+class InterfaceImpl : public Interface {
+  void StartInquiry(const uint8_t* inq_lap, uint8_t duration,
+                    uint8_t response_cnt) const override {
+    btsnd_hcic_inquiry(inq_lap, duration, response_cnt);
+  }
+  void InquiryCancel() const override { btsnd_hcic_inq_cancel(); }
+  void Disconnect(uint16_t handle, uint8_t reason) const override {
+    btsnd_hcic_disconnect(handle, reason);
+  }
+  void ChangeConnectionPacketType(uint16_t handle,
+                                  uint16_t packet_types) const override {
+    btsnd_hcic_change_conn_type(handle, packet_types);
+  }
+  void StartRoleSwitch(const RawAddress& bd_addr, uint8_t role) const override {
+    btsnd_hcic_switch_role(bd_addr, role);
+  }
+  void ConfigureDataPath(hci_data_direction_t data_path_direction,
+                         uint8_t data_path_id,
+                         std::vector<uint8_t> vendor_config) const override {
+    btsnd_hcic_configure_data_path(data_path_direction, data_path_id,
+                                   vendor_config);
+  }
 };
 
-const bluetooth::legacy::hci::Interface&
-bluetooth::legacy::hci::GetInterface() {
-  return interface_;
+namespace {
+const InterfaceImpl interface_;
 }
+
+const Interface& GetInterface() { return interface_; }
+}  // namespace bluetooth::legacy::hci

system/stack/include/hcimsgs.h
@@ -22,6 +22,7 @@
 #include <base/functional/callback_forward.h>
 
 #include <cstdint>
+#include <vector>
 
 #include "device/include/esco_parameters.h"
 #include "stack/include/bt_lap.h"
@@ -33,26 +34,31 @@
 
 /* Message by message.... */
 
+enum hci_data_direction_t {
+  HOST_TO_CONTROLLER = 0,
+  CONTROLLER_TO_HOST = 1,
+};
+
 /* Disconnect */
-namespace bluetooth {
-namespace legacy {
-namespace hci {
-struct Interface {
-  // LINK_CONTROL 0x04xx
-  void (*StartInquiry)(const LAP inq_lap, uint8_t duration,
-                       uint8_t response_cnt);
-  void (*InquiryCancel)();
-  void (*Disconnect)(uint16_t handle, uint8_t reason);
-  void (*ChangeConnectionPacketType)(uint16_t handle, uint16_t packet_types);
-  void (*StartRoleSwitch)(const RawAddress& bd_addr, uint8_t role);
+namespace bluetooth::legacy::hci {
+class Interface {
+ public:
+  virtual void StartInquiry(const LAP inq_lap, uint8_t duration,
+                            uint8_t response_cnt) const = 0;
+  virtual void InquiryCancel() const = 0;
+  virtual void Disconnect(uint16_t handle, uint8_t reason) const = 0;
+  virtual void ChangeConnectionPacketType(uint16_t handle,
+                                          uint16_t packet_types) const = 0;
+  virtual void StartRoleSwitch(const RawAddress& bd_addr,
+                               uint8_t role) const = 0;
+  virtual void ConfigureDataPath(hci_data_direction_t data_path_direction,
+                                 uint8_t data_path_id,
+                                 std::vector<uint8_t> vendor_config) const = 0;
+  virtual ~Interface() = default;
 };
 
 const Interface& GetInterface();
-}  // namespace hci
-}  // namespace legacy
-}  // namespace bluetooth
-
-/* Disconnect */
+}  // namespace bluetooth::legacy::hci
 
 /* Add SCO Connection */
 void btsnd_hcic_add_SCO_conn(uint16_t handle, uint16_t packet_types);
@@ -516,7 +522,7 @@ void btsnd_hcic_ble_set_default_periodic_advertising_sync_transfer_params(
     uint16_t conn_handle, uint8_t mode, uint16_t skip, uint16_t sync_timeout,
     uint8_t cte_type, base::OnceCallback<void(uint8_t*, uint16_t)> cb);
 
-void btsnd_hcic_configure_data_path(uint8_t data_path_direction,
+void btsnd_hcic_configure_data_path(hci_data_direction_t data_path_direction,
                                     uint8_t data_path_id,
                                     std::vector<uint8_t> vendor_config);
 