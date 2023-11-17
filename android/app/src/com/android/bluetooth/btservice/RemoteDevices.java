miyu-xu/packages-modules-Bluetooth

Handle ACL disconnect events across multiple Bluetooth profiles via AdapterService.
Tag: #refactor
Bug: 296932947
Test: atest BluetoothInstrumentationTests
Change-Id: I53d07f54acb8ddbceb113c6fddc59d6ea312f1ea

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -191,6 +191,11 @@ public class RemoteDevices {
                             + bluetoothDevice.isConnected());
 
                     if (bluetoothDevice.isConnected()) {
+                        int transport = deviceProperties.getConnectionHandle(
+                                BluetoothDevice.TRANSPORT_BREDR) != BluetoothDevice.ERROR
+                                        ? BluetoothDevice.TRANSPORT_BREDR
+                                        : BluetoothDevice.TRANSPORT_LE;
+                        mAdapterService.notifyProfileOnAclDisconnect(bluetoothDevice, transport);
                         Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
                         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, bluetoothDevice);
                         intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT
@@ -1129,6 +1134,7 @@ public class RemoteDevices {
                 mDevices.remove(key);
             }
             if (state == BluetoothAdapter.STATE_ON || state == BluetoothAdapter.STATE_TURNING_OFF) {
+                mAdapterService.notifyProfileOnAclDisconnect(device, transportLinkType);
                 intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
                 intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, transportLinkType);
             } else if (state == BluetoothAdapter.STATE_BLE_ON

android/app/src/com/android/bluetooth/map/BluetoothMapService.java
@@ -694,7 +694,6 @@ public class BluetoothMapService extends ProfileService {
         IntentFilter filter = new IntentFilter();
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         filter.addAction(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         filter.addAction(BluetoothDevice.ACTION_SDP_RECORD);
         filter.addAction(USER_CONFIRM_TIMEOUT_ACTION);
 
@@ -1199,25 +1198,31 @@ public class BluetoothMapService extends ProfileService {
                     BluetoothMapContentObserver.actionMessageSentDisconnected(context, intent,
                             result);
                 }
-            } else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)
-                    && mIsWaitingAuthorization) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                if (sRemoteDevice == null || device == null) {
-                    Log.e(TAG, "Unexpected error!");
-                    return;
-                }
-
-                if (VERBOSE) {
-                    Log.v(TAG, "ACL disconnected for " + device);
-                }
-
-                if (sRemoteDevice.equals(device)) {
-                    // Send any pending timeout now, since ACL got disconnected
-                    mSessionStatusHandler.removeMessages(USER_TIMEOUT);
-                    mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
-                }
             }
+        }
+    }
+
+    public void onAclDisconnect(BluetoothDevice device) {
+        mSessionStatusHandler.post(() -> handleAclDisconnect(device));
+    }
+
+    private void handleAclDisconnect(BluetoothDevice device) {
+        if (!mIsWaitingAuthorization) {
+            return;
+        }
+        if (sRemoteDevice == null || device == null) {
+            Log.e(TAG, "Unexpected error!");
+            return;
+        }
+
+        if (VERBOSE) {
+            Log.v(TAG, "ACL disconnected for " + device);
+        }
+
+        if (sRemoteDevice.equals(device)) {
+            // Send any pending timeout now, since ACL got disconnected
+            mSessionStatusHandler.removeMessages(USER_TIMEOUT);
+            mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
         }
     }
 

android/app/src/com/android/bluetooth/sap/SapService.java
@@ -685,7 +685,6 @@ public class SapService extends ProfileService implements AdapterService.Bluetoo
         IntentFilter filter = new IntentFilter();
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         filter.addAction(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         filter.addAction(USER_CONFIRM_TIMEOUT_ACTION);
 
         try {
@@ -910,34 +909,37 @@ public class SapService extends ProfileService implements AdapterService.Bluetoo
                 sendConnectTimeoutMessage();
                 return;
             }
-
-            if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED) && mIsWaitingAuthorization) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                if (mRemoteDevice == null || device == null) {
-                    Log.i(TAG, "Unexpected error!");
-                    return;
-                }
-
-                if (DEBUG) {
-                    Log.d(TAG, "ACL disconnected for " + device);
-                }
-
-                if (mRemoteDevice.equals(device)) {
-                    if (mRemoveTimeoutMsg) {
-                        // Send any pending timeout now, as ACL got disconnected.
-                        mSessionStatusHandler.removeMessages(USER_TIMEOUT);
-                        mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
-                    }
-                    setState(BluetoothSap.STATE_DISCONNECTED);
-                    // Ensure proper cleanup, and prepare for new connect.
-                    mSessionStatusHandler.sendEmptyMessage(MSG_SERVERSESSION_CLOSE);
-                }
-            }
         }
     }
 
-    ;
+    public void onAclDisconnect(BluetoothDevice device) {
+        mSessionStatusHandler.post(() -> handleAclDisconnect(device));
+    }
+
+    private void handleAclDisconnect(BluetoothDevice device) {
+        if (!mIsWaitingAuthorization) {
+            return;
+        }
+        if (mRemoteDevice == null || device == null) {
+            Log.i(TAG, "Unexpected error!");
+            return;
+        }
+
+        if (DEBUG) {
+            Log.d(TAG, "ACL disconnected for " + device);
+        }
+
+        if (mRemoteDevice.equals(device)) {
+            if (mRemoveTimeoutMsg) {
+                // Send any pending timeout now, as ACL got disconnected.
+                mSessionStatusHandler.removeMessages(USER_TIMEOUT);
+                mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
+            }
+            setState(BluetoothSap.STATE_DISCONNECTED);
+            // Ensure proper cleanup, and prepare for new connect.
+            mSessionStatusHandler.sendEmptyMessage(MSG_SERVERSESSION_CLOSE);
+        }
+    }
 
     //Binder object: Must be static class or memory leak may occur
 

android/app/tests/unit/src/com/android/bluetooth/mapclient/MapClientServiceTest.java
@@ -29,6 +29,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothUuid;
 import android.content.Intent;
+import android.os.Looper;
 
 import androidx.test.filters.MediumTest;
 import androidx.test.rule.ServiceTestRule;
@@ -313,10 +314,8 @@ public class MapClientServiceTest {
         mService.getInstanceMap().put(mRemoteDevice, sm);
         when(sm.getState()).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_BREDR);
-        mService.mMapReceiver.onReceive(mService, intent);
+        mService.onAclDisconnect(mRemoteDevice, BluetoothDevice.TRANSPORT_BREDR);
+        TestUtils.waitForLooperToFinishScheduledTask(Looper.getMainLooper());
 
         verify(sm).disconnect();
     }

android/app/tests/unit/src/com/android/bluetooth/pbapclient/PbapClientServiceTest.java
@@ -34,6 +34,7 @@ import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.CallLog;
+import android.os.Looper;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.MediumTest;
@@ -343,10 +344,8 @@ public class PbapClientServiceTest {
         mService.mPbapClientStateMachineMap.put(mRemoteDevice, sm);
         when(sm.getConnectionState(mRemoteDevice)).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_BREDR);
-        mService.mPbapBroadcastReceiver.onReceive(mService, intent);
+        mService.onAclDisconnect(mRemoteDevice, BluetoothDevice.TRANSPORT_BREDR);
+        TestUtils.waitForLooperToFinishScheduledTask(Looper.getMainLooper());
 
         verify(sm).disconnect(mRemoteDevice);
     }

GrapheneOS/platform_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -197,6 +197,11 @@ public class RemoteDevices {
                             + bluetoothDevice.isConnected());
 
                     if (bluetoothDevice.isConnected()) {
+                        int transport = deviceProperties.getConnectionHandle(
+                                BluetoothDevice.TRANSPORT_BREDR) != BluetoothDevice.ERROR
+                                        ? BluetoothDevice.TRANSPORT_BREDR
+                                        : BluetoothDevice.TRANSPORT_LE;
+                        mAdapterService.notifyAclDisconnected(bluetoothDevice, transport);
                         Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
                         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, bluetoothDevice);
                         intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT
@@ -1144,6 +1149,7 @@ public class RemoteDevices {
                 mDevices.remove(key);
             }
             if (state == BluetoothAdapter.STATE_ON || state == BluetoothAdapter.STATE_TURNING_OFF) {
+                mAdapterService.notifyAclDisconnected(device, transportLinkType);
                 intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
                 intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, transportLinkType);
             } else if (state == BluetoothAdapter.STATE_BLE_ON

android/app/src/com/android/bluetooth/map/BluetoothMapService.java
@@ -694,7 +694,6 @@ public class BluetoothMapService extends ProfileService {
         IntentFilter filter = new IntentFilter();
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         filter.addAction(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         filter.addAction(BluetoothDevice.ACTION_SDP_RECORD);
         filter.addAction(USER_CONFIRM_TIMEOUT_ACTION);
 
@@ -1195,25 +1194,31 @@ public class BluetoothMapService extends ProfileService {
                     BluetoothMapContentObserver.actionMessageSentDisconnected(context, intent,
                             result);
                 }
-            } else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)
-                    && mIsWaitingAuthorization) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                if (sRemoteDevice == null || device == null) {
-                    Log.e(TAG, "Unexpected error!");
-                    return;
-                }
-
-                if (VERBOSE) {
-                    Log.v(TAG, "ACL disconnected for " + device);
-                }
-
-                if (sRemoteDevice.equals(device)) {
-                    // Send any pending timeout now, since ACL got disconnected
-                    mSessionStatusHandler.removeMessages(USER_TIMEOUT);
-                    mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
-                }
             }
+        }
+    }
+
+    public void aclDisconnected(BluetoothDevice device) {
+        mSessionStatusHandler.post(() -> handleAclDisconnected(device));
+    }
+
+    private void handleAclDisconnected(BluetoothDevice device) {
+        if (!mIsWaitingAuthorization) {
+            return;
+        }
+        if (sRemoteDevice == null || device == null) {
+            Log.e(TAG, "Unexpected error!");
+            return;
+        }
+
+        if (VERBOSE) {
+            Log.v(TAG, "ACL disconnected for " + device);
+        }
+
+        if (sRemoteDevice.equals(device)) {
+            // Send any pending timeout now, since ACL got disconnected
+            mSessionStatusHandler.removeMessages(USER_TIMEOUT);
+            mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
         }
     }
 

android/app/src/com/android/bluetooth/sap/SapService.java
@@ -685,7 +685,6 @@ public class SapService extends ProfileService implements AdapterService.Bluetoo
         IntentFilter filter = new IntentFilter();
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         filter.addAction(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         filter.addAction(USER_CONFIRM_TIMEOUT_ACTION);
 
         try {
@@ -910,34 +909,37 @@ public class SapService extends ProfileService implements AdapterService.Bluetoo
                 sendConnectTimeoutMessage();
                 return;
             }
-
-            if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED) && mIsWaitingAuthorization) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                if (mRemoteDevice == null || device == null) {
-                    Log.i(TAG, "Unexpected error!");
-                    return;
-                }
-
-                if (DEBUG) {
-                    Log.d(TAG, "ACL disconnected for " + device);
-                }
-
-                if (mRemoteDevice.equals(device)) {
-                    if (mRemoveTimeoutMsg) {
-                        // Send any pending timeout now, as ACL got disconnected.
-                        mSessionStatusHandler.removeMessages(USER_TIMEOUT);
-                        mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
-                    }
-                    setState(BluetoothSap.STATE_DISCONNECTED);
-                    // Ensure proper cleanup, and prepare for new connect.
-                    mSessionStatusHandler.sendEmptyMessage(MSG_SERVERSESSION_CLOSE);
-                }
-            }
         }
     }
 
-    ;
+    public void aclDisconnected(BluetoothDevice device) {
+        mSessionStatusHandler.post(() -> handleAclDisconnected(device));
+    }
+
+    private void handleAclDisconnected(BluetoothDevice device) {
+        if (!mIsWaitingAuthorization) {
+            return;
+        }
+        if (mRemoteDevice == null || device == null) {
+            Log.i(TAG, "Unexpected error!");
+            return;
+        }
+
+        if (DEBUG) {
+            Log.d(TAG, "ACL disconnected for " + device);
+        }
+
+        if (mRemoteDevice.equals(device)) {
+            if (mRemoveTimeoutMsg) {
+                // Send any pending timeout now, as ACL got disconnected.
+                mSessionStatusHandler.removeMessages(USER_TIMEOUT);
+                mSessionStatusHandler.obtainMessage(USER_TIMEOUT).sendToTarget();
+            }
+            setState(BluetoothSap.STATE_DISCONNECTED);
+            // Ensure proper cleanup, and prepare for new connect.
+            mSessionStatusHandler.sendEmptyMessage(MSG_SERVERSESSION_CLOSE);
+        }
+    }
 
     //Binder object: Must be static class or memory leak may occur
 

android/app/tests/unit/src/com/android/bluetooth/mapclient/MapClientServiceTest.java
@@ -31,6 +31,7 @@ import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothUuid;
 import android.bluetooth.SdpMasRecord;
 import android.content.Intent;
+import android.os.Looper;
 
 import androidx.test.filters.MediumTest;
 import androidx.test.rule.ServiceTestRule;
@@ -255,28 +256,6 @@ public class MapClientServiceTest {
     }
 
     @Test
-    public void broadcastReceiver_withActionAclDisconnected_withoutDevice_doesNothing() {
-        int connectionState = BluetoothProfile.STATE_CONNECTED;
-        MceStateMachine sm = mock(MceStateMachine.class);
-        mService.getInstanceMap().put(mRemoteDevice, sm);
-        when(sm.getState()).thenReturn(connectionState);
-
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        // Device is not included in this intent
-        mService.mMapReceiver.onReceive(mService, intent);
-
-        verify(sm, never()).disconnect();
-    }
-
-    @Test
-    public void broadcastReceiver_withActionAclDisconnected_whenNotConnected_doesNothing() {
-        // No state machine exists for this device
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        mService.mMapReceiver.onReceive(mService, intent);
-    }
-
-    @Test
     public void
             broadcastReceiver_withActionAclDisconnectedNoTransport_whenConnected_doesNotCallDisconnect() {
         int connectionState = BluetoothProfile.STATE_CONNECTED;
@@ -284,9 +263,8 @@ public class MapClientServiceTest {
         mService.getInstanceMap().put(mRemoteDevice, sm);
         when(sm.getState()).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        mService.mMapReceiver.onReceive(mService, intent);
+        mService.aclDisconnected(mRemoteDevice, BluetoothDevice.ERROR);
+        TestUtils.waitForLooperToBeIdle(Looper.getMainLooper());
 
         verify(sm, never()).disconnect();
     }
@@ -299,10 +277,8 @@ public class MapClientServiceTest {
         mService.getInstanceMap().put(mRemoteDevice, sm);
         when(sm.getState()).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_LE);
-        mService.mMapReceiver.onReceive(mService, intent);
+        mService.aclDisconnected(mRemoteDevice, BluetoothDevice.TRANSPORT_LE);
+        TestUtils.waitForLooperToBeIdle(Looper.getMainLooper());
 
         verify(sm, never()).disconnect();
     }
@@ -315,10 +291,8 @@ public class MapClientServiceTest {
         mService.getInstanceMap().put(mRemoteDevice, sm);
         when(sm.getState()).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_BREDR);
-        mService.mMapReceiver.onReceive(mService, intent);
+        mService.aclDisconnected(mRemoteDevice, BluetoothDevice.TRANSPORT_BREDR);
+        TestUtils.waitForLooperToBeIdle(Looper.getMainLooper());
 
         verify(sm).disconnect();
     }

android/app/tests/unit/src/com/android/bluetooth/pbapclient/PbapClientServiceTest.java
@@ -34,6 +34,7 @@ import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.CallLog;
+import android.os.Looper;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.MediumTest;
@@ -317,30 +318,14 @@ public class PbapClientServiceTest {
     }
 
     @Test
-    public void broadcastReceiver_withActionAclDisconnectedNoTransport_doesNotCallDisconnect() {
-        int connectionState = BluetoothProfile.STATE_CONNECTED;
-        PbapClientStateMachine sm = mock(PbapClientStateMachine.class);
-        mService.mPbapClientStateMachineMap.put(mRemoteDevice, sm);
-        when(sm.getConnectionState(mRemoteDevice)).thenReturn(connectionState);
-
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        mService.mPbapBroadcastReceiver.onReceive(mService, intent);
-
-        verify(sm, never()).disconnect(mRemoteDevice);
-    }
-
-    @Test
     public void broadcastReceiver_withActionAclDisconnectedLeTransport_doesNotCallDisconnect() {
         int connectionState = BluetoothProfile.STATE_CONNECTED;
         PbapClientStateMachine sm = mock(PbapClientStateMachine.class);
         mService.mPbapClientStateMachineMap.put(mRemoteDevice, sm);
         when(sm.getConnectionState(mRemoteDevice)).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_LE);
-        mService.mPbapBroadcastReceiver.onReceive(mService, intent);
+        mService.aclDisconnected(mRemoteDevice, BluetoothDevice.TRANSPORT_LE);
+        TestUtils.waitForLooperToFinishScheduledTask(Looper.getMainLooper());
 
         verify(sm, never()).disconnect(mRemoteDevice);
     }
@@ -352,10 +337,8 @@ public class PbapClientServiceTest {
         mService.mPbapClientStateMachineMap.put(mRemoteDevice, sm);
         when(sm.getConnectionState(mRemoteDevice)).thenReturn(connectionState);
 
-        Intent intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
-        intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.TRANSPORT_BREDR);
-        mService.mPbapBroadcastReceiver.onReceive(mService, intent);
+        mService.aclDisconnected(mRemoteDevice, BluetoothDevice.TRANSPORT_BREDR);
+        TestUtils.waitForLooperToFinishScheduledTask(Looper.getMainLooper());
 
         verify(sm).disconnect(mRemoteDevice);
     }