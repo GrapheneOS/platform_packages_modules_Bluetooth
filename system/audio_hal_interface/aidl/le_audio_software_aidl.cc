miyu-xu/packages-modules-Bluetooth

Add support for new LatencyMode enum
Test: mmm packages/modules/Bluetooth
Bug: 304840881
Change-Id: Ia6a7a9f6cbd27e2aa276b45ed3b7b457bf62b4ca

system/audio_hal_interface/aidl/le_audio_software_aidl.cc
@@ -44,6 +44,7 @@ using ::bluetooth::audio::aidl::BluetoothAudioCtrlAck;
 using ::bluetooth::audio::le_audio::LeAudioClientInterface;
 using ::bluetooth::audio::le_audio::StartRequestState;
 using ::bluetooth::audio::le_audio::StreamCallbacks;
+using ::le_audio::DsaMode;
 using ::le_audio::set_configurations::SetConfiguration;
 using ::le_audio::types::LeAudioCoreCodecConfig;
 
@@ -124,7 +125,25 @@ void LeAudioTransport::StopRequest() {
   }
 }
 
-void LeAudioTransport::SetLowLatency(bool is_low_latency) {}
+void LeAudioTransport::SetLatencyMode(LatencyMode latency_mode) {
+  switch (latency_mode) {
+    case LatencyMode::FREE:
+      latency_mode_ = DsaMode::DISABLED;
+      break;
+    case LatencyMode::LOW_LATENCY:
+      latency_mode_ = DsaMode::ACL;
+      break;
+    case LatencyMode::DYNAMIC_SPATIAL_AUDIO_SOFTWARE:
+      latency_mode_ = DsaMode::ISO_SW;
+      break;
+    case LatencyMode::DYNAMIC_SPATIAL_AUDIO_HARDWARE:
+      latency_mode_ = DsaMode::ISO_HW;
+      break;
+    default:
+      LOG(WARNING) << ", invalid latency mode: " << (int)latency_mode;
+      break;
+  }
+}
 
 bool LeAudioTransport::GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                                uint64_t* total_bytes_processed,
@@ -152,7 +171,7 @@ void LeAudioTransport::SourceMetadataChanged(
     return;
   }
 
-  stream_cb_.on_metadata_update_(source_metadata);
+  stream_cb_.on_metadata_update_(source_metadata, latency_mode_);
 }
 
 void LeAudioTransport::SinkMetadataChanged(
@@ -280,8 +299,8 @@ BluetoothAudioCtrlAck LeAudioSinkTransport::SuspendRequest() {
 
 void LeAudioSinkTransport::StopRequest() { transport_->StopRequest(); }
 
-void LeAudioSinkTransport::SetLowLatency(bool is_low_latency) {
-  transport_->SetLowLatency(is_low_latency);
+void LeAudioSinkTransport::SetLatencyMode(LatencyMode latency_mode) {
+  transport_->SetLatencyMode(latency_mode);
 }
 
 bool LeAudioSinkTransport::GetPresentationPosition(
@@ -370,8 +389,8 @@ BluetoothAudioCtrlAck LeAudioSourceTransport::SuspendRequest() {
 
 void LeAudioSourceTransport::StopRequest() { transport_->StopRequest(); }
 
-void LeAudioSourceTransport::SetLowLatency(bool is_low_latency) {
-  transport_->SetLowLatency(is_low_latency);
+void LeAudioSourceTransport::SetLatencyMode(LatencyMode latency_mode) {
+  transport_->SetLatencyMode(latency_mode);
 }
 
 bool LeAudioSourceTransport::GetPresentationPosition(

system/audio_hal_interface/aidl/le_audio_software_aidl.h
@@ -34,7 +34,9 @@ using ::aidl::android::hardware::bluetooth::audio::PcmConfiguration;
 using ::aidl::android::hardware::bluetooth::audio::SessionType;
 using ::aidl::android::hardware::bluetooth::audio::UnicastCapability;
 using ::bluetooth::audio::aidl::BluetoothAudioCtrlAck;
+using ::bluetooth::audio::aidl::LatencyMode;
 using ::bluetooth::audio::le_audio::StartRequestState;
+using ::le_audio::DsaMode;
 using ::le_audio::set_configurations::AudioSetConfiguration;
 using ::le_audio::set_configurations::CodecConfigSetting;
 
@@ -74,7 +76,7 @@ class LeAudioTransport {
 
   void StopRequest();
 
-  void SetLowLatency(bool is_low_latency);
+  void SetLatencyMode(LatencyMode latency_mode);
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_processed,
@@ -114,6 +116,7 @@ class LeAudioTransport {
   PcmConfiguration pcm_config_;
   LeAudioBroadcastConfiguration broadcast_config_;
   std::atomic<StartRequestState> start_request_state_;
+  DsaMode latency_mode_;
 };
 
 // Sink transport implementation for Le Audio
@@ -130,7 +133,7 @@ class LeAudioSinkTransport
 
   void StopRequest() override;
 
-  void SetLowLatency(bool is_low_latency) override;
+  void SetLatencyMode(LatencyMode latency_mode) override;
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_read,
@@ -186,7 +189,7 @@ class LeAudioSourceTransport
 
   void StopRequest() override;
 
-  void SetLowLatency(bool is_low_latency) override;
+  void SetLatencyMode(LatencyMode latency_mode) override;
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_written,

GrapheneOS/platform_packages_modules_Bluetooth

system/audio_hal_interface/aidl/le_audio_software_aidl.cc
@@ -124,7 +124,25 @@ void LeAudioTransport::StopRequest() {
   }
 }
 
-void LeAudioTransport::SetLowLatency(bool is_low_latency) {}
+void LeAudioTransport::SetLatencyMode(LatencyMode latency_mode) {
+  switch (latency_mode) {
+    case LatencyMode::FREE:
+      dsa_mode_ = DsaMode::DISABLED;
+      break;
+    case LatencyMode::LOW_LATENCY:
+      dsa_mode_ = DsaMode::ACL;
+      break;
+    case LatencyMode::DYNAMIC_SPATIAL_AUDIO_SOFTWARE:
+      dsa_mode_ = DsaMode::ISO_SW;
+      break;
+    case LatencyMode::DYNAMIC_SPATIAL_AUDIO_HARDWARE:
+      dsa_mode_ = DsaMode::ISO_HW;
+      break;
+    default:
+      LOG(WARNING) << ", invalid latency mode: " << (int)latency_mode;
+      break;
+  }
+}
 
 bool LeAudioTransport::GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                                uint64_t* total_bytes_processed,
@@ -152,7 +170,7 @@ void LeAudioTransport::SourceMetadataChanged(
     return;
   }
 
-  stream_cb_.on_metadata_update_(source_metadata);
+  stream_cb_.on_metadata_update_(source_metadata, dsa_mode_);
 }
 
 void LeAudioTransport::SinkMetadataChanged(
@@ -280,8 +298,8 @@ BluetoothAudioCtrlAck LeAudioSinkTransport::SuspendRequest() {
 
 void LeAudioSinkTransport::StopRequest() { transport_->StopRequest(); }
 
-void LeAudioSinkTransport::SetLowLatency(bool is_low_latency) {
-  transport_->SetLowLatency(is_low_latency);
+void LeAudioSinkTransport::SetLatencyMode(LatencyMode latency_mode) {
+  transport_->SetLatencyMode(latency_mode);
 }
 
 bool LeAudioSinkTransport::GetPresentationPosition(
@@ -370,8 +388,8 @@ BluetoothAudioCtrlAck LeAudioSourceTransport::SuspendRequest() {
 
 void LeAudioSourceTransport::StopRequest() { transport_->StopRequest(); }
 
-void LeAudioSourceTransport::SetLowLatency(bool is_low_latency) {
-  transport_->SetLowLatency(is_low_latency);
+void LeAudioSourceTransport::SetLatencyMode(LatencyMode latency_mode) {
+  transport_->SetLatencyMode(latency_mode);
 }
 
 bool LeAudioSourceTransport::GetPresentationPosition(

system/audio_hal_interface/aidl/le_audio_software_aidl.h
@@ -34,7 +34,9 @@ using ::aidl::android::hardware::bluetooth::audio::PcmConfiguration;
 using ::aidl::android::hardware::bluetooth::audio::SessionType;
 using ::aidl::android::hardware::bluetooth::audio::UnicastCapability;
 using ::bluetooth::audio::aidl::BluetoothAudioCtrlAck;
+using ::bluetooth::audio::aidl::LatencyMode;
 using ::bluetooth::audio::le_audio::StartRequestState;
+using ::le_audio::DsaMode;
 using ::le_audio::set_configurations::AudioSetConfiguration;
 using ::le_audio::set_configurations::CodecConfigSetting;
 
@@ -74,7 +76,7 @@ class LeAudioTransport {
 
   void StopRequest();
 
-  void SetLowLatency(bool is_low_latency);
+  void SetLatencyMode(LatencyMode latency_mode);
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_processed,
@@ -114,6 +116,7 @@ class LeAudioTransport {
   PcmConfiguration pcm_config_;
   LeAudioBroadcastConfiguration broadcast_config_;
   std::atomic<StartRequestState> start_request_state_;
+  DsaMode dsa_mode_;
 };
 
 // Sink transport implementation for Le Audio
@@ -130,7 +133,7 @@ class LeAudioSinkTransport
 
   void StopRequest() override;
 
-  void SetLowLatency(bool is_low_latency) override;
+  void SetLatencyMode(LatencyMode latency_mode) override;
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_read,
@@ -186,7 +189,7 @@ class LeAudioSourceTransport
 
   void StopRequest() override;
 
-  void SetLowLatency(bool is_low_latency) override;
+  void SetLatencyMode(LatencyMode latency_mode) override;
 
   bool GetPresentationPosition(uint64_t* remote_delay_report_ns,
                                uint64_t* total_bytes_written,