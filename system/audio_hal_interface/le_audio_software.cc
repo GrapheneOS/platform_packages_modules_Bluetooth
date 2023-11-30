miyu-xu/packages-modules-Bluetooth

le_audio: Differentiate Broadcast and Unicast for ClientInterface
While Broadcast and Unicast Sink Client Interface Endpoints can be
instantiated simultanousely a spearate Cleanup procedure should be
performed. Also Broadcast and Unicast transports should not share common
status.

Tag: #feature
Bug: 299238288
Test: atest BluetoothInstrumentationTests
Change-Id: I1f86e2b08a56a612e9c0f8a63f7c4c84916e59db

system/audio_hal_interface/le_audio_software.cc
@@ -97,31 +97,47 @@ LeAudioClientInterface* LeAudioClientInterface::Get() {
 }
 
 void LeAudioClientInterface::Sink::Cleanup() {
-  LOG(INFO) << __func__ << " sink";
+  LOG_DEBUG("HAL transport: 0x%02x, is broadcast: %d",
+            static_cast<int>(HalVersionManager::GetHalTransport()),
+            is_broadcaster_);
+
   StopSession();
-  if (hidl::le_audio::LeAudioSinkTransport::interface) {
-    delete hidl::le_audio::LeAudioSinkTransport::interface;
-    hidl::le_audio::LeAudioSinkTransport::interface = nullptr;
-  }
-  if (hidl::le_audio::LeAudioSinkTransport::instance) {
-    delete hidl::le_audio::LeAudioSinkTransport::instance;
-    hidl::le_audio::LeAudioSinkTransport::instance = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::interface_unicast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::interface_unicast_;
-    aidl::le_audio::LeAudioSinkTransport::interface_unicast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::interface_broadcast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::interface_broadcast_;
-    aidl::le_audio::LeAudioSinkTransport::interface_broadcast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::instance_unicast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::instance_unicast_;
-    aidl::le_audio::LeAudioSinkTransport::instance_unicast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::instance_broadcast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::instance_broadcast_;
-    aidl::le_audio::LeAudioSinkTransport::instance_broadcast_ = nullptr;
+
+  /* Cleanup transport interface and instance according to type and role */
+  if (HalVersionManager::GetHalTransport() ==
+      BluetoothAudioHalTransport::HIDL) {
+    if (hidl::le_audio::LeAudioSinkTransport::interface) {
+      delete hidl::le_audio::LeAudioSinkTransport::interface;
+      hidl::le_audio::LeAudioSinkTransport::interface = nullptr;
+    }
+    if (hidl::le_audio::LeAudioSinkTransport::instance) {
+      delete hidl::le_audio::LeAudioSinkTransport::instance;
+      hidl::le_audio::LeAudioSinkTransport::instance = nullptr;
+    }
+  } else if (HalVersionManager::GetHalTransport() ==
+             BluetoothAudioHalTransport::AIDL) {
+    if (IsBroadcaster()) {
+      if (aidl::le_audio::LeAudioSinkTransport::interface_broadcast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::interface_broadcast_;
+        aidl::le_audio::LeAudioSinkTransport::interface_broadcast_ = nullptr;
+      }
+      if (aidl::le_audio::LeAudioSinkTransport::instance_broadcast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::instance_broadcast_;
+        aidl::le_audio::LeAudioSinkTransport::instance_broadcast_ = nullptr;
+      }
+    } else {
+      if (aidl::le_audio::LeAudioSinkTransport::interface_unicast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::interface_unicast_;
+        aidl::le_audio::LeAudioSinkTransport::interface_unicast_ = nullptr;
+      }
+      if (aidl::le_audio::LeAudioSinkTransport::instance_unicast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::instance_unicast_;
+        aidl::le_audio::LeAudioSinkTransport::instance_unicast_ = nullptr;
+      }
+    }
+  } else {
+    LOG_ERROR("Invalid HAL transport: 0x%02x",
+              static_cast<int>(HalVersionManager::GetHalTransport()));
   }
 }
 

GrapheneOS/platform_packages_modules_Bluetooth

system/audio_hal_interface/le_audio_software.cc
@@ -97,31 +97,47 @@ LeAudioClientInterface* LeAudioClientInterface::Get() {
 }
 
 void LeAudioClientInterface::Sink::Cleanup() {
-  LOG(INFO) << __func__ << " sink";
+  LOG_INFO("HAL transport: 0x%02x, is broadcast: %d",
+            static_cast<int>(HalVersionManager::GetHalTransport()),
+            is_broadcaster_);
+
   StopSession();
-  if (hidl::le_audio::LeAudioSinkTransport::interface) {
-    delete hidl::le_audio::LeAudioSinkTransport::interface;
-    hidl::le_audio::LeAudioSinkTransport::interface = nullptr;
-  }
-  if (hidl::le_audio::LeAudioSinkTransport::instance) {
-    delete hidl::le_audio::LeAudioSinkTransport::instance;
-    hidl::le_audio::LeAudioSinkTransport::instance = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::interface_unicast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::interface_unicast_;
-    aidl::le_audio::LeAudioSinkTransport::interface_unicast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::interface_broadcast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::interface_broadcast_;
-    aidl::le_audio::LeAudioSinkTransport::interface_broadcast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::instance_unicast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::instance_unicast_;
-    aidl::le_audio::LeAudioSinkTransport::instance_unicast_ = nullptr;
-  }
-  if (aidl::le_audio::LeAudioSinkTransport::instance_broadcast_) {
-    delete aidl::le_audio::LeAudioSinkTransport::instance_broadcast_;
-    aidl::le_audio::LeAudioSinkTransport::instance_broadcast_ = nullptr;
+
+  /* Cleanup transport interface and instance according to type and role */
+  if (HalVersionManager::GetHalTransport() ==
+      BluetoothAudioHalTransport::HIDL) {
+    if (hidl::le_audio::LeAudioSinkTransport::interface) {
+      delete hidl::le_audio::LeAudioSinkTransport::interface;
+      hidl::le_audio::LeAudioSinkTransport::interface = nullptr;
+    }
+    if (hidl::le_audio::LeAudioSinkTransport::instance) {
+      delete hidl::le_audio::LeAudioSinkTransport::instance;
+      hidl::le_audio::LeAudioSinkTransport::instance = nullptr;
+    }
+  } else if (HalVersionManager::GetHalTransport() ==
+             BluetoothAudioHalTransport::AIDL) {
+    if (IsBroadcaster()) {
+      if (aidl::le_audio::LeAudioSinkTransport::interface_broadcast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::interface_broadcast_;
+        aidl::le_audio::LeAudioSinkTransport::interface_broadcast_ = nullptr;
+      }
+      if (aidl::le_audio::LeAudioSinkTransport::instance_broadcast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::instance_broadcast_;
+        aidl::le_audio::LeAudioSinkTransport::instance_broadcast_ = nullptr;
+      }
+    } else {
+      if (aidl::le_audio::LeAudioSinkTransport::interface_unicast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::interface_unicast_;
+        aidl::le_audio::LeAudioSinkTransport::interface_unicast_ = nullptr;
+      }
+      if (aidl::le_audio::LeAudioSinkTransport::instance_unicast_) {
+        delete aidl::le_audio::LeAudioSinkTransport::instance_unicast_;
+        aidl::le_audio::LeAudioSinkTransport::instance_unicast_ = nullptr;
+      }
+    }
+  } else {
+    LOG_ERROR("Invalid HAL transport: 0x%02x",
+              static_cast<int>(HalVersionManager::GetHalTransport()));
   }
 }
 