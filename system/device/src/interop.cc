miyu-xu/packages-modules-Bluetooth

Wait to notify encryption complete when bonding
Some peripherals have issue where they cannot handle any operations when
bonding exchange is in progress. This CL waits to notify encryption
complete when bond is in process until notified by SMP that pairing is
complete.

Note: Currently this IOP is gated by name. In follow up CLs support will
be added to gate this by UUID and name.

Bug: 290392842
Test: m com.android.btservices
Manual; pair and connect LE device and ensure encryption complete events
happen in both scenarios.

Change-Id: I67810d5ef1d6b9c47a063973c370477988de5fdd

system/device/src/interop.cc
@@ -393,6 +393,7 @@ static const char* interop_feature_string_(const interop_feature_t feature) {
     CASE_RETURN_STR(INTEROP_HFP_1_7_ALLOWLIST);
     CASE_RETURN_STR(INTEROP_HFP_1_9_ALLOWLIST);
     CASE_RETURN_STR(INTEROP_IGNORE_DISC_BEFORE_SIGNALLING_TIMEOUT);
+    CASE_RETURN_STR(INTEROP_DELAY_ATT_TRAFFIC_DURING_PAIRING);
   }
   return UNKNOWN_INTEROP_FEATURE;
 }

GrapheneOS/platform_packages_modules_Bluetooth

system/device/src/interop.cc
@@ -392,6 +392,7 @@ static const char* interop_feature_string_(const interop_feature_t feature) {
     CASE_RETURN_STR(INTEROP_HFP_1_7_ALLOWLIST);
     CASE_RETURN_STR(INTEROP_HFP_1_9_ALLOWLIST);
     CASE_RETURN_STR(INTEROP_IGNORE_DISC_BEFORE_SIGNALLING_TIMEOUT);
+    CASE_RETURN_STR(INTEROP_SUSPEND_ATT_TRAFFIC_DURING_PAIRING);
   }
   return UNKNOWN_INTEROP_FEATURE;
 }